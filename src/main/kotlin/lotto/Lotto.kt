39d6a66c33d87cc84bbb49091e92e81099191b50
kyounggudev <homin222@naver.com>
2022-11-10 00:30:55 +0900

    feat : Lotto 중복 예외처리 추가

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -5,7 +5,15 @@ class Lotto(private val numbers: List<Int>) {
         require(numbers.size == 6){
             throw IllegalArgumentException("[ERROR] 로또 번호는 6개의 숫자로 이루어져야합니다.")
         }
+        require(notDuplicate(numbers)){
+            throw IllegalArgumentException("[ERROR] 로또 번호는 서로 중복돼서는 안됩니다.")
+        }
+    }
+
+    private fun notDuplicate(numbers : List<Int>) : Boolean{
+        return numbers.size == numbers.distinct().count()
     }
 
     // TODO: 추가 기능 구현
 }
+
