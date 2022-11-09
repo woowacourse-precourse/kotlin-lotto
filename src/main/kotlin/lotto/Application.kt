fe885c950474aa9c64498536fb684c66195ab185
kjyook <106497893+kjyook@users.noreply.github.com>
2022-11-10 00:12:41 +0900

    구입 금액을 입력하면 값을 받아 몇 장을 살지 정하는 함수를 구현

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -2,12 +2,34 @@ package lotto
 
 import camp.nextstep.edu.missionutils.Randoms
 
-fun randomLotto():List<Int> {       //사용자가 로또를 사면 구매한 로또 번호를 반환하는 함수
-    var numbers = Randoms.pickUniqueNumbersInRange(1,45,6)
+fun randomLotto(): Lotto {       //사용자가 로또를 사면 구매한 로또 번호를 반환하는 함수
+    var numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
     numbers.sort()
 
-    return numbers
+    println(numbers)
+
+    return Lotto(numbers)
 }
+
+fun makeLotto(): Int {
+    var number = 0
+    var manyLotto = 0
+    //구입 금액 입력받고 숫자가 아닌 입력이면 오류처리
+    try {
+        number = readLine()!!.toInt()
+    } catch (e: IllegalArgumentException) {
+        println("[ERROR] 숫자를 입력해주세요")
+    }
+
+    if (number % 1000 != 0)
+        throw IllegalArgumentException("[ERROR] 1000단위의 금액을 입력해주세요")
+    else
+        manyLotto = number / 1000
+
+    return manyLotto
+}
+
 fun main() {
-    TODO("프로그램 구현")
+    println("구입금액을 입력해 주세요.")
+    makeLotto()
 }
