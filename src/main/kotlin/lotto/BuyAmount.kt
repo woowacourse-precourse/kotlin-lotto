88dcf3cd1157114eb65c4667d58ad6b27312c67b
lgy15 <lgy159@naver.com>
2022-11-09 20:24:52 +0900

    feat: BuyAmount, Constant, WinningNumber 클래스 생성

---
+++ src/main/kotlin/lotto/BuyAmount.kt
@@ -0,0 +1,15 @@
+package lotto
+
+import camp.nextstep.edu.missionutils.Console.readLine
+
+class BuyAmount {
+
+    fun inputAmount() {
+        val inputNumber = readLine().toInt()
+        exception(inputNumber)
+    }
+
+    fun exception(number: Int) {
+
+    }
+}
---
+++ src/main/kotlin/lotto/WinningNumber.kt
@@ -0,0 +1,14 @@
+package lotto
+import camp.nextstep.edu.missionutils.Console.readLine
+
+class WinningNumber {
+
+
+    fun inputNumber() {
+        val inputNumber = readLine()
+    }
+
+    fun exception(number: Int) {
+
+    }
+}
