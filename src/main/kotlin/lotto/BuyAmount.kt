1798e215750fc607516cc4dd8bd2f6003a8b13eb
lgy15 <lgy159@naver.com>
2022-11-09 20:41:08 +0900

    feat: 구입 금액 입력 및 예외처리 구현

--- src/main/kotlin/lotto/BuyAmount.kt
+++ src/main/kotlin/lotto/BuyAmount.kt
@@ -1,15 +1,30 @@
 package lotto
 
 import camp.nextstep.edu.missionutils.Console.readLine
+import lotto.Constant.Companion.costNotIntErrorMessage
+import lotto.Constant.Companion.costNotRegularErrorMessage
+import lotto.Constant.Companion.lottoCost
+import lotto.Constant.Companion.regularMax
+import lotto.Constant.Companion.regularMin
 
 class BuyAmount {
+    val cost = inputAmount()
 
-    fun inputAmount() {
-        val inputNumber = readLine().toInt()
-        exception(inputNumber)
+    private fun inputAmount(): Int {
+        val inputAmount = readLine()
+        exception(inputAmount)
+        return inputAmount.toInt()
     }
 
-    fun exception(number: Int) {
+    private fun exception(inputAmount: String) {
+        if (!inputAmount.all { it in regularMin..regularMax}) {
+            println(costNotIntErrorMessage)
+            throw IllegalArgumentException(costNotIntErrorMessage)
+        }
 
+        if (inputAmount.toInt() % lottoCost != 0) {
+            println(costNotRegularErrorMessage)
+            throw IllegalArgumentException(costNotRegularErrorMessage)
+        }
     }
 }
--- src/main/kotlin/lotto/Constant.kt
+++ src/main/kotlin/lotto/Constant.kt
@@ -1,4 +1,11 @@
 package lotto
 
 class Constant {
+    companion object {
+        const val lottoCost = 1000
+        const val regularMin = '0'
+        const val regularMax = '9'
+        const val costNotIntErrorMessage = "[ERROR] 1000으로 나누어 떨어지는 숫자를 입력해야 합니다."
+        const val costNotRegularErrorMessage = "[ERROR] 구입 금액은 1000으로 나누어 떨어져야 합니다."
+    }
 }
