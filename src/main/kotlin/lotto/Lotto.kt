c53e0c15cb651e3f42513bf86ef1131c8442d09e
kyounggudev <homin222@naver.com>
2022-11-10 00:23:34 +0900

    feat : Lotto 길이 예외처리 추가

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -2,7 +2,9 @@ package lotto
 
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6)
+        require(numbers.size == 6){
+            throw IllegalArgumentException("[ERROR] 로또 번호는 6개의 숫자로 이루어져야합니다.")
+        }
     }
 
     // TODO: 추가 기능 구현
