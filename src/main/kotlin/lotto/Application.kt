8c1c79ba79acea4abfec4e1fc52b8cabce334adc
kyounggudev <homin222@naver.com>
2022-11-10 08:06:09 +0900

    style : ERROR_NOT_INT 오타 수정

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -6,7 +6,7 @@ const val MIN =1
 const val MAX =45
 
 const val ERROR_NO_INPUT = "[ERROR] 구입 금액을 입력하지 않았습니다."
-const val ERROR_NOT_INT = "[ERROR] 숫자가 아님"
+const val ERROR_NOT_INT = "[ERROR] 구입 금액에는 숫자만 입력해야합니다."
 const val ERROR_1000UNIT = "[ERROR] 구입 금액이 1,000원 단위여야 합니다."
 
 class Purchase(price: String?) {
