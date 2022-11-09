7e5e9747311e2193fef6e58f2d5f865551880427
kyounggudev <homin222@naver.com>
2022-11-10 02:51:49 +0900

    feat : 랜덤 발행 기능 여러개 발행으로 수정

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -4,38 +4,42 @@ import camp.nextstep.edu.missionutils.Randoms
 
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6){
+        require(numbers.size == 6) {
             throw IllegalArgumentException("[ERROR] 로또 번호는 6개의 숫자로 이루어져야합니다.")
         }
-        require(notDuplicate(numbers)){
+        require(notDuplicate(numbers)) {
             throw IllegalArgumentException("[ERROR] 로또 번호는 서로 중복돼서는 안됩니다.")
         }
-        require(inRange(numbers)){
+        require(inRange(numbers)) {
             throw IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.")
         }
         numbers.sorted()
         lottoes.add(numbers);
     }
 
-    private fun notDuplicate(numbers : List<Int>) : Boolean{
+    private fun notDuplicate(numbers: List<Int>): Boolean {
         return numbers.size == numbers.distinct().count()
     }
-    private fun inRange(numbers : List<Int>) : Boolean{
-        return numbers.all { it in MIN until MAX+1 }
+
+    private fun inRange(numbers: List<Int>): Boolean {
+        return numbers.all { it in MIN until MAX + 1 }
     }
 
 
-    companion object{
+    companion object {
         val lottoes = mutableListOf<List<Int>>()
-        fun printAll(){
+        fun printAll() {
             println("${lottoes.size}개를 구매했습니다.")
-            for( lotto in lottoes){
+            for (lotto in lottoes) {
                 println(lotto)
             }
         }
-        fun publish(){
-            val numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
-            Lotto(numbers)
+
+        fun publish(purchaseCount: Int) {
+            for (count in 1..purchaseCount) {
+                val numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
+                Lotto(numbers)
+            }
         }
     }
 }
