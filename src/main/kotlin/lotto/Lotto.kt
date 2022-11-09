b396d4df99b5cc6636bba0eb71e3838b6abe2341
kyounggudev <homin222@naver.com>
2022-11-10 02:13:08 +0900

    feat : 랜덤 발행 기능 추가

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,5 +1,7 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6){
@@ -30,6 +32,10 @@ class Lotto(private val numbers: List<Int>) {
             for( lotto in lottoes){
                 println(lotto)
             }
+        }
+        fun publish(){
+            val numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
+            Lotto(numbers)
         }
     }
 }
