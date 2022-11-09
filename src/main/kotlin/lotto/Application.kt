910ba9abf93f06dc0d3f3c221e03e1db282bd2d8
Kim Jaeyong <jy3479@naver.com>
2022-11-09 21:46:00 +0900

    기능 1번 구현

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,13 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
     TODO("프로그램 구현")
+}
+
+fun makeLotto(): Lotto = try {
+    Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6))
+} catch (e: IllegalArgumentException) {
+    makeLotto()
 }
