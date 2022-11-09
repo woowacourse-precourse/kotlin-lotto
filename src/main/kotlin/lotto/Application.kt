f9a42dc7c98b85739510c33fff34601aebbdb2cd
kjyook <106497893+kjyook@users.noreply.github.com>
2022-11-09 23:41:42 +0900

    로또를 구매하면 번호를 반환하는 함수

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,13 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Randoms
+
+fun randomLotto():List<Int> {       //사용자가 로또를 사면 구매한 로또 번호를 반환하는 함수
+    var numbers = Randoms.pickUniqueNumbersInRange(1,45,6)
+    numbers.sort()
+
+    return numbers
+}
 fun main() {
     TODO("프로그램 구현")
 }
