eb3307d46b54d4113e87e26710e9f18054dc08e6
JunJangE <wnswkd486@gmail.com>
2022-11-10 23:45:08 +0900

    feat: 구입 금액에 만큼 살 수 있는 로또의 개수를 확인하는 기능

--- src/main/kotlin/lotto/LottoShop.kt
+++ src/main/kotlin/lotto/LottoShop.kt
@@ -1,13 +1,25 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class LottoShop {
 
     fun buyLotto() {
+        println("구입금액을 입력해 주세요.")
+        val buyAmount = readLine()!!.toInt()
+        cntLotto(buyAmount)
+
+    }
+
+    fun cntLotto(purchaseAmount: Int): Int {
+        if (purchaseAmount % 1000 != 0) throw IllegalArgumentException("[ERROR] 숫자가 나누어 떨어지지 않습니다.")
+        val purchaseCnt = (purchaseAmount / 1000)
+        println("${purchaseCnt}개를 구매했습니다.")
+
+        return purchaseCnt
 
     }
 
     fun createRandomLotto() {
-
     }
-
 }
