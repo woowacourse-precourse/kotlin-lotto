edc19dc83d6bc048cff28a8eacb51ea7c424ee76
Joonhoo <mnc5@naver.com>
2022-11-10 19:42:28 +0900

    feat: Lotto 클래스 초기화시 예외사항 추가

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -1,8 +1,15 @@
 package lotto.domain
 
+import util.Exceptions
+import util.Messages
+
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6)
+        require(numbers.size == 6) {
+            throw IllegalArgumentException(Messages.ERROR_MESSAGE + Messages.ERROR_LOTTO_SIZE)
+        }
+        // 로또번호 중복 예외 확인
+        Exceptions.isValidLottoSize(numbers)
     }
 
     // TODO: 추가 기능 구현
