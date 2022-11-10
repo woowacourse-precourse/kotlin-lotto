1acf68d10519361787c657330bf96a4c3296266d
JunHyeok0205 <wnsdlfgur@naver.com>
2022-11-10 17:01:46 +0900

    feat: 당첨 번호 입력 기능 예외처리 추가

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -2,7 +2,6 @@ package lotto
 
 import camp.nextstep.edu.missionutils.Randoms
 
-
 fun main() {
     lottoPrice()
     winLottoNumber()
@@ -35,10 +34,14 @@ fun myLotto(lotto: Int) {
 
 fun winLottoNumber() {
     println("\n당첨 번호를 입력해 주세요.")
-    val number = readLine()!!.split(",")
+    val number : List<String> = readLine()!!.split(",")
     val winNum: MutableList<Int> = ArrayList()
-    for (i in 0..5) {
-        winNum.add(number[i].toInt())
+    for (element in number) {
+        val numberToInt = element.toInt()
+        if (winNum.contains(numberToInt)) throw IllegalArgumentException("[ERROR] 중복된 숫자를 입력하실 수 없습니다.")
+        if (numberToInt !in 1..45) throw IllegalArgumentException("[ERROR] 1~45사이의 숫자만 입력하실 수 있습니다.")
+        if (number.size != 6) throw IllegalArgumentException("[ERROR] 6가지의 수를 입력하십시오.")
+        winNum.add(numberToInt)
     }
     Lotto(winNum).winNumber
 }
