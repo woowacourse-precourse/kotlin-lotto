797665fdfb20283f10e3f3e2b1d5f6cf960e7f6e
lgy15 <lgy159@naver.com>
2022-11-11 06:20:02 +0900

    fix: 미션에서 요구한 random메서드로 변경

--- src/main/kotlin/lotto/Ticket.kt
+++ src/main/kotlin/lotto/Ticket.kt
@@ -1,24 +1,16 @@
 package lotto
 
-import camp.nextstep.edu.missionutils.Randoms.pickNumberInRange
+import camp.nextstep.edu.missionutils.Randoms
 import lotto.Constant.Companion.lottoSize
 import lotto.Constant.Companion.maxLottoNumber
 import lotto.Constant.Companion.minLottoNumber
 
 class Ticket {
-    private fun randomNumber() = pickNumberInRange(minLottoNumber, maxLottoNumber) // 1 ~ 45 랜덤 숫자 생성
-
-    private fun appendNumber(randomNumber: Int, numberList: MutableList<Int>) {
-        if (!numberList.contains(randomNumber)) { // 중복된 숫자가 아니면 add
-            numberList.add(randomNumber)
-        }
-    }
+    private fun randomNumber(): MutableList<Int> = Randoms.pickUniqueNumbersInRange(minLottoNumber, maxLottoNumber, lottoSize)
 
     fun makeLotto(): MutableList<Int> {
-        val lottoNumbers = mutableListOf<Int>()
-        while (lottoNumbers.size < lottoSize) {
-            appendNumber(randomNumber(), lottoNumbers)
-        }
+        val lottoNumbers = randomNumber()
+
         lottoNumbers.sort()
         return lottoNumbers
     }
