b4bef9309f6008253141ab979c9fa63f51b28688
zqwz <zqwz@naver.com>
2022-11-10 23:11:32 +0900

    test(WinningTest): 보너스 번호 예외 사항 테스트 추가
    중복된 수인 경우의 예외 사항
    범위 밖인 경우의 예외 사항

--- src/test/kotlin/lotto/WinningTest.kt
+++ src/test/kotlin/lotto/WinningTest.kt
@@ -8,17 +8,28 @@ class WinningTest {
 
     @Test
     fun `로또 당첨 번호에 중복된 수가 있으면 예외가 발생한다`() {
-        assertThrows<IllegalArgumentException> { (win.validateLuckyNumberDuplicate(listOf(1,2,3,4,5,5))) }
+        assertThrows<IllegalArgumentException> { (win.validateLuckyNumberDuplicate(listOf(1, 2, 3, 4, 5, 5))) }
     }
 
     @Test
     fun `로또 당첨 번호가 1~45 사이가 아니면 예외가 발생한다`() {
-        assertThrows<IllegalArgumentException> { (win.validateLuckyNumberRange(listOf(1,2,3,4,5,46))) }
+        assertThrows<IllegalArgumentException> { (win.validateLuckyNumberRange(listOf(1, 2, 3, 4, 5, 46))) }
     }
 
     @Test
     fun `로또 당첨 번호의 개수가 6개가 아니면 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> { (win.splitLuckyNumber("1,2,3,4,5")) }
+    }
+
+    @Test
+    fun `보너스 당첨 번호가 1~45 사이가 아닌 경우`() {
+        assertThrows<IllegalArgumentException> { (win.validateBonusNumberRange(46)) }
+    }
+
+    @Test
+    fun `보너스 당첨 번호가 로또 당첨 번호와 중복되면 예외가 발생한다`() {
+        win.splitLuckyNumber("1,2,3,4,5,6")
+        assertThrows<IllegalArgumentException> { (win.compareBonusWithLuckyNumber(4)) }
     }
 
 
