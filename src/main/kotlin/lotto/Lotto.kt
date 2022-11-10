ed8a0f4f7e814c0c2777ea07a11ffea1d373260f
mendel <mendel.urssu@gmail.com>
2022-11-11 00:49:13 +0900

    test: Lotto 클래스의 matchResult()를 위한 TDD 테스트 코드 작성
    - Lotto 클래스의 matchResult 메소드 구현 전에 테스트 코드 작성
    - Lotto.kt 파일에 enum 클래스 MathResult 추가

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -6,4 +6,9 @@ class Lotto(private val numbers: List<Int>) {
     }
 
     // TODO: 추가 기능 구현
+
+}
+
+enum class MatchResult {
+    THREE, FOUR, FIVE, BONUS, SIX, UNKNOWN
 }
--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -2,7 +2,8 @@ package lotto
 
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
-
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
 
 class LottoTest {
     @Test
@@ -21,4 +22,107 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+    private val winning = listOf(1, 9, 8, 6, 2, 7)
+    private val bonus = 4
+    private var count = 0
+
+    @BeforeEach
+    fun setUp() {
+        count = 0
+    }
+
+    @Test
+    fun `당첨번호와 비교해서 결과로 THREE를 리턴한다`() {
+        val lotto = listOf(1, 2, 3, 4, 5, 6)
+        lotto.forEach { if (winning.contains(it)) count++ }
+        val result: MatchResult = when (count) {
+            3 -> MatchResult.THREE
+            4 -> MatchResult.FOUR
+            5 -> if (lotto.contains(bonus)) {
+                MatchResult.BONUS
+            } else {
+                MatchResult.FIVE
+            }
+            6 -> MatchResult.SIX
+            else -> MatchResult.UNKNOWN
+        }
+        val expected = MatchResult.THREE
+        assertThat(result).isEqualTo(expected)
+    }
+
+    @Test
+    fun `당첨번호와 비교해서 결과로 FOUR를 리턴한다`() {
+        val lotto = listOf(1, 2, 9, 4, 5, 6)
+        lotto.forEach { if (winning.contains(it)) count++ }
+        val result: MatchResult = when (count) {
+            3 -> MatchResult.THREE
+            4 -> MatchResult.FOUR
+            5 -> if (lotto.contains(bonus)) {
+                MatchResult.BONUS
+            } else {
+                MatchResult.FIVE
+            }
+            6 -> MatchResult.SIX
+            else -> MatchResult.UNKNOWN
+        }
+        val expected = MatchResult.FOUR
+        assertThat(result).isEqualTo(expected)
+    }
+
+    @Test
+    fun `당첨번호와 비교해서 결과로 FIVE를 리턴한다`() {
+        val lotto = listOf(1, 2, 9, 8, 5, 6)
+        lotto.forEach { if (winning.contains(it)) count++ }
+        val result: MatchResult = when (count) {
+            3 -> MatchResult.THREE
+            4 -> MatchResult.FOUR
+            5 -> if (lotto.contains(bonus)) {
+                MatchResult.BONUS
+            } else {
+                MatchResult.FIVE
+            }
+            6 -> MatchResult.SIX
+            else -> MatchResult.UNKNOWN
+        }
+        val expected = MatchResult.FIVE
+        assertThat(result).isEqualTo(expected)
+    }
+
+    @Test
+    fun `당첨번호와 비교해서 결과로 BONUS를 리턴한다`() {
+        val lotto = listOf(1, 2, 9, 8, 4, 6)
+        lotto.forEach { if (winning.contains(it)) count++ }
+        val result: MatchResult = when (count) {
+            3 -> MatchResult.THREE
+            4 -> MatchResult.FOUR
+            5 -> if (lotto.contains(bonus)) {
+                MatchResult.BONUS
+            } else {
+                MatchResult.FIVE
+            }
+            6 -> MatchResult.SIX
+            else -> MatchResult.UNKNOWN
+        }
+        val expected = MatchResult.BONUS
+        assertThat(result).isEqualTo(expected)
+    }
+
+    @Test
+    fun `당첨번호와 비교해서 결과로 SIX를 리턴한다`() {
+        val lotto = listOf(1, 2, 9, 8, 7, 6)
+        lotto.forEach { if (winning.contains(it)) count++ }
+        val result: MatchResult = when (count) {
+            3 -> MatchResult.THREE
+            4 -> MatchResult.FOUR
+            5 -> if (lotto.contains(bonus)) {
+                MatchResult.BONUS
+            } else {
+                MatchResult.FIVE
+            }
+            6 -> MatchResult.SIX
+            else -> MatchResult.UNKNOWN
+        }
+        val expected = MatchResult.SIX
+        assertThat(result).isEqualTo(expected)
+    }
 }
