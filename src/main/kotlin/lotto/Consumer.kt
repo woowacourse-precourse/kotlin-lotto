fc427c91813b5806dea0d6ffd19490e6d159e35e
rhkrwngud445 <rhkrwngud445@gmail.com>
2022-11-10 22:25:46 +0900

    feat : exception check that money is lower than 1000won

--- src/main/kotlin/lotto/Consumer.kt
+++ src/main/kotlin/lotto/Consumer.kt
@@ -3,5 +3,6 @@ package lotto
 class Consumer(private val money: Int) {
     init {
         require(money % 1000 == 0) { println("[ERROR] 1000원 단위가 아닙니다.") }
+        require(money >= 1000) { println("[ERROR] 돈이 부족합니다") }
     }
 }
