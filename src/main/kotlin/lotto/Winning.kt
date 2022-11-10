f592d7e98903e3ad46c230058298facfe5223f23
zqwz <zqwz@naver.com>
2022-11-10 23:11:39 +0900

    feat(Winning): 보너스 번호 예외 사항 추가
    중복된 수인 경우의 예외 사항
    범위 밖인 경우의 예외 사항

--- src/main/kotlin/lotto/Winning.kt
+++ src/main/kotlin/lotto/Winning.kt
@@ -17,16 +17,29 @@ class Winning {
         val count = luckyNumber.filter { number ->
             number in 1..45
         }.size
-        return when(count){
+        return when (count) {
             6 -> true
             else -> throw IllegalArgumentException("[ERROR] 1~45 사이의 숫자를 입력해주세요.")
         }
     }
 
     fun validateLuckyNumberDuplicate(luckyNumber: List<Int>): Boolean {
-        return when(luckyNumber.distinct().size) {
+        return when (luckyNumber.distinct().size) {
             6 -> true
             else -> throw IllegalArgumentException("[ERROR] 중복되지 않은 수를 입력해주세요.")
+        }
+    }
+
+    fun validateBonusNumberRange(number: Int) {
+        if (number !in 1..45) {
+            throw IllegalArgumentException("[ERROR] 1~45 사이의 숫자를 입력해주세요.")
+        }
+    }
+
+    fun compareBonusWithLuckyNumber(number: Int) {
+        require(luckyNumber.size == 6) {"[ERROR] 로또 당첨 번호 6개가 필요합니다."}
+        if (luckyNumber.contains(number)) {
+            throw IllegalArgumentException("[ERROR] 로또 당첨 번호와 중복되지 않는 숫자를 입력해주세요.")
         }
     }
 }
