78202c38f956392b032df4d650eb0e662eb3220d
rhkrwngud445 <rhkrwngud445@gmail.com>
2022-11-10 23:32:19 +0900

    refactor : migrate lotto sort
    Lotto.kt to LottoShop.kt

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -4,8 +4,4 @@ class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6)
     }
-
-    fun getSortLotto() : List<Int>{
-        return numbers.sorted()
-    }
 }
--- src/main/kotlin/lotto/LottoShop.kt
+++ src/main/kotlin/lotto/LottoShop.kt
@@ -2,8 +2,8 @@ package lotto
 
 import camp.nextstep.edu.missionutils.Randoms
 
-class LottoShop (){
-    fun createLotto() : List<Int>{
-        return Lotto(Randoms.pickUniqueNumbersInRange(1,45,6)).getSortLotto()
+class LottoShop() {
+    fun createLotto(): List<Int> {
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6).sorted()
     }
 }
