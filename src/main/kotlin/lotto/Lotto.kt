62005fff32e2aca9a936ff83f4316de256f7a2cc
mendel <mendel.urssu@gmail.com>
2022-11-11 01:00:47 +0900

    feat: Lotto 클래스의 matchResult() 메소드 구현
    - matchResult 메소드 안에서 보너스 당첨 여부를 확인하기 위한 isMatchBonus 메소드도 구현

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -5,8 +5,19 @@ class Lotto(private val numbers: List<Int>) {
         require(numbers.size == 6)
     }
 
-    // TODO: 추가 기능 구현
+    fun matchResult(winning: List<Int>, bonus: Int): MatchResult {
+        var count = 0
+        numbers.forEach { if (winning.contains(it)) count++ }
+        return when (count) {
+            3 -> MatchResult.THREE
+            4 -> MatchResult.FOUR
+            5 -> isMatchBonus(bonus)
+            else -> MatchResult.SIX
+        }
+    }
 
+    private fun isMatchBonus(bonus: Int): MatchResult =
+        if (numbers.contains(bonus)) MatchResult.BONUS else MatchResult.FIVE
 }
 
 enum class MatchResult {
