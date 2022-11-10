97f13083485ee40b80224c1612b9f7f1bb93362b
82104 <tir2986@naver.com>
2022-11-10 18:24:58 +0900

    feat(lotto): 로또는 1부터 45까지의 서로 다른 임의의 수 6개를 사용해서 발행한다.
    - 로또 정보 상수로 저장

---
+++ src/main/kotlin/lotto/Const.kt
@@ -0,0 +1,5 @@
+package lotto
+
+const val LOTTO_LENGTH = 6
+const val LOTTO_MAX_NUM = 45
+const val LOTTO_MIN_NUM = 1
--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,9 +1,15 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6)
     }
 
-    // TODO: 추가 기능 구현
+    companion object {
+        fun generate() = Lotto(
+            Randoms.pickUniqueNumbersInRange(LOTTO_MIN_NUM, LOTTO_MAX_NUM, LOTTO_LENGTH)
+        )
+    }
 }
