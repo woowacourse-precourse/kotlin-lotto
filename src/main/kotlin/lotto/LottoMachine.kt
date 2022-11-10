c2186850ea6dc838cca67d2df4881bef6d72ce0d
zqwz <zqwz@naver.com>
2022-11-11 02:16:35 +0900

    feat(LottoMachine): 발행 로또 수량 기능 추가
    발행할 로또 수량을 계산하고 가져오는 기능

--- src/main/kotlin/lotto/LottoMachine.kt
+++ src/main/kotlin/lotto/LottoMachine.kt
@@ -1,10 +1,15 @@
 package lotto
 
 class LottoMachine(lottoAmount: String) {
+    private var lottoCount = 0
+
     init {
         validateTypeAmount(lottoAmount)
         validateUnitAmount(lottoAmount)
+        lottoCount = lottoAmount.toInt() / LOTTO_PRICE
     }
+
+    fun getLottoCount() = lottoCount
 
     fun validateTypeAmount(amount: String): Boolean {
         val typeCount = amount.filter { number ->
@@ -17,9 +22,13 @@ class LottoMachine(lottoAmount: String) {
     }
 
     fun validateUnitAmount(amount: String): Boolean {
-        return when (amount.toInt() % 1000) {
+        return when (amount.toInt() % LOTTO_PRICE) {
             0 -> true
             else -> throw IllegalArgumentException("[ERROR] 로또 구입 금액은 1000원 단위로 입력해주세요")
         }
+    }
+
+    companion object {
+        const val LOTTO_PRICE = 1000
     }
 }
