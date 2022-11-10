b2310bbd94800960e8c6eff8954d50bf36650d2e
DongJun-H <dongjun826@seoultech.ac.kr>
2022-11-10 16:29:39 +0900

    feat(Presentation): 구입 로또 개수 출력 구현

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -3,10 +3,15 @@ package lotto
 import camp.nextstep.edu.missionutils.Console
 
 const val INTRODUCE_MESSAGE = "구입금액을 입력해 주세요."
+const val SUCCESS_BUY_LOTTO_MESSAGE = "개를 구매했습니다."
 fun main() {
     displayIntroduceGuideMessage()
     val userBudget = getUserBudget()
+
+    val boughtLottoCount = userBudget.toInt() / 1000
+    displaySuccessBuyLottoGuideMessage(boughtLottoCount)
 }
 
 fun getUserBudget(): String = Console.readLine()
 fun displayIntroduceGuideMessage() = println(INTRODUCE_MESSAGE)
+fun displaySuccessBuyLottoGuideMessage(boughtLottoCount: Int) = println("$boughtLottoCount$SUCCESS_BUY_LOTTO_MESSAGE")
