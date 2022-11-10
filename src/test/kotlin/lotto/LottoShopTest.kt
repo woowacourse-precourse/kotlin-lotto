6908df9aff8ba46dc5dd3aa70e2f604d64dbd581
JunJangE <wnswkd486@gmail.com>
2022-11-11 02:16:08 +0900

    test: 구입 금액에 해당하는 만큼 로또를 생성하는지 테스트

--- src/test/kotlin/lotto/LottoShopTest.kt
+++ src/test/kotlin/lotto/LottoShopTest.kt
@@ -1,8 +1,7 @@
 package lotto
 
-import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
 
 class LottoShopTest {
 
@@ -10,11 +9,18 @@ class LottoShopTest {
     fun `로또를 구입 금액에 해당하는 만큼 살 수 있다`() {
         val lottoShop: LottoShop = LottoShop()
         val cntLotto = lottoShop.cntLotto(8000)
-        Assertions.assertThat(cntLotto).isEqualTo(8)
+        assertThat(cntLotto).isEqualTo(8)
     }
 
     @Test
     fun `로또를 구입 금액이 1000원으로 나누어 떨어지지 않을 경우 예외가 발생한다`() {
 
+    }
+
+    @Test
+    fun `로또 구입 금액에 해당하는 만큼 로또를 생성한다`() {
+        val lottoShop: LottoShop = LottoShop()
+        val myLottoNumber =  lottoShop.createRandomLotto(2)
+        assertThat(myLottoNumber).hasSize(2)
     }
 }
