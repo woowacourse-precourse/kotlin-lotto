539da03b3cd731da36b95637133b8218ec3d0622
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2022-11-10 14:50:58 +0900

    feat[lotto]: 로또 번호 범위에 대한 예외 처리 기능 구현

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -4,6 +4,9 @@ class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6)
         require(numbers.toSet().size == 6)
+        numbers.forEach { num ->
+            require(1 <= num && num <= 45) {"[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."}
+        }
     }
 
     // TODO: 추가 기능 구현
