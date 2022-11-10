831afdc7a0066d51ef504c2e10b33127160f0d31
lsakee <lsls4868@gmail.com>
2022-11-10 22:28:16 +0900

    feat : 문자열 확인 기능 -입력받은 값이 숫자인지 확인 하는 기능

--- src/main/kotlin/lotto/user/UserInput.kt
+++ src/main/kotlin/lotto/user/UserInput.kt
@@ -14,3 +14,12 @@ fun inputUserNumber(): String {
     println(userInputNumber.replace("000", ""))
     return userInputNumber.replace("000", "")
 }
+
+fun checkInputMessagePatten( checkOverlap: MutableList<String>) {
+    val regax="^[0-9]*$"
+    val pattern: Pattern = Pattern.compile(regax)
+    for (inputNum in checkOverlap) {
+        val matcher: Matcher = pattern.matcher(inputNum)
+        if (matcher.find().toString() == "false") throw IllegalArgumentException()
+    }
+}
