496ed34ff59f710cf6f5d149bdecdc1abbabcca0
82104 <tir2986@naver.com>
2022-11-10 18:45:25 +0900

    feat(lottoprovider): 로또 가격(1000원)으로 나누어 떨어지지 않는 경우 예외를 발생시킨다.

--- src/main/kotlin/lotto/LottoProvider.kt
+++ src/main/kotlin/lotto/LottoProvider.kt
@@ -7,6 +7,7 @@ class LottoProvider {
     fun provideLottos(cost: Int): List<Lotto> {
 
         requireWithPrefix(cost > LOTTO_COST, "구입금액은 ${LOTTO_COST}이상이어야 합니다.")
+        requireWithPrefix(cost % LOTTO_COST == 0, "구입금액은 ${LOTTO_COST}로 나누어 떨어져야 합니다.")
 
         val lottos = mutableListOf<Lotto>()
         repeat(cost / LOTTO_COST) {
