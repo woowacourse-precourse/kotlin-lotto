a95dee008dc0f41fcf6fa754588bdf19631fb8cc
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2022-11-10 17:41:05 +0900

    test[lotto]: 로또 번호와 당첨 번호를 비교하여 일치하는 개수 확인하는 테스트 코드 추가

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -1,10 +1,14 @@
 package lotto
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
 
 class LottoTest {
+
+    val answer = Lotto(listOf(1, 5, 14, 22, 33, 41))
+
     @Test
     fun `로또 번호의 개수가 6개가 넘어가면 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
@@ -26,5 +30,37 @@ class LottoTest {
         assertThrows<IllegalArgumentException> {
             Lotto(listOf(1, 2, 48, 5, 7, 9))
         }
+    }
+
+    @Test
+    fun `로또 번호가 당첨 번호와 3개 일치`() {
+        val user = Lotto(listOf(1, 2, 14, 22, 37, 38))
+        val result = 3
+
+        assertThat(user.countCompareLotto(answer)).isEqualTo(result)
+    }
+
+    @Test
+    fun `로또 번호가 당첨 번호와 4개 일치`() {
+        val user = Lotto(listOf(1, 2, 14, 22, 33, 38))
+        val result = 4
+
+        assertThat(user.countCompareLotto(answer)).isEqualTo(result)
+    }
+
+    @Test
+    fun `로또 번호가 당첨 번호와 5개 일치`() {
+        val user = Lotto(listOf(1, 2, 14, 22, 33, 41))
+        val result = 5
+
+        assertThat(user.countCompareLotto(answer)).isEqualTo(result)
+    }
+
+    @Test
+    fun `로또 번호가 당첨 번호와 6개 일치`() {
+        val user = Lotto(listOf(1, 5, 14, 22, 33, 41))
+        val result = 6
+
+        assertThat(user.countCompareLotto(answer)).isEqualTo(result)
     }
 }
