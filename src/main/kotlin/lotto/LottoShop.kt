71fcc8a1bc4fd0f898c8ccf23d12280a4eea4cac
JunJangE <wnswkd486@gmail.com>
2022-11-11 02:36:34 +0900

    refactor: 코드 수정

--- src/main/kotlin/lotto/LottoShop.kt
+++ src/main/kotlin/lotto/LottoShop.kt
@@ -4,12 +4,9 @@ import camp.nextstep.edu.missionutils.Randoms
 
 class LottoShop {
 
-    fun buyLotto() {
-        println("구입금액을 입력해 주세요.")
-        val buyAmount = readLine()!!
-        if (!buyAmount.inputTypeException()) return
-        val buyCntLotto = cntLotto(buyAmount.inputMoneyException())
-        createRandomLotto(buyCntLotto)
+    fun buyLotto(money: Int) : List<Lotto> {
+        val buyCntLotto = cntLotto(money)
+        return createRandomLotto(buyCntLotto)
     }
 
     fun cntLotto(purchaseAmount: Int): Int {
@@ -18,23 +15,7 @@ class LottoShop {
         return purchaseCnt
     }
 
-    fun String.inputMoneyException(): Int {
-        if (this.toInt() % 1000 == 0) return this.toInt()
-        throw IllegalArgumentException("[ERROR] 숫자가 나누어 떨어지지 않습니다.")
-    }
-
-    fun String.inputTypeException(): Boolean {
-        return try {
-            this.toInt()
-            true
-
-        } catch (e: NumberFormatException) {
-            println("[ERROR] 구입 금액을 잘못입력하셨습니다.")
-            false
-        }
-    }
-
-    fun createRandomLotto(purchaseCnt : Int): List<Lotto> {
+    fun createRandomLotto(purchaseCnt: Int): List<Lotto> {
         val myLottoNumber = mutableListOf<Lotto>()
         for (i in 0 until purchaseCnt) {
             val randomLotto = Randoms.pickUniqueNumbersInRange(1, 45, 6)
