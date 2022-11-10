bf56ac26a4e7cd4131943e585ea44fbd95f00737
JunJangE <wnswkd486@gmail.com>
2022-11-11 01:52:56 +0900

    feat: 로또 구입 금액을 잘못 입력할 경우 예외 처리 기능

--- src/main/kotlin/lotto/LottoShop.kt
+++ src/main/kotlin/lotto/LottoShop.kt
@@ -6,18 +6,31 @@ class LottoShop {
 
     fun buyLotto() {
         println("구입금액을 입력해 주세요.")
-        val buyAmount = readLine()!!.toInt()
-        cntLotto(buyAmount)
-
+        val buyAmount = readLine()!!
+        if (!buyAmount.inputTypeException()) return
+        val buyCntLotto = cntLotto(buyAmount.inputMoneyException())
     }
 
     fun cntLotto(purchaseAmount: Int): Int {
-        if (purchaseAmount % 1000 != 0) throw IllegalArgumentException("[ERROR] 숫자가 나누어 떨어지지 않습니다.")
         val purchaseCnt = (purchaseAmount / 1000)
         println("${purchaseCnt}개를 구매했습니다.")
-
         return purchaseCnt
+    }
 
+    fun String.inputMoneyException(): Int {
+        if (this.toInt() % 1000 == 0) return this.toInt()
+        throw IllegalArgumentException("[ERROR] 숫자가 나누어 떨어지지 않습니다.")
+    }
+
+    fun String.inputTypeException(): Boolean {
+        return try {
+            this.toInt()
+            true
+
+        } catch (e: NumberFormatException) {
+            println("[ERROR] 구입 금액을 잘못입력하셨습니다.")
+            false
+        }
     }
 
     fun createRandomLotto() {
