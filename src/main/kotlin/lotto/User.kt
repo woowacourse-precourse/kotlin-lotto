fdd8e3e0f2e361f41894581e373c1bd3b48e4c0a
zqwz <zqwz@naver.com>
2022-11-10 18:08:17 +0900

    feat(User): 로또 구입 금액 예외 사항 기능 구현
    1000원 단위가 아닌 경우의 예외 사항
    숫자가 아닌 경우의 예외 사항

---
+++ src/main/kotlin/lotto/User.kt
@@ -0,0 +1,20 @@
+package lotto
+
+class User {
+    fun validateTypeAmount(amount: String): Boolean {
+        val typeCount = amount.filter { number ->
+            number in '0'..'9'
+        }
+        return when (typeCount.length) {
+            amount.length -> true
+            else -> throw IllegalArgumentException("[ERROR] 로또 구입 금액은 숫자만 입력해주세요.")
+        }
+    }
+
+    fun validateUnitAmount(amount: String): Boolean {
+        return when (amount.toInt() % 1000) {
+            0 -> true
+            else -> throw IllegalArgumentException("[ERROR] 로또 구입 금액은 1000원 단위로 입력해주세요")
+        }
+    }
+}
