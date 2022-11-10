7e53ecd6ca00d3374bf9e5fdacda98287c035f25
dltkd1395 <dltkd13956@naver.com>
2022-11-10 20:56:23 +0900

    feat(Lotto) : 로또 번호가 오름차순인지 알 수 있는 기능 구현

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -4,8 +4,12 @@ class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6)
         require(hasNotDuplicateNumber())
+        require(isVaildAscendingOrder())
     }
 
     private fun hasNotDuplicateNumber(): Boolean =
         numbers.size == numbers.distinct().size
+
+    private fun isVaildAscendingOrder(): Boolean =
+        numbers == numbers.sorted()
 }
