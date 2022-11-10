c15e442857407bffe0d636b80376eabf71fb12f0
JunHyeok0205 <wnsdlfgur@naver.com>
2022-11-10 17:42:05 +0900

    feat: 보너스 번호 입력 기능 추가(예외처리)

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -2,9 +2,13 @@ package lotto
 
 import camp.nextstep.edu.missionutils.Randoms
 
+
+private var winNum: MutableList<Int> = ArrayList()
+
 fun main() {
     lottoPrice()
     winLottoNumber()
+    bonusNumber()
 }
 
 fun lottoPrice() {
@@ -35,7 +39,6 @@ fun myLotto(lotto: Int) {
 fun winLottoNumber() {
     println("\n당첨 번호를 입력해 주세요.")
     val number : List<String> = readLine()!!.split(",")
-    val winNum: MutableList<Int> = ArrayList()
     for (element in number) {
         val numberToInt = element.toInt()
         if (winNum.contains(numberToInt)) throw IllegalArgumentException("[ERROR] 중복된 숫자를 입력하실 수 없습니다.")
@@ -44,4 +47,15 @@ fun winLottoNumber() {
         winNum.add(numberToInt)
     }
     Lotto(winNum).winNumber
+}
+
+fun bonusNumber() {
+    println("보너스 번호를 입력해 주세요")
+    val number = readLine()!!.toInt()
+    if (number !in 1..45) {
+        throw IllegalArgumentException("[ERROR] 1~45 사이의 숫자만 입력하실 수 있습니다.")
+    }
+    if (winNum.contains(number)) {
+        throw IllegalArgumentException("[ERROR] 보너스 번호가 당첨 번호와 일치합니다. 다시 입력해 주십시오.")
+    }
 }
