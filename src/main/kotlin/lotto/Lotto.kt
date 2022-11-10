6a0d58c307d6f054bacd5c63adc529f7a7b66f8c
Kim Jaeyong <jy3479@naver.com>
2022-11-10 15:35:19 +0900

    기능 2 수정

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -6,7 +6,7 @@ class Lotto(private val numbers: List<Int>) {
         require(!isDuplicated())
     }
 
-    private fun isDuplicated() = setOf(numbers).size != numbers.size
+    private fun isDuplicated() = numbers.toSet().size != numbers.size
     fun printLotto() {
         numbers.sorted().forEachIndexed { index, i ->
             if (index == 0) print("[$i, ")
