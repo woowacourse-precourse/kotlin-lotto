ac8752679d3c1296146b277fb37a66509072e5db
82104 <tir2986@naver.com>
2022-11-10 18:33:02 +0900

    feat(lotto): 로또 번호에 중복된 수가 있으면 예외를 발생시킨다.

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -13,6 +13,10 @@ class Lotto(private val numbers: List<Int>) {
             numbers.all { it in LOTTO_MIN_NUM..LOTTO_MAX_NUM },
             "로또 번호는 ${LOTTO_MIN_NUM}부터 ${LOTTO_MAX_NUM} 사이의 숫자여야 합니다."
         )
+        requireWithPrefix(
+            numbers == numbers.distinct(),
+            "로또 번호에는 중복되는 숫자가 없어야 합니다."
+        )
     }
 
     companion object {
