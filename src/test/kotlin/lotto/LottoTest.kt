d510a318f4bd53602807f54e2fc67db78cb72db8
82104 <tir2986@naver.com>
2022-11-10 18:37:02 +0900

    test(lottotest): 로또 번호가 6개보다 적은 경우 테스트 추가

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -12,7 +12,13 @@ class LottoTest {
         }
     }
 
-    // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
+    @Test
+    fun `로또 번호의 개수가 6개보다 적다면 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(1, 2, 3, 4, 5))
+        }
+    }
+
     @Test
     fun `로또 번호에 중복된 숫자가 있으면 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
