f7734f6842654f4b7bb2c575ca75a8936df77f6e
SeungHyeon Shim <tladhehd@naver.com>
2022-11-10 21:52:12 +0900

    <feat>(lotto): 당첨 번호 입력, 보너스 번호 입력받는 기능 추가

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -17,7 +17,6 @@ ex)
 작은 단위의 테스트
 사용자의 숫자가 컴퓨터의 숫자와 하나도 일치하지 않으면 낫싱을 출력한다.
 사용자의 숫자가 컴퓨터의 숫자와 1개는 일치하고, 위치가 다르면 1볼을 출력한다.
-
 */
 
 // Enum 클래스 적용해 프로그래밍을 구현한다.
@@ -56,6 +55,29 @@ fun howMany() : Int{
     return answer
 }
 
+fun enterWinningNumber() : List<Int> {
+    var userCheck = readLine()
+    var winningNumber = mutableListOf<Int>()
+    var number=userCheck.split(',')
+    for (obj in 0 until number.size){
+        // 번호가 중복된 경우 에러 유발해야함
+        winningNumber.add(number[obj].toInt())
+    }
+    return winningNumber
+}
+
+fun enterBonusNumber(winningNumbers : List<Int>) : Int {
+    var userCheck = readLine().toInt()
+    // 보너스 번호가 로또 번호와 같은 경우
+    if (winningNumbers.contains(userCheck)){
+        throw IllegalArgumentException("[ERROR] 로또 번호와 겹칩니다")
+    }
+    // 번호 입력을 1~45사이 안한 경우
+    else if (userCheck>45 || userCheck<1){
+        throw IllegalArgumentException("[ERROR] 보너스 번호는 1~45 사이")
+    }
+    return userCheck
+}
 
 
 
@@ -63,6 +85,10 @@ fun main() {
     var cnt = 0
     var lottoNumberCollect = mutableListOf<Int>()
     var bonusNumberCollect = mutableListOf<Int>()
+    var obj = LottoGames(numbers= listOf(0,0,0,0,0,0))
+    var winningNumber = listOf<Int>()
+    var bonusNumber = 0
+
     lottoNumberCollect = pickLottoNumbers()
     bonusNumberCollect = pickBonusNumber(lottoNumberCollect)
 //    println(lottoNumberCollect)
@@ -75,12 +101,19 @@ fun main() {
     cnt=howMany()
     println()
     println(cnt.toString()+State.many.message)
+    for (round in 0 until cnt){
+        LottoGames(lottoNumberCollect)
+    }
+    println()
 
     //당첨 번호
     println(State.enterLottoNumber.message)
+    winningNumber=enterWinningNumber()
+    println(winningNumber)
 
     //보너스 번호
     println(State.enterBonusNumber.message)
+    bonusNumber=enterBonusNumber(winningNumber)
 
     //당첨 통계
     println(State.end.message)
