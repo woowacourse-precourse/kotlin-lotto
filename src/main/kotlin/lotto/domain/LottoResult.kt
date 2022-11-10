bf0bb94af0f1dbff7bbc7c14fdc20e94a5d7a5b7
82104 <tir2986@naver.com>
2022-11-10 22:14:25 +0900

    feat(lottoresult): 맞춘 개수 필드에 추가

--- src/main/kotlin/lotto/domain/LottoResult.kt
+++ src/main/kotlin/lotto/domain/LottoResult.kt
@@ -1,10 +1,10 @@
 package lotto.domain
 
-enum class LottoResult(val prize: Int) {
-    FAIL(0),
-    FIFTH(5000),
-    FOURTH(50000),
-    THIRD(1500000),
-    SECOND(30000000),
-    FIRST(2000000000)
+enum class LottoResult(val matchCnt: Int, val prize: Int) {
+    FAIL(-1, 0),
+    FIFTH(3, 5000),
+    FOURTH(4, 50000),
+    THIRD(5, 1500000),
+    SECOND(5, 30000000),
+    FIRST(5, 2000000000)
 }
