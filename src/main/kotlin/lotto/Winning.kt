76af5a53dd4144547852f59cd1625ce0b5762df4
zqwz <zqwz@naver.com>
2022-11-11 03:05:38 +0900

    feat(Winning): 로또 당첨 번호 예외 사항 확인 기능 추가
    로또 당첨 번호의 예외 사항을 확인하는 기능

--- src/main/kotlin/lotto/Winning.kt
+++ src/main/kotlin/lotto/Winning.kt
@@ -1,45 +1,55 @@
 package lotto
 
+import java.lang.NumberFormatException
 import kotlin.IllegalArgumentException
 
-class Winning {
+class Winning(winningNumber: String) {
     private val luckyNumber = mutableListOf<Int>()
+
+    init {
+        splitLuckyNumber(winningNumber)
+        require(luckyNumber.size == 6) { "[ERROR] 번호를 6개 입력해 주세요" }
+        validateLuckyNumberRange()
+        validateLuckyNumberDuplicate()
+    }
 
     fun splitLuckyNumber(numbers: String) {
         numbers.split(",").forEach { number ->
-            luckyNumber.add(number.toInt())
+            try {
+                luckyNumber.add(number.toInt())
+            } catch (exception: NumberFormatException) {
+                throw IllegalArgumentException("[ERROR] 숫자만 입력해주세요.")
+            }
         }
-        if (luckyNumber.size != 6)
-            throw IllegalArgumentException("[ERROR] 로또 당첨 번호 6개를 입력해주세요.")
     }
 
-    fun validateLuckyNumberRange(luckyNumber: List<Int>): Boolean {
+    fun validateLuckyNumberRange(): Boolean {
         val count = luckyNumber.filter { number ->
             number in 1..45
         }.size
         return when (count) {
             6 -> true
-            else -> throw IllegalArgumentException("[ERROR] 1~45 사이의 숫자를 입력해주세요.")
+            else -> throw IllegalArgumentException("[ERROR] 1~45 사이의 숫자를 입력해 주세요.")
         }
     }
 
-    fun validateLuckyNumberDuplicate(luckyNumber: List<Int>): Boolean {
+    fun validateLuckyNumberDuplicate(): Boolean {
         return when (luckyNumber.distinct().size) {
             6 -> true
-            else -> throw IllegalArgumentException("[ERROR] 중복되지 않은 수를 입력해주세요.")
+            else -> throw IllegalArgumentException("[ERROR] 중복되지 않은 수를 입력해 주세요.")
         }
     }
 
     fun validateBonusNumberRange(number: Int) {
         if (number !in 1..45) {
-            throw IllegalArgumentException("[ERROR] 1~45 사이의 숫자를 입력해주세요.")
+            throw IllegalArgumentException("[ERROR] 1~45 사이의 숫자를 입력해 주세요.")
         }
     }
 
     fun compareBonusWithLuckyNumber(number: Int) {
-        require(luckyNumber.size == 6) {"[ERROR] 로또 당첨 번호 6개가 필요합니다."}
+        require(luckyNumber.size == 6) { "[ERROR] 로또 당첨 번호 6개가 필요합니다." }
         if (luckyNumber.contains(number)) {
-            throw IllegalArgumentException("[ERROR] 로또 당첨 번호와 중복되지 않는 숫자를 입력해주세요.")
+            throw IllegalArgumentException("[ERROR] 로또 당첨 번호와 중복되지 않는 숫자를 입력해 주세요.")
         }
     }
 }
