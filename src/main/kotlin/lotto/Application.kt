5a85b60c9b4875d5acca80388a70fb21e3f2b17d
JunHyeok0205 <wnsdlfgur@naver.com>
2022-11-10 15:16:09 +0900

    doc: README 작성 feat: 로또 구입 금액 입력 기능 추가(예외처리) feat: 로또 구입 개수 출력(예외처리), 랜덤 로또 번호 출력 feat: 당첨 번호 입력 기능 추가

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,44 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Randoms
+
+
 fun main() {
-    TODO("프로그램 구현")
+    lottoPrice()
+    winLottoNumber()
+}
+
+fun lottoPrice() {
+    println("구입금액을 입력해 주세요.")
+    val lottoBuyPrice = readLine()!!.toInt()
+    if (lottoBuyPrice % 1000 != 0) {
+        throw IllegalArgumentException("[ERROR] 잘못된 값을 입력하였습니다.")
+    }
+    if (lottoBuyPrice == 0) {
+        throw IllegalArgumentException("[ERROR] 로또는 한 장 이상 사야합니다.")
+    }
+    val myLotto = lottoBuyPrice / 1000
+    myLotto(myLotto)
+}
+
+fun randomNumber(): List<Int> {
+    return Randoms.pickUniqueNumbersInRange(1, 45, 6).toList().sorted()
+}
+
+fun myLotto(lotto: Int) {
+    println("\n${lotto}개를 구매했습니다.")
+    for (i in 1..lotto) {
+        randomNumber()
+        println(randomNumber())
+    }
+}
+
+fun winLottoNumber() {
+    println("\n당첨 번호를 입력해 주세요.")
+    val number = readLine()!!.split(",")
+    val winNum: MutableList<Int> = ArrayList()
+    for (i in 0..5) {
+        winNum.add(number[i].toInt())
+    }
+    Lotto(winNum).winNumber
 }
--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -4,6 +4,6 @@ class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6)
     }
-
+    val winNumber = numbers
     // TODO: 추가 기능 구현
 }
