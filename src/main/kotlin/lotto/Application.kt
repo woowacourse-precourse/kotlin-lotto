c0af4382ad52175d935c03a2749dec402879022c
sjh970603 <sjh970603@naver.com>
2022-11-11 01:46:12 +0900

    구매자가 입력하는것이 아닌 판매자가 입력하는것임을 깨달아서 코드 갈아엎음

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -5,21 +5,23 @@ import camp.nextstep.edu.missionutils.Randoms
 fun main() {
     //TODO("프로그램 구현")
     var winningnum = winningnumselect()
+    var bonusnumber = bonusball()
     var usermoney = moneyinput()
     moneycheck(usermoney)
     var ticket = usermoney/1000
     println("${ticket}개를 구매했습니다.")
     var userlottolist = userselectnum(ticket)
     println("")
-    var wincountlist = comparenumber(winningnum,userlottolist)
-
-
+    var wincountlist = comparenumber(winningnum,userlottolist,bonusnumber)
+    var a = calculater(wincountlist)
+    println(a)
 
 }
 
 
 fun winningnumselect () : MutableList<Int> {
     val numbers = Randoms.pickUniqueNumbersInRange(1, 45 ,6)
+    println(numbers)
     return numbers
 }
 
@@ -52,11 +54,12 @@ fun userselectnum (a: Int) : Array<IntArray>
     return usernumber
 }
 
-fun comparenumber(a:MutableList<Int> , b:Array<IntArray>) : IntArray {
+fun comparenumber(a:MutableList<Int> , b:Array<IntArray> , c:MutableList<Int>) : IntArray {
     var countunion = IntArray(b.size)
     for (i in 0..b.size-1)
     {
         var count = 0
+
         for (j in 0..5)
         {
             if (a[0] == b[i][j])
@@ -83,6 +86,7 @@ fun comparenumber(a:MutableList<Int> , b:Array<IntArray>) : IntArray {
             {
                 count+=1
             }
+
         }
         countunion[i]=count
         println("")
@@ -98,7 +102,7 @@ fun calculater (a:IntArray) : Int
     var rank3 = 0
     var rank2 = 0
     var rank1 = 0
-    for (i in 0..a.size)
+    for (i in 0..a.size-1)
     {
 
 
@@ -117,19 +121,24 @@ fun calculater (a:IntArray) : Int
             rank3+=1
             money+=150000
         }
-        if (a[i]==6)
+        if (a[i]== 6 )
         {
             rank1+=1
             money+=2000000000
         }
-        if (a[i] == 15)
+        if (a[i] == 5)
         {
             rank2+=1
             money+=30000000
         }
     }
+    println("3개 일치 (5,000원) - ${rank5}개")
+    println("4개 일치 (50,000원) - ${rank4}개")
+    println("5개 일치 (1,500,000원) - ${rank3}개")
+    println("5개 일치, 보너스 볼 일치 (30,000,000원) - ${rank2}개")
+    println("6개 일치 (2,000,000,000원) - ${rank1}개")
+    return money
 }
-
 
 
 
