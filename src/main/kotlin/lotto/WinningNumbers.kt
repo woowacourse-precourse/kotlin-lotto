6af2373a567fe6e6227654e62e56f48984842aba
82104 <tir2986@naver.com>
2022-11-10 19:19:36 +0900

    feat(winningnumbers): 보너스 번호가 로또 번호에 포함되어 있으면 예외를 발생시킨다.

--- src/main/kotlin/lotto/WinningNumbers.kt
+++ src/main/kotlin/lotto/WinningNumbers.kt
@@ -3,12 +3,16 @@ package lotto
 import lotto.util.requireWithPrefix
 
 class WinningNumbers(numbers: List<Int>, val bonus: Int) {
-    val numbers: Lotto = Lotto(numbers)
+    val winNumbers: Lotto = Lotto(numbers)
 
     init {
         requireWithPrefix(
             bonus in LOTTO_MIN_NUM..LOTTO_MAX_NUM,
             "보너스 번호는 ${LOTTO_MIN_NUM}부터 ${LOTTO_MAX_NUM} 사이의 숫자여야 합니다."
+        )
+        requireWithPrefix(
+            bonus !in numbers,
+            "보너스 번호는 로또 번호와 중복되지 않는 숫자여야 합니다."
         )
     }
 }
