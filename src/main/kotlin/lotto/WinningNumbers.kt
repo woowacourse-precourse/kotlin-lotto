2b70004273f8a5e5aac94a4a0e4681dfddec540c
82104 <tir2986@naver.com>
2022-11-10 19:17:14 +0900

    feat(winningnumbers): 보너스 번호가 1부터 45까지의 수가 아닌 경우 예외를 발생시킨다.

--- src/main/kotlin/lotto/WinningNumbers.kt
+++ src/main/kotlin/lotto/WinningNumbers.kt
@@ -1,9 +1,14 @@
 package lotto
 
+import lotto.util.requireWithPrefix
+
 class WinningNumbers(numbers: List<Int>, val bonus: Int) {
     val numbers: Lotto = Lotto(numbers)
 
     init {
-
+        requireWithPrefix(
+            bonus in LOTTO_MIN_NUM..LOTTO_MAX_NUM,
+            "보너스 번호는 ${LOTTO_MIN_NUM}부터 ${LOTTO_MAX_NUM} 사이의 숫자여야 합니다."
+        )
     }
 }
