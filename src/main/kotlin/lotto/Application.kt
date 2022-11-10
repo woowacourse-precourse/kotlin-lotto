cb567b66892408c9588e3a7d42c36b42b7aa15ba
DongJun-H <dongjun826@seoultech.ac.kr>
2022-11-10 16:25:24 +0900

    feat(Presentation): 로또 구입 안내 메시지 및 입력기능 구현

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,12 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Console
+
+const val INTRODUCE_MESSAGE = "구입금액을 입력해 주세요."
 fun main() {
-    TODO("프로그램 구현")
+    displayIntroduceGuideMessage()
+    val userBudget = getUserBudget()
 }
+
+fun getUserBudget(): String = Console.readLine()
+fun displayIntroduceGuideMessage() = println(INTRODUCE_MESSAGE)
