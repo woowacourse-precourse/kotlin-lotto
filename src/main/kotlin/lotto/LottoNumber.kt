dc1ed52f2d354c20a70e739c96da9ea3399f100d
dltkd1395 <dltkd13956@naver.com>
2022-11-10 21:00:53 +0900

    feat(LottoNumber) : 1부터 45 사이의 숫자인지 알 수 있는 기능 구현

---
+++ src/main/kotlin/lotto/LottoNumber.kt
@@ -0,0 +1,11 @@
+package lotto
+
+class LottoNumber(private val lottoNumber: Int) {
+
+    init {
+        require(isValidNumberRange())
+    }
+
+    private fun isValidNumberRange(): Boolean =
+            lottoNumber in 1 until 46
+}
