3e5c2db15e1169d7d400bb1e46a7e303ef4ae566
DongJun-H <dongjun826@seoultech.ac.kr>
2022-11-10 16:37:18 +0900

    feat(Presentation): 구입한 로또에 대한 랜덤 번호 뽑기 및 출력 구현

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,6 +1,7 @@
 package lotto
 
 import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
 
 const val INTRODUCE_MESSAGE = "구입금액을 입력해 주세요."
 const val SUCCESS_BUY_LOTTO_MESSAGE = "개를 구매했습니다."
@@ -10,8 +11,20 @@ fun main() {
 
     val boughtLottoCount = userBudget.toInt() / 1000
     displaySuccessBuyLottoGuideMessage(boughtLottoCount)
+    val lottoTickets = getLottoTickets(boughtLottoCount)
 }
 
 fun getUserBudget(): String = Console.readLine()
+fun getLottoTickets(boughtLottoCount: Int): List<Lotto> {
+    val lottoTickets = mutableListOf<Lotto>()
+    for (i in 0 until boughtLottoCount) {
+        val currentLotto = Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6))
+        displayCurrentLottoNumbers(currentLotto)
+        lottoTickets.add(currentLotto)
+    }
+    return lottoTickets
+}
+
 fun displayIntroduceGuideMessage() = println(INTRODUCE_MESSAGE)
 fun displaySuccessBuyLottoGuideMessage(boughtLottoCount: Int) = println("$boughtLottoCount$SUCCESS_BUY_LOTTO_MESSAGE")
+fun displayCurrentLottoNumbers(currentLotto: Lotto) = println(currentLotto.getSortedNumbers())
