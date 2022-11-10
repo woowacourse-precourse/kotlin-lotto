3a155b77d8fa21ed4a33a94b692e567a0d60e2d7
JunJangE <wnswkd486@gmail.com>
2022-11-10 23:46:12 +0900

    test: 로또 구입 금액에 해당하는 만큼 살 수 있는지 테스트

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -1,5 +1,6 @@
 package lotto
 
+import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
@@ -21,4 +22,19 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+
+    @Test
+    fun `로또를 구입 금액에 해당하는 만큼 살 수 있다`() {
+        val lottoShop: LottoShop = LottoShop()
+        val cntLotto = lottoShop.cntLotto(8000)
+        Assertions.assertThat(cntLotto).isEqualTo(8)
+    }
+
+    @Test
+    fun `로또를 구입 금액이 1000원으로 나누어 떨어지지 않을 경우 예외가 발생한다`() {
+        val lottoShop: LottoShop = LottoShop()
+        assertThrows<IllegalArgumentException> {
+            lottoShop.cntLotto(800)
+        }
+    }
 }
