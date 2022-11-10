3f08deddee4dcd694e710ceaa71fd14c61c00ec3
sjh970603 <sjh970603@naver.com>
2022-11-10 16:21:15 +0900

    랜덤 로또번호 발생 및 유저 money 입력후 1000원단위인지 체크

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,6 +1,36 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
     //TODO("프로그램 구현")
+    var winningnum = winningnumselect()
+    var usermoney = moneyinput()
+    moneycheck(usermoney)
+    var ticket = usermoney/1000
+
+
+}
+
+
+fun winningnumselect () : MutableList<Int> {
+    val numbers = Randoms.pickUniqueNumbersInRange(1, 45 ,6)
+    return numbers
+}
+
+fun moneyinput () : Int {
+    var money = readLine()!!.toInt()
+    return money
+}
+fun moneycheck (a : Int){
+    if (a%1000 != 0)
+    {
+        throw new IllegalArgumentException("1000원 단위로 입력해주세요.")
+    }
+}
+
+fun userselectnum (a: Int) : MutableList<Int>
+{
+    for ()
 
 }
