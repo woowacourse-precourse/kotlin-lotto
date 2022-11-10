9e85d7600ab65625136f4f9c55fb6d8f3807256f
kyounggudev <homin222@naver.com>
2022-11-10 10:32:34 +0900

    feat : 당첨 통계 enum 클래스 추가

---
+++ src/main/kotlin/lotto/WinningStatistics.kt
@@ -0,0 +1,58 @@
+package lotto
+
+import java.lang.IllegalArgumentException
+import java.text.DecimalFormat
+import kotlin.math.round
+
+const val ERROR_NEGATIVE = "[ERROR] [ERROR] 수익률이 음수일 수는 없습니다."
+const val ERROR_ROUND_UNIT = "[ERROR] 수익률은 반올림을 통해 소수 첫째 자리까지만 있어야 합니다."
+
+enum class WinningStatistics(
+    val winName: String,
+    val price: Int,
+    var count: Int
+) {
+    WIN3("3개 일치", 5000, 0),
+    WIN4("4개 일치", 50000, 0),
+    WIN5("5개 일치", 1500000, 0),
+    WIN5Bonus("5개 일치, 보너스 볼 일치", 30000000, 0),
+    WIN6("6개 일치", 2000000000, 0);
+
+    companion object {
+        var roundUnit=10
+        fun print(allPrice : Int) {
+            println("당첨 통계")
+            println("---")
+            val dec = DecimalFormat("#,###")
+            enumValues<WinningStatistics>()
+                .map { "${it.winName} (${dec.format(it.price)}원) - ${it.count}개" }
+                .forEach { println(it) }
+            val returnRate = calculateReturn(allPrice)
+            println("총 수익률은 ${returnRate}%입니다.")
+        }
+
+        private fun calculateReturn(allPrice: Int):Double{
+            var returnRate : Double = 0.0
+            for (win in enumValues<WinningStatistics>()) {
+                returnRate += win.price * win.count
+            }
+            returnRate = (returnRate) / (allPrice) * 100
+            returnRate =round(returnRate* roundUnit)/roundUnit
+            returnRateException(returnRate)
+            return returnRate
+        }
+
+        fun returnRateException(returnRate : Double){
+           require(returnRate>=0){
+               println(ERROR_NEGATIVE)
+               throw IllegalArgumentException(ERROR_NEGATIVE)
+           }
+            val regex = "[0-9]+\\.[0-9]".toRegex()
+            val returnRate =returnRate.toString()
+            require(returnRate.matches(regex)){
+                println(ERROR_ROUND_UNIT)
+                throw IllegalArgumentException(ERROR_ROUND_UNIT)
+            }
+        }
+    }
+}
