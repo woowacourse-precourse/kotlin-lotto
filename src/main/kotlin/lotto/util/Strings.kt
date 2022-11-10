ad2e133c4c88e2d53c942a0e674c8d4183592c63
82104 <tir2986@naver.com>
2022-11-10 23:42:20 +0900

    refactor(util): 숫자 콤마로 분리 안될 시 에러 발생시키도록 변경

--- src/main/kotlin/lotto/util/Strings.kt
+++ src/main/kotlin/lotto/util/Strings.kt
@@ -1,12 +1,13 @@
 package lotto.util
 
 
-fun String.divideToNumsOrNull(delimiters: String): List<Int>? {
+fun String.divideToNums(delimiters: String): List<Int> {
     val parsed = this.split(delimiters)
     val nums = mutableListOf<Int>()
     for (str in parsed) {
-        val num = str.toIntOrNull() ?: return null
-        nums.add(num)
+        val num = str.toIntOrNull()
+        requireWithPrefix(num != null, "숫자를 ${delimiters}로 구분해서 입력해 주세요.")
+        nums.add(num!!)
     }
     return nums
 }
