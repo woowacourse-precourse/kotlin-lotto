ac1d9f2bd19d1d4f4542040336c6c1a7ffebe22d
82104 <tir2986@naver.com>
2022-11-10 18:44:34 +0900

    feat(lottoprovider): 로또를 1개도 구매할 수 없는 경우 예외를 발생시킨다.

--- src/main/kotlin/lotto/LottoProvider.kt
+++ src/main/kotlin/lotto/LottoProvider.kt
@@ -1,8 +1,12 @@
 package lotto
 
+import lotto.util.requireWithPrefix
+
 class LottoProvider {
 
     fun provideLottos(cost: Int): List<Lotto> {
+
+        requireWithPrefix(cost > LOTTO_COST, "구입금액은 ${LOTTO_COST}이상이어야 합니다.")
 
         val lottos = mutableListOf<Lotto>()
         repeat(cost / LOTTO_COST) {
