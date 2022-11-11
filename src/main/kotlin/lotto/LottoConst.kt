cda032b82ed50b308c04bc88fed613641b63b5c7
yulim <yulim13sky@naver.com>
2022-11-11 21:34:43 +0900

    feat: LottoConst 추가

---
+++ src/main/kotlin/lotto/LottoConst.kt
@@ -0,0 +1,6 @@
+package lotto
+
+const val ENTER_AMOUNT = "구입금액을 입력해주세요."
+const val ENTER_NUMBER = "숫자를 입력해주세요."
+const val MUST_CAN_DIVIDED = "1000원으로 나누어 떨어져야 합니다."
+const val MAX_AMOUNT = "구매 가능 최대 금액은 2147483000원입니다."
