c35f09260d4ed8b25d3a75642a0a2e88778b20f7
yulim <yulim13sky@naver.com>
2022-11-11 19:15:05 +0900

    feat: 구입 금액 입력 시 Int 범위 안에 있는지 검사하는 함수
    구입 금액 입력 시 Int 범위를 넘어가면 예외 던지기

--- src/main/kotlin/lotto/Buyer.kt
+++ src/main/kotlin/lotto/Buyer.kt
@@ -1,8 +1,18 @@
 package lotto
 
+import java.lang.IllegalArgumentException
+
 class Buyer {
-    fun enterAmount(): Int{
-        val amount = readln().toInt()
-        return amount
+    fun enterAmount(): Int {
+        val input = readln()
+        checkIsInIntRange(input)
+        return input.toInt()
+    }
+
+    private fun checkIsInIntRange(input: String) {
+        if (input.toLong() > Int.MAX_VALUE) {
+            println("[ERROR] 구매 가능 최대 금액은 2147483000원입니다.")
+            throw IllegalArgumentException("구매 가능 최대 금액은 2147483000원입니다.")
+        }
     }
 }
