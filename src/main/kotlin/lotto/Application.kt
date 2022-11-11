c22d05805d1d74b241704f3a5947360cbf5d0ed5
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2022-11-11 16:55:50 +0900

    feat[lotto]: 입력된 금액이 올바른 값을 가지는지 예외처리하는 기능 구현

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,9 +1,28 @@
 package lotto
 
+import java.lang.IllegalArgumentException
+import java.lang.NumberFormatException
+
 fun main() {
 
     val lottoGenerate = LottoGenerate()
     val lotto = lottoGenerate.createLottoNumbers()
 
-    println(lotto)
+    val money = inputMoney()
+}
+
+fun inputMoney() : Int {
+    println("구입금액을 입력해 주세요.")
+
+    var money = 0
+
+    try {
+        money = readLine()!!.toInt()
+    } catch (e : NumberFormatException) {
+        println("[ERROR] 입력 금액이 올바르지 않습니다.")
+    }
+
+    require(money % 1000 == 0) { "[ERROR] 입력 금액이 1000원으로 나누어 떨어져야합니다." }
+
+    return money
 }
