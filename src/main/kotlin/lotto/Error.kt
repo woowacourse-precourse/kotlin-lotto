ed93cabf8546a2ef0ba08c5303f07dc636a8a575
Hyemin Song <hyemdooly@gmail.com>
2022-11-11 21:43:31 +0900

    feat (Error): Add Error Class

---
+++ src/main/kotlin/lotto/Error.kt
@@ -0,0 +1,17 @@
+package lotto
+
+class Error {
+    companion object {
+        fun rangeError() {
+            println("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.")
+        }
+
+        fun sizeError() {
+            println("[ERROR] 숫자 개수는 6개여야 합니다.")
+        }
+
+        fun typeError() {
+            println("[ERROR] 숫자를 입력해야 합니다.")
+        }
+    }
+}
