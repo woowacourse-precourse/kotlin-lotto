493e8ded8a6d9b34675ba74c58ba40f0a0081425
zqwz <zqwz@naver.com>
2022-11-11 20:04:41 +0900

    feat(ErrorMessage): 상수 관리 기능 추가

---
+++ src/main/kotlin/lotto/Constant.kt
@@ -0,0 +1,9 @@
+package lotto
+
+class Constant {
+    companion object{
+        const val BONUS_NUMBER = "보너스 번호는 "
+        const val LOTTO_NUMBER = "로또 번호는 "
+        const val WINNING_NUMBER = "당첨 번호는 "
+    }
+}
