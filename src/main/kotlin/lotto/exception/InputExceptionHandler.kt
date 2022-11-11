731d401ff8c76138fad6c68bbec2f68439755cfa
whk06061 <63198157+whk06061@users.noreply.github.com>
2022-11-11 17:37:42 +0900

    refactor(lotto): 사용자 입력 예외처리하는 클래스 이름 변경
    HandleInput.kt -> InputExceptionHandle.kt로 이름 변경

--- src/main/kotlin/lotto/exception/InputExceptionHandler.kt
+++ src/main/kotlin/lotto/exception/InputExceptionHandler.kt
@@ -1,8 +1,8 @@
 package lotto.exception
 
-class HandleInput {
-    fun moneyException(money:Int){
-        if(money % 1000 != 0){
+class InputExceptionHandler {
+    fun moneyException(money: Int) {
+        if (money % 1000 != 0) {
             throw IllegalArgumentException("[ERROR] 금액이 1,000원으로 나누어 떨어지지 않습니다.")
         }
     }
