e388bc947ce4130c99ca7fa7823f988d3432cca8
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2022-11-11 22:41:17 +0900

    feat[lotto]: 입력된 보너스 번호 예외 사항 처리 기능 구현

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -11,6 +11,7 @@ fun main() {
 
     val money = inputMoney()
     val winningLotto = inputWinningLotto()
+    val bonus = inputBonusNum()
 }
 
 fun inputMoney() : Int {
@@ -42,4 +43,18 @@ fun inputWinningLotto() : Lotto? {
         println("[ERROR] 당첨 번호에 올바른 값이 입력되지 않았습니다.")
     }
     return null
+}
+
+fun inputBonusNum() : Int {
+    println("보너스 번호를 입력해 주세요.")
+
+    try {
+        val bonus = readLine()?.toInt()
+        require(bonus!! >= 1 && bonus <= 45) {"[ERROR] 보너스 번호는 1에서 45사이의 값이여야 합니다."}
+        return bonus
+    } catch ( e : NullPointerException) {
+        throw IllegalArgumentException("[ERROR] 보너스 번호를 입력하지 않았습니다.")
+    } catch ( e : NumberFormatException) {
+        throw IllegalArgumentException("[ERROR] 보너스 번호가 올바르게 입력되지 않았습니다.")
+    }
 }
