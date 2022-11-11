602e862a1e290b332edde25340491a8ff063075f
Hyemin Song <hyemdooly@gmail.com>
2022-11-12 00:02:11 +0900

    feat (Lotto): Implement Lotto class init, printLotto, matchLotto method

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -2,7 +2,37 @@ package lotto
 
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6)
+        require(numbers.size == 6) {
+            Error.sizeError()
+        }
+        require(numbers.distinct().size == 6) {
+            Error.overlapError()
+        }
     }
-    // TODO: 추가 기능 구현
+    fun printLotto() {
+        println(numbers)
+    }
+
+    fun matchLotto(winningNumbers: List<Int>, bonusNumber: Int): WinningType {
+        var hit = 0
+        var isBonusNumberHit = false
+        for(i in 0..5) {
+            if (winningNumbers[i] == numbers[i]) hit += 1
+        }
+        if(hit == 5) {
+            isBonusNumberHit = numbers.contains(bonusNumber)
+        }
+
+        return when(hit) {
+            6 -> WinningType.FIRST
+            5 -> if(isBonusNumberHit) {
+                WinningType.SECOND
+            } else {
+                WinningType.THIRD
+            }
+            4 -> WinningType.FOURTH
+            3 -> WinningType.FIFTH
+            else -> WinningType.NOTHING
+        }
+    }
 }
