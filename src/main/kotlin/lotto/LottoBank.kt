34d5f6b33ba6a1d7270c254c99ee065fc91c7ddc
JunJangE <wnswkd486@gmail.com>
2022-11-11 17:25:13 +0900

    feat: 당첨 내역 및 수익률을 출력하고 로또 게임을 종료한다.

--- src/main/kotlin/lotto/LottoBank.kt
+++ src/main/kotlin/lotto/LottoBank.kt
@@ -1,7 +1,19 @@
 package lotto
 
-class LottoBank {
-    fun yieldLotto() {
+import kotlin.math.roundToInt
 
+class LottoBank {
+
+    fun yieldLotto(rank : List<Int>, charger: Int) {
+        val rate= ((rank[0] * 5000) + (rank[1] * 50000) + (rank[2] * 1500000) + (rank[3] * 30000000) + (rank[4] * 2000000000)).toDouble() / charger
+
+        println("당첨 통계")
+        println("---")
+        println("${LottoCheck.LottoReward.FIFTH.cnt}개 일치 (${LottoCheck.LottoReward.FIFTH.prize}원) - ${rank[0]}개")
+        println("${LottoCheck.LottoReward.FOURTH.cnt}개 일치 (${LottoCheck.LottoReward.FOURTH.prize}원) - ${rank[1]}개")
+        println("${LottoCheck.LottoReward.THIRD.cnt}개 일치 (${LottoCheck.LottoReward.THIRD.prize}원) - ${rank[2]}개")
+        println("${LottoCheck.LottoReward.SECOND.cnt}개 일치, 보너스 볼 일치 (${LottoCheck.LottoReward.SECOND.prize}원) - ${rank[3]}개")
+        println("${LottoCheck.LottoReward.FIRST.cnt}개 일치 (${LottoCheck.LottoReward.FIRST.prize}원) - ${rank[4]}개")
+        println("총 수익률은 ${(rate * 1000).roundToInt().toDouble() / 10}%입니다.")
     }
 }
