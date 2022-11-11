25a33eb3abd708f3efb3dfacab0f1fbaf8a08fb2
Ojongseok <whdtjr6889@naver.com>
2022-11-11 10:35:12 +0900

    로또 구입 금액 입력(예외처리 수정)

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -5,10 +5,14 @@ fun main() {
 }
 
 fun buyLottoPrice(): Int {
-    println("구입금액을 입력해 주세요.")
+    println("로또 구입금액을 입력해 주세요.")
     val price = readLine().toString()
 
     try {
+        if (price.toInt() % 1000 != 0) {
+            println("[ERROR] 구입 금액은 1,000원 단위입니다.")
+            throw IllegalArgumentException()
+        }
         return price.toInt()
     } catch (e : NumberFormatException) {
         println("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.")
