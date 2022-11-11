2ea3ff262ccf40110d6b9d71def8613897a5f01b
boogieWoogie <kimjinuk1999@gmail.com>
2022-11-12 03:42:05 +0900

    feat(WinningNumberGenerator) : 당첨 번호를 입력받는 기능 추가

--- src/main/kotlin/lotto/WinningNumberGenerator.kt
+++ src/main/kotlin/lotto/WinningNumberGenerator.kt
@@ -1,4 +1,18 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Console
+import lotto.InputValidator.validateWinningNumber
+
 object WinningNumberGenerator {
+    lateinit var generatedWinningNumber: Array<Int>
+        private set
+
+    fun generateWinningNumber() {
+        val winningNumber = Console.readLine().split(",")
+
+        if (!validateWinningNumber(winningNumber)) {
+            throw java.lang.IllegalArgumentException("[ERROR]")
+        }
+        generatedWinningNumber = winningNumber.map { it.toInt() }.toTypedArray()
+    }
 }
