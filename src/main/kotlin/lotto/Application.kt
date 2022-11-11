7029f014ec6269a435f061bda8bfb458fa3e6336
Ojongseok <whdtjr6889@naver.com>
2022-11-11 16:37:57 +0900

    예외처리 printException() 함수 호출로 변경

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -3,14 +3,13 @@ package lotto
 import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
+    var lotto: Lotto
     val price = inputBuyLottoPrice()
-
     for (i in 0 until price / 1000) {
         createLottoNumber()
     }
     val winningNumbers = inputLottoWinningNumebers()
     val bonusNumber = inputBonusNumber()
-
 }
 
 fun inputBonusNumber(): Int {
@@ -18,14 +17,13 @@ fun inputBonusNumber(): Int {
     try {
         val bonusNumber = readLine().toString().toInt()
         if (bonusNumber < 1 || bonusNumber > 45) {
-            println("[ERROR] 보너스 번호는 1부터 45사이의 숫자여야 합니다.")
-            throw IllegalArgumentException()
+            printException("[ERROR] 로또 번호는 1부터 45사이의 숫자여야 합니다.")
         }
         return bonusNumber
     } catch (e: NumberFormatException) {
-        println("[ERROR] 숫자를 입력해주세요.")
-        throw IllegalArgumentException()
+        printException("[ERROR] 숫자를 입력해주세요.")
     }
+    throw IllegalArgumentException()
 }
 
 fun inputLottoWinningNumebers(): List<String> {
@@ -33,13 +31,11 @@ fun inputLottoWinningNumebers(): List<String> {
     val winninngNumbers = readLine().toString().split(",")
 
     if (winninngNumbers.distinct().size != 6) {
-        println("[ERROR] 6개의 중복되지 않은 숫자를 입력해주세요.")
-        throw IllegalArgumentException()
+        printException("[ERROR] 6개의 중복되지 않은 숫자를 입력해주세요.")
     }
     for (number in winninngNumbers) {
         if (number.toInt() < 1 || number.toInt() > 45) {
-            println("[ERROR] 로또 번호는 1부터 45사이의 숫자여야 합니다.")
-            throw IllegalArgumentException()
+            printException("[ERROR] 로또 번호는 1부터 45사이의 숫자여야 합니다.")
         }
     }
     return winninngNumbers
@@ -55,15 +51,17 @@ fun createLottoNumber() {
 fun inputBuyLottoPrice(): Int {
     println("로또 구입금액을 입력해 주세요.")
     val price = readLine().toString()
-
     try {
         if (price.toInt() % 1000 != 0) {
-            println("[ERROR] 구입 금액은 1,000원 단위입니다.")
-            throw IllegalArgumentException()
+            printException("[ERROR] 구입 금액은 1,000원 단위입니다.")
         }
         return price.toInt()
     } catch (e: NumberFormatException) {
-        println("[ERROR] 로또 번호는 1부터 45사이의 숫자여야 합니다.")
-        throw IllegalArgumentException()
+        printException("[ERROR] 로또 번호는 1부터 45사이의 숫자여야 합니다.")
     }
+    throw IllegalArgumentException()
+}
+fun printException(message: String) {
+    println(message)
+    throw IllegalArgumentException()
 }
