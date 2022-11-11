e532c9da6077a40baebf3979667c88084a384dd1
tmdgh1592 <tmdgh1592@naver.com>
2022-11-11 10:54:17 +0900

    feat(console): add lottery purchase message output function

---
+++ src/main/kotlin/lotto/Constants.kt
@@ -0,0 +1,3 @@
+package lotto
+
+const val LOTTO_PURCHASE_MESSAGE = "구입금액을 입력해 주세요."
---
+++ src/main/kotlin/lotto/presentation/LottoConsole.kt
@@ -0,0 +1,9 @@
+package lotto.presentation
+
+import lotto.LOTTO_PURCHASE_MESSAGE
+
+object LottoConsole {
+    fun printPurchaseMessage() {
+        println(LOTTO_PURCHASE_MESSAGE)
+    }
+}
