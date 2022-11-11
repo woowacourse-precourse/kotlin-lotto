0363cbacf933f4ec4474b6a52d7736b571cc025b
ChunBaee <mljung10@naver.com>
2022-11-11 15:21:36 +0900

    feat: 유저가 문자열 혹은 기호를 포함한 값을 입력했을 때 에러 반환하고 종료하기

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,9 +1,11 @@
 package lotto
 
 import camp.nextstep.edu.missionutils.Console
+import java.lang.IllegalArgumentException
 import java.util.regex.Pattern
 
 const val userInputMsg = "구입금액을 입력해 주세요."
+const val errorHeader = "[ERROR]"
 fun main() {
     getUserInputPrice()
 }
@@ -15,10 +17,15 @@ private fun getUserInputPrice() {
 
 private fun checkRegexAndTranslateToInt(userInput : String) : Int {
     if(!checkItOnlyContainsNumber(userInput)) {
-        //예외처리 할 것
+        showErrorAndFinish(ErrorType.NotOnlyNumber.errorType)
     }
 
     return userInput.toInt()
+}
+
+private fun showErrorAndFinish(errorType : Int) {
+    println("$errorHeader ${EnumError.ErrorType.checkErrorType(errorType)}")
+    throw IllegalArgumentException()
 }
 
 private fun checkItOnlyContainsNumber(userInput: String) : Boolean {
