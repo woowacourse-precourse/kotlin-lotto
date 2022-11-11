f504d135dd8d0bacb898273f1a454ae2ec5907f7
meltnia <themtale@gmail.com>
2022-11-11 23:22:54 +0900

    <feat> 돈에 관해 예외처리 한꺼번에 처리하는 함수 구현.

--- src/main/kotlin/lotto/LottoErrorCheck.kt
+++ src/main/kotlin/lotto/LottoErrorCheck.kt
@@ -2,6 +2,11 @@ package lotto
 
 object LottoErrorCheck {
 
+    fun checkallmoneyerror(money:String){
+        checkinputisnumber(money)
+        checkleastvalue(money.toInt())
+        checkitdoesnothavechange(money.toInt())
+    }
     fun checkinputisnumber(input:String){
         if(input.toIntOrNull()==null)
             throw IllegalArgumentException("[ERROR] 숫자를 입력해야합니다.")
@@ -10,7 +15,7 @@ object LottoErrorCheck {
         if(money<1000)
             throw IllegalArgumentException("[ERROR] 돈은 적어도 1000원 이상이어야합니다.")
     }
-    fun checkitisdivideby1000(money:Int){
+    fun checkitdoesnothavechange(money:Int){
         if(money%1000!=0)
             throw IllegalArgumentException("[ERROR] 잔돈은 없어서 1000원 단위로 해야합니다.")
     }
@@ -27,9 +32,9 @@ object LottoErrorCheck {
 
     private fun checklottonumber(checklotto:List<Int>){
         for(count:Int in 0 until checklotto.size)
-            checknumberisin1to45(checklotto[count])
+            checknumberisinlottorange(checklotto[count])
     }
-    fun checknumberisin1to45(number:Int){
+    fun checknumberisinlottorange(number:Int){
         if(number<1||number>45)
             throw IllegalArgumentException("[ERROR] 로또 번호는 1~45사이의 숫자여야 합니다.")
     }
