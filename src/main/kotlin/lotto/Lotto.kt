33c3ec538eb8dd3cbb073b27c2cebe5551e87a30
meltnia <themtale@gmail.com>
2022-11-12 01:01:54 +0900

    <feat> Lotto클래스 생성자에 중복 에러체크 구현.

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -3,6 +3,7 @@ package lotto
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "[ERROR] 로또 번호는 6자리의 숫자여야 합니다." }
+        require(numbers==numbers.distinct()){"[ERROR] 로또 번호는 중복이 없어야 합니다." }
     }
 
     // TODO: 추가 기능 구현
