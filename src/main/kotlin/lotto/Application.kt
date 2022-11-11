b52fffc90d104c3e4777b8672a1946ecc8c193aa
meltnia <themtale@gmail.com>
2022-11-11 22:33:00 +0900

    <feat>LottoGame에 돈받는 함수 생성, 숫자 만드는 함수 일부 생성

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,8 +1,9 @@
 package lotto
 
-import camp.nextstep.edu.missionutils.Randoms
-import camp.nextstep.edu.missionutils.Console
 
 fun main() {
-    TODO("프로그램 구현")
+    val game = LottoGame()
+
+    game.gamestart()
+
 }
--- src/main/kotlin/lotto/LottoGame.kt
+++ src/main/kotlin/lotto/LottoGame.kt
@@ -1,4 +1,37 @@
 package lotto
 
-class LottoGame {
+import camp.nextstep.edu.missionutils.Randoms
+import camp.nextstep.edu.missionutils.Console
+
+class LottoGame() {
+
+    var money=0
+    var lottos = mutableListOf<Int>()
+
+    fun gamestart(){
+        getusermoney()
+        makelottonumber()
+
+
+    }
+
+
+    private fun getusermoney(){
+        money=Console.readLine().toInt()
+    }
+
+    private fun makelottonumber():Int{
+        //넣기전 예외처리 먼저
+        val count = money/1000
+        printcount(count)
+
+
+        return count
+    }
+
+    private fun printcount(count:Int){
+        println("$count"+"개를 구입했습니다.")
+    }
+
+
 }
