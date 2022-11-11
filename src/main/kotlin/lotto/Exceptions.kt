0ab38496b4bd38f890ef8ab999a5a67b7dc41171
Yerin <myheaven_b@naver.com>
2022-11-11 21:08:47 +0900

    style: change valuable names

--- src/main/kotlin/lotto/Exceptions.kt
+++ src/main/kotlin/lotto/Exceptions.kt
@@ -1,29 +1,40 @@
 package lotto
 
 const val ERROR = "[ERROR]"
-const val SIZE_EXCEPTION = "로또 번호의 갯수는 6개 입니다."
-const val DUPLICATE_EXCEPTION = "로또 번호는 중복이 있으면 안됩니다."
-const val RANGE_EXCEPTION = "로또 번호는 1부터 45 사이의 숫자여야 합니다."
+const val COST_RANGE_EXCEPION = "구매 불가능한 금액입니다. 1000원 이상부터 구매 가능합니다."
+const val COST_UNIT_EXCEPION = "구매 불가능한 금액입니다. 1000원 단위로 구매 가능합니다."
+const val LOTTO_SIZE_EXCEPTION = "로또 번호의 갯수는 6개 입니다."
+const val LOTTO_DUPLICATE_EXCEPTION = "로또 번호는 중복이 있으면 안됩니다."
+const val LOTTO_RANGE_EXCEPTION = "로또 번호는 1부터 45 사이의 숫자여야 합니다."
 
 class Exceptions {
+    fun validateCost(cost: Int) {
+        if (cost < 1000) {
+            throw IllegalArgumentException(ERROR + COST_RANGE_EXCEPION)
+        }
+        if (cost % 100 != 0) {
+            throw IllegalArgumentException(ERROR + COST_UNIT_EXCEPION)
+        }
+    }
+
     fun validateLottoNumbers(numbers: List<Int>) {
         if (numbers.size != 6) {
-            throw IllegalArgumentException(ERROR + SIZE_EXCEPTION)
+            throw IllegalArgumentException(ERROR + LOTTO_SIZE_EXCEPTION)
         }
         if (numbers.distinct().size != 6) {
-            throw IllegalArgumentException(ERROR + DUPLICATE_EXCEPTION)
+            throw IllegalArgumentException(ERROR + LOTTO_DUPLICATE_EXCEPTION)
         }
         if (numbers.any { it !in 1..45 }) {
-            throw IllegalArgumentException(ERROR + RANGE_EXCEPTION)
+            throw IllegalArgumentException(ERROR + LOTTO_RANGE_EXCEPTION)
         }
     }
 
     fun validateBonusNumber(numbers: List<Int>, bonus: Int) {
         if (bonus in numbers) {
-            throw IllegalArgumentException(ERROR + DUPLICATE_EXCEPTION)
+            throw IllegalArgumentException(ERROR + LOTTO_DUPLICATE_EXCEPTION)
         }
         if (bonus !in 1..45) {
-            throw IllegalArgumentException(ERROR + RANGE_EXCEPTION)
+            throw IllegalArgumentException(ERROR + LOTTO_RANGE_EXCEPTION)
         }
     }
 }
