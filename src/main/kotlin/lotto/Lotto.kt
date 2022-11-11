c9e0ce8f94a1e5824e22c4dca1e071aed7c82c25
ponopono0322 <ponopono0322@gmail.com>
2022-11-11 22:29:40 +0900

    test: 로또 번호 검증 테스트 케이스 추가
    require를 통한 예외처리

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -2,7 +2,10 @@ package lotto
 
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6)
+        require(numbers.size == 6) { "[ERROR] 로또 번호는 6개가 필요합니다." }
+        require(numbers.toSet().size == 6) { "[ERROR] 로또 번호는 중복될 수 없습니다." }
+        require(numbers.first() >= 1) { "[ERROR] 1보다 작은 번호가 있어서는 안됩니다." }
+        require(numbers.last() <= 46) { "[ERROR] 46보다 큰 번호가 있어서는 안됩니다." }
     }
 
     // TODO: 추가 기능 구현
--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -20,5 +20,26 @@ class LottoTest {
         }
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @Test
+    fun `로또 번호에 허용된 범위 이하의 숫자가 있으면 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(-1, 0, 1, 2, 3, 4))
+        }
+    }
+
+    @Test
+    fun `로또 번호에 허용된 범위 이상의 숫자가 있으면 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(1, 2, 3, 4, 49, 50))
+        }
+    }
+
+    @Test
+    fun `로또 번호의 개수가 6개보다 적으면 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(1, 2, 3))
+        }
+    }
+
+
 }
