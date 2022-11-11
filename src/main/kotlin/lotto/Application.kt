8f18c1ca497c70ae3ca1cfd4f068d7f3d6309b21
junsu1023 <12jangdokg55@gmail.com>
2022-11-12 00:38:39 +0900

    feat: 각 장마다의 로또 번호 6개를 랜덤으로 가져오는 함수 추가

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,12 +1,24 @@
 package lotto
 
 import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
     val buyAmount = userInput().toInt()
     val lottoCnt = lottoCnt(buyAmount)
+    val myLotto = getLotto(lottoCnt)
 }
 
 fun userInput(): String = Console.readLine()
 
 fun lottoCnt(buyAmount: Int): Int = buyAmount / 1000
+
+fun getLotto(lottoCnt: Int): List<List<Int>> {
+    val myLotto = mutableListOf(listOf<Int>())
+
+    repeat(lottoCnt) {
+        myLotto.add(Randoms.pickUniqueNumbersInRange(1, 45, 6))
+    }
+
+    return myLotto
+}
