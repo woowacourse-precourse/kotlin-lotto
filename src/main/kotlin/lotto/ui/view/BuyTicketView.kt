64cb1419503d240e3f3b81a6f2fd76b040b7b96a
sujin-kk <sjoo0002@gmail.com>
2022-11-12 02:16:41 +0900

    chore(view): 뷰에서 쓰이는 상수를 정의한다.

---
+++ src/main/kotlin/lotto/ui/view/BuyTicketView.kt
@@ -0,0 +1,9 @@
+package lotto.ui.view
+
+class BuyTicketView {
+
+    companion object {
+        private const val INPUT_TICKET_MONEY_MESSAGE = "구입금액을 입력해 주세요."
+        private const val OUTPUT_BUY_TICKET_MESSAGE = "%d개를 구매했습니다."
+    }
+}
---
+++ src/main/kotlin/lotto/ui/view/ShowStatisticsView.kt
@@ -0,0 +1,11 @@
+package lotto.ui.view
+
+class ShowStatisticsView {
+
+    companion object {
+        private const val OUTPUT_STATISTICS_TITLE_MESSAGE = "당첨 통계"
+        private const val OUTPUT_HISTORY_NORMAL_MESSAGE = "%d개 일치 (%d원) - %d개"
+        private const val OUTPUT_HISTORY_BONUS_MESSAGE = "%d개 일치, 보너스 볼 일치 (%d원) - %d개"
+        private const val OUTPUT_YIELD_MESSAGE = "총 수익률은 %.1f입니다."
+    }
+}
