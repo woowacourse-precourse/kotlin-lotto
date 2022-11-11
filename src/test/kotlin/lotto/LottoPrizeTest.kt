3d30f1ffd40b61b2662ed399bd3a0d22aee8112d
dltkd1395 <dltkd13956@naver.com>
2022-11-11 21:38:38 +0900

    test(LottoPrizeTest) : 로또 번호와 보너스 번호를 비교하여 당첨금을 확인하는 테스트 추가

--- src/test/kotlin/lotto/LottoPrizeTest.kt
+++ src/test/kotlin/lotto/LottoPrizeTest.kt
@@ -19,4 +19,21 @@ class LottoPrizeTest {
         assertThat(result).isEqualTo(winningAmount)
     }
 
+    @ParameterizedTest
+    @CsvSource(
+        "3,true,5000",
+        "4,true,50000",
+        "5,false,1500000",
+        "5,true,30000000",
+        "6,true,2000000000",
+        "0,true,0"
+    )
+    fun `로또 번호와 보너스 번호를 비교하여 당첨금을 확인하다`(
+        matchedCount: Int,
+        isMaching: Boolean,
+        winningAmount: Int
+    ) {
+        val result = LottoPrize.lottoValueOf(matchedCount, isMaching).winPrize()
+        assertThat(result).isEqualTo(winningAmount)
+    }
 }
