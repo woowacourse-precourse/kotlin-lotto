d364f28430d195f5669e11292d054b2eb3cc465b
Min-Jae-Bae <qoalswo123@icloud.com>
2022-11-11 21:59:55 +0900

    [Refactor] (Purchase.kt, PurchaseTest.kt) 상수 제작 및 변수명 변경
    전역 변수 updateMoney, purchase 생성
    돈 상수화 제작
    적절한 Boolean 변수명 제작 (has, have)

--- src/main/kotlin/lotto/Purchase.kt
+++ src/main/kotlin/lotto/Purchase.kt
@@ -1,14 +1,23 @@
 package lotto
 
 class Purchase {
+    private var updateMoney = NO_MONEY
+
     fun checkMoney(money: Int) {
-        if (money % 1000 != 0) {
+        updateMoney = money
+        val haveWrongMoney = (updateMoney % LOTTERY_PRICE != 0)
+        if (haveWrongMoney) {
             throw IllegalArgumentException("[ERROR] 잘못된 값을 입력했습니다.")
         }
     }
 
-    fun updateLottoCount(money: Int): Int {
-        return money / 1000
+    fun updateLottoQuantity(money: Int): Int {
+        updateMoney = money
+        return updateMoney / LOTTERY_PRICE
     }
 
 }
+
+
+const val NO_MONEY = 0
+const val LOTTERY_PRICE = 1000
--- src/test/kotlin/lotto/PurchaseTest.kt
+++ src/test/kotlin/lotto/PurchaseTest.kt
@@ -6,20 +6,24 @@ import org.junit.jupiter.api.assertThrows
 
 
 class PurchaseTest {
+    private val purchase = Purchase()
 
     @Test
-    fun `로또 구매 금액이 나머지가 존재하면 예외 반환`(){
+    fun `로또 구매 금액이 나머지가 존재하면 예외 반환`() {
         assertThrows<IllegalArgumentException> {
-            val purchase = Purchase()
-            val result = purchase.checkMoney(14501)
-            assertThat(true).isEqualTo(result)
+            val remainder = purchase.checkMoney(TWO_THOUS_FIVE_HUND)
+            val hasRemainder = true
+            assertThat(hasRemainder).isEqualTo(remainder)
         }
     }
 
     @Test
     fun `로또 구매 금액에 맞는 로또 갯수 존재 여부 테스트`() {
-        val purchase = Purchase()
-        val result = purchase.updateLottoCount(14000)
-        assertThat(14).isEqualTo(result)
+        val lottoQuantity = purchase.updateLottoQuantity(TEN_THOUS)
+        assertThat(LOTTO_TEN_QUANTITY).isEqualTo(lottoQuantity)
     }
 }
+
+const val TWO_THOUS_FIVE_HUND = 2500
+const val LOTTO_TEN_QUANTITY = 10
+const val TEN_THOUS = 10_000
