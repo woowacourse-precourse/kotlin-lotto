890d0bf23555b779f2e6363ee8be86afa9c6d246
re4rk <37167652+re4rk@users.noreply.github.com>
2022-11-12 04:54:03 +0900

    test(lotto): 입력 받은 숫자 만큼 로또 번호들을 생성하는 기능 테스트 케이스 추가

---
+++ src/test/kotlin/lotto/domain/StoreTest.kt
@@ -0,0 +1,35 @@
+package lotto.domain
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+class StoreTest {
+    @Test
+    fun `기능 테스트`() {
+        camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest(
+            {
+                val element = Store().buyLotto(8)
+                Assertions.assertThat(element).isEqualTo(
+                    listOf(
+                        listOf(8, 21, 23, 41, 42, 43),
+                        listOf(3, 5, 11, 16, 32, 38),
+                        listOf(7, 11, 16, 35, 36, 44),
+                        listOf(1, 8, 11, 31, 41, 42),
+                        listOf(13, 14, 16, 38, 42, 45),
+                        listOf(7, 11, 30, 40, 42, 43),
+                        listOf(2, 13, 22, 32, 38, 45),
+                        listOf(1, 3, 5, 14, 22, 45)
+                    )
+                )
+            },
+            listOf(8, 21, 23, 41, 42, 43),
+            listOf(3, 5, 11, 16, 32, 38),
+            listOf(7, 11, 16, 35, 36, 44),
+            listOf(1, 8, 11, 31, 41, 42),
+            listOf(13, 14, 16, 38, 42, 45),
+            listOf(7, 11, 30, 40, 42, 43),
+            listOf(2, 13, 22, 32, 38, 45),
+            listOf(1, 3, 5, 14, 22, 45)
+        )
+    }
+}
