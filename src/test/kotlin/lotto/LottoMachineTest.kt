b7705eebc14d346b4ca9426563f642cc18716a6b
dltkd1395 <dltkd13956@naver.com>
2022-11-12 01:43:35 +0900

    test(LottoMachineTest) : 생성된 로또 개수 확인 테스트 추가

--- src/test/kotlin/lotto/LottoMachineTest.kt
+++ src/test/kotlin/lotto/LottoMachineTest.kt
@@ -1,6 +1,7 @@
 package lotto
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 
@@ -10,5 +11,12 @@ class LottoMachineTest {
     fun `로또를 금액만큼 발행한다`(money: Int, lottoCount: Int) {
         val lottoTicketCount = LottoMachine(money).sellLottos()
         assertThat(lottoTicketCount).isEqualTo(lottoCount)
+    }
+
+    @ParameterizedTest
+    @CsvSource("1000,1", "2000,2", "14000,14", "0,0")
+    fun `생성된 로또 개수 확인 테스트`(money: Int, count: Int) {
+        val result = LottoMachine(money).createLottos().size
+        assertThat(result).isEqualTo(count)
     }
 }
