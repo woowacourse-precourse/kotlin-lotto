0c073483adab917bab4a8b1eb8baf629f7ad8e29
rhkrwngud445 <rhkrwngud445@gmail.com>
2022-11-11 17:46:57 +0900

    feat : print error message that number is duplicated

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -3,12 +3,12 @@ package lotto
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6)
-        require(checkDuplication()) {println("[ERROR]")}
+        require(checkDuplication()) { println("[ERROR] 중복된 숫자가 있습니다.") }
     }
 
-    private fun checkDuplication() : Boolean{
+    private fun checkDuplication(): Boolean {
         return numbers.filter {
             numbers.contains(it)
-        }.count()<=1
+        }.count() <= 1
     }
 }
