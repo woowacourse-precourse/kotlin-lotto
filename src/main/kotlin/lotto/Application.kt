a819c942301393b8ec20a02ff667c47bccbb664c
YuGyeong98 <kuk3097@naver.com>
2022-11-11 23:36:25 +0900

    feat: 로또 구입 갯수 및 구입 갯수만큼 1~45 사이 6개의 중복되지 않은 랜덤 숫자 오름차순 출력

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,6 +1,7 @@
 package lotto
 
 import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
     val inputVerifier = InputVerifier()
@@ -8,4 +9,11 @@ fun main() {
     println("구입금액을 입력해 주세요.")
     val amountOfPurchase = Console.readLine().toInt()
     inputVerifier.validateAmountOfPurchase(amountOfPurchase)
+
+    val numberOfPurchase = amountOfPurchase.div(1000)
+    println("${numberOfPurchase}개를 구매했습니다.")
+    for (i in 0 until numberOfPurchase) {
+        val numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
+        println(numbers.sorted())
+    }
 }
