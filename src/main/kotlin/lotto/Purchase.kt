b839f9f21fb02bf904a302ccfac1a4c287025492
Min-Jae-Bae <qoalswo123@icloud.com>
2022-11-11 15:49:14 +0900

    [Feat] (Purchase.kt) 로또 구매 금액의 나머지 존재시 예외처리 기능 추가
    나머지가 존재하면 IllegalArgumentException 반환 및 에러 메시지 출력

---
+++ src/main/kotlin/lotto/Purchase.kt
@@ -0,0 +1,10 @@
+package lotto
+
+class Purchase {
+    fun checkMoney(money: Int) {
+        if (money % 1000 != 0) {
+            throw IllegalArgumentException("[ERROR] 잘못된 값을 입력했습니다.")
+        }
+    }
+
+}
