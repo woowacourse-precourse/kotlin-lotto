fe8c2f859c2b5c51209cb7c6551a345f08c4c02e
Yerin <myheaven_b@naver.com>
2022-11-11 17:06:26 +0900

    feat: add init for class Lotto

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -3,6 +3,8 @@ package lotto
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6)
+        require(numbers.distinct().size == 6)
+        require(numbers.all { it in 1..45 })
     }
 
     // TODO: 추가 기능 구현
