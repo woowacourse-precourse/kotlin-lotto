15b0fa76aa8ab14b9cbd3ae63a2a28bf0293992a
JunJangE <wnswkd486@gmail.com>
2022-11-11 19:30:11 +0900

    test: LottoBankTest 코드 추가

---
+++ src/test/kotlin/lotto/LottoBankTest.kt
@@ -0,0 +1,14 @@
+package lotto
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+class LottoBankTest {
+
+    @Test
+    fun `구매한 로또 번호가 1등 복권인 경우`() {
+        val lottoBank: LottoBank = LottoBank()
+        val lottoYield = lottoBank.yieldLotto(listOf(0, 0, 0, 0, 1), 5000)
+        Assertions.assertThat(lottoYield)
+    }
+}
