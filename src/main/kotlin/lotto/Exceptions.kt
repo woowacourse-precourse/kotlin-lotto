bdd58800e703086912a33fd8178b81f9da6b3d09
Yerin <myheaven_b@naver.com>
2022-11-12 01:03:07 +0900

    style: change valuable names

--- src/main/kotlin/lotto/Exceptions.kt
+++ src/main/kotlin/lotto/Exceptions.kt
@@ -1,19 +1,20 @@
 package lotto
 
 const val ERROR = "[ERROR]"
-const val COST_RANGE_EXCEPION = "구매 불가능한 금액입니다. 1000원 이상부터 구매 가능합니다."
-const val COST_UNIT_EXCEPION = "구매 불가능한 금액입니다. 1000원 단위로 구매 가능합니다."
+const val COST_RANGE_EXCEPTION = "구매 불가능한 금액입니다. 1000원 이상부터 구매 가능합니다."
+const val COST_UNIT_EXCEPTION = "구매 불가능한 금액입니다. 1000원 단위로 구매 가능합니다."
 const val LOTTO_SIZE_EXCEPTION = "로또 번호의 갯수는 6개 입니다."
 const val LOTTO_DUPLICATE_EXCEPTION = "로또 번호는 중복이 있으면 안됩니다."
 const val LOTTO_RANGE_EXCEPTION = "로또 번호는 1부터 45 사이의 숫자여야 합니다."
+const val BONUS_SIZE_EXCEPTION = "보너스 번호의 갯수는 1개입니다."
 
 class Exceptions {
     fun validateCost(cost: Int) {
         if (cost < 1000) {
-            throw IllegalArgumentException(ERROR + COST_RANGE_EXCEPION)
+            throw IllegalArgumentException(ERROR + COST_RANGE_EXCEPTION)
         }
         if (cost % 100 != 0) {
-            throw IllegalArgumentException(ERROR + COST_UNIT_EXCEPION)
+            throw IllegalArgumentException(ERROR + COST_UNIT_EXCEPTION)
         }
     }
 
@@ -29,7 +30,8 @@ class Exceptions {
         }
     }
 
-    fun validateBonusNumber(numbers: List<Int>, bonus: Int) {
+    fun validateBonusNumber(numbers: List<Int>, input: String) {
+        val bonus = input.toInt()
         if (bonus in numbers) {
             throw IllegalArgumentException(ERROR + LOTTO_DUPLICATE_EXCEPTION)
         }
