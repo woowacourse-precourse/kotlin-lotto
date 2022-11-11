281158ca22403744f2fbcd38ef8718c6b3cb442d
tmdgh1592 <tmdgh1592@naver.com>
2022-11-11 10:55:08 +0900

    feat(console): add lottery purchase amount input function

--- src/main/kotlin/lotto/presentation/LottoConsole.kt
+++ src/main/kotlin/lotto/presentation/LottoConsole.kt
@@ -1,9 +1,15 @@
 package lotto.presentation
 
+import camp.nextstep.edu.missionutils.Console
 import lotto.LOTTO_PURCHASE_MESSAGE
 
 object LottoConsole {
     fun printPurchaseMessage() {
         println(LOTTO_PURCHASE_MESSAGE)
+    }
+
+    fun inputPurchaseAmount(): Int {
+        val amount = Console.readLine()
+        return amount.toInt()
     }
 }
