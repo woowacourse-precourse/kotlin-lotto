d1e316b246e295171eb2f82f78fed07b06b67de3
zqwz <zqwz@naver.com>
2022-11-11 20:35:22 +0900

    test(BonusNumberTest): 보너스 번호 예외 사항 테스트 추가

---
+++ src/test/kotlin/lotto/BonusNumberTest.kt
@@ -0,0 +1,23 @@
+package lotto
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class BonusNumberTest {
+    private val bonus = BonusNumber(listOf(1, 2, 3, 4, 5, 6), "6")
+
+    @Test
+    fun `보너스 당첨 번호가 1~45 사이가 아닌 경우`() {
+        assertThrows<IllegalArgumentException> { (bonus.validateRange(46)) }
+    }
+
+    @Test
+    fun `보너스 당첨 번호가 로또 당첨 번호와 중복되면 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> { (bonus.validateDuplicate(listOf(1, 2, 3, 4, 5, 6), 6)) }
+    }
+
+    @Test
+    fun `보너스 당첨 번호에 숫자가 아닌 문자가 들어오면 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> { (bonus.validateNumeric("4a")) }
+    }
+}
