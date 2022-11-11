9a287338a24da40d9e7ff25f3bd6643b39a45753
Min-Jae-Bae <qoalswo123@icloud.com>
2022-11-11 22:13:42 +0900

    [Chore] (Purchase.kt) 전역변수 updateMoney -> expense 교체

--- src/main/kotlin/lotto/Purchase.kt
+++ src/main/kotlin/lotto/Purchase.kt
@@ -1,19 +1,19 @@
 package lotto
 
 class Purchase {
-    private var updateMoney = NO_MONEY
+    private var expense = NO_MONEY
 
     fun checkMoney(money: Int) {
-        updateMoney = money
-        val haveWrongMoney = (updateMoney % LOTTERY_PRICE != 0)
+        expense = money
+        val haveWrongMoney = (expense % LOTTERY_PRICE != 0)
         if (haveWrongMoney) {
             throw IllegalArgumentException("[ERROR] 잘못된 값을 입력했습니다.")
         }
     }
 
     fun updateLottoQuantity(money: Int): Int {
-        updateMoney = money
-        return updateMoney / LOTTERY_PRICE
+        expense = money
+        return expense / LOTTERY_PRICE
     }
 
 }
