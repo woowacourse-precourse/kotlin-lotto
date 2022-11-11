c78b017344592ac18d92992a5a35c041c780247b
boogieWoogie <kimjinuk1999@gmail.com>
2022-11-12 03:41:07 +0900

    feat(InputValidator) : 당첨 번호 입력을 위한 유효성 검사 기능 추가

--- src/main/kotlin/lotto/InputValidator.kt
+++ src/main/kotlin/lotto/InputValidator.kt
@@ -1,6 +1,42 @@
-package lotto.domain
+package lotto
 
 object InputValidator {
-    fun validateMoney(money:Int) = (money > 0) && (money % 1000 == 0)
+    fun validateMoney(money: Int) = (money > 0) && (money % 1000 == 0)
 
+    fun validateWinningNumber(winningNumber: List<String>): Boolean {
+        var isValid = true
+
+        when {
+            winningNumber.size != 6 -> isValid = false
+            !winningNumber.joinToString("").isNumeric() -> isValid = false
+            !winningNumber.isInRange() -> isValid = false
+            winningNumber.hasOverLappedNumber() -> isValid = false
+        }
+        return isValid
+    }
+
+    private fun List<String>.hasOverLappedNumber(): Boolean {
+        val existNumber = mutableSetOf<String>()
+
+        this.forEach { number ->
+            if (existNumber.contains(number)) {
+                return true
+            }
+            existNumber.add(number)
+        }
+        return false
+    }
+
+    private fun String.isNumeric(): Boolean {
+        return this.chars().allMatch { eachChar -> Character.isDigit(eachChar) }
+    }
+
+    private fun List<String>.isInRange(): Boolean {
+        this.forEach { number ->
+            if (number.toInt() > MAXIMUM_LOTTO_NUMBER || number.toInt() < MINIMUM_LOTTO_NUMBER) {
+                return false
+            }
+        }
+        return true
+    }
 }
