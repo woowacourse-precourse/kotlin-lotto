f3857607dd841a418e15fa30b5f4f3f036b29a52
JunJangE <wnswkd486@gmail.com>
2022-11-11 19:31:54 +0900

    feat: 로또 게임 기능 완성

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,16 @@
 package lotto
 
 fun main() {
-    TODO("프로그램 구현")
+    val lottoOrder: LottoOrder = LottoOrder()
+    val lottoShop: LottoShop = LottoShop()
+    val lottoWin: LottoWin = LottoWin()
+    val lottoCheck: LottoCheck = LottoCheck()
+    val lottoBank: LottoBank = LottoBank()
+
+    val userOrder = lottoOrder.orderUser()
+    if (userOrder == 0) return
+    val userLottoNumber = lottoShop.buyLotto(userOrder)
+    lottoWin.winLotto()
+    val lottoRank = lottoCheck.compareLotto(userLottoNumber)
+    lottoBank.yieldLotto(lottoRank, userOrder)
 }
