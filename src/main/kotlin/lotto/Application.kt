f9e68491ee7424305a38291c2695d02333110983
junsu1023 <12jangdokg55@gmail.com>
2022-11-12 03:13:12 +0900

    feat: 당첨 통계 출력하는 함수 추가

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -3,6 +3,14 @@ package lotto
 import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
 
+enum class CORRECTCNT(val message: String) {
+    THREE("3개 일치 (5,000원) - "),
+    FOUR("4개 일치 (50,000원) - "),
+    FIVE("5개 일치 (1,500,000원) - "),
+    BONUS("5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+    SIX("6개 일치 (2,000,000,000원) - ");
+}
+
 fun main() {
     try {
         println("구입금액을 입력해 주세요.")
@@ -24,6 +32,7 @@ fun main() {
         bonusNumberException(lottoResult, bonusNumber)
 
         val win = checkLotto(lottoResult, myLotto, bonusNumber)
+        printWin(win)
     }
     catch (e: NumberFormatException) {
         throw IllegalArgumentException("[ERROR] 숫자만 입력 가능합니다.")
@@ -70,4 +79,18 @@ fun checkLotto(lottoResult: List<Int>, myLotto: List<List<Int>>, bonusNumber: In
     }
 
     return win
+}
+
+fun printWin(win: Array<Int>) {
+    println("\n당첨 통계\n---")
+    repeat(win.size - 1) {
+        when(it) {
+            0 -> print(CORRECTCNT.THREE.message)
+            1 -> print(CORRECTCNT.FOUR.message)
+            2 -> print(CORRECTCNT.FIVE.message)
+            3 -> print(CORRECTCNT.BONUS.message)
+            4 -> print(CORRECTCNT.SIX.message)
+        }
+        println("${win[it]}개")
+    }
 }
