3dc10c3adff2c91fd725f5766f6fb4224a7b6483
zqwz <zqwz@naver.com>
2022-11-11 21:26:54 +0900

    feat(ConsoleMessage): 콘솔 메세지를 출력하는 기능 추가

---
+++ src/main/kotlin/lotto/ConsoleMessage.kt
@@ -0,0 +1,10 @@
+package lotto
+
+class ConsoleMessage {
+    companion object {
+        fun machine() = println("구입 금액을 입력해 주세요.")
+        fun purchase(count: Int) = println("\n${count}개를 구매했습니다.")
+        fun winning() = println("\n당첨 번호를 입력해 주세요.")
+        fun bonus() = println("\n보너스 번호를 입력해 주세요.")
+    }
+}
