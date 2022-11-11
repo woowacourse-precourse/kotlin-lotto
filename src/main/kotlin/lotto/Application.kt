b0185c4302638494e47332d4e74d6014d516ba6c
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2022-11-11 22:14:20 +0900

    feat[lotto]: 입력된 당첨 번호 예외 사항 처리하는 기능 구현

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,6 +1,7 @@
 package lotto
 
 import java.lang.IllegalArgumentException
+import java.lang.NullPointerException
 import java.lang.NumberFormatException
 
 fun main() {
@@ -9,6 +10,7 @@ fun main() {
     val lotto = lottoGenerate.createLottoNumbers()
 
     val money = inputMoney()
+    val winningLotto = inputWinningLotto()
 }
 
 fun inputMoney() : Int {
@@ -25,4 +27,19 @@ fun inputMoney() : Int {
     require(money % 1000 == 0) { "[ERROR] 입력 금액이 1000원으로 나누어 떨어져야합니다." }
 
     return money
+}
+
+fun inputWinningLotto() : Lotto? {
+    println("당첨 번호를 입력해 주세요.")
+
+    try {
+        val numStr = readLine()?.split(",")
+        val num = numStr!!.map { it.toInt() }
+        return Lotto(num)
+    } catch( e : NullPointerException) {
+        println("[ERROR] 당첨 번호를 입력하지 않았습니다.")
+    } catch( e : NumberFormatException) {
+        println("[ERROR] 당첨 번호에 올바른 값이 입력되지 않았습니다.")
+    }
+    return null
 }
