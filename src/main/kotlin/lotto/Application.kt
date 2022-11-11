3ca75fcec2202f56b93b5fcec3fd89c58c2a2505
junsu1023 <12jangdokg55@gmail.com>
2022-11-12 01:59:51 +0900

    refactor: NumberFormatException 예외처리 위치 변경

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -4,22 +4,27 @@ import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
-    println("구입금액을 입력해 주세요.")
-    val buyAmount = userInput()
-    buyAmountException(buyAmount)
+    try {
+        println("구입금액을 입력해 주세요.")
+        val buyAmount = userInput().toInt()
+        buyAmountException(buyAmount)
 
-    val lottoCnt = lottoCnt(buyAmount.toInt())
-    println("\n${lottoCnt}개를 구매했습니다.")
+        val lottoCnt = lottoCnt(buyAmount.toInt())
+        println("\n${lottoCnt}개를 구매했습니다.")
 
-    val myLotto = getLotto(lottoCnt)
-    printLotto(myLotto)
+        val myLotto = getLotto(lottoCnt)
+        printLotto(myLotto)
 
-    println("\n당첨 번호를 입력해 주세요.")
-    val lottoResult = userInput()
-    lottoException(lottoResult)
+        println("\n당첨 번호를 입력해 주세요.")
+        val lottoResult = userInput()
+        lottoException(lottoResult)
 
-    println("\n보너스 번호를 입력해 주세요.")
-    val bonusNumber = userInput().toInt()
+        println("\n보너스 번호를 입력해 주세요.")
+        val bonusNumber = userInput().toInt()
+    }
+    catch (e: NumberFormatException) {
+        throw IllegalArgumentException("[ERROR] 숫자만 입력 가능합니다.")
+    }
 }
 
 fun userInput(): String = Console.readLine()
@@ -42,21 +47,12 @@ fun printLotto(myLotto: List<List<Int>>) {
     }
 }
 
-fun buyAmountException(buyAmount: String) {
-    if(!buyAmount.chars().allMatch { Character.isDigit(it) }) {
-        throw IllegalArgumentException("[ERROR] 숫자만 입력가능합니다.")
-    }
-
-    if(buyAmount.toInt() % 1000 != 0) {
+fun buyAmountException(buyAmount: Int) {
+    if(buyAmount % 1000 != 0) {
         throw IllegalArgumentException("[ERROR] 입력은 1000단위로만 가능합니다.")
     }
 }
 
 fun lottoException(lottoResult: String) {
-    try {
-        Lotto(lottoResult.split(",").map { it.toInt() }).lottoException()
-    }
-    catch (e: NumberFormatException) {
-        throw IllegalArgumentException("[ERROR] 숫자만 입력 가능합니다.")
-    }
+    Lotto(lottoResult.split(",").map { it.toInt() }).lottoException()
 }
