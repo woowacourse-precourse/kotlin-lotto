2ed2dbe46aec50a47b16e682a208b9d425b862e0
Yerin <myheaven_b@naver.com>
2022-11-11 18:33:40 +0900

    style: change valuable names

--- src/main/kotlin/lotto/Exceptions.kt
+++ src/main/kotlin/lotto/Exceptions.kt
@@ -1,29 +1,29 @@
 package lotto
 
 const val ERROR = "[ERROR]"
-const val SIZE_ERROR_EXCEPTION = "로또 번호의 갯수는 6개 입니다."
-const val DUPLICATE_ERROR_EXCEPTION = "로또 번호는 중복이 있으면 안됩니다."
-const val RANGE_ERROR_EXCEPTION = "로또 번호는 1부터 45 사이의 숫자여야 합니다."
+const val SIZE_EXCEPTION = "로또 번호의 갯수는 6개 입니다."
+const val DUPLICATE_EXCEPTION = "로또 번호는 중복이 있으면 안됩니다."
+const val RANGE_EXCEPTION = "로또 번호는 1부터 45 사이의 숫자여야 합니다."
 
 class Exceptions {
     fun validateLottoNumbers(numbers: List<Int>) {
         if (numbers.size != 6) {
-            throw IllegalArgumentException(ERROR + SIZE_ERROR_EXCEPTION)
+            throw IllegalArgumentException(ERROR + SIZE_EXCEPTION)
         }
         if (numbers.distinct().size != 6) {
-            throw IllegalArgumentException(ERROR + DUPLICATE_ERROR_EXCEPTION)
+            throw IllegalArgumentException(ERROR + DUPLICATE_EXCEPTION)
         }
         if (numbers.any { it !in 1..45 }) {
-            throw IllegalArgumentException(ERROR + RANGE_ERROR_EXCEPTION)
+            throw IllegalArgumentException(ERROR + RANGE_EXCEPTION)
         }
     }
 
     fun validateBonusNumber(numbers: List<Int>, bonus: Int) {
         if (bonus in numbers) {
-            throw IllegalArgumentException(ERROR + DUPLICATE_ERROR_EXCEPTION)
+            throw IllegalArgumentException(ERROR + DUPLICATE_EXCEPTION)
         }
         if (bonus !in 1..45) {
-            throw IllegalArgumentException(ERROR + RANGE_ERROR_EXCEPTION)
+            throw IllegalArgumentException(ERROR + RANGE_EXCEPTION)
         }
     }
 }
