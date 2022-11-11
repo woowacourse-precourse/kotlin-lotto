a0c4fee6d34acc2114753077d15d99ed6173c98b
zqwz <zqwz@naver.com>
2022-11-11 19:28:53 +0900

    fix(Application): 예외 사항 테스트 통과되도록 수정

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -3,19 +3,22 @@ package lotto
 import camp.nextstep.edu.missionutils.Console
 
 fun main() {
-    println("구입금액을 입력해 주세요.")
-    val machine = LottoMachine(Console.readLine())
-    machine.pickNewLotto(machine.getLottoCount())
-    println("\n${machine.getLottoCount()}개를 구매했습니다.")
-    machine.getLottoNumbers()
-    println("\n당첨 번호를 입력해 주세요.")
-    val winning = Winning(Console.readLine())
-    println("\n보너스 번호를 입력해 주세요.")
-    winning.inputBonusNumber(Console.readLine())
-    winning.checkBonusNumberException()
-    val rank = machine.getTotalRank(winning.getLuckyNumber(),winning.getBonusNumber())
-    val statistic = Statistic(rank)
-    statistic.statisticMessage()
-    println("총 수익률은 ${statistic.calculateTotalYield(machine.getLottoCount() * 1000)}%입니다.")
-
+    try {
+        println("구입금액을 입력해 주세요.")
+        val machine = LottoMachine(Console.readLine())
+        machine.pickNewLotto(machine.getLottoCount())
+        println("\n${machine.getLottoCount()}개를 구매했습니다.")
+        machine.getLottoNumbers()
+        println("\n당첨 번호를 입력해 주세요.")
+        val winning = Winning(Console.readLine())
+        println("\n보너스 번호를 입력해 주세요.")
+        winning.inputBonusNumber(Console.readLine())
+        winning.checkBonusNumberException()
+        val rank = machine.getTotalRank(winning.getLuckyNumber(), winning.getBonusNumber())
+        val statistic = Statistic(rank)
+        statistic.statisticMessage()
+        println("총 수익률은 ${statistic.calculateTotalYield(machine.getLottoCount() * 1000)}%입니다.")
+    } catch (exception: IllegalArgumentException){
+        println("[ERROR] ")
+    }
 }
