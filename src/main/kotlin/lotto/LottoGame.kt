f121c60cd5e211b750d36609c4d99543ba0f8e89
meltnia <themtale@gmail.com>
2022-11-12 02:16:28 +0900

    <feat> LottoGame에 메세지들, 보너스번호받기, 일부 상 확인 추가.

--- src/main/kotlin/lotto/LottoGame.kt
+++ src/main/kotlin/lotto/LottoGame.kt
@@ -5,22 +5,28 @@ import camp.nextstep.edu.missionutils.Console
 
 const val LOTTO_PRICE = 1000
 
-class LottoGame() {
+class LottoGame {
 
-    var money = ""
-    val lottos = mutableListOf<List<Int>>()
-    val checkprize:MutableList<Int> = mutableListOf(0,0,0,0,0)
+    private var money = ""
+    private val lottos = mutableListOf<List<Int>>()
+    private val checkprize:MutableList<Int> = mutableListOf(0,0,0,0,0)
+    private var bonusnumber =0
 
     fun gamestart() {
         getusermoney()
         makelottonumber()
         val userlotto=getuserlotto()
-
+        getbonusnumber()
+        checklotto(userlotto)
 
     }
 
 
+    private fun messagetogivemoney(){
+        println("구입금액을 입력해 주세요.")
+    }
     private fun getusermoney() {
+        messagetogivemoney()
         money = Console.readLine()
     }
 
@@ -49,7 +55,12 @@ class LottoGame() {
         }
     }
 
+    private fun messagetogiveuserlotto(){
+        println("당첨 번호를 입력해 주세요.")
+    }
+
     private fun getuserlotto():List<Int>{
+        messagetogiveuserlotto()
         val userinput = Console.readLine()
         val userinputparsing= userinput.split(",")
         val userinputnumber= mutableListOf<Int>()
@@ -63,8 +74,35 @@ class LottoGame() {
 
     }
 
-    private fun checklotto(){
-
+    private fun messagetogiveuserbonus(){
+        println("보너스 번호를 입력해 주세요.")
+    }
+    private fun getbonusnumber(){
+        messagetogiveuserbonus()
+        val tempbonus = Console.readLine()
+        LottoErrorCheck.checkinputisnumber(tempbonus)
+        LottoErrorCheck.checknumberisinlottorange(tempbonus.toInt())
+        bonusnumber=tempbonus.toInt()
     }
 
+    private fun checklotto(userlotto:List<Int>){
+        val checklotto = Lotto(userlotto)
+        for(elements in lottos){
+            val rank = checklotto.lottocheck(userlotto,elements)
+            prizecheck(rank)
+        }
+    }
+
+    private fun prizecheck(rank:Int){
+        if(rank==3)
+            checkprize[0]+=1
+        if(rank==4)
+            checkprize[1]+=1
+//        if(rank==5)
+//            if(user)
+        if(rank==6)
+            checkprize[4]+=1
+
+
+    }
 }
