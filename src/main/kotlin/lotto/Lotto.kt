d4019d7934792807bf25b3d429ac39fae5d15a24
rhkrwngud445 <rhkrwngud445@gmail.com>
2022-11-11 17:46:17 +0900

    feat : check exception that number is duplicated

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -3,5 +3,12 @@ package lotto
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6)
+        require(checkDuplication()) {println("[ERROR]")}
+    }
+
+    private fun checkDuplication() : Boolean{
+        return numbers.filter {
+            numbers.contains(it)
+        }.count()<=1
     }
 }
