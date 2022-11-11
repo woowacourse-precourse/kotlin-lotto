fdf02fa80d2664d4cb63ab46d6920e1abefdc00f
ChunBaee <mljung10@naver.com>
2022-11-11 15:23:33 +0900

    feat: 유저가 입력한 값이 로또 구입 금액으로 나누어 떨어지는지 검사
    만약 검사 결과 나누어 떨어지지 않는다면 에러 반환 후 종료하기.

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -6,6 +6,7 @@ import java.util.regex.Pattern
 
 const val userInputMsg = "구입금액을 입력해 주세요."
 const val errorHeader = "[ERROR]"
+const val lottoEachPrice = 1000
 fun main() {
     getUserInputPrice()
 }
@@ -19,8 +20,9 @@ private fun checkRegexAndTranslateToInt(userInput : String) : Int {
     if(!checkItOnlyContainsNumber(userInput)) {
         showErrorAndFinish(ErrorType.NotOnlyNumber.errorType)
     }
+    if(!checkItCanDivideWithThousand(changeStringToInt(userInput)))
 
-    return userInput.toInt()
+    return changeStringToInt(userInput)
 }
 
 private fun showErrorAndFinish(errorType : Int) {
@@ -30,4 +32,12 @@ private fun showErrorAndFinish(errorType : Int) {
 
 private fun checkItOnlyContainsNumber(userInput: String) : Boolean {
     return Pattern.matches("^[0-9]*$", userInput)
+}
+
+private fun checkItCanDivideWithThousand(userInputPrice : Int) : Boolean {
+    return (userInputPrice % lottoEachPrice == 0)
+}
+
+private fun changeStringToInt(userInput : String) : Int {
+    return userInput.toInt()
 }
