e33917343f30d086482548b757be2c1ed5b5d758
re4rk <37167652+re4rk@users.noreply.github.com>
2022-11-12 04:13:35 +0900

    test(lotto): 1000원의 배수가 아니면 예외 처리 테스트 케이스 작성

---
+++ src/test/kotlin/lotto/domain/PurchaseTest.kt
@@ -0,0 +1,20 @@
+package lotto.domain
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+class PurchaseTest {
+
+    @Test
+    fun `기능 테스트`() {
+        val purchase = Purchase(17000)
+        Assertions.assertThat(
+            purchase.amount == 17000
+        )
+    }
+
+    @Test
+    fun `예외 테스트`() {
+        Assertions.assertThatIllegalArgumentException().isThrownBy { Purchase(null) }
+    }
+}
