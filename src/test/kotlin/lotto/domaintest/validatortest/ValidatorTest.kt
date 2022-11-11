717a2d75c4fc049236d386e06d3c04cc61dfec00
boogieWoogie <kimjinuk1999@gmail.com>
2022-11-11 23:21:01 +0900

    test(InputValidator) : 사용자가 입력한 돈의 유효성을 판단하는 기능을  테스트하기 위한 코드 추가

--- src/test/kotlin/lotto/domaintest/validatortest/ValidatorTest.kt
+++ src/test/kotlin/lotto/domaintest/validatortest/ValidatorTest.kt
@@ -1,4 +1,22 @@
 package lotto.domaintest.validatortest
 
+import lotto.domain.InputValidator
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
 class ValidatorTest {
+    @ParameterizedTest
+    @ValueSource(ints = [0, 100, 1500, 11500])
+    fun `invalidate money check`(money:Int){
+        assertFalse(InputValidator.validateMoney(money))
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [1000, 2000, 5000, 100000])
+    fun `validate money check`(money:Int){
+        assertTrue(InputValidator.validateMoney(money))
+    }
 }
