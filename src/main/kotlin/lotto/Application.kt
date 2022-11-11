3e03a652f6db29a322911e46433da671f918fdf5
ChunBaee <mljung10@naver.com>
2022-11-11 15:17:31 +0900

    feat: 유저의 입력값이 숫자만 포함되어있는지 여부 검사하기

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,6 +1,7 @@
 package lotto
 
 import camp.nextstep.edu.missionutils.Console
+import java.util.regex.Pattern
 
 const val userInputMsg = "구입금액을 입력해 주세요."
 fun main() {
@@ -9,4 +10,17 @@ fun main() {
 private fun getUserInputPrice() {
     println(userInputMsg)
     val userInput = Console.readLine()
+    checkRegexAndTranslateToInt(userInput)
+}
+
+private fun checkRegexAndTranslateToInt(userInput : String) : Int {
+    if(!checkItOnlyContainsNumber(userInput)) {
+        //예외처리 할 것
+    }
+
+    return userInput.toInt()
+}
+
+private fun checkItOnlyContainsNumber(userInput: String) : Boolean {
+    return Pattern.matches("^[0-9]*$", userInput)
 }
