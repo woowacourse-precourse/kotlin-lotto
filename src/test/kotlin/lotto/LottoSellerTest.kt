345c89504ccf2df6ccf921cf4b7c4d51349e6469
boogieWoogie <kimjinuk1999@gmail.com>
2022-11-12 00:42:09 +0900

    test(LottoSeller) : 금액에 따른 로또 발행을 테스트 추가

--- src/test/kotlin/lotto/LottoSellerTest.kt
+++ src/test/kotlin/lotto/LottoSellerTest.kt
@@ -1,4 +1,22 @@
 package lotto
 
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
 class LottoSellerTest {
+    private lateinit var LottoSeller: Seller
+
+    @BeforeEach
+    fun setUp() {
+        LottoSeller = Seller(LottoGenerator())
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [1000, 5000, 8000, 10000])
+    fun `check the number of lotto`(receivedMoney: Int) {
+        assertThat(LottoSeller.sellLotto(receivedMoney, LOTTO_PRICE).size).isEqualTo(receivedMoney / LOTTO_PRICE)
+    }
 }
