cc57c743e4159c06f092eb21fd473a0762f0ffb6
re4rk <37167652+re4rk@users.noreply.github.com>
2022-11-12 04:17:11 +0900

    feat(lotto): 구매한 티켓 개수를 저장 기능 구현

--- src/main/kotlin/lotto/domain/Purchase.kt
+++ src/main/kotlin/lotto/domain/Purchase.kt
@@ -1,6 +1,10 @@
 package lotto.domain
 
 data class Purchase(val amount: Int?) {
+    var ticket = 0
+        get() = amount?.div(1000) ?: 0
+        private set
+
     init {
         requireNotNull(amount) { "[ERROR] Required value was null." }
         require(amount % 1000 == 0) { "[ERROR] Required value must be a multiple of 1000" }
