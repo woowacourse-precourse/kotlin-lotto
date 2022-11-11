4242da2659fadf2604f9bc09eaefb10d4da54d19
sonac.kim <104493771+kimsonac@users.noreply.github.com>
2022-11-11 22:35:07 +0900

    [feat] 로또 구입 금액 입력 메소드 생성

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,27 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Randoms
+import camp.nextstep.edu.missionutils.Console
+import java.util.regex.Pattern
+
+private const val amount = 1000
 fun main() {
-    TODO("프로그램 구현")
+    getSellLottoCount()
+
 }
+private fun getSellLottoCount() : Int{
+
+    println("구입금액을 입력해 주세요.")
+
+    val input = readLine()!!
+
+    // 예외 처리
+    if (!Pattern.matches("^[0-9]$", input) || (input.toInt() % amount) == 0) {
+        throw IllegalArgumentException("[ERROR] 정확한 로또 금액을 입력해 주세요.")
+    }
+
+    return input.toInt() / amount
+
+}
+
+
