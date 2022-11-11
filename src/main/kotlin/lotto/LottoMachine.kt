aa3baaf2f55c9cf0d5d181a7da4b2774b62af6f1
EmilyCh0 <seoyeon3089@naver.com>
2022-11-11 15:14:45 +0900

    feat(all): 큰 틀 구성(주요 메서드 작성)

---
+++ src/main/kotlin/lotto/LottoMachine.kt
@@ -0,0 +1,33 @@
+package lotto
+
+class LottoMachine {
+
+    fun start() {
+
+    }
+
+    fun generateLotto(): Lotto {
+        TODO()
+    }
+
+    fun checkWinningNumberException() {
+        TODO()
+    }
+
+    fun checkBonusNumberException() {
+        TODO()
+    }
+
+    fun calculateResult() {
+        TODO()
+    }
+
+    fun calculateLottoResult() {
+        TODO()
+    }
+
+    fun getEarningsRate(): Double {
+        TODO()
+    }
+
+}
---
+++ src/main/kotlin/lotto/View.kt
@@ -0,0 +1,27 @@
+package lotto
+
+class View {
+    fun getPaymentAmount(): String {
+        TODO()
+    }
+
+    fun printNumberOfLottos() {
+        TODO()
+    }
+
+    fun printLottos() {
+        TODO()
+    }
+
+    fun getWinningNumber(): String {
+        TODO()
+    }
+
+    fun getBonusNumber(): String {
+        TODO()
+    }
+
+    fun printResultStats() {
+        TODO()
+    }
+}
