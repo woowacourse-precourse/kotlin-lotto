f30ab5d06c91871197ec3ffb6588f949bcbfa4b7
junsu1023 <12jangdokg55@gmail.com>
2022-11-12 00:32:16 +0900

    feat: 로또를 몇 장 구입했는지 계산하는 함수 추가

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -4,6 +4,9 @@ import camp.nextstep.edu.missionutils.Console
 
 fun main() {
     val buyAmount = userInput().toInt()
+    val lottoCnt = lottoCnt(buyAmount)
 }
 
 fun userInput(): String = Console.readLine()
+
+fun lottoCnt(buyAmount: Int): Int = buyAmount / 1000
