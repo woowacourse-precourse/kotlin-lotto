ab0f4dd86f5579e2bac7e09978b46f518d6ac2d9
cyjadela <adelacho@naver.com>
2022-11-12 13:40:14 +0900

    feat: 구입금액 입력 예외처리

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -7,10 +7,13 @@ fun main() {
 
     var money = try {
         Console.readLine().toInt()
-    } catch (e : NumberFormatException) {
+    } catch (e: NumberFormatException) {
         print("[ERROR] 숫자만 입력해 주세요.")
         println("구입금액을 입력해 주세요.")
         Console.readLine().toInt()
     }
-    
+
+    if ((money % 1000) != 0) {
+        throw IllegalArgumentException("[ERROR] 1000원 단위의 금액을 입력해 주세요.")
+    }
 }
