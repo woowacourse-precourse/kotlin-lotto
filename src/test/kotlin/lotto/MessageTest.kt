7b534c650c7fe36a517c5aa9decb8f5984cef0d8
HwangSooHyeon <48857651+HwangSooHyeon@users.noreply.github.com>
2022-11-12 19:42:32 +0900

    feat(MessageTest.kt): add fun showPurchasedLottoNumbers test
    showPurchasedLottoNumbers 에 대한 테스트 케이스 작성

--- src/test/kotlin/lotto/MessageTest.kt
+++ src/test/kotlin/lotto/MessageTest.kt
@@ -1,12 +1,21 @@
 package lotto
 
-import lotto.domain.Message.showCountOfLotto
+import lotto.domain.Message
+import lotto.domain.Service
 import org.junit.jupiter.api.Test
 
 class MessageTest {
     @Test
     fun `구입한 로또 장 수 출력`() {
         val countOfLotto = 14
-        showCountOfLotto(countOfLotto)
+        Message.showCountOfLotto(countOfLotto)
+    }
+
+    @Test
+    fun `구입한 로또 번호 출력`() {
+        val service = Service()
+        val countOfLotto = 8
+        val lottoNumbers = service.getLottoNumbers(countOfLotto)
+        Message.showPurchasedLottoNumbers(lottoNumbers)
     }
 }
