abf9f49ae727d227d09df49d95687e69e7ddf199
Doyeop Kim <dygames@naver.com>
2022-11-13 07:37:06 +0900

    FEATURE: 열거형 클래스 등수별 금액 정의

--- src/main/kotlin/lotto/LottoRank.kt
+++ src/main/kotlin/lotto/LottoRank.kt
@@ -1,10 +1,10 @@
 package lotto
 
-enum class LottoRank {
-    FIRST,
-    SECOND,
-    THIRD,
-    FOURTH,
-    FIFTH,
-    FAIL,
+enum class LottoRank(value: Int) {
+    FIRST(5000),
+    SECOND(50000),
+    THIRD(1500000),
+    FOURTH(30000000),
+    FIFTH(2000000000),
+    FAIL(0),
 }
