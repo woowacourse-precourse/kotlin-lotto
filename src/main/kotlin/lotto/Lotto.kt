7aa8b5d7188293432a4ffd455016712fb0159207
Doyeop Kim <dygames@naver.com>
2022-11-13 07:24:36 +0900

    FEATURE(Lotto): 당첨 내역 계산

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,14 +1,21 @@
 package lotto
 
-import camp.nextstep.edu.missionutils.Randoms
-
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6)
     }
 
-    fun calculateWinRank(winNumbers: List<Int>, bonus: Int) {
-
+    fun calculateWinRank(winNumbers: List<Int>, bonus: Int): LottoRank {
+        val commonNumbers = numbers.intersect(winNumbers.toSet())
+        val isBonusOn = numbers.contains(bonus)
+        val rank = when (commonNumbers.size) {
+            3 -> LottoRank.FIFTH
+            4 -> LottoRank.FOURTH
+            5 -> if (isBonusOn) LottoRank.SECOND else LottoRank.THIRD
+            6 -> LottoRank.FIRST
+            else -> LottoRank.FAIL
+        }
+        return rank
     }
 
     // TODO: 추가 기능 구현
