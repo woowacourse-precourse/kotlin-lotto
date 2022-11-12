c3703dbb77a5d884b4d2d2ffef83dc5e9ab617eb
dltkd1395 <dltkd13956@naver.com>
2022-11-13 01:10:41 +0900

    feat(WinningLotto) : 로또 일치하는 개수 카운터하는 기능 구현

---
+++ src/main/kotlin/lotto/WinningLotto.kt
@@ -0,0 +1,28 @@
+package lotto
+
+class WinningLotto(private val lottoNumbers: List<Lotto>, private val bonusNumber: Int) {
+    fun countWinningLotto(otherNumbers: List<Int>): Map<Int, Int> {
+        val lottoPrizes = lottoNumbers
+            .map { lotto ->
+                winLotto(lotto, otherNumbers)
+            }
+            .groupingBy { lottoPrize ->
+                lottoPrize.winPrize()
+            }
+            .eachCount()
+        return lottoPrizes.mapValues { it.value }
+    }
+
+    private fun winLotto(lotto: Lotto, otherNumbers: List<Int>): LottoPrize {
+        val matchedCount = matchLottoNumber(lotto, otherNumbers)
+        val isMatching = isMatchingBonusNumber(lotto)
+        return LottoPrize.lottoValueOf(matchedCount, isMatching)
+    }
+
+    private fun matchLottoNumber(lotto: Lotto, otherNumbers: List<Int>): Int =
+        lotto.getMachingLottoNumberCount(otherNumbers)
+
+    private fun isMatchingBonusNumber(lotto: Lotto): Boolean =
+        lotto.isMachingBonusNumber(bonusNumber)
+
+}
