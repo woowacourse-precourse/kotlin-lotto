916bfceac30465a0b3f00896f9bd64eba61a750b
ponopono0322 <ponopono0322@gmail.com>
2022-11-12 23:00:44 +0900

    feat: 당첨 번호 비교 함수 구현
    - compareOriginal: 랜덤 배열과 당첨 번호 비교 함수
    - compareBouns: 보너스 번호를 포함한 비교 함수

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -10,15 +10,34 @@ class Lotto(private val numbers: List<Int>) {
         require(numbers.toSet().size == NumberRange.MAX.number) {
             "[ERROR] 로또 번호는 중복될 수 없습니다."
         }
-        require(numbers.first() >= NumberRange.START.number) {
-            "[ERROR] 로또 번호는 ${NumberRange.START.number}부터 ${NumberRange.END.number} 사이의 숫자여야 합니다."
-        }
-        require(numbers.last() <= NumberRange.END.number) {
-            "[ERROR] 로또 번호는 ${NumberRange.START.number}부터 ${NumberRange.END.number} 사이의 숫자여야 합니다."
+        numbers.forEach {
+            require(it in NumberRange.START.number..NumberRange.END.number) {
+                "[ERROR] 로또 번호는 ${NumberRange.START.number}부터 ${NumberRange.END.number} 사이의 숫자여야 합니다."
+            }
         }
     }
 
     fun getList(): List<Int> {
         return this.numbers
     }
+
+    fun compareOriginal(lotteryNumbers: Lotto): Int {
+        val answer = lotteryNumbers.getList().toSet()
+        val lottoNum = this.numbers.toSet()
+        val match = answer.intersect(lottoNum).size
+
+        return if (match >= 3) { match } else { 0 }
+    }
+
+    fun compareBonus(lotteryNumbers: Lotto, bonusNumber: Int): Boolean {
+        val answer = lotteryNumbers.getList().toSet()
+        val bonusNumbers = this.numbers.toMutableList()
+        bonusNumbers.add(bonusNumber)
+        val lottoNum = bonusNumbers.toSet()
+
+        if (answer.intersect(lottoNum).size == 6) return true
+        return false
+
+    }
+
 }
