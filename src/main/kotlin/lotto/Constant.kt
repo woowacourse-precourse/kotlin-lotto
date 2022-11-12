7d4db38344ff1bb5a5276942c1d8dca0010a0b89
wonseok <dnjstjr245@gmail.com>
2022-11-13 04:39:00 +0900

    [add] 문자열, 숫자 등의 값을 하드코딩하지 않기 위한 Constant 클래스 추가

---
+++ src/main/kotlin/lotto/Constant.kt
@@ -0,0 +1,29 @@
+package lotto
+
+object Constant {
+    const val DEFAULT_NUMBER = 0
+    const val LOTTO_MIN_NUMBER = 1
+    const val LOTTO_MAX_NUMBER = 45
+
+    const val THREE_COINCIDE_WINNING_PRIZE_NUMBER = 5_000
+    const val FOUR_COINCIDE_WINNING_PRIZE_NUMBER = 50_000
+    const val FIVE_COINCIDE_WINNING_PRIZE_NUMBER = 1_500_000
+    const val FIVE_AND_BONUS_COINCIDE_WINNING_PRIZE_NUMBER = 30_000_000
+    const val SIX_COINCIDE_WINNING_PRIZE_NUMBER = 2_000_000_000
+
+    const val LOTTO_RESULT_THREE_COINCIDE = "3개 일치 (5,000원) - %d개"
+    const val LOTTO_RESULT_FOUR_COINCIDE = "4개 일치 (50,000원) - %d개"
+    const val LOTTO_RESULT_FIVE_COINCIDE = "5개 일치 (1,500,000원) - %d개"
+    const val LOTTO_RESULT_FIVE_AND_BONUS_COINCIDE = "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개"
+    const val LOTTO_RESULT_SIX_COINCIDE = "6개 일치 (2,000,000,000원) - %d개"
+
+    const val ERROR_MESSAGE_HEADER = "[ERROR] "
+    const val ERROR_MESSAGE_BODY_NUMBER_SHOULD_BE_IN_RANGE = "로또 번호는 1부터 45 사이의 숫자여야 합니다."
+    const val TOTAL_PUBLISHED_LOTTO_NUMBER = "%d개를 구매했습니다."
+    const val TOTAL_RATE_OF_RETURN = "총 수익률은 %.1f%%입니다."
+
+    const val INPUT_PURCHASE_AMOUNT = "구입금액을 입력해 주세요."
+    const val INPUT_WINNING_NUMBER = "당첨 번호를 입력해 주세요."
+    const val INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요."
+    const val WINNING_STATS = "당첨 통계\n---"
+}
