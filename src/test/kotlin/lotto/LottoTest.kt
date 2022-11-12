306ceb0ed411f8d5108697b9c160d7563d337c8d
boogieWoogie <kimjinuk1999@gmail.com>
2022-11-13 03:10:59 +0900

    test(Lotto) : 로또의 당첨결과를 테스트하는 기능 추가

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -1,7 +1,12 @@
 package lotto
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 
 
 class LottoTest {
@@ -21,4 +26,53 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+    @Test
+    fun `first place check`() {
+        assertThat(Lotto(listOf(1, 2, 3, 4, 5, 6))
+            .checkLotto(listOf(1, 2, 3, 4, 5, 6), 7))
+            .isEqualTo(LottoStatus.FIRST_PLACE)
+    }
+
+    @Test
+    fun `second place check`() {
+        assertThat(Lotto(listOf(1, 2, 3, 4, 5, 7))
+            .checkLotto(listOf(1, 2, 3, 4, 5, 6), 7))
+            .isEqualTo(LottoStatus.SECOND_PLACE)
+    }
+
+    @Test
+    fun `third place check`() {
+        assertThat(Lotto(listOf(1, 2, 3, 4, 5, 45))
+            .checkLotto(listOf(1, 2, 3, 4, 5, 6), 7))
+            .isEqualTo(LottoStatus.THIRD_PLACE)
+    }
+
+    @Test
+    fun `fourth place check`() {
+        assertThat(Lotto(listOf(1, 2, 3, 43, 5, 45))
+            .checkLotto(listOf(1, 2, 3, 4, 5, 6), 7))
+            .isEqualTo(LottoStatus.FOURTH_PLACE)
+    }
+
+    @Test
+    fun `fifth place check`() {
+        assertThat(Lotto(listOf(1, 2, 32, 43, 5, 45))
+            .checkLotto(listOf(1, 2, 3, 4, 5, 6), 7))
+            .isEqualTo(LottoStatus.FIFTH_PLACE)
+    }
+
+    @Test
+    fun `none check with two numbers are matched`(){
+        assertThat(Lotto(listOf(1, 2, 32, 43, 7, 45))
+            .checkLotto(listOf(1, 2, 3, 4, 5, 6), 7))
+            .isEqualTo(LottoStatus.NONE)
+    }
+
+    @Test
+    fun `none check with a number is matched`(){
+        assertThat(Lotto(listOf(1, 7, 32, 43, 8, 45))
+            .checkLotto(listOf(1, 2, 3, 4, 5, 6), 7))
+            .isEqualTo(LottoStatus.NONE)
+    }
+
 }
