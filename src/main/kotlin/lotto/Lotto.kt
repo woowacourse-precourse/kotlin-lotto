ac43ba3ceed53b7d7beac55fafcd9bbde61e5e5e
boogieWoogie <kimjinuk1999@gmail.com>
2022-11-13 02:54:56 +0900

    feat(Lotto) : 로또 하나에 대한 당첨결과를 종합하는 기능 추가

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -5,5 +5,20 @@ class Lotto(private val numbers: List<Int>) {
         require(numbers.size == 6)
     }
 
-    // TODO: 추가 기능 구현
+    fun checkLotto(winningNumber: List<Int>, bonusNumber: Int): LottoStatus {
+        val catchCount = numbers.filter { number -> winningNumber.contains(number) }.size
+        var result = LottoStatus.NONE
+
+        when (catchCount) {
+            3 -> result = LottoStatus.FIFTH_PLACE
+            4 -> result = LottoStatus.FOURTH_PLACE
+            5 -> result = if (!numbers.contains(bonusNumber)) {
+                LottoStatus.THIRD_PLACE
+            } else {
+                LottoStatus.SECOND_PLACE
+            }
+            6 -> result = LottoStatus.FIRST_PLACE
+        }
+        return result
+    }
 }
---
+++ src/main/kotlin/lotto/LottoStatus.kt
@@ -0,0 +1,5 @@
+package lotto
+
+enum class LottoStatus {
+    NONE, FIFTH_PLACE, FOURTH_PLACE, THIRD_PLACE, SECOND_PLACE, FIRST_PLACE
+}
