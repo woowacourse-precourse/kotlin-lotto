08b0532760ff6e402066aa1b9cc63ecb6adf86a8
EmilyCh0 <seoyeon3089@naver.com>
2022-11-12 11:25:38 +0900

    fix(Lotto): Lotto 초기화 시 1..45 범위 체크 추가

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -4,6 +4,9 @@ class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6)
         require(numbers.distinct().size == 6)
+        numbers.forEach {
+            require(it in 1..45)
+        }
     }
 
     // TODO: 추가 기능 구현
