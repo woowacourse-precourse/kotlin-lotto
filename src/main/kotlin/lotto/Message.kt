9c63299c49fbfe98508fb6fb2250cb5d7e707dd9
HwangSooHyeon <48857651+HwangSooHyeon@users.noreply.github.com>
2022-11-12 11:29:22 +0900

    feat(Message.kt): add Message class
    Message 클래스 추가
    구입금액, 당첨 번호, 보너스 번호 메시지 작성

---
+++ src/main/kotlin/lotto/Message.kt
@@ -0,0 +1,13 @@
+package lotto
+
+object Message {
+    fun showInputPurchasingAmount() {
+        println("구입금액을 입력해 주세요.")
+    }
+    fun showInputWinningNumber() {
+        println("당첨 번호를 입력해 주세요.")
+    }
+    fun showInputBonusNumber() {
+        println("보너스 번호를 입력해 주세요.")
+    }
+}
