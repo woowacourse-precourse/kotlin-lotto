f7289a9ac8cc3b7b7fc554debeb31a4bd58f7d4e
cyjadela <adelacho@naver.com>
2022-11-12 13:40:47 +0900

    feat: 로또 개수 출력

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -16,4 +16,8 @@ fun main() {
     if ((money % 1000) != 0) {
         throw IllegalArgumentException("[ERROR] 1000원 단위의 금액을 입력해 주세요.")
     }
+
+    var lottoCount = money / 1000
+
+    println("\n${lottoCount}개를 구매했습니다.")
 }
