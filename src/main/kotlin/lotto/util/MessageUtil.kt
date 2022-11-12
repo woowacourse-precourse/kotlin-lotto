a75b20a86e63382d0e9b266cee4c3d550b3962a8
yeongjin <dudwls901@naver.com>
2022-11-12 11:26:18 +0900

    [Feat] 잘못된 입력 ErrorType 추가

--- src/main/kotlin/lotto/util/MessageUtil.kt
+++ src/main/kotlin/lotto/util/MessageUtil.kt
@@ -1,13 +1,14 @@
 package lotto.util
 
-enum class ErrorType{
-    WRONG_MONEY_INPUT
+enum class ErrorType {
+    CANNOT_DIVISIBLE_INPUT, WRONG_TYPE_INPUT
 }
 
-fun ErrorType.parseErrorType(): String = when(this){
-    ErrorType.WRONG_MONEY_INPUT -> "1,000원 단위로 입력해 주세요."
+fun ErrorType.parseErrorType(): String = when (this) {
+    ErrorType.CANNOT_DIVISIBLE_INPUT -> "1,000원 단위로 입력해 주세요."
+    ErrorType.WRONG_TYPE_INPUT -> "숫자를 입력해 주세요."
 }
 
-fun printErrorMessage(errorType: ErrorType){
+fun printErrorMessage(errorType: ErrorType) {
     println("[ERROR] ${errorType.parseErrorType()}")
 }
