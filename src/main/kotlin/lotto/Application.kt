7afbb0164210e1a7b8030e1b25e39ce9d4f923d8
JEGHTNER <jeheon0717@naver.com>
2022-11-12 15:27:27 +0900

    publishLotto

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -15,13 +15,27 @@ fun createBonusNum(lottoNum:List<Int>):Int
     ret = Randoms.pickNumberInList(remainder)
     return ret
 }
+
+fun publishLotto(money : Int):List<List<Int>>
+{
+    var table = mutableListOf<List<Int>>()
+    var i :Int = 0
+    while (i < (money / 1000)){
+        table.add(createLottoNum())
+        i++;
+    }
+    return table
+}
 fun main() {
     val numbers = createLottoNum()
     val numbers2 = listOf<Int>(1,2,3,4,5,5)
     val bonus = createBonusNum(numbers)
+    var table = mutableListOf<List<Int>>()
     println(numbers)
     println(bonus)
     //Lotto(numbers)
     //Lotto(numbers2)
+    table = publishLotto(8000) as MutableList<List<Int>>
+    println(table)
 
 }
