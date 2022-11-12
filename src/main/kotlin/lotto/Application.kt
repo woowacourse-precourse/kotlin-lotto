5c04a51f2d47039a7853dc4af2cd7d47da78adbf
cyjadela <adelacho@naver.com>
2022-11-12 13:49:41 +0900

    feat: 당첨 내역 출력

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -88,5 +88,12 @@ fun main() {
         }
     }
 
+    println("\n당첨 통계")
+    println("---")
+    println("3개 일치 (5,000원) - ${answerList[3]}개")
+    println("4개 일치 (50,000원) - ${answerList[4]}개")
+    println("5개 일치 (1,500,000원) - ${answerList[5]}개")
+    println("5개 일치, 보너스 볼 일치 (30,000,000원) - ${answerList[7]}개")
+    println("6개 일치 (2,000,000,000원) - ${answerList[6]}개")
 
 }
