78a66ae4d4eba93eeea0694754d165219fb884d4
YuGyeong98 <kuk3097@naver.com>
2022-11-12 14:46:14 +0900

    feat: 당첨 번호, 보너스 번호와 구매한 로또 번호를 비교하여 당첨된 숫자 구하기

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
     val inputVerifier = InputVerifier()
+    val purchasedNumberMap = mutableMapOf<Int, MutableList<Int>>()
 
     println("구입금액을 입력해 주세요.")
     val amountOfPurchase = Console.readLine().toInt()
@@ -14,7 +15,8 @@ fun main() {
     println("${numberOfPurchase}개를 구매했습니다.")
     for (i in 0 until numberOfPurchase) {
         val numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
-        println(numbers.sorted())
+        purchasedNumberMap[i] = numbers.sorted().toMutableList()
+        println(purchasedNumberMap[i])
     }
 
     println("당첨 번호를 입력해 주세요.")
@@ -25,4 +27,20 @@ fun main() {
     println("보너스 번호를 입력해 주세요.")
     val bonusNumber = Console.readLine().toInt()
     inputVerifier.validateBonusNumber(bonusNumber)
+
+    println("당첨 통계")
+    println("---")
+    for (i in 0 until numberOfPurchase) {
+        val unionOfWinning = listOf(winningNumberList, purchasedNumberMap[i]).flatMap { it.orEmpty() }
+        val intersectionOfWinning = unionOfWinning
+            .groupBy { it }
+            .filter { it.value.size > 1 }
+            .flatMap { it.value }
+            .distinct()
+
+        purchasedNumberMap[i]?.add(bonusNumber)
+        val intersectionOfBonus = purchasedNumberMap[i]?.filter { item ->
+            (purchasedNumberMap[i]?.count { it == item } ?: 0) > 1
+        }?.distinct()
+    }
 }
