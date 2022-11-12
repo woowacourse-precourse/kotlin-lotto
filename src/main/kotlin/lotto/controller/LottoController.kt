8ecf5c3a700f704059a44cdab7bc47383727f3cb
jiingom <vvankatam0@gmail.com>
2022-11-12 23:14:58 +0900

    feat: 발행할 로또의 개수를 구하는 기능 구현

--- src/main/kotlin/lotto/controller/LottoController.kt
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -7,10 +7,21 @@ class LottoController {
     private val lottoView = LottoView()
 
     fun start() {
+        var inputMoney = 0
         try {
             val inputMoney = lottoView.getInputMoney()
         } catch (e: IllegalArgumentException) {
             println(e.message)
         }
+
+        val numberOfIssueLotto = numberOfLotto(inputMoney)
+    }
+
+    private fun numberOfLotto(money: Int): Int {
+        return money / PRICE_PER_LOTTO
+    }
+
+    companion object {
+        const val PRICE_PER_LOTTO = 1000
     }
 }
