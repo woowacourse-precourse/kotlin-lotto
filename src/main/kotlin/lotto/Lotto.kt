b975069916662d755c880402f125dd358ac8755c
bmsk <vv99911@gmail.com>
2022-11-13 03:13:49 +0900

    [FEAT] #1 - create Lotto classes

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -3,7 +3,22 @@ package lotto
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6)
+        require(numbers.toSet().size == 6)
     }
 
-    // TODO: 추가 기능 구현
+    fun contains(number: Int): Boolean {
+        return numbers.contains(number)
+    }
+}
+
+class LottoWinningStat (private val purchaseAmount: Int, private val lottoNumbers: List<Int>, private val bonusNumber: Int){
+    private val lotto: Lotto = Lotto(lottoNumbers)
+
+
+    private fun validateIsIncludedInLotto(number: Int, lotto: Lotto) {
+        if(lotto.contains(number)){
+            println("[ERROR] 보너스 번호가 당첨 번호와 중복됨")
+            throw IllegalArgumentException()
+        }
+    }
 }
