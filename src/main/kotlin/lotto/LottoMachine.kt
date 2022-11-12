8e7bd2f0806e6b4579af64247e9d9b034f7c736d
dltkd1395 <dltkd13956@naver.com>
2022-11-12 20:48:53 +0900

    feat(LottoMachine) : 금액에 맞게 로또가 생성되는 기능 추가

--- src/main/kotlin/lotto/LottoMachine.kt
+++ src/main/kotlin/lotto/LottoMachine.kt
@@ -4,6 +4,20 @@ class LottoMachine(private val money: Int) {
     fun sellLottos(): Int =
         money / LOTTO_PRICE
 
+    fun createLottos(): List<List<Int>> =
+        List(sellLottos()) {
+            createLotto()
+        }
+
+    private fun createLotto(): List<Int> {
+        val lotto = LottoGenerator().generate()
+        lotto.map { number ->
+            LottoNumber(number)
+        }
+        Lotto(lotto)
+        return lotto
+    }
+
     companion object {
         private const val LOTTO_PRICE = 1000
     }
