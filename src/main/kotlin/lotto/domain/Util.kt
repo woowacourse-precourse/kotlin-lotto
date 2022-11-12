1501da27e7709a2f314b7a416d205155de1b01fe
HwangSooHyeon <48857651+HwangSooHyeon@users.noreply.github.com>
2022-11-12 19:34:53 +0900

    feat(Util.kt): add fun createLottoNumbers, readLine
    createLottoNumbers
     - 로또 번호를 생성하는 함수
    readLine
     - console input을 읽는 함수

---
+++ src/main/kotlin/lotto/domain/Util.kt
@@ -0,0 +1,14 @@
+package lotto.domain
+
+import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
+
+object Util {
+    fun createLottoNumbers(): List<Int> {
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6)
+    }
+
+    fun readLine(): String {
+        return Console.readLine()
+    }
+}
