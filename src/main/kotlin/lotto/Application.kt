b998d1311f9f90cd7e8838a0697468e4cdf2f2cb
swjw1110 <swjw1110@naver.com>
2022-11-13 03:26:18 +0900

    feat: add Generate Logic of the game
    what is added:
    - generating 6 random numbers and return Lotto object
    
    what is changed:
    - change printing logic of Lotto class
    - change main logic to fit with the game

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,8 +1,20 @@
 package lotto
 
 import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
+    val purchasePrice = getPurchasePrice()
+    val purchasedLotto = mutableListOf<Lotto>()
+
+    for (i in 0 until purchasePrice / 1000){
+        purchasedLotto.add(generateLottoNumber())
+    }
+
+    println("${purchasePrice / 1000}개를 구매하였습니다")
+    purchasedLotto.forEach {
+        println(it.toString())
+    }
 }
 
 fun getPurchasePrice(): Int {
@@ -19,4 +31,9 @@ fun getPrizeNumbers(): List<Int> {
 fun getBonusNumber(): Int {
     println("보너스 번호를 입력해 주세요.")
     return Console.readLine().toInt()
+}
+
+fun generateLottoNumber(): Lotto{
+    val lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6).sorted()
+    return Lotto(lottoNumbers)
 }
--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -6,4 +6,8 @@ class Lotto(private val numbers: List<Int>) {
     }
 
     // TODO: 추가 기능 구현
+
+    override fun toString(): String {
+        return numbers.toString()
+    }
 }
