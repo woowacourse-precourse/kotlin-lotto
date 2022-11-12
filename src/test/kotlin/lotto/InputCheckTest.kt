6c687d315a48b4af01f2192bf9f0953b9ed0a261
mendel <mendel.urssu@gmail.com>
2022-11-13 02:09:13 +0900

    test: 로또 당첨 번호 입력 제대로 받았는지 유효성 검사 TDD 테스트 코드 작성

--- src/test/kotlin/lotto/InputCheckTest.kt
+++ src/test/kotlin/lotto/InputCheckTest.kt
@@ -9,7 +9,15 @@ class InputCheckTest {
     @ParameterizedTest
     @CsvSource(value = ["15000:true", "15010:false", "1000:true", "15a0:false"], delimiter = ':')
     fun `로또 구입 가격 입력을 제대로 받았는지 검사 true`(input: String, expected: Boolean) {
-        val result=checkInputLotteryPurchase(input.trim())
+        val result = checkInputLotteryPurchase(input.trim())
+        assertThat(result).isEqualTo(expected)
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = ["1,2,3,4,5,6:true", "1,2,3,,4,5,6:false", "1,2,3:false", "1,2,4,4,5,6:false", "1,2,a,4,5,6:false"],
+        delimiter = ':')
+    fun `로또 당첨 번호 제대로 입력했는지 검사`(input: String, expected: Boolean) {
+        val result = checkInputWinningLottery(input)
         assertThat(result).isEqualTo(expected)
     }
 
@@ -26,4 +34,44 @@ class InputCheckTest {
 
     private fun isNumber(char: Char): Boolean = char.code in 48..57
     private fun isOneThousandUnit(input: String): Boolean = input.toInt() % 1000 == 0
+
+    private fun checkInputWinningLottery(input: String): Boolean {
+        val inputTrim = input.trim()
+        if (!isConsistAllCommaAndNumber(inputTrim)) return false
+        if (!isAlternatingCommaAndNumber(inputTrim)) return false
+        val numbers = convertSeparateComma(inputTrim)
+        numbers.forEach { if (!isLotteryRange(it)) return false }
+        if (numbers.size != 6) return false
+        if (numbers.toSet().size != 6) return false
+        return true
+    }
+
+    private fun isConsistAllCommaAndNumber(input: String): Boolean {
+        input.forEach {
+            if (!(isNumber(it) || isComma(it) || it == ' ')) return false
+        }
+        return true
+    }
+
+    private fun isAlternatingCommaAndNumber(input: String): Boolean {
+        var nowComma = false
+        for (i in input.indices) {
+            if (input[i] == ',') {
+                if (nowComma) return false // 콤마가 나왓는데 또 연속으로 나온 경우
+                nowComma = true
+            } else if (isNumber(input[i])) {
+                nowComma = false
+            }
+        }
+        return !nowComma //마지막에 콤마가 오면 안됨
+    }
+
+    private fun convertSeparateComma(input: String): List<Int> {
+        val numbers = input.split(",")
+        return numbers.map { it.toInt() }
+    }
+
+    private fun isLotteryRange(num: Int) = num in 1..45
+
+    private fun isComma(char: Char): Boolean = char.code == 44
 }
