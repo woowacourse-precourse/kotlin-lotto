d4711026f246572d0500533b23f1f451cb906919
JEGHTNER <jeheon0717@naver.com>
2022-11-12 14:26:47 +0900

    fun Lotto fixed : exception add

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -6,5 +6,9 @@ fun createLottoNum(): List<Int> {
 }
 fun main() {
     val numbers = createLottoNum()
+    val numbers2 = listOf<Int>(1,2,3,4,5,5)
+    println(numbers)
     Lotto(numbers)
+    Lotto(numbers2)
+
 }
--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -2,8 +2,7 @@ package lotto
 
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6)
+        require(numbers.size == 6){"[ERROR] : 숫자가 6개가 아닙니다."}
+        require(numbers.distinct().size==6){"[ERROR] : 중복된 숫자가 있습니다"}
     }
-
-    // TODO: 추가 기능 구현
 }
