ed41b994ed90e802798e00a4a13a7cc18ba17f8d
HwangSooHyeon <48857651+HwangSooHyeon@users.noreply.github.com>
2022-11-12 19:30:35 +0900

    feat(Service.kt): add fun getLottoNumbers(countOfLotto: Int)
    getLottoNumbers(countOfLotto: Int)
     - 구매 갯수 만큼 로또 번호를 생성해 리턴하는 함수

--- src/main/kotlin/lotto/domain/Service.kt
+++ src/main/kotlin/lotto/domain/Service.kt
@@ -3,9 +3,23 @@ package lotto.domain
 import lotto.domain.ExceptionHandler.checkInputPurchasingAmount
 
 class Service {
+    /**
+     * 구입금액을 입력 받고 구매 갯수를 리턴하는 함수
+     * */
     fun getPurchasingAmount(): Int {
         val inputPurchasingAmount = Util.readLine()
         checkInputPurchasingAmount(inputPurchasingAmount)
         return (inputPurchasingAmount.toInt() / 1000)
+    }
+
+    /**
+     * 구매 갯수 만큼 로또 번호를 생성해 리턴하는 함수
+     * */
+    fun getLottoNumbers(countOfLotto: Int): MutableList<List<Int>> {
+        val lottoNumbers: MutableList<List<Int>> = mutableListOf()
+        for (i in 0 until countOfLotto) {
+            lottoNumbers.add(Util.createLottoNumbers().sorted())
+        }
+        return lottoNumbers
     }
 }
