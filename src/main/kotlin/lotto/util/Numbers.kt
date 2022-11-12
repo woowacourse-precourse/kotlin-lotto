0c335330ea213c88c48d0d1a8df60c06693c30cb
82104 <tir2986@naver.com>
2022-11-12 15:07:22 +0900

    refactor(util): readInt 예외 처리 수정
    - require -> NumberFormatException을 catch 하도록 수정

--- src/main/kotlin/lotto/util/Numbers.kt
+++ src/main/kotlin/lotto/util/Numbers.kt
@@ -5,13 +5,11 @@ import java.text.DecimalFormat
 
 private const val ERROR_NOT_NUMBER = "숫자를 입력해주세요."
 
-fun Int.toPriceFormatted(): String = DecimalFormat("#,###")
-    .format(this)
+fun Int.toPriceFormatted(): String = DecimalFormat("#,###").format(this)
 
-fun readInt(): Int {
-    val num = Console.readLine()
-        .replace("\\s".toRegex(), "")
-        .toIntOrNull()
-    require(num != null) { ERROR_NOT_NUMBER }
-    return num
+fun readInt(): Int = try {
+    Console.readLine().replace("\\s".toRegex(), "").toInt()
+} catch (e: NumberFormatException) {
+    throw IllegalArgumentException(ERROR_NOT_NUMBER)
 }
+
