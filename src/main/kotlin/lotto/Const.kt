10d1a6a41f61c822e5922f2a30bb35f5d08365b1
boogieWoogie <kimjinuk1999@gmail.com>
2022-11-13 04:17:03 +0900

    refactor(Const) : 반복적으로 사용되는 숫자들 const로 정의

--- src/main/kotlin/lotto/Const.kt
+++ src/main/kotlin/lotto/Const.kt
@@ -1,3 +1,10 @@
 package lotto
 
-const val LOTTO_PRICE=1000
+const val MINIMUM_LOTTO_NUMBER = 1
+const val MAXIMUM_LOTTO_NUMBER = 45
+
+const val SIZE_OF_LOTTO_NUMBERS = 6
+const val LOTTO_PRICE = 1000
+
+const val ERROR_NUMBER_RANGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."
+const val ERROR_EXIST_OVERLAPPED_NUMBER = "[ERROR] 중복된 번호를 허용하지 않습니다."
