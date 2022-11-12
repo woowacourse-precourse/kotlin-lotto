d420f99c1a118a9fb002a15431a57129aa256025
dltkd1395 <dltkd13956@naver.com>
2022-11-13 02:41:42 +0900

    feat(Lotto, LottoNumber) : 에러 문구 추가

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -3,8 +3,8 @@ package lotto
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6)
-        require(hasNotDuplicateNumber())
-        require(isVaildAscendingOrder())
+        require(hasNotDuplicateNumber()) { println(NOT_DUPLICATE_NUMBER)}
+        require(isVaildAscendingOrder()) { println(IS_ASCENDING_ORDER)}
     }
 
     fun getMachingLottoNumberCount(otherNumbers: List<Int>): Int =
@@ -18,4 +18,9 @@ class Lotto(private val numbers: List<Int>) {
 
     private fun isVaildAscendingOrder(): Boolean =
         numbers == numbers.sorted()
+
+    companion object {
+        const val NOT_DUPLICATE_NUMBER = "[ERROR] 로또 번호에 중복되는 숫자가 없어야 합니다."
+        const val IS_ASCENDING_ORDER = "[EROOR] 로또 번호는 오름차순으로 정렬되어야 합니다."
+    }
 }
--- src/main/kotlin/lotto/LottoNumber.kt
+++ src/main/kotlin/lotto/LottoNumber.kt
@@ -3,9 +3,13 @@ package lotto
 class LottoNumber(private val lottoNumber: Int) {
 
     init {
-        require(isValidNumberRange())
+        require(isValidNumberRange()) { println(IS_NUMBER_RANGE)}
     }
 
     private fun isValidNumberRange(): Boolean =
         lottoNumber in 1 until 46
+
+    companion object {
+        private const val IS_NUMBER_RANGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자야야 합니다."
+    }
 }
