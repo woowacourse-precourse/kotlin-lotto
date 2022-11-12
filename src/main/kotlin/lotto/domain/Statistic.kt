59b50f015afd2678117b02fb849f0f3f6fda2a75
zqwz <zqwz@naver.com>
2022-11-12 15:35:16 +0900

    refactor(Statistic): Rank enum class 분리

--- src/main/kotlin/lotto/domain/Statistic.kt
+++ src/main/kotlin/lotto/domain/Statistic.kt
@@ -1,17 +1,10 @@
 package lotto.domain
 
 import lotto.util.Constant
+import lotto.util.Rank
 import kotlin.math.roundToInt
 
 class Statistic(private val ranks: List<Int>) {
-    enum class Rank(val message: String, val money: Int, val index: Int) {
-        FIFTH("3개 일치 (5,000원) -", 5000, 5),
-        FOURTH("4개 일치 (50,000원) -", 50000, 4),
-        THIRD("5개 일치 (1,500,000원) -", 1500000, 3),
-        SECOND("5개 일치, 보너스 볼 일치 (30,000,000원) -", 30000000, 2),
-        FIRST("6개 일치 (2,000,000,000원) -", 2000000000, 1);
-    }
-
     private fun calculateProfitRate(lottoAmount: Int) = (addTotalWinningMoney() / lottoAmount.toLong().toDouble()) * 100
 
     private fun roundProfitRate(rate: Double) = (rate * 10).roundToInt().toDouble() / 10
---
+++ src/main/kotlin/lotto/util/Rank.kt
@@ -0,0 +1,9 @@
+package lotto.util
+
+enum class Rank(val message: String, val money: Int, val index: Int) {
+    FIFTH("3개 일치 (5,000원) -", 5000, 5),
+    FOURTH("4개 일치 (50,000원) -", 50000, 4),
+    THIRD("5개 일치 (1,500,000원) -", 1500000, 3),
+    SECOND("5개 일치, 보너스 볼 일치 (30,000,000원) -", 30000000, 2),
+    FIRST("6개 일치 (2,000,000,000원) -", 2000000000, 1);
+}
