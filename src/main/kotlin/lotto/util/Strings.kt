c1fdad5012e8f01fd178061eebb289489056c620
82104 <tir2986@naver.com>
2022-11-12 15:02:09 +0900

    refactor(util): divideToNums 예외 처리 수정
    - require -> NumberFormatException을 catch 하도록 수정

--- src/main/kotlin/lotto/util/Strings.kt
+++ src/main/kotlin/lotto/util/Strings.kt
@@ -2,14 +2,12 @@ package lotto.util
 
 private const val ERROR_DIVIDE_NUMBER_FORMAT = "숫자를 %s로 구분해서 입력해 주세요."
 
-fun String.divideToNums(delimiters: String): List<Int> {
-    val trimmed = this.replace("\\s".toRegex(), "")
-    val parsed = trimmed.split(delimiters)
-    val nums = mutableListOf<Int>()
-    for (str in parsed) {
-        val num = str.toIntOrNull()
-        require(num != null) { ERROR_DIVIDE_NUMBER_FORMAT.format(delimiters) }
-        nums.add(num)
+fun String.divideToNums(delimiters: String): List<Int> =
+    try {
+        this.replace("\\s".toRegex(), "")
+            .split(delimiters)
+            .map { it.toInt() }
+    } catch (e: NumberFormatException) {
+        throw IllegalArgumentException(ERROR_DIVIDE_NUMBER_FORMAT.format(delimiters))
     }
-    return nums
-}
+
