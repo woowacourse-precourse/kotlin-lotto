abbd47d48730c00e02df739385699b37467be55a
HwangSooHyeon <48857651+HwangSooHyeon@users.noreply.github.com>
2022-11-13 01:03:02 +0900

    feat(Application.kt): 기능 목록 8번까지 작성
    구입금액, 로또 번호 생성, 당첨 번호, 보너스 번호까지 작성

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,28 @@
 package lotto
 
+import lotto.domain.Message.showCountOfLotto
+import lotto.domain.Message.showInputBonusNumber
+import lotto.domain.Message.showInputPurchasingAmount
+import lotto.domain.Message.showInputWinningNumber
+import lotto.domain.Message.showPurchasedLottoNumbers
+import lotto.domain.Service
+
 fun main() {
     TODO("프로그램 구현")
+    playLotto()
 }
+
+fun playLotto() {
+    val service = Service()
+    showInputPurchasingAmount()
+    val countOfLotto = service.getPurchasingAmount()
+    showCountOfLotto(countOfLotto)
+    val lottoNumbers = service.getLottoNumbers(countOfLotto)
+    showPurchasedLottoNumbers(lottoNumbers)
+    showInputWinningNumber()
+    val winningNumbers = service.getWinningNumbers()
+    showInputBonusNumber()
+    val bonusNumber = service.getBonusNumber(winningNumbers)
+}
+
+
