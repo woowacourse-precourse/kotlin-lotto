1e7227eff60b5591780b59601e53ffe7ebb9f34e
JEGHTNER <jeheon0717@naver.com>
2022-11-12 14:55:21 +0900

    fun createBonusNum

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -4,11 +4,24 @@ import camp.nextstep.edu.missionutils.Randoms
 fun createLottoNum(): List<Int> {
     return Randoms.pickUniqueNumbersInRange(1, 45, 6)
 }
+fun createBonusNum(lottoNum:List<Int>):Int
+{
+    var remainder = (1..45).toMutableList()
+    var ret :Int
+    for(i in lottoNum) {
+        if (remainder.contains(i))
+            remainder.remove(i)
+    }
+    ret = Randoms.pickNumberInList(remainder)
+    return ret
+}
 fun main() {
     val numbers = createLottoNum()
     val numbers2 = listOf<Int>(1,2,3,4,5,5)
+    val bonus = createBonusNum(numbers)
     println(numbers)
-    Lotto(numbers)
-    Lotto(numbers2)
+    println(bonus)
+    //Lotto(numbers)
+    //Lotto(numbers2)
 
 }
