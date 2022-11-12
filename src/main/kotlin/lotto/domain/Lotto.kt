abe1955f347b462acd8fd67a7454fc3c2c19ab18
tmdgh1592 <tmdgh1592@naver.com>
2022-11-12 23:08:15 +0900

    refactor: modify Lotto class to delegate to List<LottoNumber>

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -1,11 +1,10 @@
 package lotto.domain
 
-class Lotto(private val numbers: List<Int>) {
+class Lotto(private val numbers: List<LottoNumber>) : List<LottoNumber> by numbers {
     init {
         require(numbers.size == 6)
     }
 
     override fun toString(): String =
         numbers.joinToString(", ", "[", "]")
-
 }
