9f73a275ce0508f4b1c21d1baddcd5339d608295
zqwz <zqwz@naver.com>
2022-11-12 15:11:48 +0900

    feat(Constant): 상수 추가

--- src/main/kotlin/lotto/Constant.kt
+++ src/main/kotlin/lotto/Constant.kt
@@ -2,14 +2,25 @@ package lotto
 
 class Constant {
     companion object {
-        const val BONUS_NUMBER = "보너스 번호는 "
         const val LOTTO_NUMBER = "로또 번호는 "
-        const val WINNING_NUMBER = "당첨 번호는 "
         const val LOTTO_AMOUNT = "로또 구입 금액은 "
         const val LOTTO_COUNT = 6
         const val LOTTO_PRICE = 1000
-        const val ZERO = 0
         const val START_LOTTO_RANGE = 1
         const val END_LOTTO_RANGE = 45
+        const val BONUS_NUMBER = "보너스 번호는 "
+        const val WINNING_NUMBER = "당첨 번호는 "
+        const val SPLIT_WINNING_NUMBER = ","
+        const val ZERO = 0
+        const val FIRST = 1
+        const val SECOND = 2
+        const val THIRD = 3
+        const val FOURTH = 4
+        const val FIFTH = 5
+        const val THREE_MATCH = 3
+        const val FOUR_MATCH = 4
+        const val FIVE_MATCH = 5
+        const val SIX_MATCH = 6
+        const val NOTHING = 0
     }
 }
