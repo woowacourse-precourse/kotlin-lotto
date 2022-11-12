200fe919371de8abe7bfbe6ebd2749fa89e19cdf
cyjadela <adelacho@naver.com>
2022-11-12 13:31:03 +0900

    feat: 구입금액 입력

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,16 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    TODO("프로그램 구현")
+    println("구입금액을 입력해 주세요.")
+
+    var money = try {
+        Console.readLine().toInt()
+    } catch (e : NumberFormatException) {
+        print("[ERROR] 숫자만 입력해 주세요.")
+        println("구입금액을 입력해 주세요.")
+        Console.readLine().toInt()
+    }
+    
 }
