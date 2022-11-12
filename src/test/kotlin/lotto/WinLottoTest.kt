0e50478402d085baa37c2d3a131c779076eb1936
yeongjin <dudwls901@naver.com>
2022-11-12 17:46:48 +0900

    [Test] WinLotto 중복, 숫자 개수 테스트

---
+++ src/test/kotlin/lotto/WinLottoTest.kt
@@ -0,0 +1,19 @@
+package lotto
+
+import org.junit.jupiter.api.Test
+
+class WinLottoTest {
+    @Test
+    fun `로또 번호의 개수가 7개가 넘어가면 예외가 발생한다`() {
+        org.junit.jupiter.api.assertThrows<IllegalArgumentException> {
+            WinLotto(listOf(1, 2, 3, 4, 5, 6, 7, 8))
+        }
+    }
+
+    @Test
+    fun `로또 번호에 중복된 숫자가 있으면 예외가 발생한다`() {
+        org.junit.jupiter.api.assertThrows<IllegalArgumentException> {
+            WinLotto(listOf(1, 2, 3, 4, 5, 6, 1))
+        }
+    }
+}
