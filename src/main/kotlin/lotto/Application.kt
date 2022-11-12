d477c9bb82adc5cfbc5f9d085e65100a53b35a6f
sungsaaan <32791382+sungsaaan@users.noreply.github.com>
2022-11-13 00:53:26 +0900

    feat: [구매] 금액 입력 및 로또 구매 개수 문구출력 기능 추가
    printEnterPriceMessage()
    printLottoPurchaseCountMessage() 구현

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Console
 import java.lang.IllegalArgumentException
 
 fun main() {
-
+    purchaseLotto()
 }
 
 fun isNumber(string: String) = string.toIntOrNull() != null
@@ -21,10 +21,17 @@ fun isSatisfyPriceConditions(string: String) =
         isPositiveNumber(string) &&
         isNotWhiteSpace(string)
 
+fun printEnterPriceMessage() = print("구매금액을 입력해주세요.\n")
+
 fun getPurchasePrice(): Int {
     val purchasePrice = Console.readLine()
 
     return if (isSatisfyPriceConditions(purchasePrice)) purchasePrice.toInt()
     else throw IllegalArgumentException("[ERROR] 구매 금액은 공백 없이 천 단위의 수여야 합니다.")
 }
+
+fun printLottoPurchaseCountMessage(price: Int) = print("${price/1000}개를 구매했습니다.")
+
+
+
 
