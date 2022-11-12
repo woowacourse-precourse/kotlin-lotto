6c2c6039253fa2e41188ce5189e3596b9c39e16b
Min-Jae-Bae <qoalswo123@icloud.com>
2022-11-12 18:42:35 +0900

    [Test] 로또 번호에 포함된 번호가 아닐 경우 예외 발생 테스트 추가
    1 ~ 45 사이의 숫자가 아닌 경우 IllegalArgumentException 반환

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -12,13 +12,12 @@ class LottoTest {
         }
     }
 
-    // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
     @Test
-    fun `로또 번호에 중복된 숫자가 있으면 예외가 발생한다`() {
+    fun `로또 번호에 포함된 번호가 아닐 경우 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
-            Lotto(listOf(1, 2, 3, 4, 5, 5))
+            Lotto(listOf(1, 2, 3, 4, 5, 46)).checkNumber()
         }
+
     }
 
-    // 아래에 추가 테스트 작성 가능
 }
