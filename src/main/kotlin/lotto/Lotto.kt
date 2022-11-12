970ebfbc485ab05135f587e18b4c42855c396969
lsakee <lsls4868@gmail.com>
2022-11-12 23:33:21 +0900

    refactor: Lotto 번호를 생성

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,9 +1,18 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class Lotto(private val numbers: List<Int>) {
+
     init {
         require(numbers.size == 6)
+        lottoCreate()
     }
-
+    fun lottoCreate(){
+        val numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
+        println(numbers.sorted())
+    }
     // TODO: 추가 기능 구현
 }
+
+
