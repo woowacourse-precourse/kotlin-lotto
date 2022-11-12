00fccedd07753ce2916b99e8071cbb4cefc42a89
yeongjin <dudwls901@naver.com>
2022-11-12 11:27:25 +0900

    [Feat] 로또 구매 금액 1000원 단위로 입력 받기 구현

--- src/main/kotlin/lotto/util/InputUtil.kt
+++ src/main/kotlin/lotto/util/InputUtil.kt
@@ -2,15 +2,20 @@ package lotto.util
 
 val br = System.`in`.bufferedReader()
 
-fun readSpendMoney(): Int{
-    return try{
-        br.readLine().toInt()
-    }catch (e: java.lang.Exception){
+fun readSpendMoney(): Int {
+    return try {
+        val spendMoney = br.readLine().toInt()
+        if (spendMoney % 1000 != 0) {
+            printErrorMessage(ErrorType.CANNOT_DIVISIBLE_INPUT)
+            throw IllegalArgumentException()
+        }
+        spendMoney
+    } catch (e: java.lang.Exception) {
         printErrorMessage(ErrorType.WRONG_TYPE_INPUT)
         throw IllegalArgumentException()
     }
 }
 
-fun bufferedReaderClose(){
+fun bufferedReaderClose() {
     br.close()
 }
