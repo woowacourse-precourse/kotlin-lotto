682160d1536b6496d5c8ae076e0463f222ab6a02
sungsaaan <32791382+sungsaaan@users.noreply.github.com>
2022-11-13 00:39:36 +0900

    feat: [구매] 구매 금액 입력받는 기능 추가
    isNumber()
    isThousands()
    isPositiveNumber()
    isNotWhiteSpace()
    isSatisfyPriceConditions()
    getPurchasePrice() 구현

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,30 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Console
+import java.lang.IllegalArgumentException
+
 fun main() {
-    TODO("프로그램 구현")
+
 }
+
+fun isNumber(string: String) = string.toIntOrNull() != null
+
+fun isThousands(number: String) = number.toInt() % 1000 == 0
+
+fun isPositiveNumber(number: String) = number.toInt() >= 0
+
+fun isNotWhiteSpace(string: String) = !string.contains(" ")
+
+fun isSatisfyPriceConditions(string: String) =
+        isNumber(string) &&
+        isThousands(string) &&
+        isPositiveNumber(string) &&
+        isNotWhiteSpace(string)
+
+fun getPurchasePrice(): Int {
+    val purchasePrice = Console.readLine()
+
+    return if (isSatisfyPriceConditions(purchasePrice)) purchasePrice.toInt()
+    else throw IllegalArgumentException("[ERROR] 구매 금액은 공백 없이 천 단위의 수여야 합니다.")
+}
+
