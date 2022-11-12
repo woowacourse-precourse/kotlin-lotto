ba39ba4aeba715df3bffb5ca4afa56b9d218ab8e
ChoHJin <cjc9739@naver.com>
2022-11-13 04:31:55 +0900

    feat: 로또 구매 기능 & 문자열 숫자 판별 기능
    1. 문자열 검사 : 숫자 판별 함수로 문자열을 검사하여 그 문자열이 모두 숫자로 이루어져 있으면 1000원 단위로 나눠 구매한 로또의 갯수를 리턴한다.
    
    예외사항 :
    1. 금액이 1000원으로 나누어 떨어지지 않을 경우 예외 처리
    2. 입력받은 문자열이 숫자가 아닌경우 예외 처리

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,33 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
+
+fun isNumber(s : String?) : Boolean {
+    return if(s.isNullOrEmpty()) false
+    else s.all{Character.isDigit(it)}
+}
+
+fun purchaseLotto() : Int {
+    println("구매금액을 입력해 주세요.")
+    val inputMoney = Console.readLine()!!.trim()
+
+    if(!isNumber(inputMoney)) {
+        throw IllegalArgumentException("[ERROR] 구입 금액은 숫자로 입력하셔야 됩니다.")
+    }
+    if(inputMoney.toInt() % 1000 != 0) {
+        throw IllegalArgumentException("[ERROR] 구매 금액은 1000원 단위로 입력해주세요.")
+    }
+
+    return inputMoney.toInt() / 1000
+}
+
+//fun getLottoTicket(countLotto : Int) {
+//
+//}
+
 fun main() {
-    TODO("프로그램 구현")
+//    TODO("프로그램 구현")
+    print(purchaseLotto())
+
 }
