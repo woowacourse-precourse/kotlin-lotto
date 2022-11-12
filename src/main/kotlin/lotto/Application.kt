8575b090c131e5a9024d02776f2a497f31a46691
DongJun-H <dongjun826@seoultech.ac.kr>
2022-11-12 21:05:29 +0900

    feat(Presentation): 로또 결과 안내 메시지 출력

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -7,6 +7,8 @@ const val INTRODUCE_MESSAGE = "구입금액을 입력해 주세요."
 const val SUCCESS_BUY_LOTTO_MESSAGE = "개를 구매했습니다."
 const val WINNING_NUMBERS_INPUT_MESSAGE = "당첨 번호를 입력해 주세요."
 const val WINNING_BONUS_NUMBER_INPUT_MESSAGE = "보너스 번호를 입력해 주세요."
+const val RESULT_TITLE_MESSAGE = "당첨 통계"
+const val RESULT_HORIZONTAL_LINE_MESSAGE = "---"
 
 fun main() {
     displayIntroduceGuideMessage()
@@ -21,6 +23,8 @@ fun main() {
 
     displayWinningBonusNumberInputGuideMessage()
     val winningLottoBonusNumber = getWinningBonusNumber()
+
+    displayResultGuideMessage()
 }
 
 fun getUserBudget(): String = Console.readLine()
@@ -42,3 +46,7 @@ fun displaySuccessBuyLottoGuideMessage(boughtLottoCount: Int) = println("$bought
 fun displayCurrentLottoNumbers(currentLotto: Lotto) = println(currentLotto.getSortedNumbers())
 fun displayWinningNumbersInputGuideMessage() = println(WINNING_NUMBERS_INPUT_MESSAGE)
 fun displayWinningBonusNumberInputGuideMessage() = println(WINNING_BONUS_NUMBER_INPUT_MESSAGE)
+fun displayResultGuideMessage() = run {
+    println(RESULT_TITLE_MESSAGE)
+    println(RESULT_HORIZONTAL_LINE_MESSAGE)
+}
