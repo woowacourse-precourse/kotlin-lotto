a9c3b770c89b4e1bfac6c43aea004f87f143a9e6
82104 <tir2986@naver.com>
2022-11-12 16:44:48 +0900

    test(formatter): LottoResultFormatter 테스트 추가

---
+++ src/test/kotlin/lotto/ui/LottoResultFormatterTest.kt
@@ -0,0 +1,34 @@
+package lotto.ui
+
+import lotto.domain.LottoResultTest
+import org.junit.jupiter.api.Test
+
+@Suppress("NonASCIICharacters")
+class LottoResultFormatterTest {
+
+    @Test
+    fun `당첨 통계 테스트`() {
+        val formatter = LottoResultFormatter(makeLottoResult())
+        val result = formatter.getStatInfo()
+        val expects = listOf(
+            "3개 일치 (5,000원) - 1개",
+            "4개 일치 (50,000원) - 0개",
+            "5개 일치 (1,500,000원) - 0개",
+            "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
+            "6개 일치 (2,000,000,000원) - 0개"
+        )
+        assert(expects.all { it in result })
+    }
+
+    @Test
+    fun `총 수익률 테스트`() {
+        val formatter = LottoResultFormatter(makeLottoResult())
+        val result = formatter.getProfitInfo()
+        val expect = "62.5%"
+        assert(result == expect)
+    }
+
+    companion object {
+        fun makeLottoResult() = LottoResultTest.makeLottoResult()
+    }
+}
