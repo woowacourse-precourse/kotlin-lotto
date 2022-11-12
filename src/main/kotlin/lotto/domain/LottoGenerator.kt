2522bddc52dddd5a713f595a06922faf793a8535
jiingom <vvankatam0@gmail.com>
2022-11-13 01:33:54 +0900

    style: 로또 당 숫자의 개수를 상수로 나타내도록 수정

--- src/main/kotlin/lotto/domain/LottoGenerator.kt
+++ src/main/kotlin/lotto/domain/LottoGenerator.kt
@@ -8,12 +8,16 @@ class LottoGenerator {
         var lottoNum: MutableList<Int>
         while (true) {
             val nums = Randoms.pickUniqueNumbersInRange(1, 45, 6)
-            if (nums.distinct().size == 6) {
+            if (nums.distinct().size == NUMBERS_PER_LOTTO) {
                 lottoNum = nums
                 break
             }
         }
         lottoNum.sort()
         return Lotto(lottoNum)
+    }
+
+    companion object {
+        const val NUMBERS_PER_LOTTO = 6
     }
 }
