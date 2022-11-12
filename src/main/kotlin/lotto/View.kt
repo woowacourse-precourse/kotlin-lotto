76feb5a4cfbfbe5ead1e61460bbd16fcbb267236
EmilyCh0 <seoyeon3089@naver.com>
2022-11-12 10:25:22 +0900

    feat(View): 당첨 번호 입력받기

--- src/main/kotlin/lotto/View.kt
+++ src/main/kotlin/lotto/View.kt
@@ -21,7 +21,8 @@ class View {
         }
 
         fun getWinningNumber(): String {
-            TODO()
+            println("당첨 번호를 입력해 주세요.")
+            return Console.readLine()
         }
 
         fun getBonusNumber(): String {
