090385047a74fcc8a0f7dc9cc54a6872bca1c972
Min-Jae-Bae <qoalswo123@icloud.com>
2022-11-12 17:58:26 +0900

    [Feat] 로또 번호 생성 기능 추가
    로또 갯수로 로또 번호를 생성하는 createNumber 메소드 추가

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,9 +1,18 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6)
     }
 
-    // TODO: 추가 기능 구현
+    fun createNumber() {
+        val purchase = Purchase()
+        val lottoQuantity = purchase.createExpense()
+        for (index in 0 until lottoQuantity) {
+            val numberList = Randoms.pickUniqueNumbersInRange(1, 45, numbers.size)
+            println(numberList.sorted())
+        }
+    }
 }
