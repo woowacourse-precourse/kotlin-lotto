f7bf1a19c16844ceda848df05fbeef88386f4db8
JEGHTNER <jeheon0717@naver.com>
2022-11-12 14:05:50 +0900

    fun createLottoNum()

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,10 @@
 package lotto
+import camp.nextstep.edu.missionutils.Randoms
 
+fun createLottoNum(): List<Int> {
+    return Randoms.pickUniqueNumbersInRange(1, 45, 6)
+}
 fun main() {
-    TODO("프로그램 구현")
+    val numbers = createLottoNum()
+    Lotto(numbers)
 }
