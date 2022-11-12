5cc2025c9b464f0f4a464080d5cf229f2654111a
zqwz <zqwz@naver.com>
2022-11-12 17:26:35 +0900

    refactor(ErrorMessage): 출력문 적용

--- src/main/kotlin/lotto/util/ErrorMessage.kt
+++ src/main/kotlin/lotto/util/ErrorMessage.kt
@@ -3,16 +3,14 @@ package lotto.util
 class ErrorMessage {
     companion object {
         private const val ERROR = "[ERROR] "
-        fun intError(kind: String) = ERROR + kind + "숫자여야 합니다."
+        fun intError(kind: String) = println(ERROR + kind + "숫자여야 합니다.")
 
-        fun sizeError(kind: String) = ERROR + kind + "6개가 필요합니다."
+        fun sizeError(kind: String) = println(ERROR + kind + "6개가 필요합니다.")
 
-        fun rangeError(kind: String) = ERROR + kind + "1~45 사이의 숫자여야 합니다."
+        fun rangeError(kind: String) = println(ERROR + kind + "1~45 사이의 숫자여야 합니다.")
 
-        fun duplicateError(kind: String) = ERROR + kind + "중복되지 않은 수여야 합니다."
+        fun duplicateError(kind: String) = println(ERROR + kind + "중복되지 않은 수여야 합니다.")
 
-        fun unitError(kind: String) = ERROR + kind + "1000원 단위여야 합니다."
-
-        fun systemError() = ERROR + "시스템을 종료합니다."
+        fun unitError(kind: String) = println(ERROR + kind + "1000원 단위여야 합니다.")
     }
 }
