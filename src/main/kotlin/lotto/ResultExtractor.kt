d546722362b22e5d7385723127649925ed0365b1
boogieWoogie <kimjinuk1999@gmail.com>
2022-11-13 02:58:22 +0900

    feat(ResultExtractor) : 구입한 로또 전체에 대해서 결과를 모두 종합하는 기능 추가

--- src/main/kotlin/lotto/ResultExtractor.kt
+++ src/main/kotlin/lotto/ResultExtractor.kt
@@ -1,4 +1,22 @@
 package lotto
 
-object ResultExtarctor {
+object ResultExtractor {
+    fun extractResult(
+        generatedLotto: List<Lotto>,
+        winningNumbers: List<Int>,
+        bonusNumber: Int,
+    ): MutableMap<LottoStatus, Int> {
+        val result = mutableMapOf<LottoStatus, Int>()
+
+        generatedLotto.forEach { eachLotto ->
+            val lottoStatus = eachLotto.checkLotto(winningNumbers, bonusNumber)
+
+            result[lottoStatus] = if (!result.contains(lottoStatus)) {
+                1
+            } else {
+                result[lottoStatus]!! + 1
+            }
+        }
+        return result
+    }
 }
