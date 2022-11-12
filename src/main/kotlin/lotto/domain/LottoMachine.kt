84242a09d127022b77a8902332fdaa4801874f8c
Joonhoo <mnc5@naver.com>
2022-11-12 16:48:20 +0900

    feat: 구매한 로또 번호들의 당첨 결과 반환 함수 추가

--- src/main/kotlin/lotto/domain/LottoMachine.kt
+++ src/main/kotlin/lotto/domain/LottoMachine.kt
@@ -16,6 +16,34 @@ class LottoMachine(
         winningNumber = inputWinningNumber().toMutableList()
         bonus = inputBonusNumber()
     }
+
+    fun compareNumber(): List<Int> {
+        val result = mutableListOf(0, 0, 0, 0, 0)
+        lotteries.forEach { lotto ->
+            when (resultOfLotto(lotto)) {
+                Rating.SIX -> result[0]++
+                Rating.FIVE -> result[1]++
+                Rating.FIVE_AND_BONUS -> result[2]++
+                Rating.FOUR -> result[3]++
+                Rating.THREE -> result[4]++
+                else -> Unit
+            }
+        }
+        return result.toList()
+    }
+
+    // 로또의 번호 매칭 결과
+    private fun resultOfLotto(lotto: Lotto): Rating {
+        var matches = 0
+        var bonusMatch = false
+        val userLotto = lotto.getOrderedNumbers()
+        userLotto.forEach { number ->
+            if (winningNumber.contains(number)) matches++
+            if (number == bonus) bonusMatch = true
+        }
+        return Rating.lottoResult(matches, bonusMatch)
+    }
+
     private fun inputWinningNumber(): List<Int> {
         Printers.inputWinningNumber()
         val number = Console.readLine()
