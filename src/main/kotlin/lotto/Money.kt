bbd426ff1a63aba492cb57ff39f87317d12ee6ac
dltkd1395 <dltkd13956@naver.com>
2022-11-13 02:41:06 +0900

    feat(Money) : 1000원 단위로 나누어 떨어지는지 알 수 있는 기능 구현

--- src/main/kotlin/lotto/Money.kt
+++ src/main/kotlin/lotto/Money.kt
@@ -3,6 +3,7 @@ package lotto
 class Money(private val money: String) {
     init {
         require(isNumber()) { println(IS_NUMBER) }
+        require(canDividedMoney()) { println(DIVIDED_MONEY) }
     }
 
     private fun isNumber(): Boolean =
@@ -10,7 +11,11 @@ class Money(private val money: String) {
             number.isDigit()
         }
 
+    private fun canDividedMoney(): Boolean =
+        money.toInt() % 1000 == 0
+
     companion object {
         private const val IS_NUMBER = "[ERROR] 금액은 숫자이여야 합니다."
+        private const val DIVIDED_MONEY = "[ERROR] 1000원 단위로 나누어 떨어져야 합니다."
     }
 }
