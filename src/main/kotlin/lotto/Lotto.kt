571129c059dfd216d49acf6f7f3d9723bd939c2b
ponopono0322 <ponopono0322@gmail.com>
2022-11-12 18:29:08 +0900

    refactor: 허용 로또 숫자 범위 에러 문구 수정
    로또 숫자 범위(1~45) 사이를 넘었을 때 출력되는 문구 수정

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -11,10 +11,10 @@ class Lotto(private val numbers: List<Int>) {
             "[ERROR] 로또 번호는 중복될 수 없습니다."
         }
         require(numbers.first() >= NumberRange.START.number) {
-            "[ERROR] ${NumberRange.START.number}보다 작은 번호가 있어서는 안됩니다."
+            "[ERROR] 로또 번호는 ${NumberRange.START.number}부터 ${NumberRange.END.number} 사이의 숫자여야 합니다."
         }
         require(numbers.last() <= NumberRange.END.number) {
-            "[ERROR] ${NumberRange.END.number}보다 큰 번호가 있어서는 안됩니다."
+            "[ERROR] 로또 번호는 ${NumberRange.START.number}부터 ${NumberRange.END.number} 사이의 숫자여야 합니다."
         }
     }
 
