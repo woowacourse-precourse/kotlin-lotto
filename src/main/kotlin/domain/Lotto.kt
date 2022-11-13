f7c278fa9bd13ae935b86006501de93f06db79e4
lee989898 <wnsrud1352@gmail.com>
2022-11-14 07:39:44 +0900

    [feat](Lotto.kt) 로또번호와 당청번호 비교해 같은 숫자 세는 기능 구현

--- src/main/kotlin/domain/Lotto.kt
+++ src/main/kotlin/domain/Lotto.kt
@@ -1,8 +1,17 @@
 package domain
 
-class Lotto(private val numbers: List<Int>) {
+class Lotto(val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "로또 번호는 6개야 합니다." }
         numbers.forEach { require(it in 1..45) { "로또의 각 번호는 1~45 사이여아 합니다." } }
+    }
+
+    fun countSameNumber(lotto: Lotto): Int {
+        val differences = numbers.toMutableList()
+        differences.removeAll(lotto.numbers)
+        return 6 - differences.size
+    }
+    operator fun contains(number: Int): Boolean {
+        return numbers.contains(number)
     }
 }
