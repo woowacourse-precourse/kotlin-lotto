ecbac0f01510d868dcf43e13deae9222e9e85351
phs <gustjd0808@pusan.ac.kr>
2022-11-14 01:33:58 +0900

    Revert "refator: make 'domain' package and Lotto class migrate to 'domain' package"
    This reverts commit 86c4651b31a2ec204f2e71f4fa469643e21d1610.

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,4 +1,4 @@
-package lotto.domain
+package lotto
 
 class Lotto(private val numbers: List<Int>) {
     init {
--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -1,6 +1,5 @@
 package lotto
 
-import lotto.domain.Lotto
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
