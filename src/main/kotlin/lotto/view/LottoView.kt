525493f85979775426eee22f3451026392ee9d6e
ghddbwns9808 <ghddbwns9808@gmail.com>
2022-11-13 23:13:19 +0900

    feat: add printLotto function Add a function that print generated lottos

--- src/main/kotlin/lotto/view/LottoView.kt
+++ src/main/kotlin/lotto/view/LottoView.kt
@@ -8,4 +8,10 @@ class LottoView {
         println("구입금액을 입력해 주세요.")
         return readLine()!!.toLong()
     }
+
+    fun printLotto(lottos: ArrayList<List<Int>>){
+        println("\n${lottos.size}개를 구매했습니다.")
+        for(i in lottos.indices)
+            println(lottos[i])
+    }
 }
