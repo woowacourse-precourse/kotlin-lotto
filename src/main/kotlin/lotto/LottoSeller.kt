79fdc174d5208ab0f68bd0974789b781cb2543af
mendel <mendel.urssu@gmail.com>
2022-11-13 14:40:53 +0900

    fix: 구입 가격을 구입 개수로 변환하는 과정의 버그 수정
    - InputConverter의 convertPurchaseCount에서 % 연산을 / 연산으로 변경
    - LottoSeller의
    sellLottery 메소드의 매개변수를 money에서 count로 변경

--- src/main/kotlin/lotto/LottoSeller.kt
+++ src/main/kotlin/lotto/LottoSeller.kt
@@ -3,12 +3,10 @@ package lotto
 import camp.nextstep.edu.missionutils.Randoms
 
 class LottoSeller() {
-    private fun sellLotteryCount(money: Int): Int = money / LOTTO_SELL_PRICE
 
     private fun issueLottery(): List<Int> = Randoms.pickUniqueNumbersInRange(1, 45, 6)
 
-    fun sellLottery(money: Int): List<Lotto> {
-        val count = sellLotteryCount(money)
+    fun sellLottery(count: Int): List<Lotto> {
         val sellLotteries = mutableListOf<Lotto>()
         for (i in 0 until count) {
             sellLotteries.add(Lotto(issueLottery()))
