aad7f7121d3f92a036289d2244d19195653e0944
jiingom <vvankatam0@gmail.com>
2022-11-13 12:39:11 +0900

    feat: 로또 번호에 중복되는 숫자가 있는지 확인하는 기능 구현

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -1,9 +1,15 @@
 package lotto.domain
 
 class Lotto(private val numbers: List<Int>) {
+
     init {
         require(numbers.size == 6)
+        isLottoNumberDuplicated(numbers)
     }
 
-    // TODO: 추가 기능 구현
+    private fun isLottoNumberDuplicated(lottoNumList: List<Int>) {
+        if (lottoNumList.distinct().size != 6) {
+            throw IllegalArgumentException("[ERROR]: 로또는 중복되지 않는 6개의 숫자를 가져야 한다.")
+        }
+    }
 }
