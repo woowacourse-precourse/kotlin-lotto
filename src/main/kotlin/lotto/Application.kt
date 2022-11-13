21d5ac20db8d84f8256ca289498c9d13a318f2a5
Doyeop Kim <dygames@naver.com>
2022-11-13 17:36:21 +0900

    FIX: 수익률 검사에서 잘못된 계산식 수정

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -4,11 +4,11 @@ import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
 
 fun calculateYield(winRanks: Map<LottoRank, Int>, lottoMoney: Int): Float {
-    var winMoney = 0
+    var winMoney = 0f
     for ((k, v) in winRanks) {
         winMoney += k.value * v
     }
-    return winMoney / lottoMoney * 100f
+    return (winMoney / lottoMoney) * 100f
 }
 
 fun calculateWinRanks(lottos: List<Lotto>, winNumber: List<Int>, bonusNumber: Int): Map<LottoRank, Int> {
