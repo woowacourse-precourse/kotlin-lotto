bd289567a69abc3ed0e8a533dc8de8e1c6683124
kyoung_mo <kumong97@knu.ac.kr>
2022-11-14 02:36:21 +0900

    기능 목록 정립

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,5 +1,12 @@
 package lotto
 
+//함수(또는 메서드)의 길이가 15라인을 넘어가지 않도록 구현한다.
+//함수(또는 메서드)가 한 가지 일만 잘 하도록 구현한다.
+//else를 지양한다.
+//힌트: if 조건절에서 값을 return하는 방식으로 구현하면 else를 사용하지 않아도 된다.
+//때로는 if/else, when문을 사용하는 것이 더 깔끔해 보일 수 있다. 어느 경우에 쓰는 것이 적절할지 스스로 고민해 본다.
+//Enum 클래스를 적용해 프로그래밍을 구현한다.
+
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6)
--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -3,6 +3,9 @@ package lotto
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
+//도메인 로직에 단위 테스트를 구현해야 한다. 단, UI(System.out, System.in, Scanner) 로직은 제외한다.
+//핵심 로직을 구현하는 코드와 UI를 담당하는 로직을 분리해 구현한다.
+//단위 테스트 작성이 익숙하지 않다면 test/kotlin/lotto/LottoTest를 참고하여 학습한 후 테스트를 구현한다.
 
 class LottoTest {
     @Test
@@ -20,5 +23,6 @@ class LottoTest {
         }
     }
 
-    // 아래에 추가 테스트 작성 가능
+    // 로또 번호에 1~45 범위 초과시 예외가 발생한다.
+    // 로또 구매 금액이 1000원으로 나누어 떨어지지 않을 경우 예외가 발생한다.
 }
