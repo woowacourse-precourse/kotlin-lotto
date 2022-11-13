b12a4675bf86cea7c089a477212fa1cc81490b93
username <ieejo716@naver.com>
2022-11-13 22:59:26 +0900

    [FEAT] : 입력한 값이 숫자가 아닌 경우

--- src/main/kotlin/lotto/InputLotto.kt
+++ src/main/kotlin/lotto/InputLotto.kt
@@ -4,10 +4,31 @@ import camp.nextstep.edu.missionutils.Console
 
 class InputLotto() {
 
+    //todo 로또 구매할 금액
+    //todo 로또 몇장이 살 수 있는지 확인
+    //todo 입력 값 에러 검사
+
+    fun inputPrice() {
+        val input = Console.readLine()
+
+    }
+
+    private fun checkInputType(input: String) {
+        try {
+            input.toInt()
+        } catch (e: Exception) {
+            invalidInputValue(TYPE_ERROR)
+        }
+    }
 
 
     private fun invalidInputValue(message: String) {
         throw IllegalArgumentException("[ERROR] : $message")
+    }
+
+    companion object {
+        const val TYPE_ERROR = "숫자가 아닙니다."
+        const val VALUE_ERROR = "올바른 값이 아닙니다."
     }
 
 }
