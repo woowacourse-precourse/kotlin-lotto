1105238e4dbeee99bbdc904ae4e3f2d2e567c2d9
Lee-Sunho <sunho9889@soongsil.ac.kr>
2022-11-13 19:32:19 +0900

    feat(InputView): 당첨 번호 입력 받는 메소드 추가

--- src/main/kotlin/lotto/views/InputView.kt
+++ src/main/kotlin/lotto/views/InputView.kt
@@ -1,9 +1,19 @@
 package lotto.views
 
 import camp.nextstep.edu.missionutils.Console
+import lotto.constants.Constant
 
 object InputView {
     fun inputMoney(): Int {
         return Console.readLine().toInt()
+    }
+
+    fun inputWinningNumber(): MutableList<Int> {
+        val winningNumbers = mutableListOf<Int>()
+        val input: List<String> = Console.readLine().trim().split(',')
+        for (index in Constant.START_INDEX until input.size) {
+            winningNumbers.add(input[index].toInt())
+        }
+        return winningNumbers
     }
 }
