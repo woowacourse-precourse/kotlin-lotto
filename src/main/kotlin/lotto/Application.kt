31d64b62d58ef981172bf349bbf408d643c54808
Lim-Joosung <kinglim1224@naver.com>
2022-11-13 14:44:33 +0900

    구매한 로또 티켓 개수 출력 및 티켓 개수만큼 로또 당첨번호 생성 구현

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,6 +1,7 @@
 package lotto
 
 import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
     println("구입금액을 입력해주세요")
@@ -13,6 +14,17 @@ fun main() {
     if(lottoPay.toInt()%1000 != 0){
         println("[ERROR] 금액을 1000원 단위로 입력해주세요")
         throw IllegalArgumentException()
+    }
+
+    lottoTicket = lottoPay.toInt()/1000
+
+    println("${lottoTicket}개를 구매했습니다.")
+    var lotto = mutableListOf<List<Int>>()
+    while(lottoTicket > 0){
+        val numbers = Randoms.pickUniqueNumbersInRange(1,46,6).sorted()
+        println(numbers)
+        lotto.add(numbers)
+        lottoTicket = lottoTicket!! - 1
     }
 }
 fun chkNum(str: String) : Boolean {
