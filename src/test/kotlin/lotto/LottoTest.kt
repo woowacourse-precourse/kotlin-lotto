c3ee4bb84b82f40fcc2ed85d32e3e46accbf0c06
ldj526 <59276421+ldj526@users.noreply.github.com>
2022-11-13 19:47:34 +0900

    test: 예외처리에 대한 테스트

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -1,7 +1,11 @@
 package lotto
 
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import java.io.ByteArrayInputStream
+import java.io.InputStream
 
 
 class LottoTest {
@@ -20,5 +24,22 @@ class LottoTest {
         }
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @Test
+    fun `숫자 외 다른 것을 입력했을 때 예외가 발생한다`() {
+        val amount = "가나다"
+        val `in`: InputStream = ByteArrayInputStream(amount.toByteArray())
+        System.setIn(`in`)
+        val assertThrows = assertThrows<IllegalArgumentException> {
+            ExceptionHandler.checkNumber()
+        }
+        assertEquals(assertThrows.message, "[ERROR] 숫자만 입력하세요")
+    }
+
+    @Test
+    fun `1000으로 나누어 떨어지지 않으면 예외가 발생한다`(){
+        val amount = 1500
+        assertThrows<IllegalArgumentException> {
+            ExceptionHandler.checkAmount(amount)
+        }
+    }
 }
