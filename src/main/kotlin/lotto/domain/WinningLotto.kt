9c42733063f7df4a85d91ae44d20c2610c80ba79
re4rk <37167652+re4rk@users.noreply.github.com>
2022-11-14 00:44:48 +0900

    feat(lotto): 입력받은 로또와 당첨 번호를 비교해 당첨 순위를 알려주는 기능 구현

--- src/main/kotlin/lotto/domain/WinningLotto.kt
+++ src/main/kotlin/lotto/domain/WinningLotto.kt
@@ -1,6 +1,6 @@
 package lotto.domain
 
-class WinningLotto(private val winningNumbers: List<Int>, bonusNumber: Int) {
+class WinningLotto(private val winningNumbers: List<Int>, private val bonusNumber: Int) {
     init {
         require(winningNumbers.size == 6) { "[ERROR] 입력은 6개만 있어야합니다." }
         require(winningNumbers.toSet().size == 6) { "[ERROR] 중복된 숫자가 있으면 안됩니다." }
@@ -8,5 +8,21 @@ class WinningLotto(private val winningNumbers: List<Int>, bonusNumber: Int) {
 
         require(bonusNumber in 1..45) { "[ERROR] 로또 번호는 1부터 45까지 사이의 숫자여야 합니다." }
         require(bonusNumber !in winningNumbers) { "[ERROR0] 중복된 숫자가 있으면 안됩니다." }
+    }
+
+    fun checkResult(guessNumber: List<Int>): Pair<Int, Boolean> {
+        return Pair(
+            winningNumbers.countSameNumbers(guessNumber),
+            bonusNumber in guessNumber
+        )
+    }
+
+    fun getRank(guessNumber: List<Int>): Int {
+        val (countSame, bonus) = checkResult(guessNumber)
+        return LottoRank.of(countSame, bonus).ordinal + 1
+    }
+
+    private fun List<Int>.countSameNumbers(guess: List<Int>): Int {
+        return this.filter { it in guess }.size
     }
 }
