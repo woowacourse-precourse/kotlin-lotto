de3e55e60356bc0284031cadf8355ceb2213c441
LHS0731 <gustjq2536@gmail.com>
2022-11-13 14:31:05 +0900

    구입금액 예외처리

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -7,11 +7,15 @@ fun main() {
 
     println("구입금액을 입력해 주세요.")
     var moneyInput = Console.readLine()
+    if(stringCheck(moneyInput)) {
+        print("[ERROR]")
+        throw NoSuchElementException()
+    }
 
     var lottoCount = moneyInput!!.toInt()/1000
     println("${lottoCount}개를 구매했습니다.")
 
-    var randomlotto = MakeRandomLotto(lottoCount)
+    var randomlotto = makeRandomLotto(lottoCount)
 
     println("당첨 번호를 입력해 주세요.")
     var myNumber = Console.readLine()
@@ -21,7 +25,7 @@ fun main() {
     var bonusNumber = Console.readLine()
 }
 
-fun MakeRandomLotto(count:Int): MutableList<List<Int>> {
+fun makeRandomLotto(count:Int): MutableList<List<Int>> {
     var lottolist = mutableListOf<List<Int>>()
     for (i in 1..count) {
         val numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6).sorted()
@@ -29,4 +33,17 @@ fun MakeRandomLotto(count:Int): MutableList<List<Int>> {
         println(numbers)
     }
     return lottolist
+}
+
+fun stringCheck(str: String):Boolean{
+    var element:Char
+    var flag = false
+
+    for(i in str.indices){
+        element = str.elementAt(i)
+        if (element.toInt()<48||element.toInt()>57){
+            flag = true
+        }
+    }
+    return flag
 }
