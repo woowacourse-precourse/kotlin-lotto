11d624a06d770da056ebf2b872b3e46f0c28fa98
wonseok <dnjstjr245@gmail.com>
2022-11-13 19:50:28 +0900

    [feat] Lotto 클래스 구현 -> 발행된 로또의 정보를 저장하는 로직 추가

--- src/main/kotlin/lotto/model/Lotto.kt
+++ src/main/kotlin/lotto/model/Lotto.kt
@@ -1,11 +1,39 @@
 package lotto.model
 
+import lotto.model.LottoData.bonusNumber
+import lotto.model.LottoData.lottoStats
+import lotto.model.LottoData.winningNumbers
 import lotto.util.Constants.WINNING_NUMBER_COUNT
+import lotto.util.LottoResult
 
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == WINNING_NUMBER_COUNT)
     }
 
-    // TODO: 당첨번호 및 보너스 번호와 비교해서 카운트 하는 기능
+    fun saveLottoData() {
+        var count = 0
+        var isBonusCoincided = false
+        numbers.forEach {
+            if (winningNumbers.contains(it)) count++
+            if (it == bonusNumber) isBonusCoincided = true
+        }
+        when (count) {
+            3 -> lottoStats[LottoResult.THREE_COINCIDE] = lottoStats[LottoResult.THREE_COINCIDE]!! + 1
+            4 -> lottoStats[LottoResult.FOUR_COINCIDE] = lottoStats[LottoResult.FOUR_COINCIDE]!! + 1
+            5 -> {
+                if (!isBonusCoincided) {
+                    lottoStats[LottoResult.FIVE_COINCIDE] = lottoStats[LottoResult.FIVE_COINCIDE]!! + 1
+                } else {
+                    lottoStats[LottoResult.FIVE_AND_BONUS_COINCIDE] =
+                        lottoStats[LottoResult.FIVE_AND_BONUS_COINCIDE]!! + 1
+                }
+            }
+
+            6 -> lottoStats[LottoResult.SIX_COINCIDE] = lottoStats[LottoResult.SIX_COINCIDE]!! + 1
+            else -> Unit
+        }
+    }
+
+
 }
