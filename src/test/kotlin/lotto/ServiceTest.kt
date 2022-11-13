e2b916c113278d938cf647c1d217e20d4e09f3e9
HwangSooHyeon <48857651+HwangSooHyeon@users.noreply.github.com>
2022-11-13 22:53:18 +0900

    refactor(ServiceTest.kt): modify import name
    Message 클래스 명이 Messages 클래스 명으로 변경됨에 따라 import 변경

--- src/test/kotlin/lotto/ServiceTest.kt
+++ src/test/kotlin/lotto/ServiceTest.kt
@@ -1,9 +1,8 @@
 package lotto
 
 import lotto.domain.Lotto
-import lotto.domain.Message
+import lotto.domain.Messages
 import lotto.domain.Service
-import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 
 class ServiceTest {
@@ -13,7 +12,7 @@ class ServiceTest {
         val service = Service()
         val countOfLotto = 8
         val lottoNumbers = service.getLottoNumbers(countOfLotto)
-        Message.showPurchasedLottoNumbers(lottoNumbers)
+        Messages.showPurchasedLottoNumbers(lottoNumbers)
         val winningNumbers = Lotto(listOf(4,3,7,9,12,36))
         val bonusNumber = 45
         val winningLotto = service.isWinningLotto(lottoNumbers, winningNumbers, bonusNumber)
