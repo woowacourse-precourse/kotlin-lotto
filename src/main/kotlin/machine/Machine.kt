c9195d44805ba41a98ccc88ac9a73b4e33957d02
jklom0326 <jklom0326@gmail.com>
2022-11-13 21:57:32 +0900

    feat(Machine.kt): 로또 번호를 만드는 함수 구현
    class(Machine): makeLottoNumbers() 함수
    로또번호를 생성하고 출력한다.

--- src/main/kotlin/machine/Machine.kt
+++ src/main/kotlin/machine/Machine.kt
@@ -1,9 +1,27 @@
 package machine
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class Machine(val inputMoney: Int) {
+    private val count = inputMoney / 1000
+    val lotterys = mutableListOf<List<Int>>()
+
     init {
         require(inputMoney % 1000 == 0) { NOT_THOUSANDS_NUMBER }
     }
+
+    fun makeLottoNumbers(count: Int = this.count): MutableList<List<Int>> {
+        println("${count}개를 구매했습니다.")
+        var lotterysCount = 0
+        while (lotterysCount < count) {
+            lotterys += Randoms.pickUniqueNumbersInRange(1, 45, 6)
+                .toList()
+            println(lotterys[lotterysCount].sorted())
+            lotterysCount++
+        }
+        return lotterys
+    }
+
 
     companion object {
         const val NOT_THOUSANDS_NUMBER = "[ERROR] 입력된 금액이 1000원단위여야 합니다."
