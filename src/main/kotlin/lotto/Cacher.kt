172b4793909b67b32320aefacfa7c1ac4df90b77
권용민 <briandr@naver.com>
2022-11-13 20:53:10 +0900

    refactor(Cacher): 1000 상수화

--- src/main/kotlin/lotto/Cacher.kt
+++ src/main/kotlin/lotto/Cacher.kt
@@ -5,11 +5,12 @@ import camp.nextstep.edu.missionutils.Console.readLine
 const val MONEY_NOT_NATURAL_NUMBER = "[ERROR] 구입금액은 자연수여야합니다."
 const val UNIT_NOT_THOUSAND = "[ERROR] 구입금액은 1000원 단위여야합니다."
 const val INPUT_IS_EMPTY = "[ERROR] 입력값이 비었습니다."
+const val INPUT_STANDARD_UNIT = 1000
 
 class Cacher {
 
     fun calculateNumberOfLottos(money: Int): Int {
-        return money / 1000
+        return money / INPUT_STANDARD_UNIT
     }
 
     fun enterPurchaseMoney(inputMoney: String = readLine()): Int {
