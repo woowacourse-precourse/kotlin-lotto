03479609af7ea0309b3a14694dc17951a49067ce
sojk401 <sojk401@gmail.com>
2022-11-14 01:31:48 +0900

    feat(lotto): 당첨 번호 입력받는 기능 구현

--- src/main/kotlin/lotto/Seller.kt
+++ src/main/kotlin/lotto/Seller.kt
@@ -1,15 +1,25 @@
 package lotto
 
 import camp.nextstep.edu.missionutils.Randoms
+import camp.nextstep.edu.missionutils.Console
+
+val LOTTO_SIZE = 6
 
 class Seller {
     fun generateLotto(LottoCnt: Int) {
-        val LOTTO_SIZE = 6
 
         println("\n${LottoCnt}개를 구입했습니다.")
         for (cnt in 0 until LottoCnt) {
             var randLotto = Randoms.pickUniqueNumbersInRange(1, 45, LOTTO_SIZE).sorted()
             println(randLotto)
         }
+    }
+
+    fun getWinningNumber() {
+        var judgment = Judgment()
+
+        println("\n당첨 번호를 입력해주세요.")
+        var winningLottoNumber = Console.readLine()
+        //judgment.checkWinningNumber(winningLottoNumber)
     }
 }
