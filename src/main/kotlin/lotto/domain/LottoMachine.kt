cf4ee86c2f9623308a2fa2b52941c97911aadec2
Yerin <myheaven_b@naver.com>
2022-11-13 12:45:05 +0900

    refactor: fix error message

--- src/main/kotlin/lotto/domain/LottoMachine.kt
+++ src/main/kotlin/lotto/domain/LottoMachine.kt
@@ -3,7 +3,7 @@ package lotto.domain
 import camp.nextstep.edu.missionutils.Randoms
 
 class LottoMachine (cost: Int) {
-    private val ticketCount = cost % 1000
+    private val ticketCount = cost / 1000
 
     init {
         require(cost >= 1000) { COST_RANGE_EXCEPTION }
@@ -16,8 +16,7 @@ class LottoMachine (cost: Int) {
 
         repeat(ticketCount) {
             lottoTicket = Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6))
-            lottoTicket.sort()
-            lottoTickets.add(lottoTicket)
+            lottoTickets.add(lottoTicket.sort())
         }
 
         println(ticketCount.toString() + TICKET_BUY_MESSAGE)
@@ -25,8 +24,8 @@ class LottoMachine (cost: Int) {
     }
 
     companion object {
-        const val COST_RANGE_EXCEPTION = "구매 불가능한 금액입니다. 1000원 이상부터 구매 가능합니다."
-        const val COST_UNIT_EXCEPTION = "구매 불가능한 금액입니다. 1000원 단위로 구매 가능합니다."
+        const val COST_RANGE_EXCEPTION = "[ERROR] 구매 불가능한 금액입니다. 1000원 이상부터 구매 가능합니다."
+        const val COST_UNIT_EXCEPTION = "[ERROR] 구매 불가능한 금액입니다. 1000원 단위로 구매 가능합니다."
         const val TICKET_BUY_MESSAGE = "개를 구매했습니다."
     }
 }
