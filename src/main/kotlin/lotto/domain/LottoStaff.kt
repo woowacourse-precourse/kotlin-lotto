ffa43eb4b1484b6a69686421e92bd950891c8a84
ChoiWooSuk <wooseokchoi0508@gmail.com>
2022-11-13 17:23:43 +0900

    feat(LottoStaff): 1부터 45까지의 서로 다른 6개의 숫자를 뽑아 오름차순으로 정렬한 로또 생성 기능 구현

--- src/main/kotlin/lotto/domain/LottoStaff.kt
+++ src/main/kotlin/lotto/domain/LottoStaff.kt
@@ -1,12 +1,19 @@
 package lotto.domain
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class LottoStaff() {
 
-    fun countLotto(amount:Int):Int{
-        return amount/ LOTTO_PRICE
+    fun countLotto(amount: Int): Int {
+        return amount / LOTTO_PRICE
     }
 
-    companion object{
-        const val LOTTO_PRICE= 1000
+    fun generateLotto():Lotto{
+        val numbers=Randoms.pickUniqueNumbersInRange(1, 45, 6)
+        return Lotto(numbers.sorted())
+    }
+
+    companion object {
+        const val LOTTO_PRICE = 1000
     }
 }
