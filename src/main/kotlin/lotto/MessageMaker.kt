ef0f4f2628c59ffc2e2b31e40f5ab9cf309d4054
mendel <mendel.urssu@gmail.com>
2022-11-13 16:45:12 +0900

    refactor: MessageMaker 클래스의 메소드 수정
    - purchaseCountMessage() 메소드에서 출력 시 앞에 개행 추가
    - companion object에 출력을
    위한
    상수 값 추가 및 수정
    - companion object에 에러타입을 저장하기 위한 변수 추가
    -
    errorMessage()
    메소드를 companion object로 이동
    - MatchMessage enum클래스의 money타입
    String으로 수정
    -
    Error enum클래스에 에러 경우 추가

--- src/main/kotlin/lotto/MessageMaker.kt
+++ src/main/kotlin/lotto/MessageMaker.kt
@@ -6,7 +6,7 @@ class MessageMaker {
     }
 
     fun purchaseCountMessage(count: Int) {
-        println("$count" + PURCHASE_COUNT_MESSAGE)
+        println("\n$count" + PURCHASE_COUNT_MESSAGE)
     }
 
     fun purchaseLotteryListMessage(lottery: List<Lotto>) {
@@ -45,29 +45,31 @@ class MessageMaker {
         println("$ALL_YIELD ${String.format("%.1f", result)}$IS")
     }
 
-    fun errorMessage(error: Error) {
-        println(error.error)
-    }
-
     companion object {
-        private const val PURCHASE_INPUT_PLEASE_MESSAGE = "구매금액을 입력해 주세요."
+        private const val PURCHASE_INPUT_PLEASE_MESSAGE = "구입금액을 입력해 주세요."
         private const val PURCHASE_COUNT_MESSAGE = "개를 구매했습니다."
-        private const val WINNING_LOTTERY_INPUT_MESSAGE = "당첨 번호를 입력해 주세요."
-        private const val BONUS_INPUT_MESSAGE = "보너스 번호를 입력해 주세요."
-        private const val WINNING_STATISTICS_MESSAGE = "당첨 통계\n---"
+        private const val WINNING_LOTTERY_INPUT_MESSAGE = "\n당첨 번호를 입력해 주세요."
+        private const val BONUS_INPUT_MESSAGE = "\n보너스 번호를 입력해 주세요."
+        private const val WINNING_STATISTICS_MESSAGE = "\n당첨 통계\n---"
         private const val ALL_YIELD = "총 수익률은"
-        private const val IS = "입니다."
+        private const val IS = "%입니다."
+
+        lateinit var errorType: Error
+        fun errorMessage() {
+            println(errorType.error)
+        }
     }
 
-    enum class MatchMessage(val matchMessage: String, val money: Int) {
-        THREE_MATCH("3개 일치", 5000),
-        FOUR_MATCH("4개 일치", 50000),
-        FIVE_MATCH("5개 일치", 1500000),
-        BONUS_MATCH("5개 일치, 보너스 볼 일치", 30000000),
-        SIX_MATCH("6개 일치", 2000000000),
+    enum class MatchMessage(val matchMessage: String, val money: String) {
+        THREE_MATCH("3개 일치", "5,000원"),
+        FOUR_MATCH("4개 일치", "50,000원"),
+        FIVE_MATCH("5개 일치", "1,500,000원"),
+        BONUS_MATCH("5개 일치, 보너스 볼 일치", "30,000,000원"),
+        SIX_MATCH("6개 일치", "2,000,000,000원"),
     }
 
     enum class Error(val error: String) {
+        PURCHASE_IS_NOT_ONE_INTEGER("[ERROR] 구입 금액은 숫자 한개여야 합니다."),
         PURCHASE_MONEY("[ERROR] 구입 금액은 1000원 단위의 숫자여야 합니다."),
         LOTTERY_RANGE("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."),
         WINNING_INPUT_COUNT_ERROR("[ERROR] 로또 당첨 번호를 콤마로 구분해서 1부터 45 사이의 숫자 6개를 입력해주세요."),
