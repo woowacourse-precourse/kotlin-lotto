0cb32d895f35a9cc73db24526a256a35c85b81e6
LHS0731 <gustjq2536@gmail.com>
2022-11-13 14:00:22 +0900

    목표 설정

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -3,3 +3,4 @@ package lotto
 fun main() {
     TODO("프로그램 구현")
 }
+
--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -2,7 +2,7 @@ package lotto
 
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6)
+        require(numbers.size == 6){"[ERROR]"}
     }
 
     // TODO: 추가 기능 구현
