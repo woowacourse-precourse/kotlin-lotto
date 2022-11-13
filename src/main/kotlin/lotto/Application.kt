4e741d10d000492c9d5ba24c505b251b55306066
ghddbwns9808 <ghddbwns9808@gmail.com>
2022-11-13 23:16:34 +0900

    feat: add setLotto, computeLotto, printResultLotto function Add functions that run lotto program

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,51 @@
 package lotto
 
+import lotto.logic.Rank
+import lotto.logic.Revenue
+import lotto.util.ExceptionChecker
+import lotto.util.LottoGenerator
+import lotto.logic.WinLotto
+import lotto.view.LottoView
+import kotlin.properties.Delegates
+
+val lottoIO = LottoView()
+val exceptionChecker = ExceptionChecker()
+val generator = LottoGenerator()
+val win = WinLotto()
+val revenue = Revenue()
+
+var price by Delegates.notNull<Long>()
+
 fun main() {
-    TODO("프로그램 구현")
+    setLotto()
+}
+
+fun setLotto(){
+    price = lottoIO.getPrice()
+    exceptionChecker.priceExceptionCheck(price)
+    val n = price / 1000
+
+    val lottos = ArrayList<List<Int>>()
+    for(i in 0 until n){
+        lottos.add(generator.generate())
+    }
+
+    lottoIO.printLotto(lottos)
+
+    computeLotto(lottos)
+}
+
+fun  computeLotto(lottos: ArrayList<List<Int>>){
+    val winnigNum = lottoIO.getWinningNumber()
+    exceptionChecker.validLottoNumExceptionCheck(winnigNum)
+    val bonusNum = lottoIO.getBonusNumber()
+    exceptionChecker.validBonusNumExceptionCheck(winnigNum, bonusNum)
+    val rankResult = win.getRankResult(lottos, winnigNum, bonusNum)
+
+    printResultLotto(rankResult)
+}
+
+fun printResultLotto(rankResult: MutableMap<Rank, Int>){
+    lottoIO.printRankResult( rankResult)
+    lottoIO.printRevenue(revenue.getRevenue(rankResult,price))
 }
