65b56cb182639e53cf2a98b59eb8a0e5a6252457
sojk401 <sojk401@gmail.com>
2022-11-14 01:46:38 +0900

    refactor(lotto): 당첨 번호를 입력 받아 Int형 리스트로 변환

--- src/main/kotlin/lotto/Seller.kt
+++ src/main/kotlin/lotto/Seller.kt
@@ -7,7 +7,6 @@ val LOTTO_SIZE = 6
 
 class Seller {
     fun generateLotto(LottoCnt: Int) {
-
         println("\n${LottoCnt}개를 구입했습니다.")
         for (cnt in 0 until LottoCnt) {
             var randLotto = Randoms.pickUniqueNumbersInRange(1, 45, LOTTO_SIZE).sorted()
@@ -17,9 +16,13 @@ class Seller {
 
     fun getWinningNumber() {
         var judgment = Judgment()
+        var winningLottoNumber = Console.readLine()
+        var winningNumber = mutableListOf<Int>()
 
         println("\n당첨 번호를 입력해주세요.")
-        var winningLottoNumber = Console.readLine()
-        //judgment.checkWinningNumber(winningLottoNumber)
+        winningLottoNumber.split(",").forEach() {
+            it.toIntOrNull()?.let { it -> winningNumber.add(it) }
+        }
+        println(winningNumber)
     }
 }
