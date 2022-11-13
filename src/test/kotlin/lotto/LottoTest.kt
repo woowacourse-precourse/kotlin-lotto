82cfcca2e5dfa40d87b866398f051314f6f999df
권용민 <briandr@naver.com>
2022-11-13 20:36:10 +0900

    test(Test): Cacher 클래스 기능 테스트
    * 입력값 유효성 검사 테스트
        - 입력값이 비어있는지 검사
        - 입력값이 자연수인지 검사
        - 입력값이 1000원 단위로 나누어 떨어지는지 검사
    
    * 구입한 로또의 개수 계산 테스트

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -1,8 +1,9 @@
 package lotto
 
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
-
 
 class LottoTest {
     @Test
@@ -21,4 +22,40 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+
+    @Nested
+    inner class CacherTest {
+        private val cacher = Cacher()
+
+        @Test
+        fun `금액입력 형식 확인`() {
+            assertThat(cacher.enterPurchaseMoney("")).isEqualTo(123000)
+        }
+
+        @Test
+        fun `금액입력이 숫자가 아닐 경우 예외가 발생한다`() {
+            assertThrows<IllegalArgumentException> {
+                cacher.enterPurchaseMoney("123가나")
+            }
+        }
+
+        @Test
+        fun `금액입력이 자연수가 아닐 경우 예외가 발생한다`() {
+            assertThrows<IllegalArgumentException> {
+                cacher.enterPurchaseMoney("-12345")
+            }
+        }
+
+        @Test
+        fun `금액입력이 천원 단위로 나누어 떨어지지 않을 경우 예외가 발생한다`() {
+            assertThrows<IllegalArgumentException> {
+                cacher.enterPurchaseMoney("12300")
+            }
+        }
+
+        @Test
+        fun `로또의 개수를 계산한다`() {
+            assertThat(cacher.calculateNumberOfLottos(23000)).isEqualTo(23)
+        }
+    }
 }
