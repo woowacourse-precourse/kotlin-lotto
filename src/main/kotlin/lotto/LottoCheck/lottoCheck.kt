3ae0364172bb52c90936320f8dd1953d51668145
lsakee <lsls4868@gmail.com>
2022-11-13 21:06:40 +0900

    feat: 수익률을 계산하는 기능

--- src/main/kotlin/lotto/LottoCheck/lottoCheck.kt
+++ src/main/kotlin/lotto/LottoCheck/lottoCheck.kt
@@ -1,5 +1,7 @@
 package lotto.LottoCheck
 
+import lotto.lottoStore.LottoReward
+
 fun checkWinaLotto(lottoNumber: MutableList<List<Int>>, bonusNumber: Int, winningNumber: String): MutableList<Int> {
     var count: Int
     var lottoWinningAll = mutableListOf(0, 0, 0, 0, 0)
@@ -50,4 +52,16 @@ fun printresult(resultWinning:MutableList<Int>){
     println("5개 일치 (1,500,000원) -"+resultWinning[2]+"개")
     println("5개 일치, 보너스 볼 일치 (30,000,000원) -"+resultWinning[4]+"개")
     println("6개 일치 (2,000,000,000원) -"+resultWinning[3]+"개")
+}
+fun plusMoney(buyLottoMoney: String, checkWinningScore: MutableList<Int>): Double {
+    val buyLottoMoney = buyLottoMoney.toInt() * 1000
+    var getMoney = 0
+    getMoney += checkWinningScore[0] * LottoReward.THREE.value
+    getMoney += checkWinningScore[1] * LottoReward.FOUR.value
+    getMoney += checkWinningScore[2] * LottoReward.FIVE.value
+    getMoney += checkWinningScore[3] * LottoReward.FIVEBONUS.value
+    getMoney += checkWinningScore[4] * LottoReward.SIX.value
+    val yieldUser: Double = getMoney.toDouble() / buyLottoMoney.toDouble()
+    println(yieldUser * 100)
+    return yieldUser
 }
