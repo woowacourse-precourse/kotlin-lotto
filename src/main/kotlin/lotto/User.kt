fccc54b841960c42b17b2f0f5b98ffee1c4819fd
DESKTOP-8PSAE1K\nuyhus <minsh2344@gmail.com>
2022-11-13 20:53:26 +0900

    feat: 로또 구입 금액을 입력받는 기능 구현
    숫자 형식이 아닐 경우 예외 처리
    1000원 단위가 아닐 경우 예외 처리

---
+++ src/main/kotlin/lotto/User.kt
@@ -0,0 +1,30 @@
+package lotto
+
+class User {
+    fun getMoney(): Int {
+        println("구입금액을 입력해 주세요.")
+        val input = readLine()
+
+        checkNumberFormat(input)
+        checkMoneyException(input)
+
+        return input?.toInt() ?: 0
+    }
+
+    private fun checkNumberFormat(input: String?) {
+        try {
+            input?.toInt()
+        } catch (e: NumberFormatException) {
+            throw IllegalArgumentException("[ERROR] 숫자 형식으로 입력해야 합니다.")
+        }
+    }
+
+    private fun checkMoneyException(input: String?) {
+        val inputToInt = input?.toInt()
+        if (inputToInt != null) {
+            require(inputToInt % 1000 == 0 && inputToInt != 0) {
+                "[ERROR] 1000원 단위로 입력해야 합니다."
+            }
+        }
+    }
+}
