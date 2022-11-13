cb7e478c916b4df6dc0bda7b3a296a60d4eb21d9
YuGyeong98 <kuk3097@naver.com>
2022-11-13 21:55:39 +0900

    test: UI 로직 테스트 구현

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -1,5 +1,8 @@
 package lotto
 
+import exception.LottoBonus
+import exception.LottoPurchase
+import exception.LottoWinning
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
@@ -12,7 +15,6 @@ class LottoTest {
         }
     }
 
-    // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
     @Test
     fun `로또 번호에 중복된 숫자가 있으면 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
@@ -20,5 +22,45 @@ class LottoTest {
         }
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @Test
+    fun `구입 금액이 숫자가 아니면 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            LottoPurchase("1000j")
+        }
+    }
+
+    @Test
+    fun `구입 금액이 1,000원으로 나누어 떨어지지 않으면 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            LottoPurchase("8500")
+        }
+    }
+
+    @Test
+    fun `당첨 번호가 1부터 45 사이의 숫자가 아니면 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            LottoWinning("1,2,3,4,46,47")
+        }
+    }
+
+    @Test
+    fun `당첨 번호에 중복된 숫자가 존재하면 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            LottoWinning("1,2,3,4,3,10")
+        }
+    }
+
+    @Test
+    fun `당첨 번호 개수가 6개가 아니면 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            LottoWinning("1,2,3,4,5,6,7")
+        }
+    }
+
+    @Test
+    fun `보너스 번호가 1부터 45 사이의 숫자가 아니면 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            LottoBonus(50)
+        }
+    }
 }
