32d2cf65ab6bdea68d2d55173e11f922e7cc89db
meltnia <themtale@gmail.com>
2022-11-14 02:39:12 +0900

    <fix> 로또 숫자 범위 오류 체크 하도록 변경.

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -4,6 +4,8 @@ class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "[ERROR] 로또 번호는 6자리의 숫자여야 합니다." }
         require(numbers == numbers.distinct()) { "[ERROR] 로또 번호는 중복이 없어야 합니다." }
+        for(number:Int in numbers.indices)
+            LottoErrorCheck.checknumberisinlottorange(numbers[number])
     }
 
     companion object {
