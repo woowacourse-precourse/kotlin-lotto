4c1b0804287fd43c4f7ce61d55aa28566507a040
Yerin <myheaven_b@naver.com>
2022-11-13 12:44:15 +0900

    refactor: change class WinningNumber as data class

--- src/main/kotlin/lotto/domain/WinningNumber.kt
+++ src/main/kotlin/lotto/domain/WinningNumber.kt
@@ -1,29 +1,17 @@
 package lotto.domain
 
-import camp.nextstep.edu.missionutils.Console.readLine
-import lotto.Exceptions
-
-const val REQUEST_WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요."
-const val REQUEST_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요."
-
-class WinningNumber {
-    fun generate(): Pair<List<Int>, Int> {
-        val winningNumbers = getWinningNumbers()
-        val bonusNumber = getBonusNumber(winningNumbers)
-        return Pair(winningNumbers, bonusNumber)
+class WinningNumber(winningNumbers: List<Int>, bonusNumber: Int) {
+    init {
+        require(winningNumbers.size == 6) { LOTTO_SIZE_EXCEPTION }
+        require(winningNumbers.distinct().size == 6) { LOTTO_DUPLICATE_EXCEPTION }
+        require(winningNumbers.all { it in 1..45 }) { LOTTO_RANGE_EXCEPTION }
+        require(bonusNumber !in winningNumbers) { LOTTO_DUPLICATE_EXCEPTION }
+        require(bonusNumber in 1..45 ) { LOTTO_RANGE_EXCEPTION }
     }
 
-    private fun getWinningNumbers(): List<Int> {
-        println(REQUEST_WINNING_NUMBER_MESSAGE)
-        val input = readLine().split(", ")
-        Exceptions().validateWinningNumbers(input)
-        return input.map{ it.toInt() }
-    }
-
-    private fun getBonusNumber(numbers: List<Int>): Int {
-        println(REQUEST_BONUS_NUMBER_MESSAGE)
-        val input = readLine()
-        Exceptions().validateBonusNumber(numbers, input)
-        return input.toInt()
+    companion object{
+        const val LOTTO_SIZE_EXCEPTION = "[ERROR] 로또 번호의 갯수는 6개 입니다."
+        const val LOTTO_DUPLICATE_EXCEPTION = "[ERROR] 로또 번호는 중복이 있으면 안됩니다."
+        const val LOTTO_RANGE_EXCEPTION = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."
     }
 }
