3754fff803f2ac05480ef97af8bd9f42f5bf1e26
qwe910205 <38097088+qwe910205@users.noreply.github.com>
2022-11-13 17:47:17 +0900

    fix(userinterface): 로또 구입 금액이 최대 금액을 초과시 출력하는 에러 메시지 수정
    포맷팅 오류 수정
    로또 구입 금액은 {}원을 초과할 수 없습니다.
    -> 로또 구입 금액은 %s원을 초과할 수 없습니다.

--- src/main/kotlin/lotto/userinterface/UserInterface.kt
+++ src/main/kotlin/lotto/userinterface/UserInterface.kt
@@ -2,11 +2,13 @@ package lotto.userinterface
 
 import camp.nextstep.edu.missionutils.Console
 import java.math.BigInteger
+import java.text.DecimalFormat
 
 
 object UserInterface {
 
     private const val PURCHASE_AMOUNT_LIMIT = 2000000000
+    private val decimalFormat = DecimalFormat("#,###")
 
     fun askPurchaseAmount(): Int {
         val readValue = Console.readLine()
@@ -15,7 +17,7 @@ object UserInterface {
         require(purchaseAmount <= BigInteger(PURCHASE_AMOUNT_LIMIT.toString())) {
             String.format(
                 PURCHASE_AMOUNT_EXCESS_LIMIT,
-                PURCHASE_AMOUNT_LIMIT
+                decimalFormat.format(PURCHASE_AMOUNT_LIMIT)
             )
         }
         return 0
