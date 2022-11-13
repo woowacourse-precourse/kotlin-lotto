30ea33ed8f5d6fd5733f56de7a76f470101523bc
ghddbwns9808 <ghddbwns9808@gmail.com>
2022-11-13 22:53:27 +0900

    feat: add duplicateChecker function Add a function that check duplicated number in lotto numbers

---
+++ src/main/kotlin/lotto/logic/Lotto.kt
@@ -0,0 +1,12 @@
+package lotto.logic
+
+class Lotto(private val numbers: List<Int>) {
+    init {
+        require(numbers.size == 6)
+        require(this.duplicateChecker())
+    }
+
+    private fun duplicateChecker(): Boolean{
+        return this.numbers.distinct() == this.numbers
+    }
+}
