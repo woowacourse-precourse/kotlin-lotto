68c221b785ef08b0defec9a56d07a3feba40353d
Yerin <myheaven_b@naver.com>
2022-11-13 10:53:16 +0900

    refactor: add exception for input

--- src/main/kotlin/lotto/domain/LottoGame.kt
+++ src/main/kotlin/lotto/domain/LottoGame.kt
@@ -1,22 +1,29 @@
 package lotto.domain
 
 import camp.nextstep.edu.missionutils.Console
-import lotto.Exceptions
-
-const val GAME_START_MESSAGE = "구입금액을 입력해 주세요."
 
 class LottoGame {
     fun start() {
         val playerCost = getCost()
         val playerTicket = getLottoTicket(playerCost)
+        val winningNumbers = getWinningNumbers()
+        val result = checkLotto(winningNumbers, playerTicket)
+        printResult(result)
     }
 
     private fun getCost(): Int {
         println(GAME_START_MESSAGE)
-        return Console.readLine().toInt()
+        val playerInput = Console.readLine()
+        try {
+            return playerInput.toInt()
+        } catch (e: NumberFormatException) {
+            throw IllegalArgumentException(INPUT_EXCEPTION)
+        }
     }
 
-    private fun getLottoTicket(cost: Int): List<Lotto> {
-        return LottoGenerator(cost).generate()
+
+    companion object {
+        const val GAME_START_MESSAGE = "구입금액을 입력해 주세요."
+        const val INPUT_EXCEPTION = "잘못된 입력입니다."
     }
 }
