ca692056627f2efa58de0a8826fd6e710b1d515b
권용민 <briandr@naver.com>
2022-11-13 18:43:54 +0900

    feat(Cacher): 구입금액 입력값 유효성 검사     - 입력값이 비어있는지 검사     - 입력값이 자연수인지 검사     - 입력값이 1000원 단위로 나누어 떨어지는지 검사

--- src/main/kotlin/lotto/Cacher.kt
+++ src/main/kotlin/lotto/Cacher.kt
@@ -1,7 +1,40 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Console.readLine
+
+const val MONEY_NOT_NATURAL_NUMBER = "[ERROR] 구입금액은 자연수여야합니다."
+const val UNIT_NOT_THOUSAND = "[ERROR] 구입금액은 1000원 단위여야합니다."
+const val INPUT_IS_EMPTY = "[ERROR] 입력값이 비었습니다."
+
 class Cacher {
     fun enterMoney() {}
 
-    fun calculateNumberOfLottos(): Int {}
+    fun enterPurchaseMoney(inputMoney: String = readLine()): Int {
+        isInputValid(inputMoney)
+        return inputMoney.toInt()
+    }
+
+    private fun isInputValid(inputMoney: String) {
+        isInputEmpty(inputMoney)
+        isNaturalNumber(inputMoney)
+        isThousandUnit(inputMoney)
+    }
+
+    private fun isInputEmpty(inputMoney: String) {
+        if (inputMoney.isNullOrEmpty()) {
+            throw IllegalArgumentException(INPUT_IS_EMPTY)
+        }
+    }
+
+    private fun isNaturalNumber(inputMoney: String) {
+        if (!inputMoney.all { Character.isDigit(it) }) {
+            throw IllegalArgumentException(MONEY_NOT_NATURAL_NUMBER)
+        }
+    }
+
+    private fun isThousandUnit(inputMoney: String) {
+        if (inputMoney.takeLast(3) != "000") {
+            throw IllegalArgumentException(UNIT_NOT_THOUSAND)
+        }
+    }
 }
