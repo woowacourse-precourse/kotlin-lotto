a346c11ece377061787f705358350a18d571a413
tmdgh1592 <tmdgh1592@naver.com>
2022-11-13 11:53:41 +0900

    refactor(exception): modify exception parameters
    - parameter: Lotto -> List<LottoNumber>
    - validateLottoSize
    - validateLottoNumberDuplication

--- src/main/kotlin/lotto/domain/LottoExceptionHandler.kt
+++ src/main/kotlin/lotto/domain/LottoExceptionHandler.kt
@@ -1,6 +1,6 @@
 package lotto.domain
 
-import lotto.*
+import lotto.utils.*
 
 object LottoExceptionHandler {
     fun validateIntType(amount: String) {
@@ -19,11 +19,11 @@ object LottoExceptionHandler {
         if (number !in LOTTO_START_NUMBER..LOTTO_END_NUMBER) throwException(LOTTO_NUMBER_OUT_OF_RANGE_EXCEPTION_MESSAGE)
     }
 
-    fun validateLottoSize(lotto: Lotto) {
+    fun validateLottoSize(lotto: List<LottoNumber>) {
         require(lotto.size == LOTTO_SIZE) { LOTTO_SIZE_EXCEPTION_MESSAGE }
     }
 
-    fun validateLottoNumberDuplication(lotto: Lotto) {
+    fun validateLottoNumberDuplication(lotto: List<LottoNumber>) {
         val notDuplicatedLotto = lotto.distinctBy { lottoNumber ->
             lottoNumber.number
         }
