381667fa5baa156c600011fa83427f49ccb5ce67
krrong <khk9664@naver.com>
2022-11-13 21:31:52 +0900

    feat(ExceptionHandler): 구입금액이 천원으로 나눠지고, 숫자인지 확인

--- src/main/kotlin/lotto/ExceptionHandler.kt
+++ src/main/kotlin/lotto/ExceptionHandler.kt
@@ -23,9 +23,27 @@ object ExceptionHandler {
     }
 
     /**
-     * 1000으로 나누어 떨어지는지 확인
+     * 구입금액에 대한 예외 확인(숫자가 아니거나 1000원으로 나누어지지 않는 경우)
      */
-    fun isDivideBy1000() {
+    fun checkInputMoney(money: String): Boolean {
+        if (isOnlyInt(money)) {
+            if (isDivideBy1000(money.toInt())) {
+                return true
+            }
+        }
+        return false
+    }
 
+    fun isDivideBy1000(money: Int): Boolean {
+        return (money % 1000) == 0
+    }
+
+    fun isOnlyInt(money: String): Boolean {
+        for (i in money) {
+            if (i.digitToIntOrNull() == null) {
+                return false
+            }
+        }
+        return true
     }
 }
