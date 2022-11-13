bfef2c2224465ff9bc51b150269390951ce9af0d
jiingom <vvankatam0@gmail.com>
2022-11-13 13:59:35 +0900

    refactor: 입력받은 구입 금액이 0원인 경우도 예외 발생하도록 추가

--- src/main/kotlin/lotto/view/LottoView.kt
+++ src/main/kotlin/lotto/view/LottoView.kt
@@ -12,7 +12,7 @@ class LottoView {
     }
 
     private fun validMoneyMount(inputMoney: String) {
-        if (!isNumber(inputMoney) || inputMoney.toInt() % PRICE_PER_LOTTO != 0) {
+        if (!isNumber(inputMoney) || inputMoney.toInt() % PRICE_PER_LOTTO != 0 || inputMoney.toInt() == 0) {
             throw IllegalArgumentException("[ERROR]: 1,000원 단위의 숫자로만 입력되어야 합니다.")
         }
     }
