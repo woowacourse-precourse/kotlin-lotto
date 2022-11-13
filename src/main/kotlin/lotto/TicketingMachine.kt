86e3b4aef1ab684cc38df2438ff62ce4c55a3564
권용민 <briandr@naver.com>
2022-11-14 03:43:54 +0900

    refactor(TicketingMachine): 로또 발급 구현
    전달받은 매수만큼의 로또를 발급한다.

--- src/main/kotlin/lotto/TicketingMachine.kt
+++ src/main/kotlin/lotto/TicketingMachine.kt
@@ -1,6 +1,19 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class TicketingMachine {
 
-    fun createLottoNumbers(purchasedLottoCount: Int): List<Lotto> {}
+    fun createLottoNumbers(purchasedLottoCount: Int): List<Lotto> {
+        val autoCreatedLottos = mutableListOf<Lotto>()
+        while (autoCreatedLottos.size < purchasedLottoCount) {
+            autoCreatedLottos.add(createLotto())
+        }
+        return autoCreatedLottos
+    }
+
+    private fun createLotto(): Lotto {
+        val randoms: List<Int> = Randoms.pickUniqueNumbersInRange(1, 45, 6)
+        return Lotto(randoms.sorted())
+    }
 }
