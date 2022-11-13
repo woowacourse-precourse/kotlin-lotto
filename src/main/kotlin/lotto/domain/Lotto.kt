2aaa73e62a11b2061c15cee96ee82abf44721757
qwe910205 <38097088+qwe910205@users.noreply.github.com>
2022-11-13 19:23:41 +0900

    feat(domain): 로또를 생성할 때 주어지는 숫자 중 중복된 숫자가 있으면 예외를 던진다

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -4,6 +4,7 @@ class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6)
         require(numbers.all { it in 1..45 })
+        require(numbers.toSet().size == numbers.size)
     }
 
     fun numbers() = numbers
