6b68f44deb52762b1a76f89c1d63d9436d7334d8
jjiiin <wjdwlin4051@gmail.com>
2022-11-13 22:12:39 +0900

    feat(Lotto): Lotto 클래스에서 비교, get 메소드 구현

---
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -0,0 +1,30 @@
+package lotto.domain
+
+
+class Lotto(private val numbers: List<Int>) {
+    enum class ConstantVariable(val number: Int) {
+        LOTTO_SIZE(6),
+        LOTTO_PRICE(1000)
+    }
+
+    init {
+        require(numbers.size == 6)
+        require(numbers.toSet().size == ConstantVariable.LOTTO_SIZE.number)
+    }
+
+    fun contains(number: Int): Boolean {
+        if (numbers.contains(number)) {
+            return true
+        }
+        return false
+    }
+
+    fun getValues(index: Int): Int {
+        return numbers[index]
+    }
+
+    fun print(): List<Int> {
+        return numbers
+    }
+
+}
