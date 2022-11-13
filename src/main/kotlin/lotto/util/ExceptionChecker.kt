17eacb49fde572c09147ffda7750ae1e34fbe442
ghddbwns9808 <ghddbwns9808@gmail.com>
2022-11-13 23:01:11 +0900

    feat: add validLottoNumExceptionCheck function Add a function that check exception about winning number

--- src/main/kotlin/lotto/util/ExceptionChecker.kt
+++ src/main/kotlin/lotto/util/ExceptionChecker.kt
@@ -5,4 +5,11 @@ class ExceptionChecker {
         if((price % 1000).toInt() != 0)
             throw IllegalArgumentException("[ERROR] 로또는 1000원 단위로 구매 가능합니다.")
     }
+
+    fun validLottoNumExceptionCheck(winningNum: List<Int>){
+        for(i in winningNum.indices){
+            if(winningNum[i] < 1 || winningNum[i] > 45)
+                throw IllegalArgumentException("[ERROR] 로또 번호는 1~45의 정수여야 합니다.")
+        }
+    }
 }
