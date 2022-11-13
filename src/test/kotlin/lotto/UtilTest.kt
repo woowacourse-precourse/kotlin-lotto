eb184e8f0bdfbca59432b816a9c787b93d6ff2ab
agfalcon <agfalcon12@gmail.com>
2022-11-14 00:23:02 +0900

    2022.11.14 UtilTest 작성 완료

--- src/test/kotlin/lotto/UtilTest.kt
+++ src/test/kotlin/lotto/UtilTest.kt
@@ -30,10 +30,27 @@ class UtilTest {
     }
 
     @Test
-    fun isRightNumCountTest(){
+    fun isRightRangeTest(){
         assertThat(isRightNumCOunt("1,2,3,4,5,6")).isEqualTo(true)
         assertThrows<IllegalArgumentException> {
-            isRightNumCOunt("1,2,3,4,5")
+            isRightNumCOunt("1,2,3,4,5,46")
+        }
+    }
+    @Test
+    fun isOverlapTest(){
+        assertThat(isOverlap("1,2,3,4,5,6")).isEqualTo(false)
+        assertThrows<IllegalArgumentException> {
+            isOverlap("1,2,3,4,5,5")
+        }
+    }
+
+    @Test
+    fun checkBonusNumTest(){
+        assertThat(checkBonusNum("1")).isEqualTo(true)
+        assertThrows<IllegalArgumentException> {
+            checkBonusNum("1,2")
+            checkBonusNum("a")
+            checkBonusNum("47")
         }
     }
 }
