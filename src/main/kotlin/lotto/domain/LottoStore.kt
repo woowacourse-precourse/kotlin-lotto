f1a4a24b3c5629e10141078e8522c07f4cf894c7
tmdgh1592 <tmdgh1592@naver.com>
2022-11-13 11:26:35 +0900

    feat(lotto store): add lottery purchase function

---
+++ src/main/kotlin/lotto/domain/LottoStore.kt
@@ -0,0 +1,32 @@
+package lotto.domain
+
+import camp.nextstep.edu.missionutils.Randoms
+import lotto.LOTTO_END_NUMBER
+import lotto.LOTTO_SIZE
+import lotto.LOTTO_START_NUMBER
+import lotto.presentation.LottoConsole.inputPurchaseAmount
+import lotto.presentation.LottoConsole.printPurchaseMessage
+import lotto.presentation.LottoConsole.printPurchaseResult
+
+class LottoStore {
+
+    fun purchase(): List<Lotto> {
+        printPurchaseMessage()
+        val amount: Money = inputPurchaseAmount()
+        val lottos = arrayListOf<Lotto>()
+
+        for (i in 0 until amount.convertLottoCount()) {
+            lottos.add(publish())
+        }
+
+        printPurchaseResult(lottos)
+        return lottos
+    }
+
+    private fun publish(): Lotto {
+        val uniqueNumbers = Randoms.pickUniqueNumbersInRange(LOTTO_START_NUMBER, LOTTO_END_NUMBER, LOTTO_SIZE)
+            .map { number -> LottoNumber(number) }
+
+        return Lotto(uniqueNumbers)
+    }
+}
