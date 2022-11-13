2c8669e32f650d8871599f45f60761a0c3ed5789
idra79haza <idra79haza@github.com>
2022-11-13 19:53:44 +0900

    feat(checkinputAnswer): checkinputAnswer
    checkinputAnswer to check wrong input of answer

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,14 +1,18 @@
 package lotto
 
-import utils.Messages
+import utils.Messages.ERROR_ANSWER_DIFFERENT
+import utils.Messages.ERROR_PREFIX_MESSAGE
 
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6)
     }
     fun checkinputAnswer(): Boolean {
-        if (!numbers.toString().contains(",")) throw IllegalArgumentException(Messages.ERROR_PREFIX_MESSAGE + Messages.ERROR_ANSWER_COMMA)
-        if (numbers.toString().length != 11) throw IllegalArgumentException(Messages.ERROR_PREFIX_MESSAGE + Messages.ERROR_ANSWER_SIX)
+        for (i in numbers){
+            val compareList = numbers - i
+            if (compareList.contains(i))
+                throw IllegalArgumentException(ERROR_PREFIX_MESSAGE+ERROR_ANSWER_DIFFERENT)
+        }
         // 같은 숫자가 있는 지 체크하는 로직 추가되어야
         return true
     }
