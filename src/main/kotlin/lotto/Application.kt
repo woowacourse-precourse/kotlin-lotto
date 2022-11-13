89948b8148295c5d6a72a3c2fc4a2d48054ad23d
JunJangE <wnswkd486@gmail.com>
2022-11-13 16:23:45 +0900

    refactor: ui로직과 비즈니스 로직 분리

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,22 +1,7 @@
 package lotto
 
-import lotto.domain.LottoCheck
-import lotto.domain.LottoShop
-import lotto.domain.LottoWin
-import lotto.presentation.LottoBank
-import lotto.presentation.LottoOrder
+import lotto.presentation.LottoGame
 
 fun main() {
-    val lottoOrder: LottoOrder = LottoOrder()
-    val lottoShop: LottoShop = LottoShop()
-    val lottoWin: LottoWin = LottoWin()
-    val lottoCheck: LottoCheck = LottoCheck()
-    val lottoBank: LottoBank = LottoBank()
-
-    val userOrder = lottoOrder.orderUser()
-    if (userOrder == 0) return
-    val userLottoNumber = lottoShop.buyLotto(userOrder)
-    lottoWin.winLotto()
-    val lottoRank = lottoCheck.compareLotto(userLottoNumber)
-    lottoBank.yieldLotto(lottoRank, userOrder)
+    LottoGame().gameStart()
 }
