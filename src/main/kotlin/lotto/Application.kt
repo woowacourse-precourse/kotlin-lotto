0ee9db227dc6c13ffdd0e2b930aeb2355f5d1df3
dev-worthwhile <dev.lboram@gmail.com>
2022-11-13 22:35:08 +0900

    feat: added error code

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -3,3 +3,9 @@ package lotto
 fun main() {
     TODO("프로그램 구현")
 }
+
+enum class ErrorCode(val message : String){
+    NUMERIC_REQUIRED("숫자만 입력해주세요"),
+    COMMA_REQUIRED("쉼표로 숫자를 구분해주세요"),
+    PRICE_UNQUALIFIED("1000원 단위로 입력해주세요")
+}
