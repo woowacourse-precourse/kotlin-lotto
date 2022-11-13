4b61f5517bb6dd82e9ed6608bdc6847901314357
Lee-Sunho <sunho9889@soongsil.ac.kr>
2022-11-14 02:40:07 +0900

    test: WinningLottoTest 테스트 코드 생성

---
+++ src/test/kotlin/lotto/domain/WinningLottoTest.kt
@@ -0,0 +1,28 @@
+package lotto.domain
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import java.lang.IllegalArgumentException
+
+class WinningLottoTest {
+    @Test
+    fun `당첨 번호 예외처리 - 6개가 아닌 경우`() {
+        var winningLotto = WinningLotto()
+        var numbers = mutableListOf<Int>(1, 2, 3, 4, 5, 6, 7)
+        assertThrows<IllegalArgumentException> { winningLotto.validateWinningNumbers(numbers) }
+    }
+
+    @Test
+    fun `당첨 번호 예외처리 - 중복이 있는 경우`() {
+        var winningLotto = WinningLotto()
+        var numbers = mutableListOf<Int>(1, 2, 3, 4, 5, 5)
+        assertThrows<IllegalArgumentException> { winningLotto.validateWinningNumbers(numbers) }
+    }
+
+    @Test
+    fun `당첨 번호 예외처리 - 범위를 벗어난 경우`() {
+        var winningLotto = WinningLotto()
+        var numbers = mutableListOf<Int>(1, 2, 3, 47, 5, 5)
+        assertThrows<IllegalArgumentException> { winningLotto.validateWinningNumbers(numbers) }
+    }
+}
