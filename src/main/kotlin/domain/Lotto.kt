3f36be7761b3d310cdcb36e0e5c167006cd40ab6
lee989898 <wnsrud1352@gmail.com>
2022-11-13 21:09:39 +0900

    [feat](Lotto.kt) 로또 번호 검증 기능 구현

--- src/main/kotlin/domain/Lotto.kt
+++ src/main/kotlin/domain/Lotto.kt
@@ -2,8 +2,7 @@ package domain
 
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6)
+        require(numbers.size == 6) { "로또 번호는 6개야 합니다." }
+        numbers.forEach { require(it in 1..45) { "로또의 각 번호는 1~45 사이여아 합니다." } }
     }
-
-    // TODO: 추가 기능 구현
 }
