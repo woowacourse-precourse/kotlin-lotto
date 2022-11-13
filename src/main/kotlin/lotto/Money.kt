54c8dd48f997a1fe02d25eaaba4c72a3897d6e69
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2022-11-13 20:32:30 +0900

    refactor[lotto]: 입력한 돈 예외 사항 처리하는 기능 리팩토링

--- src/main/kotlin/lotto/Money.kt
+++ src/main/kotlin/lotto/Money.kt
@@ -6,12 +6,8 @@ class Money {
 
     constructor(money : String) {
 
-        if (!money.matches(Regex("\\d+"))) {
-                println("[ERROR] 돈은 숫자로만 이루어져야 합니다.")
-                throw IllegalArgumentException("[ERROR]")
-        }
-
-        require(money.toInt() % 1000 == 0) {println("[ERROR]")}
+        require(money.matches(Regex("\\d+"))) {println("[ERROR] 돈은 숫자로만 이루어져야 합니다.")}
+        require(money.toInt() % 1000 == 0) {println("[ERROR] 돈은 1000으로 나누어져야합니다.")}
         this.money = money.toInt()
     }
 
