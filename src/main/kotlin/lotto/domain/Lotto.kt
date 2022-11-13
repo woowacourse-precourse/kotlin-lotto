5b9af2856eb7c756ad762ae33f145b0e7d83c02a
qwe910205 <38097088+qwe910205@users.noreply.github.com>
2022-11-13 19:20:57 +0900

    feat(domain): 로또를 생성할 때 주어지는 어떤 숫자가 1에서 45가 아니면 예외를 던진다

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -3,6 +3,7 @@ package lotto.domain
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6)
+        require(numbers.all { it in 1..45 })
     }
 
     fun numbers() = numbers
