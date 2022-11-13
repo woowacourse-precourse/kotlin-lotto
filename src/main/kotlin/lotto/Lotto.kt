f2efe3a246e749b0aebd25f78fe1124aa26109b1
minju kim <62362753+alswn2348@users.noreply.github.com>
2022-11-13 22:10:49 +0900

    feat(Lotto): 발행된 로또 번호와 입력한 당첨번호를 비교할수 있다.
    Lotto 클래스 checkWinning 함수 추가

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,14 +1,33 @@
 package lotto
-
+const val NOTHING = -1
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6)
 
         Exceptions.checkLotto(numbers)
     }
+    enum class WinningGroup(val same: Int, val Winning: Int) {
+        FirstPlace(6, 4),
+        SecondPlace(-1, 3),
+        ThirdPlace(5, 2),
+        FourthPlace(4, 1),
+        FifthPlace(3, 0),
+    }
 
     fun getNumber():String{
         return numbers.toString()
+    }
+
+    fun checkWinning(drawingNumber: Pair<List<String>, String>): Int {
+        val winning = numbers.count {
+            drawingNumber.first
+                .contains(it.toString())
+        }
+        if (winning == 5) {
+            if (drawingNumber.second.contentEquals(numbers.toString()))
+                return WinningGroup.SecondPlace.Winning
+        }
+        return WinningGroup.values().find { it.same==winning }?.Winning ?: return NOTHING
     }
 
 }
