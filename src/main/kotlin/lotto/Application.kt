802824a36b0de80bd789fbe1162479aa6849f44c
ChunBaee <mljung10@naver.com>
2022-11-14 03:34:20 +0900

    feat: 유저가 입력한 금액을 바탕으로 랜덤 수 생성
    유저가 입력한 금액으로 로또가 몇 장 구매 가능한지 판단 후 그 수만큼 6개의 1부터 45 사이의 수를 가지는 Int형 배열 생성

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Console
 import java.util.regex.Pattern
 
 const val userInputMsg = "구입금액을 입력해 주세요."
+const val userBoughtLottoCount = "%d개를 구매했습니다."
 const val LOTTO_EACH_PRICE = 1000
 const val INPUT_ERROR_CODE = 999
 
@@ -15,4 +16,7 @@ fun main() {
 private fun getUserInputPrice() {
     println(userInputMsg)
     userInputPrice = UserInputPrice(Console.readLine()).checkUserInputPriceRegexAndTranslateToInt()
+    if(userInputPrice != INPUT_ERROR_CODE) {
+        val randomLottoNumbers = RandomNumbers(userInputPrice).getRandomLottoNumbers()
+    }
 }
