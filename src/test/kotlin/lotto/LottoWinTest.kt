ecacca34624646a0a2b5bc4b631ff184a8397e0d
JunJangE <wnswkd486@gmail.com>
2022-11-13 16:23:11 +0900

    test: LottoWintTest 수정

--- src/test/kotlin/lotto/LottoWinTest.kt
+++ src/test/kotlin/lotto/LottoWinTest.kt
@@ -1,6 +1,7 @@
 package lotto
 
-import lotto.domain.LottoWin
+import lotto.presentation.util.inputWinningBonusNumberException
+import lotto.presentation.util.inputWinningNumberException
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
@@ -9,14 +10,14 @@ class LottoWinTest {
     @Test
     fun `당첨 번호에 중복된 숫자가 있는 경우 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
-            LottoWin().inputWinningNumberException(listOf("1","2","3","4","5","5"))
+            inputWinningNumberException(listOf("1","2","3","4","5","5"))
         }
     }
 
     @Test
     fun `당첨 번호와 중복되는 보너스 번호를 입력 받은 경우 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
-            LottoWin().inputWinningBonusNumberException(listOf(1,2,3,4,5,6), 4)
+            inputWinningBonusNumberException(listOf(1,2,3,4,5,6), 4)
         }
     }
 }
