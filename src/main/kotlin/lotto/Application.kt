31889f08a33b788ceabd1068d5a80d99cd095161
krrong <khk9664@naver.com>
2022-11-13 21:36:09 +0900

    fix(Application.kt): 로또 구입 금액 입력 시 예외 처리

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -4,12 +4,16 @@ import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
-    println(makeLotto().toString())
 }
 
 fun getMoney(): Int {
     println("구입금액을 입력해 주세요.")
-    return Console.readLine().trim().toInt()
+    val money = Console.readLine().trim()
+
+    if (!ExceptionHandler.checkInputMoney(money)) {
+        throw IllegalArgumentException("getMoney()함수 오류 입력받은 값이 숫자가 아니거나 1000원 단위가 아닙니다.")
+    }
+    return money.toInt()
 }
 
 fun makeLotto(): Lotto {
