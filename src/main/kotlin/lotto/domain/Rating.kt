c31eb5613804074000edc5bf54d2cd23031dcecf
HwangSooHyeon <48857651+HwangSooHyeon@users.noreply.github.com>
2022-11-13 22:33:16 +0900

    Refactor(Rating.kt): add abstract functions
    Rating에 getHit, getPrize, getBonus 추상 메서드를 만들고 구현

--- src/main/kotlin/lotto/domain/Rating.kt
+++ src/main/kotlin/lotto/domain/Rating.kt
@@ -5,10 +5,86 @@ enum class Rating(
     private val prize: Int,
     private val bonus: Boolean
 ) {
-    FIRST(6, 2_000_000_000, false),
-    SECOND(5, 30_000_000, true),
-    THIRD(5, 1_500_000, false),
-    FOURTH(4, 50_000, false),
-    FIFTH(3, 5_000, false),
-    LOSE(0, 0, false);
+    FIRST(6, 2_000_000_000, false) {
+        override fun getHit(): Int {
+            return FIRST.hit
+        }
+
+        override fun getPrize(): Int {
+            return FIRST.prize
+        }
+
+        override fun getBonus(): Boolean {
+            return FIRST.bonus
+        }
+    },
+    SECOND(5, 30_000_000, true) {
+        override fun getHit(): Int {
+            return SECOND.hit
+        }
+
+        override fun getPrize(): Int {
+            return SECOND.prize
+        }
+
+        override fun getBonus(): Boolean {
+            return SECOND.bonus
+        }
+    },
+    THIRD(5, 1_500_000, false) {
+        override fun getHit(): Int {
+            return THIRD.hit
+        }
+
+        override fun getPrize(): Int {
+            return THIRD.prize
+        }
+
+        override fun getBonus(): Boolean {
+            return THIRD.bonus
+        }
+    },
+    FOURTH(4, 50_000, false) {
+        override fun getHit(): Int {
+            return FOURTH.hit
+        }
+
+        override fun getPrize(): Int {
+            return FOURTH.prize
+        }
+
+        override fun getBonus(): Boolean {
+            return FOURTH.bonus
+        }
+    },
+    FIFTH(3, 5_000, false) {
+        override fun getHit(): Int {
+            return FIFTH.hit
+        }
+
+        override fun getPrize(): Int {
+            return FIFTH.prize
+        }
+
+        override fun getBonus(): Boolean {
+            return FIFTH.bonus
+        }
+    },
+    LOSE(0, 0, false) {
+        override fun getHit(): Int {
+            return LOSE.hit
+        }
+
+        override fun getPrize(): Int {
+            return LOSE.prize
+        }
+
+        override fun getBonus(): Boolean {
+            return LOSE.bonus
+        }
+    };
+
+    abstract fun getHit(): Int
+    abstract fun getPrize(): Int
+    abstract fun getBonus(): Boolean
 }
