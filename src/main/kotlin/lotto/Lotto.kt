f886f4cb725b4901d03e9bcabaac0617f45aedfa
Lim-Joosung <kinglim1224@naver.com>
2022-11-13 17:01:13 +0900

    사용자가 입력한 복권 번호의 중복 숫자 검사 및 예외처리 구현

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -2,8 +2,14 @@ package lotto
 
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6)
+        require(numbers.size == 6){
+            println("[ERROR] 숫자 6개를 입력해주십시오")
+            throw IllegalArgumentException()
+        }
+        require(numbers.toSet().size == 6){
+            println("[ERROR] 중복된 숫자가 있습니다.")
+            throw IllegalArgumentException()
+        }
     }
 
-    // TODO: 추가 기능 구현
 }
