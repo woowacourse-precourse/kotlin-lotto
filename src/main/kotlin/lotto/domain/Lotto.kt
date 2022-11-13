f737216eb6fdf9a46d639dbec2cb59dd740cbb61
re4rk <37167652+re4rk@users.noreply.github.com>
2022-11-14 02:19:54 +0900

    fix(lotto): 중복 로또 번호 입력 처리 기능 추가

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -2,7 +2,9 @@ package lotto.domain
 
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6)
+        require(numbers.size == 6) { "[ERROR] 입력은 6개만 있어야합니다." }
+        require(numbers.toSet().size == 6) { "[ERROR] 중복된 숫자가 있으면 안됩니다." }
+        require(numbers.filter { it in 1..45 }.size == 6) { "[ERROR] 로또 번호는 1부터 45까지 사이의 숫자여야 합니다." }
     }
 
     fun toList(): List<Int> = numbers
