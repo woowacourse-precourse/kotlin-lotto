df08d3e5d0e0a53bdb46a3e721534c61a7fddfec
re4rk <37167652+re4rk@users.noreply.github.com>
2022-11-14 01:17:08 +0900

    feat(lotto): 수익률을 계산하는 기능 구현

--- src/main/kotlin/lotto/domain/Purchase.kt
+++ src/main/kotlin/lotto/domain/Purchase.kt
@@ -5,6 +5,12 @@ data class Purchase(val amount: Int?) {
         get() = amount?.div(1000) ?: 0
         private set
 
+    var totalPrize = 0
+    var resultRank = mutableListOf(0, 0, 0, 0, 0, 0)
+    var rateOfReturn: Float = 0.0F
+        get() = (totalPrize * 10000 / (amount ?: 1) / 100.0F)
+        private set
+
     init {
         requireNotNull(amount) { "[ERROR] Required value was null." }
         require(amount % 1000 == 0) { "[ERROR] Required value must be a multiple of 1000" }
