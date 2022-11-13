e0efa8f8be543a004faba1e6a41253cb66ae40df
idra79haza <idra79haza@github.com>
2022-11-13 22:50:21 +0900

    feat(Ranking): Ranking print with enum
    Ranking print with enum with dynamic input changes

---
+++ src/main/kotlin/lotto/Rank.kt
@@ -0,0 +1,40 @@
+package lotto
+import utils.Messages.FIFTH_RANK_MESSAGE
+import utils.Messages.FIRST_RANK_MESSAGE
+import utils.Messages.FOURTH_RANK_MESSAGE
+import utils.Messages.SECOND_RANK_MESSAGE
+import utils.Messages.STATICS_COUNT_MESSAGE
+import utils.Messages.THIRD_RANK_MESSAGE
+
+class Ranking {
+    enum class Rank {
+        FIFTH {
+            override fun print(winList: MutableList<Int>) {
+                println(FIFTH_RANK_MESSAGE+winList[0]+ STATICS_COUNT_MESSAGE)
+            }
+        },
+        FOURTH {
+            override fun print(winList: MutableList<Int>) {
+                println(FOURTH_RANK_MESSAGE)
+            }
+        },
+        THIRD {
+            override fun print(winList: MutableList<Int>) {
+                println(THIRD_RANK_MESSAGE)
+            }
+        },
+        SECOND {
+            override fun print(winList: MutableList<Int>) {
+                println(SECOND_RANK_MESSAGE)
+            }
+        },
+        FIRST {
+            override fun print(winList: MutableList<Int>) {
+                println(FIRST_RANK_MESSAGE)
+            }
+        };
+        open fun print(winList: MutableList<Int>) {
+            println("")
+        }
+    }
+}
