698576d1a972f8bf9deab5f510797acc97be735c
ghddbwns9808 <ghddbwns9808@gmail.com>
2022-11-13 23:10:00 +0900

    feat: add getRevenue function Add a function that calculate revenue and return it

---
+++ src/main/kotlin/lotto/logic/Revenue.kt
@@ -0,0 +1,17 @@
+package lotto.logic
+
+class Revenue {
+
+    fun getRevenue(rankResult: MutableMap<Rank, Int>, price: Long): Float{
+        var totalRevenue = 0
+
+        totalRevenue += 2000000000 * rankResult[Rank.First]!!
+        totalRevenue += 30000000 * rankResult[Rank.Second]!!
+        totalRevenue += 1500000 * rankResult[Rank.Third]!!
+        totalRevenue += 50000 * rankResult[Rank.Fourth]!!
+        totalRevenue += 5000 * rankResult[Rank.Fifth]!!
+
+        return totalRevenue.toFloat() / price.toFloat() * 100
+
+    }
+}
