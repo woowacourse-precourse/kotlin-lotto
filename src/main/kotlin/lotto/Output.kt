95ad3293ecd517a986173c2c33941ae7a8a27247
ldj526 <59276421+ldj526@users.noreply.github.com>
2022-11-13 19:28:49 +0900

    feat: 출력값들 모아둔 Object 생성

---
+++ src/main/kotlin/lotto/Output.kt
@@ -0,0 +1,47 @@
+package lotto
+
+object Output {
+    fun purchaseAmount() {
+        println("구입금액을 입력해 주세요.")
+    }
+
+    fun resultPurchase(count: Int) {
+        println("${count}개를 구매했습니다.")
+    }
+
+    fun winningNumber() {
+        println("당첨 번호를 입력해 주세요.")
+    }
+
+    fun bonusNumber() {
+        println("보너스 번호를 입력해 주세요.")
+    }
+
+    fun winningStatistics() {
+        println("당첨 통계\n---")
+    }
+
+    fun resultYield(number: Double) {
+        println("총 수익률은 ${number}%입니다.")
+    }
+
+    fun resultThree(number: Int) {
+        println("3개 일치 (5,000원) - ${number}개")
+    }
+
+    fun resultFour(number: Int) {
+        println("4개 일치 (50,000원) - ${number}개")
+    }
+
+    fun resultFive(number: Int) {
+        println("5개 일치 (1,500,000원) - ${number}개")
+    }
+
+    fun resultFivePlus(number: Int) {
+        println("5개 일치, 보너스 볼 일치 (30,000,000원) - ${number}개")
+    }
+
+    fun resultSix(number: Int) {
+        println("6개 일치 (2,000,000,000원) - ${number}개")
+    }
+}
