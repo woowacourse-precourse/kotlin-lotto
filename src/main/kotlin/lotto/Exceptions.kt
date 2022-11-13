43a59487cee133c3cefc1a8f24200d34dcfc22c1
Yerin <myheaven_b@naver.com>
2022-11-13 10:33:39 +0900

    refactor: move exception functinos to class Lotto

--- src/main/kotlin/lotto/Exceptions.kt
+++ src/main/kotlin/lotto/Exceptions.kt
@@ -1,12 +1,10 @@
 package lotto
 
-const val ERROR = "[ERROR] "
 const val COST_RANGE_EXCEPTION = "구매 불가능한 금액입니다. 1000원 이상부터 구매 가능합니다."
 const val COST_UNIT_EXCEPTION = "구매 불가능한 금액입니다. 1000원 단위로 구매 가능합니다."
-const val LOTTO_SIZE_EXCEPTION = "로또 번호의 갯수는 6개 입니다."
-const val LOTTO_DUPLICATE_EXCEPTION = "로또 번호는 중복이 있으면 안됩니다."
-const val LOTTO_RANGE_EXCEPTION = "로또 번호는 1부터 45 사이의 숫자여야 합니다."
+
 const val INPUT_EXCEPTION = "잘못된 입력입니다."
+
 class Exceptions {
     fun validateCost(cost: Int) {
         if (cost < 1000) {
@@ -17,17 +15,7 @@ class Exceptions {
         }
     }
 
-    fun validateLottoNumbers(numbers: List<Int>) {
-        if (numbers.size != 6) {
-            throw IllegalArgumentException(ERROR + LOTTO_SIZE_EXCEPTION)
-        }
-        if (numbers.distinct().size != 6) {
-            throw IllegalArgumentException(ERROR + LOTTO_DUPLICATE_EXCEPTION)
-        }
-        if (numbers.any { it !in 1..45 }) {
-            throw IllegalArgumentException(ERROR + LOTTO_RANGE_EXCEPTION)
-        }
-    }
+
 
     fun validateWinningNumbers(input: List<String>) {
         val numbers = listOf<Int>()
--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -1,11 +1,33 @@
 package lotto.domain
 
-import lotto.Exceptions
-
-class Lotto(private val numbers: List<Int>) {
+class Lotto(numbers: List<Int>) {
     init {
-        Exceptions().validateLottoNumbers(numbers)
+        validateLottoSize(numbers)
+        validateLottoDuplicate(numbers)
+        validateLottoRange(numbers)
     }
 
-    // TODO: 추가 기능 구현
+    private fun validateLottoSize (numbers: List<Int>) {
+        if (numbers.size != 6) {
+            throw IllegalArgumentException(LOTTO_SIZE_EXCEPTION)
+        }
+    }
+
+    private fun validateLottoDuplicate (numbers: List<Int>) {
+        if (numbers.distinct().size != 6) {
+            throw IllegalArgumentException(LOTTO_DUPLICATE_EXCEPTION)
+        }
+    }
+
+    private fun validateLottoRange (numbers: List<Int>) {
+        if (numbers.any { it !in 1..45 }) {
+            throw IllegalArgumentException(LOTTO_RANGE_EXCEPTION)
+        }
+    }
+
+    companion object {
+        const val LOTTO_SIZE_EXCEPTION = "[ERROR] 로또 번호의 갯수는 6개 입니다."
+        const val LOTTO_DUPLICATE_EXCEPTION = "[ERROR] 로또 번호는 중복이 있으면 안됩니다."
+        const val LOTTO_RANGE_EXCEPTION = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."
+    }
 }
