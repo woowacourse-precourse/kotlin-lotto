6346b0e35cbb212d80cf9d46d1b52829f979ab3c
DormunDoorlock <skrudgh2001@naver.com>
2022-11-13 23:24:04 +0900

    feat: 1번 로또 구입 금액 예외 처리

---
+++ src/main/kotlin/lotto/domain/LottoGenerator.kt
@@ -0,0 +1,40 @@
+package lotto.domain
+
+class LottoGenerator {
+
+    fun createLottoNumber(priceString:String){
+        val price = checkPriceInt(priceString)
+        checkPriceUnderZero(price)
+        checkPriceThousandsUnit(price)
+
+    }
+    private fun checkPriceInt(price:String): Int {
+        try {
+            return Integer.parseInt(price)
+        } catch (e: NumberFormatException){
+            println("[ERROR] 로또 구입 금액이 정수가 아닙니다.")
+            throw e
+        }
+    }
+    private fun checkPriceUnderZero(price: Int){
+        try {
+            require(price > MIN_PRICE)
+        } catch (e: IllegalArgumentException){
+            println("[ERROR] 로또 구입 금액이 0 이하입니다.")
+            throw e
+        }
+    }
+    private fun checkPriceThousandsUnit(price: Int){
+        try {
+            require(price % UNIT == 0)
+        } catch (e: IllegalArgumentException){
+            println("[ERROR] 로또 구입 금액이 1,000원 단위가 아닙니다.")
+            throw e
+        }
+    }
+
+    companion object {
+        const val UNIT = 1000
+        const val MIN_PRICE = 0
+    }
+}
