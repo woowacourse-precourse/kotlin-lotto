adb4da61661010a2536aaa7968f7af7d2ed93c86
dev-worthwhile <dev.lboram@gmail.com>
2022-11-13 23:32:17 +0900

    feat: randomly pick lotto numbers

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,6 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Randoms
 import camp.nextstep.edu.missionutils.Console as cs
 
 fun main() {
@@ -7,8 +8,20 @@ fun main() {
     val amountPaid = cs.readLine()
 }
 
-enum class ErrorCode(val message : String){
+enum class ErrorCode(val message: String) {
     NUMERIC_REQUIRED("숫자만 입력해주세요"),
     COMMA_REQUIRED("쉼표로 숫자를 구분해주세요"),
     PRICE_UNQUALIFIED("1000원 단위로 입력해주세요")
+}
+
+fun issueLotto(): List<Int> {
+    val lottoNum = mutableListOf<Int>()
+    while (lottoNum.size < 6) {
+        val randomNumber = Randoms.pickNumberInRange(1, 45)
+        if (!lottoNum.contains(randomNumber)) {
+            lottoNum.add(randomNumber)
+        }
+    }
+
+    return lottoNum
 }
