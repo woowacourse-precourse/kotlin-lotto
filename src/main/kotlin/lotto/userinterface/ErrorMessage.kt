6963cdb9ffddbf9fd76026eb628991245664964d
qwe910205 <38097088+qwe910205@users.noreply.github.com>
2022-11-13 17:03:25 +0900

    feat: 로또 구입 금액을 입력받을 때 숫자가 아닌 값을 입력하면 예외를 던짐

---
+++ src/main/kotlin/lotto/userinterface/ErrorMessage.kt
@@ -0,0 +1,4 @@
+package lotto.userinterface
+
+private const val HEADER = "[ERROR] "
+const val PURCHASE_AMOUNT_NOT_DIGIT = HEADER + "로또 구입 금액은 1000으로 숫자여야 합니다."
--- src/main/kotlin/lotto/userinterface/UserInterface.kt
+++ src/main/kotlin/lotto/userinterface/UserInterface.kt
@@ -1,8 +1,13 @@
 package lotto.userinterface
 
+import camp.nextstep.edu.missionutils.Console
+
+
 object UserInterface {
 
     fun askPurchaseAmount(): Int {
+        val readValue = Console.readLine()
+        require(readValue.all { it.isDigit() }) { PURCHASE_AMOUNT_NOT_DIGIT }
         return 0
     }
 }
