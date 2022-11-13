2f8843a95410643f9afb8f40240a59a267c52d12
권용민 <briandr@naver.com>
2022-11-14 04:34:46 +0900

    test(LottoMachine): 당첨번호 및 보너스 번호 발급 구현
     - 입력된 당첨번호 및 보너스 번호가 자연수인지 확인
     - 입력된 당첨번호 및 보너스 번호가 1~45인지 확인
     - 입력된 당첨번호가 6자리인지 확인

--- src/main/kotlin/lotto/LottoMachine.kt
+++ src/main/kotlin/lotto/LottoMachine.kt
@@ -3,6 +3,7 @@ package lotto
 import camp.nextstep.edu.missionutils.Console.readLine
 
 const val INVALID_NUMBER_RANGE = "입력된 숫자가 1에서 45사이가 아닙니다."
+const val INVALID_NUMBER_OF_WINNING_NUMBERS = "당첨 번호는 6개여야합니다."
 
 class LottoMachine {
     private val _winningNumbers = mutableListOf<Int>()
@@ -11,7 +12,40 @@ class LottoMachine {
     private var _bonusNumber: Int = 0
     val bonusNumber: Int get() = _bonusNumber
 
-    fun enterWinningNumber(input: String = readLine()) {}
+    fun publishWinningNumber(input: String = readLine()) {
+        val numbers = input.split(",").map { number ->
+            checkInputValid(number)
+            number.toInt()
+        }
+        checkNumberOfWinningNumbers(numbers)
+        _winningNumbers.addAll(numbers)
+    }
 
-    fun enterBonusNumber(number: String = readLine()) {}
+    fun publishBonusNumber(number: String = readLine()) {
+        checkInputValid(number)
+        _bonusNumber = number.toInt()
+    }
+
+    private fun checkNumberOfWinningNumbers(numbers: List<Int>) {
+        if (numbers.size != 6) {
+            throw IllegalArgumentException(INVALID_NUMBER_OF_WINNING_NUMBERS)
+        }
+    }
+
+    private fun checkInputValid(input: String) {
+        isNumber(input)
+        isNumberInRange(input.toInt())
+    }
+
+    private fun isNumber(number: String) {
+        if (!number.all { Character.isDigit(it) }) {
+            throw IllegalArgumentException(MONEY_NOT_NATURAL_NUMBER)
+        }
+    }
+
+    private fun isNumberInRange(number: Int) {
+        if (number !in 1..45) {
+            throw IllegalArgumentException(INVALID_NUMBER_RANGE)
+        }
+    }
 }
