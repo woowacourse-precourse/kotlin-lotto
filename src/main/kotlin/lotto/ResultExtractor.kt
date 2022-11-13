491dd13973403063f663d4635f2cdd0c23f42f4f
boogieWoogie <kimjinuk1999@gmail.com>
2022-11-13 22:54:14 +0900

    feat(ResultExtractor) : 결과에 따른 출력을 담당하는 기능 구현

--- src/main/kotlin/lotto/ResultExtractor.kt
+++ src/main/kotlin/lotto/ResultExtractor.kt
@@ -1,5 +1,9 @@
 package lotto
 
+import java.awt.font.FontRenderContext
+import java.text.DecimalFormat
+import kotlin.math.round
+
 object ResultExtractor {
     fun extractResult(
         generatedLotto: List<Lotto>,
@@ -18,5 +22,22 @@ object ResultExtractor {
             }
         }
         return result.toMap()
+    }
+
+    fun showResult(extractedResult: Map<LottoStatus, Int>) {
+        LottoStatus.values().forEach { eachLottoStatus ->
+            var eachStatusCount = 0
+            extractedResult[eachLottoStatus]?.let { eachStatusCount = it }
+
+            val msg = when (eachLottoStatus) {
+                LottoStatus.FIFTH_PLACE -> "3개 일치 (5,000원) - %d개".format(eachStatusCount)
+                LottoStatus.FOURTH_PLACE -> "4개 일치 (50,000원) - %d개".format(eachStatusCount)
+                LottoStatus.THIRD_PLACE -> "5개 일치 (1,500,000원) - %d개".format(eachStatusCount)
+                LottoStatus.SECOND_PLACE -> "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개".format(eachStatusCount)
+                LottoStatus.FIRST_PLACE -> "6개 일치 (2,000,000,000원) - %d개".format(eachStatusCount)
+                else -> return@forEach
+            }
+            println(msg)
+        }
     }
 }
