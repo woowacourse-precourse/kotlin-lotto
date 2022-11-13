20472ef958645cc65d96a38be7e06ae93ca4c225
idra79haza <idra79haza@github.com>
2022-11-13 16:56:17 +0900

    feat(checkRightLotto): checkRightLotto
    checkRightLotto to check error

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,9 +1,11 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange
+
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6)
     }
 
-    // TODO: 추가 기능 구현
+    val pickLottoNums = pickUniqueNumbersInRange(1, 45, 6)
 }
