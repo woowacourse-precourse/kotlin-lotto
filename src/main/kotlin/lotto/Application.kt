6c24d2d3033b7b65cc19028b7008341fe0083c1d
LHS0731 <gustjq2536@gmail.com>
2022-11-13 14:18:34 +0900

    구입금액별 랜덤로또 생성하는 함수 구현

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,6 +1,26 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
-    TODO("프로그램 구현")
+
+    println("구입금액을 입력해 주세요.")
+    var moneyInput = Console.readLine()
+
+    var lottoCount = moneyInput!!.toInt()/1000
+    println("${lottoCount}개를 구매했습니다.")
+
+    var randomlotto = MakeRandomLotto(lottoCount)
+    println(randomlotto)
 }
 
+fun MakeRandomLotto(count:Int): MutableList<List<Int>> {
+    var lottolist = mutableListOf<List<Int>>()
+    for (i in 1..count) {
+        val numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6).sorted()
+        lottolist.add(numbers)
+        println(numbers)
+    }
+    return lottolist
+}
