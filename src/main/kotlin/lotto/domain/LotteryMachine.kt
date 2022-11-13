b0ed0226134d0a4146bb6101bb5cea5d45a34113
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2022-11-13 21:43:41 +0900

    feat(Lotto) : 로또 랜덤값 생성 로직 예외상황 추가해서 다시 구현

--- src/main/kotlin/lotto/domain/LotteryMachine.kt
+++ src/main/kotlin/lotto/domain/LotteryMachine.kt
@@ -8,9 +8,18 @@ class LotteryMachine {
     fun operate(lottoCount: Long): List<Lotto> {
         val lottoTicket = mutableListOf<Lotto>()
         while (lottoTicket.size < lottoCount) {
-            lottoTicket.add(Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6).sorted()))
+            lottoTicket.add(Lotto(createRandomLotto()))
         }
 
         return lottoTicket
+    }
+
+    private fun createRandomLotto(): List<Int> {
+        var lotto = mutableSetOf<Int>()
+        while (lotto.size != 6) {
+            lotto = Randoms.pickUniqueNumbersInRange(1, 45, 6).toSet() as MutableSet<Int>
+        }
+
+        return lotto.toList().sorted()
     }
 }
--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -3,7 +3,6 @@ package lotto
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
-
 class LottoTest {
     @Test
     fun `로또 번호의 개수가 6개가 넘어가면 예외가 발생한다`() {
@@ -19,6 +18,4 @@ class LottoTest {
             Lotto(listOf(1, 2, 3, 4, 5, 5))
         }
     }
-
-    // 아래에 추가 테스트 작성 가능
 }
