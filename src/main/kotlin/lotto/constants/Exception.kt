b1a90387dc3cbd2a1cfc7b747804ae85d2e59b8e
Lee-Sunho <sunho9889@soongsil.ac.kr>
2022-11-13 19:03:21 +0900

    feat(Lotto): checkDuplication 메소드 구현

--- src/main/kotlin/lotto/constants/Exception.kt
+++ src/main/kotlin/lotto/constants/Exception.kt
@@ -1,7 +1,8 @@
 package lotto.constants
 
 class Exception {
-    companion object{
+    companion object {
         const val EXCEPTION_INVALID_MONEY = "[ERROR] 1,000원 단위로 나누어 떨어지는 금액을 입력해주세요."
+        const val EXCEPTION_DUPLICATED_NUMBER = "[ERROR] 중복된 숫자가 있습니다."
     }
 }
--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -1,8 +1,17 @@
 package lotto.domain
 
+import lotto.constants.Exception
+
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6)
+        checkDuplication(numbers)
+    }
+
+    private fun checkDuplication(numbers: List<Int>) {
+        if (numbers.distinct().size != numbers.size) {
+            throw IllegalArgumentException(Exception.EXCEPTION_DUPLICATED_NUMBER)
+        }
     }
 
     fun getNumbers(): List<Int> {
