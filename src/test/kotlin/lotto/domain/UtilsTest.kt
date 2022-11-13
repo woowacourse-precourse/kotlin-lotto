655560724b692d18319c49831393e380f87bf606
re4rk <37167652+re4rk@users.noreply.github.com>
2022-11-13 21:06:37 +0900

    test(lotto): 올바르지 않은 문자가 들어오면 예외처리 하는지 테스트 케이스 추가

---
+++ src/test/kotlin/lotto/domain/UtilsTest.kt
@@ -0,0 +1,30 @@
+package lotto.domain
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class UtilsTest {
+    @Test
+    fun `splitToIntList 기능 테스트`() {
+        assertThat("1,2,3,4,5".splitToIntList() == listOf(1, 2, 3, 4, 5))
+        assertThat("1".splitToIntList() == listOf(1))
+    }
+
+    @Test
+    fun `splitToIntList separator 이외에 숫자가 없으면 에러를 발생`() {
+        assertThrows<IllegalArgumentException> {
+            "1,2,3,4,a".splitToIntList()
+        }
+    }
+
+    @Test
+    fun `checkNull 기능 테스트`() {
+        assertThat(0.checkNull())
+    }
+
+    @Test
+    fun `checkNull 에러 테스트`() {
+        assertThrows<IllegalArgumentException> { null.checkNull() }
+    }
+}
