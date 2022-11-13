916fa746fc692db958265266b7f0005a80afdd0b
ghddbwns9808 <ghddbwns9808@gmail.com>
2022-11-13 23:14:56 +0900

    feat: add printRankResult function Add a function that print RankResult

--- src/main/kotlin/lotto/view/LottoView.kt
+++ src/main/kotlin/lotto/view/LottoView.kt
@@ -24,4 +24,13 @@ class LottoView {
         println("\n보너스 번호를 입력해 주세요.")
         return readLine()!!.toInt()
     }
+
+    fun printRankResult(rankResult: MutableMap<Rank, Int>){
+        println("\n당첨 통계\n" + "---")
+        println("3개 일치 (5,000원) - ${rankResult[Rank.Fifth]}개\n" +
+                "4개 일치 (50,000원) - ${rankResult[Rank.Fourth]}개\n" +
+                "5개 일치 (1,500,000원) - ${rankResult[Rank.Third]}개\n" +
+                "5개 일치, 보너스 볼 일치 (30,000,000원) - ${rankResult[Rank.Second]}개\n" +
+                "6개 일치 (2,000,000,000원) - ${rankResult[Rank.First]}개")
+    }
 }
