1f81b75bc0624fb44f5d16da934b960787908bf7
krrong <khk9664@naver.com>
2022-11-13 20:46:57 +0900

    feat(Application.kt): 로또 생성 기능 추가
    로또 번호를 오름차순으로 정렬하여 생성 후 Lotto 객체 반환

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,13 +1,18 @@
 package lotto
 
 import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
-    val money = getMoney()
-    println(money)
+    println(getLotto().toString())
 }
 
-fun getMoney(): Int{
+fun getMoney(): Int {
     println("구입금액을 입력해 주세요.")
     return Console.readLine().trim().toInt()
+}
+
+fun getLotto(): Lotto {
+    val numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
+    return Lotto(numbers.sorted())
 }
