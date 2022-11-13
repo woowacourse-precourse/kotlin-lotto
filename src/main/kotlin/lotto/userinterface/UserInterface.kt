444bbec0569e1fdb58da195a9771b7d66499424f
qwe910205 <38097088+qwe910205@users.noreply.github.com>
2022-11-13 17:42:50 +0900

    feat(userinterface): 로또 구입 금액이 20억을 초과하면 예외를 던진다

--- src/main/kotlin/lotto/userinterface/UserInterface.kt
+++ src/main/kotlin/lotto/userinterface/UserInterface.kt
@@ -1,13 +1,23 @@
 package lotto.userinterface
 
 import camp.nextstep.edu.missionutils.Console
+import java.math.BigInteger
 
 
 object UserInterface {
 
+    private const val PURCHASE_AMOUNT_LIMIT = 2000000000
+
     fun askPurchaseAmount(): Int {
         val readValue = Console.readLine()
         require(readValue.all { it.isDigit() }) { PURCHASE_AMOUNT_NOT_DIGIT }
+        val purchaseAmount = BigInteger(readValue)
+        require(purchaseAmount <= BigInteger(PURCHASE_AMOUNT_LIMIT.toString())) {
+            String.format(
+                PURCHASE_AMOUNT_EXCESS_LIMIT,
+                PURCHASE_AMOUNT_LIMIT
+            )
+        }
         return 0
     }
 }
