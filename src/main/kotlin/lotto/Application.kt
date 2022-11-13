6183a30f1db84796d4ea9ef366c1108ee1ac9139
vrexpert <vrexpert2302@gmail.com>
2022-11-14 00:37:08 +0900

    feat(lotto): 발행한 모든 로또의 당첨 등수의 개수 countRankings 구현
    CountRankings.countRankings() : 당첨 번호, 보너스 번호, 발행한 로또를 받아서 각 로또의 등수를 해시 맵에 저장 후 리턴
    Lotto : numbers 에 접근하지 못하기 때문에 값을 받아오는 getNumbers 함수 구현

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,17 @@
 package lotto
 
+import lotto.domain.CountRankings
+import lotto.domain.RandomGenerator
+
 fun main() {
-    TODO("프로그램 구현")
+    val lottos = mutableListOf<Lotto>()
+    val count = 4
+    val winningNumber = listOf(1,2,3,4,5,6)
+    val bonusNumber = 7
+    for (index in 0..3){
+        val lotto = Lotto(RandomGenerator().createRandomNumbers())
+        lottos.add(lotto)
+        println(lotto.getNumbers())
+    }
+    println(CountRankings().countRankings(winningNumber,bonusNumber,lottos))
 }
