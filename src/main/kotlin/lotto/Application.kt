034dd6805a429cefeccc24a52c1a6de1a0506f65
yeonsu10 <94501462+yeonsu10@users.noreply.github.com>
2022-11-13 23:44:53 +0900

    feat: 로또 구매 금액 입력 함수, 메세지 출력 클래스 생성

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,24 @@
 package lotto
 
 fun main() {
-    TODO("프로그램 구현")
+    val price = inputPrice()
+}
+
+
+enum class Message(){
+    PRICE, COUNT, NUMBER, BONUS,
+}
+
+fun getMessage(message: Message) {
+    when (message) {
+        Message.PRICE -> println("구입금액을 입력해 주세요.")
+        Message.COUNT -> println("개를 구매했습니다.")
+        Message.NUMBER -> println("당첨 번호를 입력해 주세요.")
+        Message.BONUS -> println("보너스 번호를 입력해 주세요.")
+    }
+}
+
+fun inputPrice(): Int {
+    getMessage(Message.PRICE)
+    return readLine()!!.toInt()
 }
