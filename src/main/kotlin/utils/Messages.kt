35efd103004e84187e6e2a8c2248b9df8da30b02
idra79haza <idra79haza@github.com>
2022-11-14 01:04:41 +0900

    refactor(Message): change messages
    change messages

--- src/main/kotlin/utils/Messages.kt
+++ src/main/kotlin/utils/Messages.kt
@@ -1,23 +1,5 @@
 package utils
 
-//class Messages {
-//    companion object {
-//        const val BUY_PRICE_INQUIRE_MESSAGE = "구입금액을 입력해 주세요."
-//        const val BUY_AMOUNT_MESSAGE = "개를 구매했습니다."
-//        const val ANSWER_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요."
-//        const val BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요."
-//        const val STATICS_TITLE_MESSAGE = "당첨 통계"
-//        const val STATICS_SPLITTER_MESSAGE = "---"
-//        const val FIFTH_RANK_MESSAGE = "3개 일치 (5,000원) - ${.2f}"
-//        const val FOURTH_RANK_MESSAGE = "4개 일치 (50,000원) - "
-//        const val THIRD_RANK_MESSAGE = "5개 일치 (1,500,000원) - "
-//        const val SECOND_RANK_MESSAGE = "5개 일치, 보너스 볼 일치 (30,000,000원) - "
-//        const val FIRST_RANK_MESSAGE = "6개 일치 (2,000,000,000원) - "
-//        const val STATICS_COUNT_MESSAGE = "개"
-//        const val FIRST_RANK_MESSAGE = "6개 일치 (2,000,000,000원) - "
-//    }
-//}
-
 object Messages {
     // 입력을 받기 위한 메세지
     const val BUY_PRICE_INQUIRE_MESSAGE = "구입금액을 입력해 주세요."
@@ -28,23 +10,23 @@ object Messages {
     // 통계를 위한 메세지
     const val STATICS_TITLE_MESSAGE = "당첨 통계"
     const val STATICS_SPLITTER_MESSAGE = "---"
-//    const val FIFTH_RANK_MESSAGE = "3개 일치 (5,000원) - %d개"
     const val FIFTH_RANK_MESSAGE = "3개 일치 (5,000원) - "
     const val FOURTH_RANK_MESSAGE = "4개 일치 (50,000원) - "
     const val THIRD_RANK_MESSAGE = "5개 일치 (1,500,000원) - "
     const val SECOND_RANK_MESSAGE = "5개 일치, 보너스 볼 일치 (30,000,000원) - "
     const val FIRST_RANK_MESSAGE = "6개 일치 (2,000,000,000원) - "
     const val STATICS_COUNT_MESSAGE = "개"
-    const val STATICS_REVENUE_MESSAGE = "총 수익률은 ${.1f}%입니다."
+    const val STATICS_REVENUE_PREFIX_MESSAGE = "총 수익률은 "
+    const val STATICS_REVENUE_SUFFIX_MESSAGE = "%입니다."
 
     // 에러 표기를 위한 메세지
-    const val ERROR_PREFIX_MESSAGE = "[ERROR] "
-    const val ERROR_LOTTO_INPUT = "맞는 로또 금액을 제시해 주세요."
-    const val ERROR_ANSWER_COMMA = "콤마를 넣어서 정답을 입력해 주세요"
-    const val ERROR_ANSWER_SIX = "숫자 6개를 입력해주세요"
-    const val ERROR_ANSWER_DIFFERENT = "서로 다른 숫자 6개를 입력해주세요"
-    const val ERROR_BONUS_ONE = "숫자 1개를 입력해주세요"
-    const val ERROR_BONUS_DIFFERENT = "정답 숫자 6개와 다른 숫자를 입력해주세요"
+    const val ERROR_PREFIX_MESSAGE = "[ERROR]"
+    const val ERROR_LOTTO_INPUT = " 맞는 로또 금액을 제시해 주세요."
+    const val ERROR_ANSWER_COMMA = " 콤마를 넣어서 정답을 입력해 주세요"
+    const val ERROR_ANSWER_SIX = " 숫자 6개를 입력해주세요"
+    const val ERROR_ANSWER_DIFFERENT = " 서로 다른 숫자 6개를 입력해주세요"
+    const val ERROR_BONUS_ONE = " 숫자 1개를 입력해주세요"
+    const val ERROR_BONUS_DIFFERENT = " 정답 숫자 6개와 다른 숫자를 입력해주세요"
 
     // 숫자 벨류
     const val FROM_INDEX = 0
