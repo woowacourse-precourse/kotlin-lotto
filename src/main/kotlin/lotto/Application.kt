37142c041855c2715f8b87d205089f969f2a190b
DormunDoorlock <skrudgh2001@naver.com>
2022-11-13 23:29:09 +0900

    feat: 1번 로또 구입 금액 예외 처리

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,15 @@
 package lotto
 
+import lotto.domain.Lotto
+import lotto.domain.LottoGenerator
+import camp.nextstep.edu.missionutils.Console
+import java.lang.IllegalArgumentException
+
 fun main() {
-    TODO("프로그램 구현")
+    try {
+        val lottoPrice = Console.readLine()
+        val lottoGenerator = LottoGenerator()
+        lottoGenerator.createLottoNumber(lottoPrice)
+    }catch(e:IllegalArgumentException){
+    }
 }
