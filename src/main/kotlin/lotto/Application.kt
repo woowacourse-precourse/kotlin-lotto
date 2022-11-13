c3019d56b57f21a17dabd512a192d6e0bc6d831f
Lim-Joosung <kinglim1224@naver.com>
2022-11-13 14:35:38 +0900

    구입금액 입력 구현 및 입력 예외 처리 구현

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,31 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    TODO("프로그램 구현")
+    println("구입금액을 입력해주세요")
+    var lottoPay = Console.readLine()
+    var lottoTicket : Int
+    if(chkNum(lottoPay)){
+        print("[ERROR] 숫자만 입력해주세요")
+        throw NoSuchElementException()
+    }
+    if(lottoPay.toInt()%1000 != 0){
+        println("[ERROR] 금액을 1000원 단위로 입력해주세요")
+        throw IllegalArgumentException()
+    }
+}
+fun chkNum(str: String) : Boolean {
+    var temp: Char
+
+    var result = false
+
+    for (i in 0 until str.length) {
+        temp = str.elementAt(i)
+        if (temp.toInt() < 48 || temp.toInt() > 57) {
+            result = true
+        }
+    }
+
+    return result
 }
