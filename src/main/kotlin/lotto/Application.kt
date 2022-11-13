c2d336d42ff3be76ef2818540569d23008c9bfcf
Jaehun.Kim <rlawogns0411@naver.com>
2022-11-13 16:11:41 +0900

    feat : 구입금액 입력 및 체크

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,17 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
     TODO("프로그램 구현")
+    var inputMoney = Console.readLine()
+}
+fun inputMoney(): Int? {
+    print("구입금앱을 입력해 주세요.")
+    return Console.readLine().toInt()
+}
+fun validInputMoney(inputMoney : Int){
+    if(inputMoney%1000 != 0)
+        throw IllegalArgumentException("[ERROR] 구입금액은 1000원 단위입니다.")
 }
