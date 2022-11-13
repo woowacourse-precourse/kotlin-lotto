b4078ededa064c226dd9f90d6df08fdcf52b0b7d
Lee-Sunho <sunho9889@soongsil.ac.kr>
2022-11-13 18:32:51 +0900

    test(PurchaseTest): 로또 출력 테스트

--- src/test/kotlin/lotto/domain/PurchaseTest.kt
+++ src/test/kotlin/lotto/domain/PurchaseTest.kt
@@ -1,16 +1,35 @@
 package lotto.domain
 
+import camp.nextstep.edu.missionutils.test.Assertions.*
+import lotto.views.OutputView
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.assertj.core.api.Assertions.assertThat
 import java.lang.IllegalArgumentException
 
-class PurchaseTest{
+class PurchaseTest {
     @Test
     fun `구입 금액이 1000원 단위로 나누어떨어지지 않을 시 예외 처리`() {
         assertThrows<IllegalArgumentException> {
             Purchase().validateMoney(5500)
         }
     }
+
+    @Test
+    fun `로또 출력 테스트`() {
+        val lotto1 = listOf<Int>(8, 21, 23, 41, 42, 43)
+        val lotto2 = listOf<Int>(3, 5, 11, 16, 32, 38)
+        val lotto3 = listOf<Int>(7, 11, 16, 35, 36, 44)
+        val lotto4 = listOf<Int>(1, 8, 11, 31, 41, 42)
+
+        val lottos = LottoWrapper()
+        lottos.add(Lotto(lotto1))
+        lottos.add(Lotto(lotto2))
+        lottos.add(Lotto(lotto3))
+        lottos.add(Lotto(lotto4))
+
+        OutputView.printLottos(lottos)
+    }
+
 
 }
