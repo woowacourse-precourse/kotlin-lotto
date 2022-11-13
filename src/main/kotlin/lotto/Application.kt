5f51272259d6a7dc4356e4331ec9ce591ca1faa1
mendel <mendel.urssu@gmail.com>
2022-11-13 16:50:19 +0900

    refactor: main 메소드 수정
    - controller 객체를 사용해서 로또 진행
    - 에러 발생 시 메시지 출력 처리

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,10 @@
 package lotto
 
 fun main() {
-    TODO("프로그램 구현")
+    try {
+        val controller = Controller()
+        controller.run()
+    } catch (e: Exception) {
+        MessageMaker.errorMessage()
+    }
 }
