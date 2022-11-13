43eec523d8276539433289ea45351bd80372243e
Yerin <myheaven_b@naver.com>
2022-11-13 16:12:58 +0900

    refactor: add functions for enum Reward

--- src/main/kotlin/lotto/domain/Reward.kt
+++ src/main/kotlin/lotto/domain/Reward.kt
@@ -1,9 +1,45 @@
 package lotto.domain
 
-enum class Reward(val same: String, val reward: String) {
-    FIRST("6", "2,000,000,000"),
-    SECOND("5","30,000,000"),
-    THIRD("5", "1,500,000"),
-    FORTH("4", "50,000"),
-    FIFTH("3", "5,000"),
+enum class Reward(val same: Int, val bonus: Boolean, val money: String) {
+    FIRST(6, false, "2,000,000,000"),
+    SECOND(5, true, "30,000,000"),
+    THIRD(5, false, "1,500,000"),
+    FORTH(4, false, "50,000"),
+    FIFTH(3, false, "5,000");
+
+    companion object {
+        private const val INPUT_EXCEPTION = "잘못된 입력입니다. 순위를 입력해주세요"
+        fun getRank(cnt: Int, bonus: Boolean): String {
+            return when {
+                cnt == FIRST.same -> "FIRST"
+                cnt == SECOND.same && SECOND.bonus -> "SECOND"
+                cnt == THIRD.same -> "THIRD"
+                cnt == FORTH.same -> "FORTH"
+                cnt == FIFTH.same -> "FIFTH"
+                else -> "NOTHING"
+            }
+        }
+
+        fun getPrizeMoney(rank: String): String {
+            return when (rank) {
+                "FIRST" -> FIRST.money
+                "SECOND" -> SECOND.money
+                "THIRD" -> THIRD.money
+                "FORTH" -> FORTH.money
+                "FIFTH" -> FIFTH.money
+                else -> throw IllegalArgumentException(INPUT_EXCEPTION)
+            }
+        }
+
+        fun getSameCount(rank: String): Int {
+            return when (rank) {
+                "FIRST" -> FIRST.same
+                "SECOND" -> SECOND.same
+                "THIRD" -> THIRD.same
+                "FORTH" -> FORTH.same
+                "FIFTH" -> FIFTH.same
+                else -> throw IllegalArgumentException(INPUT_EXCEPTION)
+            }
+        }
+    }
 }
