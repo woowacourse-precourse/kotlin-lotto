ade3266f12787cec4efeb04d977fb691e11176bd
re4rk <37167652+re4rk@users.noreply.github.com>
2022-11-13 20:01:57 +0900

    refactor(lotto): inputLottoNumers에서 inputLottoWinningNumbers로 변경

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -7,7 +7,7 @@ fun inputPurchaseAmount(): Int? {
     return inputInt()
 }
 
-fun inputLottoNumbers(): List<Int> {
+fun inputLottoWinningNumbers(): List<Int> {
     println("당첨 번호를 입력해 주세요.")
     return input().split(',').map { it.toInt() }
 }
