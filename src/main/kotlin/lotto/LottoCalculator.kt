3dd2cb335efba1f373c3593fdecd402a2f688539
rhkrwngud445 <rhkrwngud445@gmail.com>
2022-11-14 15:56:00 +0900

    feat(LottoCalcultaor) : put each winning

--- src/main/kotlin/lotto/LottoCalculator.kt
+++ src/main/kotlin/lotto/LottoCalculator.kt
@@ -1,6 +1,11 @@
 package lotto
 
-const val LOTTO_SIZE = 6
+const val FIRST_PLACE_COUNT = 6
+const val FIRST_PLACE_INDEX = 0
+const val SECOND_PLACE_COUNT = 5
+const val SECOND_PLACE_INDEX = 1
+const val WINNINGS_COUNT_MIN = 3
+const val LOTTO_SIZE=6
 
 class LottoCalculator(
     private val consumerLottos: List<List<Int>>,
@@ -11,6 +16,13 @@ class LottoCalculator(
         val winnings = mutableListOf<Int>(0, 0, 0, 0, 0)
         for (consumerLotto in consumerLottos) {
             val correctCount = compareNumbers(consumerLotto)
+            if (correctCount == SECOND_PLACE_COUNT && consumerLotto.contains(bonusNumber)) {
+                winnings[SECOND_PLACE_INDEX]++
+            } else if (correctCount == FIRST_PLACE_COUNT) {
+                winnings[FIRST_PLACE_INDEX]++
+            } else if (correctCount >= WINNINGS_COUNT_MIN) {
+                winnings[7 - correctCount]++
+            }
         }
         return winnings
     }
