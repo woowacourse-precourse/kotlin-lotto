a4d0d0e7579570ec39022eb1d780e2469df898dc
jiingom <vvankatam0@gmail.com>
2022-11-14 16:19:31 +0900

    feat: 랜덤하게 생성된 로또의 번호가 1~45 사이로 유효한지 확인하는 기능 구현

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -5,6 +5,15 @@ class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) {"[ERROR]: 로또 번호의 개수는 6개여야 합니다."}
         isLottoNumberDuplicated(numbers)
+        hasLottoNumberValidRange()
+    }
+
+    private fun hasLottoNumberValidRange() {
+        for (number in numbers) {
+            if (number < MINIMUM_LOTTO_NUMBER || number > MAXIMUM_LOTTO_NUMBER) {
+                throw IllegalArgumentException("[ERROR]: 로또 번호는 1~45 사이의 숫자여야 한다.")
+            }
+        }
     }
 
     private fun isLottoNumberDuplicated(lottoNumList: List<Int>) {
@@ -15,5 +24,10 @@ class Lotto(private val numbers: List<Int>) {
 
     fun getLottoNumberList(): List<Int> {
         return numbers
+    }
+
+    companion object {
+        const val MINIMUM_LOTTO_NUMBER = 1
+        const val MAXIMUM_LOTTO_NUMBER = 45
     }
 }
