631ba56f31d2de2b2b86b2b1a0bd217ee3433b18
qwe910205 <38097088+qwe910205@users.noreply.github.com>
2022-11-14 17:18:49 +0900

    fix(application): 과제 요구 사항을 만족하기 위해 예외 캐치 코드 작성
    예외가 발생하면 예외 메세지를 콘솔에 출력 후 종료해야 한다는 요구사항을
    만족하기 위해 Application에 try-catch문 작성

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -4,12 +4,17 @@ import lotto.service.LottoService
 import lotto.userinterface.UserInterface
 
 fun main() {
-    val purchaseAmount = UserInterface.askPurchaseAmount()
-    val lotteries = LottoService.purchaseLotteries(purchaseAmount)
-    UserInterface.showPurchaseResult(lotteries)
+    try {
+        val purchaseAmount = UserInterface.askPurchaseAmount()
+        val lotteries = LottoService.purchaseLotteries(purchaseAmount)
+        UserInterface.showPurchaseResult(lotteries)
 
-    val winningNumbers = UserInterface.askWinningNumbers()
-    val bonusNumber = UserInterface.askBonusNumberNotIn(winningNumbers)
-    val winningStatistics = LottoService.getWinningStatistics(winningNumbers, bonusNumber)
-    UserInterface.showWinningStatistics(winningStatistics)
+        val winningNumbers = UserInterface.askWinningNumbers()
+        val bonusNumber = UserInterface.askBonusNumberNotIn(winningNumbers)
+        val winningStatistics = LottoService.getWinningStatistics(winningNumbers, bonusNumber)
+        UserInterface.showWinningStatistics(winningStatistics)
+    } catch (e: Exception) {
+        println(e.message)
+    }
+
 }
