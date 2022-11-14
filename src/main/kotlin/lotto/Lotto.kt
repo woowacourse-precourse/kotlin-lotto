2f647d205cbf48759c99b44016a63b266cc71324
ChoHJin <cjc9739@naver.com>
2022-11-15 02:42:02 +0900

    refactor: 숫자 중복 검사 기능과 예외사항 처리 기능 구현
    중복된 숫자를 검사하는 기능을 추가하고 기존의 개수 검사와 새로 추가된 중복검사 기능에 예외사항을 출력할 수 있도록 기능을 구현함

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -2,11 +2,15 @@ package lotto
 
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6)
+        require(numbers.size == 6) {throw IllegalArgumentException(ERROR_LENGTH)}
+        require(numbers.toSet().size == 6) {throw IllegalArgumentException(ERROR_DISTINCT_NUMBERS)}
     }
 
     // TODO: 추가 기능 구현
     fun getLottoNumbers() = numbers.sorted()
 
-
+    companion object {
+        const val ERROR_LENGTH = "[ERROR] 로또 번호는 6개가 입력되어야 합니다."
+        const val ERROR_DISTINCT_NUMBERS = "[ERROR] 중복된 숫자가 입력되었습니다."
+    }
 }
