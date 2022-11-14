c3e521cc3dfed0a94222a54efdef6363192c542c
ldj526 <59276421+ldj526@users.noreply.github.com>
2022-11-14 14:32:19 +0900

    refactor: Lotto 클래스에서 받은 프로퍼티 List로 변환

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -4,10 +4,15 @@ import camp.nextstep.edu.missionutils.Randoms
 
 class Lotto(private val numbers: List<Int>) {
 
+    private var winningNumber = listOf<Int>()
 
     init {
         require(numbers.size == 6)
+        require(numbers.toSet().size == 6)
+        winningNumber = numbers
     }
 
-
+    fun changeToElement(): List<Int> {
+        return winningNumber.toList()
+    }
 }
