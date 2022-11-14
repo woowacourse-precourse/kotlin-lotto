a6a8608fe89b17ec759b9f313ef6baba96996ecc
ParkSeungMin <qazwsxedc1226@gmail.com>
2022-11-15 03:58:51 +0900

    Test[테스트추가]: 당첨 번호 입력 기능 테스트 코드 추가
    당첨 번호에 대한 예외처리 테스트 코드를 ValidCheckerTest 클래스에 작성했다. 기능이 정상적으로 동작하는 것을 확인했다.

--- src/test/kotlin/lotto/ValidCheckerTest.kt
+++ src/test/kotlin/lotto/ValidCheckerTest.kt
@@ -34,4 +34,40 @@ class ValidCheckerTest {
         Assertions.assertEquals(result2,true)
     }
 
+    @Test
+    fun `당첨 번호가 중복되면 true 리턴`() {
+        var input= listOf<Int>(1,2,2,3,4,5)
+
+        var result=validCheker.checkDuplicate(input)
+
+        Assertions.assertEquals(result,true)
+    }
+    @Test
+    fun `당첨 번호의 갯수가 6개가 아니면 true 리턴`() {
+        var input= listOf<Int>(1,2,3,4,5)
+        var input2= listOf<Int>(1,2,3,4,5,6,7,8)
+
+        var result=validCheker.checkSize(input)
+        var result2=validCheker.checkSize(input2)
+
+        Assertions.assertEquals(result,true)
+        Assertions.assertEquals(result2,true)
+    }
+
+    @Test
+    fun `당첨 번호의 범위가 1부터 45가 아니면 true 리턴`() {
+        var input= listOf<Int>(1,2,3,4,5,45)
+        var input2= listOf<Int>(1,2,3,4,5,46)
+        var input3= listOf<Int>(0,2,3,4,5,45)
+
+        var result=validCheker.checkRagne(input)
+        var result2=validCheker.checkRagne(input2)
+        var result3=validCheker.checkRagne(input3)
+
+        Assertions.assertEquals(result,false)
+        Assertions.assertEquals(result2,true)
+        Assertions.assertEquals(result3,true)
+    }
+
+
 }
