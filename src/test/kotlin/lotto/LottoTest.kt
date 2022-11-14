f4ed48377ba2edd30c6aa30de6909ecd9352f947
idra79haza <idra79haza@github.com>
2022-11-14 17:43:04 +0900

    feat(add test cases): add test cases
    add test cases for other number cases

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -12,13 +12,28 @@ class LottoTest {
         }
     }
 
-    // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
     @Test
     fun `로또 번호에 중복된 숫자가 있으면 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
             Lotto(listOf(1, 2, 3, 4, 5, 5))
         }
     }
-
-    // 아래에 추가 테스트 작성 가능
+    @Test
+    fun `로또 번호의 개수가 6개가 안되면 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(1, 2, 3, 4, 5))
+        }
+    }
+    @Test
+    fun `로또 번호 입력에 콤마를 넣지 않으면 예외를 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(123456))
+        }
+    }
+    @Test
+    fun `1부터 45에 포함되는 숫자가 입력으로 안 온 경우 예외를 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(1,2,3,5,70,-1))
+        }
+    }
 }
