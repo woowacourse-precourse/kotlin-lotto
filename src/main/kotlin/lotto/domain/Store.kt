b3d03e464d6f7c77c4ac106c9370c3468115a0aa
re4rk <37167652+re4rk@users.noreply.github.com>
2022-11-14 12:16:44 +0900

    fix(lotto): 지불된 금액이 음의 정수거나 0일 경우 예외처리한다.

--- src/main/kotlin/lotto/domain/Store.kt
+++ src/main/kotlin/lotto/domain/Store.kt
@@ -1,6 +1,7 @@
 package lotto.domain
 
 import lotto.resources.ERROR_INPUT_VALUE_MULTIPLE_OF_THOUSAND_ONLY
+import lotto.resources.ERROR_INPUT_VALUE_POSITIVE_NUMBER_ONLY
 import lotto.resources.LOTTO_TICKET_PRICE
 
 class Store {
@@ -8,6 +9,7 @@ class Store {
 
     private fun getTicket(amount: Int): Int {
         require(amount % LOTTO_TICKET_PRICE == 0) { ERROR_INPUT_VALUE_MULTIPLE_OF_THOUSAND_ONLY }
+        require(amount > 0) { ERROR_INPUT_VALUE_POSITIVE_NUMBER_ONLY }
         return amount / LOTTO_TICKET_PRICE
     }
 }
