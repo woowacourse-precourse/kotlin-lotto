e9e937d3ddf06aafda7205a6309301a8234e2452
username <ieejo716@naver.com>
2022-11-14 20:18:25 +0900

    [FEAT] : 로또 6자리 출력

--- src/main/kotlin/lotto/OutputLotto.kt
+++ src/main/kotlin/lotto/OutputLotto.kt
@@ -5,4 +5,6 @@ import camp.nextstep.edu.missionutils.Randoms
 class OutputLotto {
 
     fun issueLotto(): List<Int> = Randoms.pickUniqueNumbersInRange(1, 45, 6).sorted()
+
+    fun printLotto(lotto: List<Int>) = println(lotto)
 }
