bc5a24cced50b5ae546c8dda43a6f12c81358507
changhwan <chang5hustle@gmail.com>
2022-11-14 13:46:53 +0900

    feat(lotto) : 로또번호 입력 로직 작성

--- src/main/kotlin/lotto/Customer.kt
+++ src/main/kotlin/lotto/Customer.kt
@@ -1,4 +1,23 @@
 package lotto
 
-class Customer {
+import camp.nextstep.edu.missionutils.Console.*
+import kotlin.properties.Delegates
+
+class Customer() {
+    fun getMainLottoNumbers(): List<String> = readLine().split(",")
+
+    fun getBonusLottoNumber(): String = readLine()
+
+    companion object {
+        var mainLottoNumbers = listOf<Int>()
+        var bonusLottoNumber by Delegates.notNull<Int>()
+
+        fun updateMainLottoNumbers(mainLottoNumbers: List<Int>) {
+            this.mainLottoNumbers = mainLottoNumbers
+        }
+
+        fun updateBonusLottoNumber(bonusLottoNumber: Int) {
+            this.bonusLottoNumber = bonusLottoNumber
+        }
+    }
 }
