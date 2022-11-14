76308d24189c8cdad7435cae557a742dd5e528f9
조성록 <33805423+Dev-Joco@users.noreply.github.com>
2022-11-14 19:35:42 +0900

    feat(LottoGenerator.kt): 랜덤 로또 번호 생성 함수 generate() 구현

--- src/main/kotlin/lotto/domain/LottoGenerator.kt
+++ src/main/kotlin/lotto/domain/LottoGenerator.kt
@@ -1,3 +1,13 @@
 package lotto.domain
 
-object LottoGenerator
+import camp.nextstep.edu.missionutils.Randoms
+import lotto.data.Lotto
+
+object LottoGenerator {
+
+    fun generate(size: Int): List<Lotto> {
+        return List(size) { Lotto(
+            Randoms.pickUniqueNumbersInRange(Lotto.MIN_NUMBER, Lotto.MAX_NUMBER, Lotto.LENGTH)
+        ) }
+    }
+}
