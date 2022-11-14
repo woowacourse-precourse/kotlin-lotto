d0ced5c085989af7c2b292b6c9741e56db04c97c
rhkrwngud445 <rhkrwngud445@gmail.com>
2022-11-14 21:19:20 +0900

    refactor(Lotto) : convert hard coding to constant

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -2,12 +2,12 @@ package lotto
 
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6) { println("[ERROR] 6개의 숫자가 아닙니다.") }
+        require(numbers.size == LOTTO_SIZE) { println("[ERROR] 6개의 숫자가 아닙니다.") }
         require(!checkDuplication()) { println("[ERROR] 중복된 숫자가 있습니다.") }
     }
 
     private fun checkDuplication(): Boolean {
-        return numbers.distinct().size < 6
+        return numbers.distinct().size < LOTTO_SIZE
     }
 
     fun contains(number : Int) : Boolean{
