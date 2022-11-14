106378df3d821130cf18bdb64e82f4a97bd46a63
jklom0326 <jklom0326@gmail.com>
2022-11-15 06:26:18 +0900

    fix(Machine.kt): 변수명 변경

--- src/main/kotlin/lotto/Machine.kt
+++ src/main/kotlin/lotto/Machine.kt
@@ -5,12 +5,12 @@ import camp.nextstep.edu.missionutils.Console
 class Machine() {
     private var inputMoney = 0
     private var ticket = 0
-    var issuedLotterys = mutableListOf<List<Int>>()
+    var issuedLottery = mutableListOf<List<Int>>()
 
     fun operate() {
         inputMoney = putinMoney()
         ticket = inputMoney / 1000
-        makeLottoNumbers(ticket)
+        issuedLottery = makeLottoNumbers(ticket)
     }
 
     private fun putinMoney(): Int {
@@ -24,19 +24,19 @@ class Machine() {
         printButLottoMessage(ticket)
         var lotteryCount = 0
         while (lotteryCount < ticket) {
-            issuedLotterys += Lotto(Ticket().makeLottoTicket()).getNumbers()
-            printTickets(issuedLotterys, lotteryCount)
+            issuedLottery += Lotto(Ticket().makeLottoTicket()).getNumbers()
+            printTickets(issuedLottery, lotteryCount)
             lotteryCount++
         }
-        return issuedLotterys
+        return issuedLottery
     }
 
     private fun printButLottoMessage(count: Int) = println("${count}$BUY_LOTTO_MESSAGE")
 
     private fun printTickets(
-        lottery: MutableList<List<Int>>,
+        issuedLottery: MutableList<List<Int>>,
         lotteryCount: Int
-    ) = println(lottery[lotteryCount])
+    ) = println(issuedLottery[lotteryCount])
 
     companion object {
         const val NOT_THOUSANDS_NUMBER = "[ERROR] 입력된 금액이 1000원단위여야 합니다."
