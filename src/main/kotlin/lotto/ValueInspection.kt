958f5c305a0bc582e29e21aeeba3bcacfa1f7271
otter <a33397397@gmail.com>
2022-11-14 21:34:23 +0900

    refactor: String 하드코딩을 const value로 변경

--- src/main/kotlin/lotto/ValueInspection.kt
+++ src/main/kotlin/lotto/ValueInspection.kt
@@ -3,19 +3,17 @@ package lotto
 class ValueInspection {
 
     fun price(input: String): Boolean {
-
         if (input.all { it !in '0'..'9' }) {
-            IllegalArgumentException(ERROR_ONLY_NUMBER)
+            IllegalArgumentException(ERROR_MESSAGE_ONLY_NUMBER)
         }
-
         if (input.toInt() % 1000 != 0) {
-            IllegalArgumentException("[ERROR] 1000원 단위의 금액만 지불이 가능합니다.")
+            IllegalArgumentException(ERROR_MESSAGE_ONLY_UNIT_1000)
         }
-
         return true
     }
 
 
 }
 
-const val ERROR_ONLY_NUMBER = "[ERROR] 숫자만 입력할 수 있습니다."
+const val ERROR_MESSAGE_ONLY_NUMBER = "[ERROR] 숫자만 입력할 수 있습니다."
+const val ERROR_MESSAGE_ONLY_UNIT_1000 = "[ERROR] 1000원 단위의 금액만 지불이 가능합니다."
