56c6e5b89917805e89d9bdaa5e17543e7641bd79
DormunDoorlock <skrudgh2001@naver.com>
2022-11-14 16:54:07 +0900

    test: 2번 로또 수량 기능 test

--- src/test/kotlin/lotto/LottoGeneratorTest.kt
+++ src/test/kotlin/lotto/LottoGeneratorTest.kt
@@ -3,6 +3,7 @@ package lotto
 import lotto.domain.LottoGenerator
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import org.assertj.core.api.Assertions.assertThat
 
 class LottoGeneratorTest {
 
@@ -27,5 +28,19 @@ class LottoGeneratorTest {
             val lottoGenerator = LottoGenerator()
             lottoGenerator.createLottoNumber("1700")
         }
+    }
+
+    @Test
+    fun `로또 구입 금액으로 로또 갯수 구하는 기능 테스트`() {
+        val lottoGenerator = LottoGenerator()
+        val result = lottoGenerator.getLottoCountValue(1000)
+        assertThat(result).isEqualTo(1)
+    }
+
+    @Test
+    fun `로또 구입 금액으로 로또 갯수 구하는 기능 테스트2`() {
+        val lottoGenerator = LottoGenerator()
+        val result = lottoGenerator.getLottoCountValue(10000)
+        assertThat(result).isEqualTo(10)
     }
 }
