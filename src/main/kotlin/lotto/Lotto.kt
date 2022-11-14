75760cb33feb184a238c0a89d1ec30fd19f0fabe
jklom0326 <jklom0326@gmail.com>
2022-11-15 07:14:16 +0900

    refactor(Lotto.kt): 메시지 처리
    - ERROR 출력 메시지를 Message로 분리

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,12 +1,15 @@
 package lotto
 
+import lotto.Message.Companion.DUPLICATED_NUMBERS
+import lotto.Message.Companion.NOT_NUMBER
+import lotto.Message.Companion.NOT_SIX_NUMBERS
 
 class Lotto(private val numbers: List<Int>) {
 
     init {
         require(numbers.size == 6) { NOT_SIX_NUMBERS }
         require(numbers.distinct().size == 6) { DUPLICATED_NUMBERS }
-        require(correctNumbers(numbers)) { "[ERROR] 입력된 수가 로또번호가 아닙니다." }
+        require(correctNumbers(numbers)) { NOT_NUMBER }
     }
 
     fun getNumbers(): List<Int> {
@@ -18,11 +21,6 @@ class Lotto(private val numbers: List<Int>) {
             if (number !in 1..45) return false
         }
         return true
-    }
-
-    companion object {
-        const val NOT_SIX_NUMBERS = "[ERROR] 입력된 수가 6자리가 아닙니다."
-        const val DUPLICATED_NUMBERS = "[ERROR] 중복된 숫자가 있습니다."
     }
 
 }
--- src/main/kotlin/lotto/Message.kt
+++ src/main/kotlin/lotto/Message.kt
@@ -3,6 +3,10 @@ package lotto
 class Message {
     companion object {
         const val NOT_THOUSANDS_NUMBER = "[ERROR] 입력된 금액이 1000원단위여야 합니다."
+        const val NOT_SIX_NUMBERS = "[ERROR] 입력된 수가 6자리가 아닙니다."
+        const val DUPLICATED_NUMBERS = "[ERROR] 중복된 숫자가 있습니다."
+        const val NOT_NUMBER = "[ERROR] 입력된 수가 로또번호가 아닙니다."
+
         const val BUY_LOTTO_MESSAGE = "개를 구매했습니다."
         const val INPUT_MONEY_MESSAGE = "구입 금액을 입력해주세요"
     }
