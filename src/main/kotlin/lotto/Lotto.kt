be8ab3496bfabde719671fc368ece061cd7187b6
rhkrwngud445 <rhkrwngud445@gmail.com>
2022-11-14 15:06:07 +0900

    fix : check lotto duplication logic

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -3,12 +3,10 @@ package lotto
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { println("[ERROR] 6개의 숫자가 아닙니다.") }
-        require(checkDuplication()) { println("[ERROR] 중복된 숫자가 있습니다.") }
+        require(!checkDuplication()) { println("[ERROR] 중복된 숫자가 있습니다.") }
     }
 
     private fun checkDuplication(): Boolean {
-        return numbers.filter {
-            numbers.contains(it)
-        }.count() <= 1
+        return numbers.distinct().size < 6
     }
 }
