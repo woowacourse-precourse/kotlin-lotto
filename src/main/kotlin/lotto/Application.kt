798f312d6cae08971197fd69eb9e81d965c046c9
idra79haza <idra79haza@github.com>
2022-11-14 23:38:58 +0900

    refactor(Application): lowercase for fun from Controller class
    lowercase for fun from Controller class

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -4,14 +4,14 @@ fun main() {
     view.View().buyPriceInquireMessage() // 구매 로또 금액 받기
     val inputPrice = LottoGame().inputNum()
     if (inputPrice == -1) { return }
-    val numberOfLotto = Controller().GetLottoPrice(inputPrice)
+    val numberOfLotto = Controller().getLottoPrice(inputPrice)
 
     val lottos = LottoGame().generateRandomLottos(numberOfLotto) // 생성된 로또들 print해서 보여주기
     view.View().printEachLotto(lottos)
 
-    val answerNumbers = Controller().AnswerLottoNum() // 정답 로또 번호 받기
+    val answerNumbers = Controller().answerLottoNum() // 정답 로또 번호 받기
 
-    val inputBonusPrice = Controller().BonusLottoNum() // 보너스 번호 받기
+    val inputBonusPrice = Controller().bonusLottoNum() // 보너스 번호 받기
 
     Controller().winLotto(lottos, answerNumbers, inputBonusPrice, inputPrice) // 통계 결과 내기
 }
