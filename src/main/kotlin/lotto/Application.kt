823c1150d7c8283575b6c6437aefa855068e9817
dohun_ <silver2679@gmail.com>
2022-11-15 01:36:00 +0900

    Refactor(Application): try, catch 추가

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -3,13 +3,18 @@ package lotto
 import lotto.domain.LottoWin
 import lotto.ui.LottoBuyer
 import lotto.ui.LottoSeller
+import java.lang.IllegalArgumentException
 
 fun main() {
-    var lottoWinningNumbers = arrayListOf<String>()
-    val lottoMoney = LottoBuyer().inputLottoPurchasePrice()//구입 금액 입력
-    val purchasedLotto = LottoSeller().calLottoCount(lottoMoney)//발행한 로또
-    val lottoWinNumber = LottoSeller().inputPrizeNumber(lottoWinningNumbers)//당첨 번호
-    val lottoBonus = LottoSeller().inputBonusNumber()//보너스 번호
-    val lottoPrizeMoney = LottoWin().calPrize(purchasedLotto, lottoWinNumber, lottoBonus)
-    LottoWin().printProfitRate(lottoMoney, lottoPrizeMoney)
+    try{
+        var lottoWinningNumbers = arrayListOf<String>()
+        val lottoMoney = LottoBuyer().inputLottoPurchasePrice()//구입 금액 입력
+        val purchasedLotto = LottoSeller().calLottoCount(lottoMoney)//발행한 로또
+        val lottoWinNumber = LottoSeller().inputPrizeNumber(lottoWinningNumbers)//당첨 번호
+        val lottoBonus = LottoSeller().inputBonusNumber()//보너스 번호
+        val lottoPrizeMoney = LottoWin().calPrize(purchasedLotto, lottoWinNumber, lottoBonus)
+        LottoWin().printProfitRate(lottoMoney, lottoPrizeMoney)
+    }catch (e: IllegalArgumentException){
+        println("[ERROR] 잘못된 값을 입력하였습니다.")
+    }
 }
