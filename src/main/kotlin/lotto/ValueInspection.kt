e3ad9ddebfb273525dcc90a2ac07c7183fec297a
otter <a33397397@gmail.com>
2022-11-14 21:51:30 +0900

    feat: 당첨 번호의 입력값을 검사하는 기능 작성

--- src/main/kotlin/lotto/ValueInspection.kt
+++ src/main/kotlin/lotto/ValueInspection.kt
@@ -12,8 +12,24 @@ class ValueInspection {
         return true
     }
 
+    fun jackpot(input: String): Boolean {
+        val jackpotList: List<Int> = input.split(",").map { it.toInt() }
+        if (!input.contains(",")) {
+            IllegalArgumentException(ERROR_MESSAGE_INPUT_FORMAT)
+        }
+        if (jackpotList.all { it !in 0..45 }) {
+            IllegalArgumentException(ERROR_MESSAGE_ONLY_NUMBER)
+        }
+        if (jackpotList.size != 6) {
+            IllegalArgumentException(ERROR_MESSAGE_ONLY_SIX_NUMBER)
+        }
+        return true
+    }
+
 
 }
 
 const val ERROR_MESSAGE_ONLY_NUMBER = "[ERROR] 숫자만 입력할 수 있습니다."
+const val ERROR_MESSAGE_ONLY_SIX_NUMBER = "[ERROR] 6개의 숫자만 입력할 수 있습니다."
+const val ERROR_MESSAGE_INPUT_FORMAT = "[ERROR] 입력 형식이 잘못되었습니다."
 const val ERROR_MESSAGE_ONLY_UNIT_1000 = "[ERROR] 1000원 단위의 금액만 지불이 가능합니다."
