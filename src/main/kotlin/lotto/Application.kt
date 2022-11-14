bd995b851b902bfbd9e6f1ca953a70c8ea7ee542
choHarmony <jws0183@naver.com>
2022-11-15 01:56:18 +0900

    feat: 랜덤 로또 숫자 고르기

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Console
 
 lateinit var purchaseSum: String
 var numOfLotto = 0
+lateinit var selectedNum: Array<Int>
 
 fun main() {
 
@@ -25,4 +26,11 @@ fun checkCostInput() {
     if (purchaseSum.toInt() % 1000 != 0) {
         throw IllegalArgumentException("[ERROR] 금액을 천원 단위의 숫자로 입력해주세요.")
     }
+}
+
+fun pickLottoNum(): Array<Int> {
+    selectedNum = Randoms.pickUniqueNumbersInRange(1, 45, 6).toTypedArray()
+    selectedNum.sort()
+
+    return selectedNum
 }
