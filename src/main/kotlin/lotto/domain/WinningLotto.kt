d13e3f17a857883c8fd9e8a2b0c60b9b1c097979
re4rk <37167652+re4rk@users.noreply.github.com>
2022-11-14 13:16:32 +0900

    refactor(lotto): 매개변수를 Lotto로 받도록 변경

--- src/main/kotlin/lotto/domain/WinningLotto.kt
+++ src/main/kotlin/lotto/domain/WinningLotto.kt
@@ -2,15 +2,11 @@ package lotto.domain
 
 import lotto.resources.*
 
-class WinningLotto(private val winningNumbers: List<Int>, private val bonusNumber: Int) {
+// Lotto를 상속받는 클래스로 만들면 winningNumbers가 로또가 맞는지 확인하는 동작을 안거쳐도 됨
+// 아니면 처음부터 List<Int>가 아닌 로또를 건내줘도 되고
+class WinningLotto(private val winningNumbers: Lotto, private val bonusNumber: Int) {
     init {
-        require(winningNumbers.size == LOTTO_NUMBER_COUNT) { ERROR_INPUT_VALUE_SIX_ONLY }
-        require(winningNumbers.toSet().size == LOTTO_NUMBER_COUNT) { ERROR_INPUT_VALUE_NO_DUPLICATES }
-        require(winningNumbers.filter { it in LOTTO_START_NUMBER..LOTTO_END_NUMBER }.size == LOTTO_NUMBER_COUNT) {
-            ERROR_INPUT_VALUE_ONE_OR_FORTY_FIVE_ONLY
-        }
-
-        require(bonusNumber !in winningNumbers) { ERROR_INPUT_VALUE_NO_DUPLICATES }
+        require(bonusNumber !in winningNumbers.toList()) { ERROR_INPUT_VALUE_NO_DUPLICATES }
         require(bonusNumber in LOTTO_START_NUMBER..LOTTO_END_NUMBER) {
             ERROR_INPUT_VALUE_ONE_OR_FORTY_FIVE_ONLY
         }
@@ -18,7 +14,7 @@ class WinningLotto(private val winningNumbers: List<Int>, private val bonusNumbe
 
     private fun getResult(guessNumbers: List<Int>): Pair<Int, Boolean> {
         return Pair(
-            winningNumbers.countSameNumbers(guessNumbers),
+            winningNumbers.toList().countSameNumbers(guessNumbers),
             bonusNumber in guessNumbers
         )
     }
