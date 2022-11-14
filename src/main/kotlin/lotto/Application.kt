af1a347f0588823d2eb07d8d97da6d388191a2b4
dlgocks1 <cksgodl1@naver.com>
2022-11-14 23:19:46 +0900

    feat(application) : 로또 시작 코드 작성

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,20 +1,9 @@
 package lotto
 
 import lotto.controller.LottoController
-import java.lang.Math.pow
 import java.math.RoundingMode
 import java.text.DecimalFormat
-import kotlin.math.floor
-import kotlin.math.pow
-import kotlin.math.round
 
 fun main() {
-    TODO("프로그램 구현")
-}
-
-fun calEarningRate(purchasePrize: Int, winningPrize: Int): String {
-    val dec = DecimalFormat("#,###.0")
-    return dec.format(
-        (winningPrize * 100.0).toBigDecimal().divide(purchasePrize.toBigDecimal(), 1, RoundingMode.FLOOR)
-    )
+    LottoController().play()
 }
