9f8e72751c64b107a4fccbbef3d48aee67d33456
choHarmony <jws0183@naver.com>
2022-11-15 01:43:46 +0900

    feat: 로또 구입 금액 입력받기

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,15 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Randoms
+import camp.nextstep.edu.missionutils.Console
+
+lateinit var purchaseSum: String
+
 fun main() {
-    TODO("프로그램 구현")
+
+}
+
+fun costInput() {
+    println("구입 금액을 입력해주세요.")
+    purchaseSum = Console.readLine()
 }
