58ada21d72f71529619cb97c8d2293558dbf808d
lanpia <95679243+lanpia@users.noreply.github.com>
2022-11-15 08:03:42 +0900

    feat (Application.kt) : making list for inputnumber - 당첨번호를 입력받는다 	- 구분자 "," 가 아닌경우 에러송출 	- 6자리이상 입력하는 경우 에러송출 	- 0 또는 숫자가 아닌경우 에러 송출 	- 1 ~ 45 사이의 숫자가 아닌경우 에러송출 - 보너스번호를 입력받는다 	- 0인 경우 에러송출 	- 하나 이상의 수를 입력하는 경우 에러송출 	- 1 ~ 45 사이의 숫자가 아닌경우 에러송출

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -11,4 +11,22 @@ fun userbuy() : Int{
     if(buing == null) throw IllegalArgumentException ("[ERROR]")
     howmany = buing / 1000
     return(howmany)
-} 
+}
+
+fun inputnum() : List<Int>{
+    var inputlotto  : List<Int> = List(7) {0}
+    println("당첨 번호를 입력해 주세요.")
+    inputlotto = readLine()!!.split(",").map { it.toIntOrNull() ?: 0 }
+    if(inputlotto.size != inputlotto.distinct().size) throw IllegalArgumentException ("[ERROR]")
+    if(inputlotto.size > 6 ) throw IllegalArgumentException ("[ERROR]")
+    for( i : Int in 0 .. 5 step(1))
+        if(!(inputlotto[i] > 0 && inputlotto[i] <46)) throw IllegalArgumentException ("[ERROR]")
+    println("보너스 번호를 입력해 주세요.")
+    inputlotto += readLine()!!.toIntOrNull() ?: 0
+    if(inputlotto.contains(0)) throw IllegalArgumentException ("[ERROR]")
+    if(inputlotto.size != inputlotto.distinct().size) throw IllegalArgumentException ("[ERROR]")
+    for( i : Int in 0 .. 6 step(1))
+        if(!(inputlotto[i] > 0 && inputlotto[i] <46)) throw IllegalArgumentException ("[ERROR]")
+    return (inputlotto)
+    }
+    
