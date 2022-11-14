7641fd53cd59d55440b5c490db9d2c6740554c93
phs <gustjd0808@pusan.ac.kr>
2022-11-15 03:27:18 +0900

    feat(enum): change private property to public property

--- src/main/kotlin/lotto/enum/Reward.kt
+++ src/main/kotlin/lotto/enum/Reward.kt
@@ -2,7 +2,7 @@ package lotto.enum
 
 import java.text.DecimalFormat
 
-enum class Reward(private val reward: Int) {
+enum class Reward(val reward: Int) {
     FIFTH_REWARD(5_000),
     FOURTH_REWARD(50_000),
     THIRD_REWARD(1_500_000),
