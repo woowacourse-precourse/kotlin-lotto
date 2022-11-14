4b3f5d5e03719a30ee8a40120c2d0778ac95bc64
username <ieejo716@naver.com>
2022-11-15 03:35:37 +0900

    [FEAT] : 로또 모아 놓은 리스트 추가

--- src/main/kotlin/lotto/OutputLotto.kt
+++ src/main/kotlin/lotto/OutputLotto.kt
@@ -4,6 +4,8 @@ import camp.nextstep.edu.missionutils.Randoms
 
 class OutputLotto {
 
+    val lottoTickets = mutableListOf<List<Int>>()
+
     fun issueLotto(): List<Int> = Randoms.pickUniqueNumbersInRange(1, 45, 6).sorted()
 
     fun printLotto(lotto: List<Int>) = println(lotto)
