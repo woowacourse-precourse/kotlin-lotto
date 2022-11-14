445ab3e7f1299159b8cd4a31ed182d67a3a29d56
DESKTOP-8PSAE1K\nuyhus <minsh2344@gmail.com>
2022-11-15 02:30:02 +0900

    feat: 로또 번호에 중복된 숫자가 있을 경우 예외 처리 기능 구현

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -3,6 +3,7 @@ package lotto
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6)
+        require(numbers.distinct() == numbers)
     }
 
     fun compareWinningNumber(winningNumber: List<Int>): Int {
