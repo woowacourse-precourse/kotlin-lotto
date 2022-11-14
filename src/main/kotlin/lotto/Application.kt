5091b6364de4e6cc7d86d6bcd5beddaba3ccd0db
idra79haza <idra79haza@github.com>
2022-11-14 17:37:51 +0900

    refactor(Application): change Application
    change Application for passing exception cases

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -7,27 +7,25 @@ import java.util.Locale
 fun main() {
     LottoGame().buyPriceInquireMessage()
     val inputPrice = LottoGame().inputNum()
-    val flagRightLotto = LottoGame().checkRightLotto(inputPrice)
+    if (inputPrice == -1){
+        return
+    }
     val numberOfLotto = LottoGame().calculateNumberOfLotto(inputPrice)
-    LottoGame().resultRightLotto(flagRightLotto)
     LottoGame().tellNumOfLotto(numberOfLotto)
     val lottos = LottoGame().generateRandomLottos(numberOfLotto)
     LottoGame().printEachLotto(lottos)
 
     // 정답 로또 번호 받기
     LottoGame().answerNumbersMessage()
-    val answerNumbers= LottoGame().inputAnswerNumbers()
+    val answerNumbers = LottoGame().inputAnswerNumbers()
     Lotto(answerNumbers).checkinputAnswer()
 
     // 보너스 번호 받기
     LottoGame().bonusNumberMessage()
     val inputBonusPrice = LottoGame().inputNum().toString()
     LottoGame().checkinputBonus(inputBonusPrice)
-    
+
     // 통계 결과 내기
-    println(lottos)
-    println(answerNumbers)
-    println(inputBonusPrice)
     val winList = LottoGame().computeRank(lottos, answerNumbers, inputBonusPrice.toInt())
     LottoGame().printStatsRank(winList)
     val revenuePercent = LottoGame().statsRevenue(inputPrice, winList)
