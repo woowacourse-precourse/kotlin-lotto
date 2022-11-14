c1ab6581e1cf82e82af12bf0415390abbdcb1d79
[alsswl] <[alswl4223@naver.com]>
2022-11-14 13:43:02 +0900

    feat : 사용자의 돈을 입력받고 잘못되면 에러를 발생

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -2,8 +2,19 @@ package lotto
 import camp.nextstep.edu.missionutils.Randoms
 import camp.nextstep.edu.missionutils.Console
 
+fun checkUserMoney(userMoney:String){
+   var userMoney = userMoney.toIntOrNull()
+    if(userMoney == null) {
+        print("[ERROR] 숫자를 입력하세요")
+        throw IllegalArgumentException()}
+
+    if(userMoney%1000 != 0) {
+        print("[ERROR] 1000으로 나누어 떨어지는 수를 입력하세요")
+        throw IllegalArgumentException()}
+}
 
 fun main() {
+   var userMoney = Console.readLine()!!.toString();
+    checkUserMoney(userMoney)
 
-    Console.readLine();
 }
