e3ae182527c89c48c648d58ca6fe0c210487b596
jklom0326 <jklom0326@gmail.com>
2022-11-15 04:14:43 +0900

    feat(Lotto.kt): 입력 숫자에 대한 예외 처리
    - 입력이 1 - 45 사이의 숫자가 아닌 경우 예외 메시지 출력

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,9 +1,24 @@
 package lotto
 
+
 class Lotto(private val numbers: List<Int>) {
+
     init {
-        require(numbers.size == 6)
+        require(numbers.size == 6) { NOT_SIX_NUMBERS }
+        require(numbers.distinct().size == 6) { DUPLICATED_NUMBERS }
+        require(correctNumbers(numbers)) { "[ERROR] 입력된 수가 로또번호가 아닙니다." }
     }
 
-    // TODO: 추가 기능 구현
+    private fun correctNumbers(numbers: List<Int>): Boolean {
+        for (number in numbers) {
+            if (number !in 1..45) return false
+        }
+        return true
+    }
+
+    companion object {
+        const val NOT_SIX_NUMBERS = "[ERROR] 입력된 수가 6자리가 아닙니다."
+        const val DUPLICATED_NUMBERS = "[ERROR] 중복된 숫자가 있습니다."
+    }
+
 }
