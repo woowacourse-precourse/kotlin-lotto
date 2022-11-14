e69bae4dc0dfaab4690d01c45dea8865a2c83b07
re4rk <37167652+re4rk@users.noreply.github.com>
2022-11-15 02:20:12 +0900

    form(lotto): TODO에서 구현 끝난 주석 삭제

--- src/main/kotlin/lotto/domain/WinningLotto.kt
+++ src/main/kotlin/lotto/domain/WinningLotto.kt
@@ -2,8 +2,6 @@ package lotto.domain
 
 import lotto.resources.*
 
-// Lotto를 상속받는 클래스로 만들면 winningNumbers가 로또가 맞는지 확인하는 동작을 안거쳐도 됨
-// 아니면 처음부터 List<Int>가 아닌 로또를 건내줘도 되고
 class WinningLotto(private val winningNumbers: Lotto, private val bonusNumber: Int) {
     init {
         require(bonusNumber !in winningNumbers.toList()) { ERROR_INPUT_VALUE_NO_DUPLICATES }
