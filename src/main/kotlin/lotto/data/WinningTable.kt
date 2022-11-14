2fff28cb5681c4a70348eb6931d3adb46e8b9a67
조성록 <33805423+Dev-Joco@users.noreply.github.com>
2022-11-15 02:53:51 +0900

    feat(WinningTable.kt): inner class Record.class 정의

--- src/main/kotlin/lotto/data/WinningTable.kt
+++ src/main/kotlin/lotto/data/WinningTable.kt
@@ -4,4 +4,8 @@ class WinningTable(
     private val money: Int,
     private val lotteries: List<Lotto>,
     private val winning: WinningLotto,
-)
+) {
+
+
+    data class Record(val info: LottoRank, var count: Int)
+}
