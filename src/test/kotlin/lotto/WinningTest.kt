615da37f1429aa99588d72e8dcced5bae5b6af18
Min-Jae-Bae <qoalswo123@icloud.com>
2022-11-14 20:05:28 +0900

    [Test] (WinningTest.kt) 8000원 지불, 5000원 상금일 때 수익률 반환 테스트 추가
    수익률 62.5 반환

--- src/test/kotlin/lotto/WinningTest.kt
+++ src/test/kotlin/lotto/WinningTest.kt
@@ -44,43 +44,11 @@ class WinningTest {
     }
 
     @Test
-    fun `5등이 하나도 없을 때 0 반환`() {
+    fun `8000원 지불, 5000원 상금 받았을 때 올바른 수익률 반환`() {
         val winning = Winning()
-        val rankList = listOf(Rank.LAST, Rank.LAST, Rank.LAST)
-        val resultFifthCount = winning.updateFifthCount(rankList)
-        assertThat(0).isEqualTo(resultFifthCount)
+        val resultYield = winning.createYield(5000)
+        assertThat(62.5).isEqualTo(resultYield)
     }
 
-    @Test
-    fun `4등이 1개 있을 때 1 반환`() {
-        val winning = Winning()
-        val rankList = listOf(Rank.LAST, Rank.LAST, Rank.FOURTH)
-        val resultFourthCount = winning.updateFourthCount(rankList)
-        assertThat(1).isEqualTo(resultFourthCount)
-    }
-
-    @Test
-    fun `3등이 2개 있을 때 2 반환`() {
-        val winning = Winning()
-        val rankList = listOf(Rank.LAST, Rank.SECOND, Rank.SECOND)
-        val resultThirdCount = winning.updateThirdCount(rankList)
-        assertThat(2).isEqualTo(resultThirdCount)
-    }
-
-    @Test
-    fun `2등이 하나도 없을 때 0 반환`() {
-        val winning = Winning()
-        val rankList = listOf(Rank.LAST, Rank.LAST, Rank.LAST)
-        val resultSecondCount = winning.updateSecondCount(rankList)
-        assertThat(0).isEqualTo(resultSecondCount)
-    }
-
-    @Test
-    fun `1등이 1개 있을 때 1 반환`() {
-        val winning = Winning()
-        val rankList = listOf(Rank.LAST, Rank.LAST, Rank.FIRST)
-        val resultFirstCount = winning.updateFirstCount(rankList)
-        assertThat(1).isEqualTo(resultFirstCount)
-    }
 
 }
