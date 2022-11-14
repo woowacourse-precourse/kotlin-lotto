53b5ddec5f11ae855d181d5377a0b03f1e0c467b
Lee-Sunho <sunho9889@soongsil.ac.kr>
2022-11-15 04:47:09 +0900

    feat(Application): 기능 통합하여 main문 작성

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,18 @@
 package lotto
 
+import lotto.domain.*
+import lotto.views.InputView
+import lotto.views.OutputView
+
 fun main() {
-    TODO("프로그램 구현")
+    val purchase = Purchase()
+    val amount = purchase.buyLotto()
+    OutputView.printAmount(amount)
+    val lottos = purchase.createLottos(amount)
+    OutputView.printLottos(lottos)
+    val winningLotto = WinningLotto().createWinningLotto()
+    val bonusNumber = BonusNumber().createBonusNumber()
+    val rewards = getReward(lottos, winningLotto, bonusNumber)
+    OutputView.printCountReward(countReward(rewards))
+    OutputView.printYield(calculateYield(rewards, amount))
 }
