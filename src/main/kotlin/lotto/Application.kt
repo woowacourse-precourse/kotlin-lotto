2abcfa76d7c035bde8a3307429f4bf2b23bec9db
[alsswl] <[alswl4223@naver.com]>
2022-11-14 12:16:57 +0900

    commit test

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -2,12 +2,8 @@ package lotto
 import camp.nextstep.edu.missionutils.Randoms
 import camp.nextstep.edu.missionutils.Console
 
-enum class Grade(val grade:Int){First(2000000000),
-    Second(30000000),
-    Third(1500000),
-    Fourth(50000),
-    Fifth(5000);
-}
+
 fun main() {
 
+    Console.readLine();
 }
