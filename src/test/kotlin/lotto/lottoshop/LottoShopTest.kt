a0b56fdd0c293af01d50b23eb07c2284896eeac0
changhwan <chang5hustle@gmail.com>
2022-11-14 21:08:31 +0900

    test(lotto) : 당첨번호 예외검사 함수 테스트

--- src/test/kotlin/lotto/lottoshop/LottoShopTest.kt
+++ src/test/kotlin/lotto/lottoshop/LottoShopTest.kt
@@ -51,4 +51,62 @@ class LottoShopTest {
             }
         }
     }
+
+    @Nested
+    inner class TestMainLottoNumbers {
+        @Test
+        fun `로또 당첨 번호 테스트 정상`() {
+            val input = listOf("1","2","3","4","5","6")
+            assertThat(lottoShop.getValidateMainLottoNumbers(input)).isEqualTo(listOf(1,2,3,4,5,6))
+        }
+
+        @Test
+        fun `로또 당첨번호 테스트 1~45범위 아닌 번호 포함`() {
+            assertThrows<IllegalArgumentException> {
+                lottoShop.getValidateMainLottoNumbers(listOf("1","2","3","56","5","6"))
+            }
+        }
+
+        @Test
+        fun `로또 당첨번호 테스트 1~45범위 아닌 번호 포함(음수 포함)`() {
+            assertThrows<IllegalArgumentException> {
+                lottoShop.getValidateMainLottoNumbers(listOf("1","2","3","-1","5","6"))
+            }
+        }
+
+        @Test
+        fun `로또 당첨번호 테스트 번호 7개인경우`() {
+            assertThrows<IllegalArgumentException> {
+                lottoShop.getValidateMainLottoNumbers(listOf("1","2","3","4","5","6","7"))
+            }
+        }
+
+        @Test
+        fun `로또 당첨번호 테스트 번호 5개인경우`() {
+            assertThrows<IllegalArgumentException> {
+                lottoShop.getValidateMainLottoNumbers(listOf("1","2","3","4","5"))
+            }
+        }
+
+        @Test
+        fun `로또 당첨번호 테스트 알파벳이 포함된경우`() {
+            assertThrows<IllegalArgumentException> {
+                lottoShop.getValidateMainLottoNumbers(listOf("1","2","ab","4","5","6","7"))
+            }
+        }
+
+        @Test
+        fun `로또 당첨번호 테스트 한글이 포함된경우`() {
+            assertThrows<IllegalArgumentException> {
+                lottoShop.getValidateMainLottoNumbers(listOf("1","2","ㅅㄴ","4","5","6","7"))
+            }
+        }
+
+        @Test
+        fun `로또 당첨번호 테스트 기호가 포함된경우`() {
+            assertThrows<IllegalArgumentException> {
+                lottoShop.getValidateMainLottoNumbers(listOf("1","2","-","4","5","6","7"))
+            }
+        }
+    }
 }
