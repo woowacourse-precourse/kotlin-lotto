9073dc325b5aac794301cc949d440f1c64e9eb6a
ponopono0322 <ponopono0322@gmail.com>
2022-11-14 15:32:49 +0900

    refactor: checker 함수 기능 분할
    main 함수 내 기능 분리
    - inputChecker가 사용자 입력 체크: bonusChecker, rangeChecker, lotteryChecker, purchaseChecker
    
    - bonusChecker 기능 분할: 중복 체크 하는 것을 rangeChecker가 담당

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -37,15 +37,11 @@ fun lotteryChecker(): List<Int> {
     return lotteryNumbers
 }
 
-fun bonusChecker(lotteryNumbers: List<Int>): Int {
+fun bonusChecker(): Int {
     println(StringResource.BONUS.resource)
     val bonusNumber: Int
     try {
         bonusNumber = readLine().trim().toInt()
-        if (lotteryNumbers.contains(bonusNumber)) {
-            println(StringResource.DUPLICATE.resource)
-            return -1
-        }
         if (bonusNumber !in NumberRange.START.number..NumberRange.END.number) {
             println(StringResource.RANGEERROR.resource)
             return -1
@@ -54,16 +50,45 @@ fun bonusChecker(lotteryNumbers: List<Int>): Int {
         println(StringResource.NUMBERERROR.resource)
         return -1
     }
+
     return bonusNumber
 }
 
-fun main() {
+fun rangeChecker(bonusNumber: Int, lotteryNumber: List<Int>): Boolean {
+    if (bonusNumber in lotteryNumber) {
+        println(StringResource.DUPLICATE.resource)
+        return true
+    }
+
+    return false
+}
+
+fun inputChecker(): Triple<Int, List<Int>, Int> {
     val purchase = purchaseChecker()
-    if (purchase == -1) return
+    if (purchase == -1) {
+        return Triple(-1, listOf(), -1)
+    }
     val lotteryNumbers = lotteryChecker()
-    if (lotteryNumbers.isEmpty()) return
-    val bonusNumber = bonusChecker(lotteryNumbers)
-    if (bonusNumber == -1) return
+    if (lotteryNumbers.isEmpty()) {
+        return Triple(-1, listOf(), -1)
+    }
+    val bonusNumber = bonusChecker()
+    if (bonusNumber == -1) {
+        return Triple(-1, listOf(), -1)
+    }
+
+    return Triple(purchase, lotteryNumbers, bonusNumber)
+}
+
+fun main() {
+    val (purchase, lotteryNumbers, bonusNumber) = inputChecker()
+    if (purchase == -1 && lotteryNumbers.isEmpty() && bonusNumber == -1) {
+        return
+    }
+    if (rangeChecker(bonusNumber, lotteryNumbers)) {
+        return
+    }
+
     val lotto = Lottery(purchase, Lotto(lotteryNumbers), bonusNumber)
 
     lotto.printQuickPick()
