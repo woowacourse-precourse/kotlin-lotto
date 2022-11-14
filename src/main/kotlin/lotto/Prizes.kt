42605fb85d949e82370b1ae100c1ee9dbc7ffcad
vrexpert <vrexpert2302@gmail.com>
2022-11-14 17:21:16 +0900

    feat(lotto): 당첨금과 등수 번호 개수 가독성을 위해 Enum 클래스 작성
    Ranks(등수) : FIRST(1),SECOND(2),THIRD(3),FOURTH(4),FIFTH(5)
    Prizes(상금) : FIRST(2_000_000_000),SECOND(30_000_000),THIRD(1_500_000),FOURTH(50_000),FIFTH(5000)
    Count(개수) : SIX(6),FIVE(5),FOUR(4),THREE(3)

--- src/main/kotlin/lotto/Prizes.kt
+++ src/main/kotlin/lotto/Prizes.kt
@@ -1,5 +1,9 @@
 package lotto
 
-enum class Prizes (val rank:Int){
-    FRIST(2_000_000_000),SECOND(30_000_000),THIRD(1_500_000),FOURTH(50_000),FIFTH(5000)
+enum class Prizes (val money:Int){
+    FIRST(2_000_000_000),
+    SECOND(30_000_000),
+    THIRD(1_500_000),
+    FOURTH(50_000),
+    FIFTH(5000)
 }
