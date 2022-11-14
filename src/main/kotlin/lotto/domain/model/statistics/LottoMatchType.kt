c80c2be570d0784f14bdf8a4f90786a72119944d
sujin-kk <sjoo0002@gmail.com>
2022-11-14 20:22:35 +0900

    feat(view/statistics): 당첨 통계를 출력하는 코드를 작성한다.

---
+++ src/main/kotlin/lotto/domain/model/statistics/LottoMatchType.kt
@@ -0,0 +1,5 @@
+package lotto.domain.model.statistics
+
+enum class LottoMatchType {
+    NORMAL, BONUS
+}
--- src/main/kotlin/lotto/ui/view/statistics/ShowStatisticsView.kt
+++ src/main/kotlin/lotto/ui/view/statistics/ShowStatisticsView.kt
@@ -1,13 +1,24 @@
 package lotto.ui.view.statistics
 
+import lotto.domain.model.statistics.LottoMatchType
+
 class ShowStatisticsView {
 
     fun printStatisticsTitleMessage() {
         println(OUTPUT_STATISTICS_TITLE_MESSAGE)
     }
 
-    fun printWinningMatchMessage() {
-        println(OUTPUT_STATISTICS_TITLE_MESSAGE)
+    fun printWinningMatchMessage(matchNumberCount: Int, reward: Int, matchTicketCount: Int, matchType: LottoMatchType) {
+
+        if (matchType == LottoMatchType.NORMAL)
+            println(OUTPUT_WINNING_MATCH_NORMAL_MESSAGE.format(matchNumberCount, reward, matchTicketCount))
+
+        if (matchType == LottoMatchType.BONUS)
+            println(OUTPUT_WINNING_MATCH_BONUS_MESSAGE.format(matchNumberCount, reward, matchTicketCount))
+    }
+
+    fun printTotalProfitRateMessage(totalRate: Double) {
+        println(OUTPUT_TOTAL_PROFIT_RATE_MESSAGE.format(totalRate))
     }
 
 
@@ -15,6 +26,6 @@ class ShowStatisticsView {
         private const val OUTPUT_STATISTICS_TITLE_MESSAGE = "당첨 통계\n---"
         private const val OUTPUT_WINNING_MATCH_NORMAL_MESSAGE = "%d개 일치 (%d원) - %d개"
         private const val OUTPUT_WINNING_MATCH_BONUS_MESSAGE = "%d개 일치, 보너스 볼 일치 (%d원) - %d개"
-        private const val OUTPUT_YIELD_MESSAGE = "총 수익률은 %.1f입니다."
+        private const val OUTPUT_TOTAL_PROFIT_RATE_MESSAGE = "총 수익률은 %.1d입니다."
     }
 }
