c07d0e9c652c472785091dcb300cdfd4753c5972
kyounggudev <homin222@naver.com>
2022-11-14 23:44:47 +0900

    refactor : try catch문으로 변경

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,19 +1,19 @@
 package lotto
 
+import java.lang.IllegalArgumentException
+
 const val MIN =1
 const val MAX =45
 
-val purchase = Purchase(Purchase.inputPrice())
-
-const val ERROR_NO_INPUT = "[ERROR] 구입 금액을 입력하지 않았습니다."
-const val ERROR_NOT_INT = "[ERROR] 구입 금액에는 숫자만 입력해야합니다."
-const val ERROR_1000UNIT = "[ERROR] 구입 금액이 1,000원 단위여야 합니다."
-
-
 fun main() {
-    Lotto.publish(purchase.count)
-    Lotto.printCount()
-    LottoWin.input()
-    WinningStatistics.calculateWin(Lotto.lottoes)
-    WinningStatistics.print(purchase.price)
+    try {
+        val purchase = Purchase(Purchase.inputPrice())
+        Lotto.publish(purchase.count)
+        Lotto.printCount()
+        LottoWin.input()
+        WinningStatistics.calculateWin(Lotto.lottoes)
+        WinningStatistics.print(purchase.price)
+    }catch (exception: IllegalArgumentException){
+
+    }
 }
