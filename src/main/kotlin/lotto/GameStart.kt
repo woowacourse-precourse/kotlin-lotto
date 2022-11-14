ebbcb30fead1ce2546ccc8cdc79c830b30a32142
JEGHTNER <jeheon0717@naver.com>
2022-11-14 15:57:19 +0900

    functions set to private

--- src/main/kotlin/lotto/GameStart.kt
+++ src/main/kotlin/lotto/GameStart.kt
@@ -17,11 +17,11 @@ class GameStart {
     private var table = listOf<List<Int>>()
     private var ranks = Array(8,{0})
     private var bonusNum = 0
-    fun createLottoNum(): List<Int> {
+    private fun createLottoNum(): List<Int> {
         return Randoms.pickUniqueNumbersInRange(1, 45, 6)
     }
 
-    fun publishLotto(money : Int):List<List<Int>>
+    private fun publishLotto(money : Int):List<List<Int>>
     {
         var table = mutableListOf<List<Int>>()
         var i :Int = 0
@@ -31,12 +31,12 @@ class GameStart {
         }
         return table
     }
-    fun printLottos(table : List<List<Int>>){
+    private fun printLottos(table : List<List<Int>>){
         println("${table.size}개를 구매했습니다.")
         for (i in table)
             println(i)
     }
-    fun inputMoney() : Int{
+    private fun inputMoney() : Int{
         println("구입금액을 입력해 주세요.")
         var money = Console.readLine()
         require(money.all{it.isDigit()}){"[ERROR] : 숫자만 입력해야 합니다."}
@@ -44,7 +44,7 @@ class GameStart {
 
         return money.toInt()
     }
-    fun inputWinNum() : List<Int> {
+    private fun inputWinNum() : List<Int> {
         println("당첨 번호를 입력해 주세요.")
         var nums = (Console.readLine().split(","))
         require(nums.size == 6){"[ERROR] : 숫자가 6개가 아닙니다."}
@@ -54,7 +54,7 @@ class GameStart {
 
         return nums.map{it.toInt()}
     }
-    fun inputBonusNum(winNum:List<Int>):Int{
+    private fun inputBonusNum(winNum:List<Int>):Int{
         println("보너스 번호를 입력해 주세요.")
         var remainder = (1..45).toMutableList()
         for(i in winNum) {
@@ -67,7 +67,7 @@ class GameStart {
         require(remainder.contains(ret.toInt())){"[ERROR] : 올바른 숫자가 아닙니다."}
         return ret.toInt()
     }
-    fun getWinningHistory(table: List<List<Int>>, winNum: List<Int>,bonusNum:Int) : Array<Int>{
+    private fun getWinningHistory(table: List<List<Int>>, winNum: List<Int>, bonusNum:Int) : Array<Int>{
         var ranks = Array(8,{0})
         for (lst in table){
             if (lst.filter{it in winNum}.size >=3)
@@ -79,14 +79,14 @@ class GameStart {
         }
         return ranks
     }
-    fun printWinningHistory(ranks:Array<Int>){
+    private fun printWinningHistory(ranks:Array<Int>){
         println("3개 일치 (5,000원) - " + ranks[3] + "개")
         println("4개 일치 (50,000원) - " + ranks[4]+ "개")
         println("5개 일치 (1,500,000원) - " + ranks[5]+ "개")
         println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + ranks[7]+ "개")
         println("6개 일치 (2,000,000,000원) - " + ranks[6]+ "개")
     }
-    fun getWinRatio(money:Int,ranks: Array<Int>):Double{
+    private fun getWinRatio(money:Int, ranks: Array<Int>):Double{
         var prizeSum : Double = 0.00
         for (i in 3..7) {
             if (i == 3)
@@ -102,7 +102,7 @@ class GameStart {
         }
         return (prizeSum/money * 100)
     }
-    fun printWinRatio(winRatio:Double){
+    private fun printWinRatio(winRatio:Double){
         println("총 수익률은 ${winRatio}%입니다.")
     }
     fun gameStart(){
