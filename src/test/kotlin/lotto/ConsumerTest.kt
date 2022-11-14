17b0d87542a2a2ae82d1bb8aa08bff74b2fc3d61
rhkrwngud445 <rhkrwngud445@gmail.com>
2022-11-14 17:39:08 +0900

    test : add test that compare lotto for place

---
+++ src/test/kotlin/lotto/ConsumerTest.kt
@@ -0,0 +1,59 @@
+package lotto
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class ConsumerTest {
+    lateinit var consumer : Consumer
+
+    @BeforeEach
+    fun setUp(){
+        consumer = Consumer(8000)
+    }
+
+    @Test
+    fun `1등 테스트`(){
+        consumer.myLotto.clear()
+        consumer.myLotto.add(mutableListOf(8, 21, 23, 41, 42, 47))
+        val result = consumer.compareLotto(Lotto(mutableListOf(8, 21, 23, 41, 42, 47)),7)
+        print(consumer.myLotto[0])
+        Assertions.assertThat(result[0]).isEqualTo(1)
+    }
+
+    @Test
+    fun `2등 테스트`(){
+        consumer.myLotto.clear()
+        consumer.myLotto.add(mutableListOf(8, 21, 23, 41, 42, 43))
+        val result = consumer.compareLotto(Lotto(mutableListOf(8, 21, 23, 41, 42, 47)),43)
+        print(consumer.myLotto[0])
+        Assertions.assertThat(result[1]).isEqualTo(1)
+    }
+
+    @Test
+    fun `3등 테스트`(){
+        consumer.myLotto.clear()
+        consumer.myLotto.add(mutableListOf(1, 2, 3, 4, 5, 6))
+        val result = consumer.compareLotto(Lotto(mutableListOf(1, 2, 3, 4, 5, 7)),42)
+        print(consumer.myLotto[0])
+        Assertions.assertThat(result[2]).isEqualTo(1)
+    }
+
+    @Test
+    fun `4등 테스트`(){
+        consumer.myLotto.clear()
+        consumer.myLotto.add(mutableListOf(1, 2, 3, 4, 5, 6))
+        val result = consumer.compareLotto(Lotto(mutableListOf(1, 2, 3, 4, 8, 7)),42)
+        print(consumer.myLotto[0])
+        Assertions.assertThat(result[3]).isEqualTo(1)
+    }
+
+    @Test
+    fun `5등 테스트`(){
+        consumer.myLotto.clear()
+        consumer.myLotto.add(mutableListOf(1, 2, 3, 4, 5, 6))
+        val result = consumer.compareLotto(Lotto(mutableListOf(1, 2, 3, 9, 8, 7)),42)
+        print(consumer.myLotto[0])
+        Assertions.assertThat(result[4]).isEqualTo(1)
+    }
+}
