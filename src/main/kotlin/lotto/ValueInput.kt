78b76ac0d688e68d443502cc0a9ffecd139d9b87
otter <a33397397@gmail.com>
2022-11-14 22:43:40 +0900

    feat: 보너스 번호 입력 기능 추가

--- src/main/kotlin/lotto/ValueInput.kt
+++ src/main/kotlin/lotto/ValueInput.kt
@@ -1,24 +1,25 @@
 package lotto
 
 class ValueInput {
+    private val input: String = readln()
+    private val inspection: ValueInspection = ValueInspection()
 
     fun price(): Int {
         println(REQUIRE_MESSAGE_LOTTO_PRICE)
-
-        val input: String = readln()
-        ValueInspection().price(input)
-
+        inspection.price(input)
         return input.toInt()
     }
 
     fun jackpot(): List<Int> {
         println(REQUIRE_MESSAGE_JACKPOT_NUMBER)
-
-        val input: String = readln()
-        ValueInspection().price(input)
-
+        inspection.jackpot(input)
         return input.split(",").map { it.toInt() }
     }
 
-    
+    fun bonus(): Int {
+        println(REQUIRE_MESSAGE_BONUS_NUMBER)
+        inspection.bonus(input)
+        return input.toInt()
+    }
+
 }
