6a3bc6b378b232413cf68217860edb45ea518d17
sojk401 <sojk401@gmail.com>
2022-11-14 16:17:01 +0900

    feat(lotto): 일치한 번호가 5개일 때 보너스 번호가 일치하는지 판단하는 기능 구현

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -8,10 +8,26 @@ class Lotto(private val numbers: List<Int>) {
     }
 
     fun getMatchingNumber(lottoNumber: List<List<Int>>) {
-        for (randLotto in lottoNumber) {
-            var matchingNumber = LOTTO_SIZE - (numbers.minus(randLotto)).size
-            println("${randLotto}와 ${numbers} 비교: ${matchingNumber}개 일치")
-        }
+        // 발행된 로또와 당첨 번호를 비교한다.
+        // 일치하는 번호 개수는 (로또길이 - (당첨번호 - 로또번호))로 구할 수 있다.
+        // 일치하는 번호 개수가 5이면 보너스 번호 일치 여부를 확인한다.
 
+        var matchingResult = mutableListOf<String>()
+        for (randLotto in lottoNumber) {
+            var matchingNumber = (LOTTO_SIZE - (numbers.minus(randLotto)).size).toString()
+            if (matchingNumber == "5") {
+                matchingNumber = matchBonusNumber(randLotto)
+            }
+            matchingResult.add(matchingNumber)
+        }
+        println(matchingResult)
+    }
+
+    fun matchBonusNumber(randLotto: List<Int>): String{
+        var seller = Seller()
+        if (seller.bonusNumber in randLotto.joinToString("")) {
+            return "5bonus"
+        }
+        return "5"
     }
 }
