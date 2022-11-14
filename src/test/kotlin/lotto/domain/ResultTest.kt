0d801a3ad58b28d93d8058351e6d3cc13cda4cb6
Jongwon-0518 <77ha@naver.com>
2022-11-14 23:09:54 +0900

    test: ResultTest.kt - 수익률 계산

--- src/test/kotlin/lotto/domain/ResultTest.kt
+++ src/test/kotlin/lotto/domain/ResultTest.kt
@@ -15,4 +15,12 @@ internal class ResultTest {
         val lottoLog = arrayListOf<Lotto>(lottoNumber, Lotto(listOf<Int>(1, 2, 20, 4, 5, 6)), Lotto(listOf<Int>(1, 2, 3, 4, 5, 17)), Lotto(listOf<Int>(11, 12, 13, 14, 15, 27)))
         assertEquals(list, Result.compare(lottoLog, lottoNumber, bonusNumber))
     }
+
+    @Test
+    fun `기능테스트(calculateRatio)`() {
+        val result = listOf<Int>(1, 0, 0, 0, 1)
+        val price = 2000
+        val answer:Double = 100000250.0
+        assertEquals(answer, Result.calculateRatio(price, result))
+    }
 }
