376b1908b6bd3898aab70222f78e63fb0dfb2c9c
[alsswl] <[alswl4223@naver.com]>
2022-11-14 11:29:07 +0900

    feat : emun클래스 생성

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,13 @@
 package lotto
+import camp.nextstep.edu.missionutils.Randoms
+import camp.nextstep.edu.missionutils.Console
 
+enum class Grade(val grade:Int){First(2000000000),
+    Second(30000000),
+    Third(1500000),
+    Fourth(50000),
+    Fifth(5000);
+}
 fun main() {
-    TODO("프로그램 구현")
+
 }
