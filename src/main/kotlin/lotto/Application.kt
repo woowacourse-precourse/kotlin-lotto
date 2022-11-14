b375efd01ef419d88532c8c42b0382445cf510f2
Jaehun-Jo <chojh922@gmail.com>
2022-11-14 17:01:40 +0900

    [Feat] 입력 받은 금액 예외처리 메서드 추가(입력 받은 값이 숫자인지 체크)

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -4,7 +4,11 @@ import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
-    lotto()
+    try {
+        lotto()
+    } catch (e: IllegalArgumentException) {
+
+    }
 }
 
 fun lotto() {
@@ -16,9 +20,24 @@ fun lotto() {
 fun inputMoney(): Int {
     println("구입금액을 입력해 주세요.")
     val money = Console.readLine()
+    checkMoneyThrowException(money)
     val lottoCount = money.toInt() / 1000
     println("${lottoCount}개를 구매했습니다.")
     return lottoCount
+}
+
+fun checkMoneyThrowException(userInput: String) {
+    throwIsMoneyNumberException(userInput)
+}
+
+fun throwIsMoneyNumberException(userInput: String) {
+    val numbers = userInput.toCharArray().filter { char ->
+        char in '0'..'9'
+    }
+    if (numbers.size != userInput.length) {
+        println("[ERROR] 구입 금액은 숫자여야 합니다.")
+        throw IllegalArgumentException()
+    }
 }
 
 fun inputWinningNumber(): List<Int> {
