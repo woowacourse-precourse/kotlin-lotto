c75ac1365ea7f980dade4dcf768413170e29a572
kyounggudev <homin222@naver.com>
2022-11-15 00:53:21 +0900

    test : 예외처리 테스트케이스에 [ERROR]문구 확인 추가 및 리팩토링

--- src/test/kotlin/lotto/LottoWinTest.kt
+++ src/test/kotlin/lotto/LottoWinTest.kt
@@ -1,44 +1,65 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.test.NsTest
+import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
 
-class LottoWinTest {
+class LottoWinTest :NsTest(){
     @Test
     fun `당첨 번호의 개수가 6개가 아니다`() {
         assertThrows<IllegalArgumentException> {
             LottoWin.inputNumbers("1,2,3,4,5,6,7")
         }
+        Assertions.assertThat(output()).contains(ERROR_MESSAGE)
     }
+
     @Test
     fun `당첨 번호에 숫자가 아닌 수가 들어온다`() {
         assertThrows<IllegalArgumentException> {
             LottoWin.inputNumbers("1,2,3,4,5,a")
         }
+        Assertions.assertThat(output()).contains(ERROR_MESSAGE)
     }
+
     @Test
     fun `당첨 번호에 범위 밖의 값이 있다`() {
         assertThrows<IllegalArgumentException> {
             LottoWin.inputNumbers("1,2,3,4,5,47")
         }
+        Assertions.assertThat(output()).contains(ERROR_MESSAGE)
     }
+
     @Test
     fun `당첨 번호가 중복된다`() {
         assertThrows<IllegalArgumentException> {
             LottoWin.inputNumbers("1,2,3,4,5,5")
         }
+        Assertions.assertThat(output()).contains(ERROR_MESSAGE)
     }
+
     @Test
     fun `보너스 번호가 숫자가 아니다`() {
         assertThrows<IllegalArgumentException> {
             LottoWin.inputBonus("a")
         }
+        Assertions.assertThat(output()).contains(ERROR_MESSAGE)
     }
+
     @Test
     fun `보너스 번호가 범위 밖의 값이다`() {
         assertThrows<IllegalArgumentException> {
             LottoWin.inputBonus("0")
         }
+        Assertions.assertThat(output()).contains(ERROR_MESSAGE)
+    }
+
+    override fun runMain() {
+        main()
+    }
+
+    companion object {
+        private const val ERROR_MESSAGE = "[ERROR]"
     }
 }
