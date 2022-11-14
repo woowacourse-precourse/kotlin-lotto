ba0f0ef25ede410916ede853e66eb479d0cbdbca
vrexpert <vrexpert2302@gmail.com>
2022-11-14 17:43:16 +0900

    feat(lotto):입력 요구 함수 작성 및 입력 요구 메세지 삽입
    pleaseInput() 함수 작성
    문자열을 인자로 넘겨주어 InputUi 함수에 입력 요구 메세지 삽입

--- src/main/kotlin/lotto/ui/InputUi.kt
+++ src/main/kotlin/lotto/ui/InputUi.kt
@@ -3,15 +3,19 @@ package lotto.ui
 import camp.nextstep.edu.missionutils.Console
 
 class InputUi {
+    val printUi = PrintUi()
     fun inputMoney(): Int {
+        printUi.pleaseInput("구입금액을")
         return Console.readLine().toInt()
     }
 
     fun inputWinningNumbers():List<Int>{
+        printUi.pleaseInput("당첨 번호를")
         return Console.readLine().split(",").map(String::toInt)
     }
 
     fun inputBonusNumber():Int{
+        printUi.pleaseInput("보너스 번호를")
         return Console.readLine().toInt()
     }
 }
---
+++ src/main/kotlin/lotto/ui/PrintUi.kt
@@ -0,0 +1,7 @@
+package lotto.ui
+
+class PrintUi {
+    fun pleaseInput(message:String){
+        println("$message 입력해 주세요.")
+    }
+}
