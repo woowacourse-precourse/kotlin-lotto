2528d5d91730a643e3fa42c5b8fb54c6e9eaab5a
조성록 <33805423+Dev-Joco@users.noreply.github.com>
2022-11-15 03:18:22 +0900

    feat(LottoGame.kt): 로또 게임 실행 함수 play() 정의

--- src/main/kotlin/lotto/domain/LottoGame.kt
+++ src/main/kotlin/lotto/domain/LottoGame.kt
@@ -1,6 +1,24 @@
 package lotto.domain
 
+import lotto.data.WinningLotto
+import lotto.data.WinningTable
+import lotto.ui.Console
+
 class LottoGame {
 
+    fun play() {
+        val money = Console.requestPurchaseAmount()
+        val lotteries = LottoShop.buy(money)
 
+        Console.print(lotteries)
+
+        val winningLotto = WinningLotto(
+            numbers = Console.requestWinningNumbers(),
+            bonusNumber = Console.requestBonusNumber(),
+        )
+
+        Console.print(
+            WinningTable(money, lotteries, winningLotto)
+        )
+    }
 }
