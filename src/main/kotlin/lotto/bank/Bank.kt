f12aeee2bb1f5e3e9b73d20961353b5db39d348c
changhwan <chang5hustle@gmail.com>
2022-11-15 01:01:49 +0900

    feat(lotto) : Bank 인터페이스 로또번호 당첨내역,수익률 계산 함수 작성

--- src/main/kotlin/lotto/bank/Bank.kt
+++ src/main/kotlin/lotto/bank/Bank.kt
@@ -1,4 +1,14 @@
 package lotto.bank
 
+import lotto.Lotto
+
 interface Bank {
+
+    fun calcPrizeGrade()
+
+    fun calcWinningMainLottoNumbers(receivedMainLottoNumbers: List<Int>, lotto: Lotto): Int
+
+    fun calcWinningBonusLottoNumber(receivedBonusLottoNumber: Int, lotto: Lotto): Boolean
+
+    fun calcEarningRate(purchaseAmount: Int, prizeGrades: List<Int>)
 }
