e3ce459e43ee85571efdfedd71d95ea48e7b60c4
nayoung511 <nayoungbae511@gmail.com>
2022-11-15 00:46:57 +0900

    feat(Lotto.kt): enum 형식에 따라 결과를 도출하는 함수
    맞춘 숫자의 갯수로 enum class에서 어떤 원소에 해당하는지 반환

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -15,7 +15,30 @@ class Lotto(private val numbers: List<Int>) {
             if ( index == 0 ) print("[$i, ")
             if ( index > 0 && index < numbers.size-1) print("$i, ")
             if ( index == numbers.size-1 ) println("$i]")
-
         }
+    }
+
+    // 결과 출력
+    fun generateResult(winLottoNumber:List<Int>, bonusNumber: Int): LottoWinningPlace {
+        // 해당 로또에 당첨 번호가 몇 개나 있는지 판별
+        val correctNumber = numbers.count { winLottoNumber.contains(it) }
+        // 1등 (6개)
+        if ( correctNumber == 6 ) return LottoWinningPlace.FIRST
+
+        // 2등, 3등 (5개)
+        if ( correctNumber == 5 ) {
+            // 2등: 해당 로또가 보너스 번호를 가지고 있는지
+            if ( numbers.contains(bonusNumber) ) return LottoWinningPlace.SECOND
+            return LottoWinningPlace.THIRD
+        }
+
+        // 4등 (4개)
+        if ( correctNumber == 4 ) return LottoWinningPlace.FOURTH
+
+        // 5등 (3개)
+        if ( correctNumber == 3 ) return LottoWinningPlace.FIFTH
+
+        // 탈락
+        return LottoWinningPlace.NOTHING
     }
 }
