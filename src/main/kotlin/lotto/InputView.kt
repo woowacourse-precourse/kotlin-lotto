bd15edc1d0f9dc3a94d708dde8760f363ab3b1b4
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2022-11-14 14:35:48 +0900

    refactor[lotto]: 보너스 번호 입력받는 기능 리팩토링(InputView클래스)

--- src/main/kotlin/lotto/InputView.kt
+++ src/main/kotlin/lotto/InputView.kt
@@ -18,14 +18,28 @@ class InputView {
         return Lotto(winninglottos.map { it.toInt() })
     }
 
+    fun getInputBonus() : Int {
+        return inputBonus().toInt()
+    }
+
+    private fun inputBonus(): String {
+        println(INPUT_BONUS_MSG)
+        return checkInputBonus(Console.readLine())
+    }
+
+    private fun checkInputBonus(input: String?): String {
+        validator.checkBonusInput(input)
+        return input!!
+    }
+
     fun inputWinningLotto(): String {
         println(INPUT_WINNING_LOTTO_MSG)
         return checkInputWinningLotto(Console.readLine())
     }
 
-    private fun checkInputWinningLotto(input: String): String {
+    private fun checkInputWinningLotto(input: String?): String {
         validator.checkWinnigNumberInput(input)
-        return input
+        return input!!
     }
 
     fun inputMoney(): String {
@@ -33,15 +47,16 @@ class InputView {
         return checkInputMoney(Console.readLine())
     }
 
-    fun checkInputMoney(input: String): String {
+    fun checkInputMoney(input: String?): String {
 
         validator.checkMoneyInput(input)
-        return input
+        return input!!
 
     }
 
     companion object {
-        const val INPUT_MONEY_MSG = "구입금액을 입력해 주세요."
+        const val INPUT_MONEY_MSG = "구입 금액을 입력해 주세요."
         const val INPUT_WINNING_LOTTO_MSG = "당첨 번호를 입력해 주세요."
+        const val INPUT_BONUS_MSG = "보너스 번호를 입력해 주세요."
     }
 }
