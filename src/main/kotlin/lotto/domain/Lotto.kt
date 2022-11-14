afebb4b1fad291b9d79b78f12af953efcac5df59
re4rk <37167652+re4rk@users.noreply.github.com>
2022-11-14 18:55:12 +0900

    refactor(lotto): 간결한 코드로 교체

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -6,7 +6,7 @@ class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == LOTTO_NUMBER_COUNT) { ERROR_INPUT_VALUE_SIX_ONLY }
         require(numbers.toSet().size == LOTTO_NUMBER_COUNT) { ERROR_INPUT_VALUE_NO_DUPLICATES }
-        require(numbers.filter { it in LOTTO_START_NUMBER..LOTTO_END_NUMBER }.size == LOTTO_NUMBER_COUNT) {
+        require(numbers.firstOrNull { it !in LOTTO_START_NUMBER..LOTTO_END_NUMBER } == null) {
             ERROR_INPUT_VALUE_ONE_OR_FORTY_FIVE_ONLY
         }
     }
