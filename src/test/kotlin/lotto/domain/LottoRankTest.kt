cc1b5a93aba6fc84334ed77bec492e0d31809d1d
re4rk <37167652+re4rk@users.noreply.github.com>
2022-11-14 19:17:38 +0900

    test(lotto): LottoRank 클래스 테스트 케이스 구분되도록 분리

--- src/test/kotlin/lotto/domain/LottoRankTest.kt
+++ src/test/kotlin/lotto/domain/LottoRankTest.kt
@@ -1,19 +1,26 @@
 package lotto.domain
 
+import lotto.resources.LOTTO_NUMBER_COUNT
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class LottoRankTest {
     @Test
-    fun `기능 테스트`() {
+    fun `해당 값들이 잘 나온다`() {
         assertThat(
-            LottoRank.of(6, false) == LottoRank.FIRST &&
-            LottoRank.of(5, true) == LottoRank.SECOND &&
-            LottoRank.of(5, false) == LottoRank.THIRD &&
-            LottoRank.of(4, false) == LottoRank.FOURTH &&
-            LottoRank.of(3, false) == LottoRank.FIFTH &&
-            LottoRank.of(2, false) == LottoRank.NONE &&
-            LottoRank.of(3, true) == LottoRank.NONE
+            LottoRank.of(LOTTO_NUMBER_COUNT, false) == LottoRank.FIRST &&
+            LottoRank.of(LOTTO_NUMBER_COUNT - 1, true) == LottoRank.SECOND &&
+            LottoRank.of(LOTTO_NUMBER_COUNT - 1, false) == LottoRank.THIRD &&
+            LottoRank.of(LOTTO_NUMBER_COUNT - 2, false) == LottoRank.FOURTH &&
+            LottoRank.of(LOTTO_NUMBER_COUNT - 3, false) == LottoRank.FIFTH &&
+            LottoRank.of(LOTTO_NUMBER_COUNT - 4, false) == LottoRank.NONE
+        )
+    }
+    @Test
+    fun `없는 값은 NONE이 나온다`() {
+        assertThat(
+            LottoRank.of(LOTTO_NUMBER_COUNT, true) == LottoRank.NONE &&
+            LottoRank.of(LOTTO_NUMBER_COUNT - 5, true) == LottoRank.NONE
         )
     }
 }
