11a5142f01edc949c074fe421f66665714dd6963
Min-Jae-Bae <qoalswo123@icloud.com>
2022-11-14 15:27:12 +0900

    [Feat] (Winning.kt) 당첨 내역을 출력하는 기능 추가
    당첨 내역을 출력하는 updateRank 기능 추가

--- src/main/kotlin/lotto/Winning.kt
+++ src/main/kotlin/lotto/Winning.kt
@@ -10,22 +10,54 @@ enum class Rank(val reward: Int) {
 }
 
 class Winning {
-    // Rank 반환,, 돈 추가
-    fun updateRank(matchCount: Int): Any {
-/*        val lottoNumber = Lotto(listOf(1,2,3,4,5,6))
-        val matchCountList = lottoNumber.createNumber()*/
-        val rankResult = when (matchCount) {
-            NONE_MATCH, ONE_MATCH, TWO_MATCH -> Rank.LAST
-            THREE_MATCH -> Rank.FIFTH
-            FOUR_MATCH -> Rank.FOURTH
-            FIVE_MATCH -> Rank.THIRD
-            SIX_MATCH -> Rank.FIRST
-            else -> Rank.SECOND
+
+    fun createRankList(matchCount: Int): List<Rank> {
+        val lottoNumber = Lotto(listOf(1, 2, 3, 4, 5, 6))
+        val matchCountList = lottoNumber.createNumber()
+        val rankList = matchCountList.map { matchCount ->
+            when (matchCount) {
+                NONE_MATCH, ONE_MATCH, TWO_MATCH -> Rank.LAST
+                THREE_MATCH -> Rank.FIFTH
+                FOUR_MATCH -> Rank.FOURTH
+                FIVE_MATCH -> Rank.THIRD
+                SIX_MATCH -> Rank.FIRST
+                else -> Rank.SECOND
+            }
         }
-
-        return rankResult
-
+        return rankList
     }
+
+    fun updateRank() {
+        val rankList = createRankList(0)
+        println("\n당첨 통계\n---")
+        println("3개 일치 (5,000원) - ${updateFifthCount(rankList)}개")
+        println("4개 일치 (50,000원) - ${updateFourthCount(rankList)}개")
+        println("5개 일치 (1,500,000원) - ${updateThirdCount(rankList)}개")
+        println("5개 일치, 보너스 볼 일치 (30,000,000원) - ${updateSecondCount(rankList)}개")
+        println("6개 일치 (2,000,000,000원) - ${updateFirstCount(rankList)}개")
+    }
+
+    fun updateFifthCount(rankList: List<Rank>): Int {
+        return rankList.count { rank -> rank == Rank.FIFTH }
+    }
+
+    fun updateFourthCount(rankList: List<Rank>): Int {
+        return rankList.count { rank -> rank == Rank.FOURTH }
+    }
+
+    fun updateThirdCount(rankList: List<Rank>): Int {
+        return rankList.count { rank -> rank == Rank.THIRD }
+    }
+
+    fun updateSecondCount(rankList: List<Rank>): Int {
+        return rankList.count { rank -> rank == Rank.SECOND }
+    }
+
+    fun updateFirstCount(rankList: List<Rank>): Int {
+        return rankList.count { rank -> rank == Rank.FIRST }
+    }
+
+
 }
 
 const val NONE = 0
