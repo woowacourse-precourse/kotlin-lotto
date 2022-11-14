c4198cb4562ff11041a4d6e6c0ab7d2e6647115d
nayoung511 <nayoungbae511@gmail.com>
2022-11-15 00:40:55 +0900

    feat(Application.kt): 사용자 입력만큼의 로또를 담을 리스트 생성 함수

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -10,6 +10,25 @@ fun generateLotto(): Lotto {
     return Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6))
 }
 
+/*
+* Parameters: userAmountTobuy
+* Return: Lotto 생성자를 담은 리스트
+* Do: 사용자가 입력한 금액만큼 로또를 담을 리스트 생성
+* */
+fun buyLotto(userAmountTobuy: Int): List<Lotto> {
+    println("\n"+"$userAmountTobuy"+"개를 구매했습니다.")
+
+    // 전체 로또번호를 담을 리스트
+    var lottoList: MutableList<Lotto> = mutableListOf()
+
+    // 리스트에 갯수만큼 로또를 생성하여 담아준다
+    for ( i in 0..userAmountTobuy - 1 ) {
+        lottoList.add(generateLotto())
+    }
+
+    return lottoList
+}
+
 
 fun main() {
     // 1. 사용자 입력 받기
