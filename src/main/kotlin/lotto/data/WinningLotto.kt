8864f76ccd71c8b10bce57708a732ed745858fef
조성록 <33805423+Dev-Joco@users.noreply.github.com>
2022-11-14 19:26:49 +0900

    feat(WinningLotto.kt): 생성자 및 예외 처리

--- src/main/kotlin/lotto/data/WinningLotto.kt
+++ src/main/kotlin/lotto/data/WinningLotto.kt
@@ -1,5 +1,19 @@
 package lotto.data
 
+import lotto.error.ErrorMessages
+
+private typealias WinningLottoError = ErrorMessages.WinningLottoEnum
+
 class WinningLotto(numbers: List<Int>, bonusNumber: Int) {
 
+    private val lotto: Lotto
+    private val bonus: Int
+
+    init {
+        require(bonusNumber in Lotto.Range) { WinningLottoError.OutOfRangeNumbers }
+        require(!numbers.contains(bonusNumber)) { WinningLottoError.NotDuplicated }
+
+        lotto = Lotto(numbers)
+        bonus = bonusNumber
+    }
 }
