fd57ab6a1718eb43b096b7493e7f4f2ede9d717b
dohun_ <silver2679@gmail.com>
2022-11-14 23:08:33 +0900

    Feat(LottoBuyer): 숫자인지 문자인지 판단하는 함수 추가, 예외 처리 추가

--- src/main/kotlin/lotto/LottoBuyer.kt
+++ src/main/kotlin/lotto/LottoBuyer.kt
@@ -4,6 +4,18 @@ import camp.nextstep.edu.missionutils.Console
 
 class LottoBuyer {
 
+    fun chkNum(str: String) : Boolean {
+        var temp: Char
+        var result = false
+        for (i in 0 until str.length) {
+            temp = str.elementAt(i)
+            if (temp.toInt() < 48 || temp.toInt() > 57) {
+                result = true
+            }
+        }
+        return result
+    }
+
     fun inputLottoPurchasePrice(): Int {// 로또 구입 금액
         println("구입금액을 입력해 주세요.")
         val inputPrice = Console.readLine()
@@ -15,6 +27,7 @@ class LottoBuyer {
         when {
             inputPrice.toInt() < 1000 -> return throw IllegalArgumentException("[ERROR] 천원보다 적은 숫자 입니다.")
             inputPrice.toInt() % 1000 != 0 -> return throw IllegalArgumentException("[ERROR] 천원으로 나누어 떨어지지 않는 숫자입니다.")
+            chkNum(inputPrice) -> return throw IllegalArgumentException("[ERROR] 숫자가 아닙니다.")
         }
     }
 
