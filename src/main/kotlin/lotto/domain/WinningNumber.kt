97a2ee1472ac440ea457061d6a2d14f403402da5
qwe910205 <38097088+qwe910205@users.noreply.github.com>
2022-11-14 13:07:58 +0900

    feat(domain): 당첨 번호에 대해 로또의 당첨 결과 반환 기능 구현

--- src/main/kotlin/lotto/domain/WinningNumber.kt
+++ src/main/kotlin/lotto/domain/WinningNumber.kt
@@ -14,6 +14,16 @@ class WinningNumber(private val numbers: List<Int>, private val bonusNumber: Int
     fun bonusNumber() = bonusNumber
 
     fun winningResult(lotto: Lotto): WinningResult {
-        return WinningResult.FIRST
+        val matchCount = numbers.count { lotto.numbers().contains(it) }
+        val matchedBonus = lotto.numbers().contains(bonusNumber)
+
+        return when {
+            matchCount == 6 -> WinningResult.FIRST
+            matchCount == 5 && matchedBonus -> WinningResult.SECOND
+            matchCount == 5 && !matchedBonus -> WinningResult.THIRD
+            matchCount == 4 -> WinningResult.FOURTH
+            matchCount == 3 -> WinningResult.FIFTH
+            else -> WinningResult.NO_LUCK
+        }
     }
 }
