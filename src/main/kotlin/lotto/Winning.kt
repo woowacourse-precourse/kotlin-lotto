3202fa3681fa02de55138562157093787bc93c64
Min-Jae-Bae <qoalswo123@icloud.com>
2022-11-14 11:20:10 +0900

    [Feat] (Winning.kt) 번호 6개, 번호 5개와 보너스 일치할 때 각 반환 기능 추가
    번호 6개: FIRST 반환
    번호 5개, 보너스: SECOND

--- src/main/kotlin/lotto/Winning.kt
+++ src/main/kotlin/lotto/Winning.kt
@@ -4,7 +4,9 @@ enum class Rank(val reward: Int) {
     LAST(0),
     FIFTH(5000),
     FOURTH(50000),
-    THIRD(1500000)
+    THIRD(1500000),
+    SECOND(30000000),
+    FIRST(2000000000)
 }
 
 class Winning {
@@ -17,7 +19,8 @@ class Winning {
             3 -> Rank.FIFTH
             4 -> Rank.FOURTH
             5 -> Rank.THIRD
-            else -> matchCount
+            6 -> Rank.FIRST
+            else -> Rank.SECOND
         }
 
         return rankResult
