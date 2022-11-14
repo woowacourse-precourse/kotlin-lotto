7708e5510c89504e63d553d74c39b3f21bfc227d
changhwan <chang5hustle@gmail.com>
2022-11-14 22:17:32 +0900

    feat(lotto) : 로또 번호 발행 함수 오름차순 정렬 기능 추가

--- src/main/kotlin/lotto/lottoshop/lottomachine/LottoMachineImpl.kt
+++ src/main/kotlin/lotto/lottoshop/lottomachine/LottoMachineImpl.kt
@@ -1,5 +1,6 @@
 package lotto.lottoshop.lottomachine
 
+import camp.nextstep.edu.missionutils.Randoms.*
 import lotto.Lotto
 import lotto.LottoRule.LOTTO_NUMBER_RANGE_END
 import lotto.LottoRule.LOTTO_NUMBER_RANGE_START
@@ -8,11 +9,11 @@ import lotto.LottoRule.LOTTO_SIZE
 class LottoMachineImpl : LottoMachine {
     override fun drawLotto(): Lotto {
         return Lotto(
-            camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange(
+            pickUniqueNumbersInRange(
                 LOTTO_NUMBER_RANGE_START,
                 LOTTO_NUMBER_RANGE_END,
                 LOTTO_SIZE
-            )
+            ).sorted()
         )
     }
 }
