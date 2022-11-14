890c22575b1ef770f4bcb299b94a5e05c9931dfb
WoeunJung <leena09123@naver.com>
2022-11-15 00:02:53 +0900

    feat: add get Total Lotto Price function

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,20 @@
 package lotto
 
+import kotlin.system.exitProcess
+
 fun main() {
-    TODO("프로그램 구현")
+    val SIZE = getTotalLottoPrice()
+}
+
+fun getTotalLottoPrice(): Int {
+    println("구입 금액을 입력해주세요.")
+    try {
+        val price = readLine()?.toInt() ?: throw IllegalArgumentException()
+        if (price % 1000 != 0) throw IllegalArgumentException()
+        println()
+        return price/1000
+    } catch(e: IllegalArgumentException) {
+        println("[ERROR] 구입한 로또 가격 입력 오류")
+        exitProcess(0)
+    }
 }
