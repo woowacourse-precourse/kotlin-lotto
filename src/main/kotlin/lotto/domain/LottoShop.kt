707aec4214b9ca07549df9a176873f20d3fab87a
조성록 <33805423+Dev-Joco@users.noreply.github.com>
2022-11-14 19:43:23 +0900

    feat(LottoShop.kt): 구입 금액 예외 처리 함수 checkValidMoney() 구현

--- src/main/kotlin/lotto/domain/LottoShop.kt
+++ src/main/kotlin/lotto/domain/LottoShop.kt
@@ -1,3 +1,19 @@
 package lotto.domain
 
-object LottoShop
+import lotto.error.ErrorMessages
+
+private typealias LottoShopError = ErrorMessages.LottoShopEnum
+
+object LottoShop {
+
+    const val UNIT_PRICE = 1_000
+    const val MIN_VALUE = 1_000
+    const val MAX_VALUE = 2_000_000_000
+
+    val MoneyRange = MIN_VALUE..MAX_VALUE
+
+    private fun checkValidMoney(money: Int) {
+        require(money in MIN_VALUE..MAX_VALUE) { LottoShopError.OutOfRangeNumbers }
+        require(money % UNIT_PRICE == 0) { LottoShopError.NotMultiple }
+    }
+}
