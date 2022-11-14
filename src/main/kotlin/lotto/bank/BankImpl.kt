8ea107018a36798af4ee46104526397cf257ba54
changhwan <chang5hustle@gmail.com>
2022-11-15 01:03:19 +0900

    feat(lotto) : Bank 구현체 필드 생성 및 뼈대 작성

--- src/main/kotlin/lotto/bank/BankImpl.kt
+++ src/main/kotlin/lotto/bank/BankImpl.kt
@@ -1,4 +1,25 @@
 package lotto.bank
 
-class BankImpl {
+import lotto.Lotto
+import net.bytebuddy.agent.builder.AgentBuilder.RedefinitionStrategy.ResubmissionScheduler.AtFixedRate
+import kotlin.properties.Delegates
+
+class BankImpl : Bank {
+    var prizeGrades = listOf<Int>()
+    var earningRate by Delegates.notNull<Float>()
+    override fun calcPrizeGrade() {
+        TODO("Not yet implemented")
+    }
+
+    override fun calcWinningMainLottoNumbers(receivedMainLottoNumbers: List<Int>, lotto: Lotto) {
+        TODO("Not yet implemented")
+    }
+
+    override fun calcWinningBonusLottoNumber(receivedBonusLottoNumber: Int, lotto: Lotto) {
+        TODO("Not yet implemented")
+    }
+
+    override fun calcEarningRate(purchaseAmount: Int, prizeGrades: List<Int>) {
+        TODO("Not yet implemented")
+    }
 }
