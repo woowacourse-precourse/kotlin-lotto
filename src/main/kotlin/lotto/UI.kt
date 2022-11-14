465f69f68390917d486885496ae26246ab12768d
no1msh <no1msh1217@gmail.com>
2022-11-15 03:17:07 +0900

    feat: 사용자의 입출력을 담당하는 클래스 구현, 주요 출력 문구들 상수화

---
+++ src/main/kotlin/lotto/UI.kt
@@ -0,0 +1,20 @@
+package lotto
+
+class UI {
+
+    companion object {
+        const val REQUEST_INPUT_PURCHASING_AMOUNT = "구입금액을 입력해 주세요."
+        const val PURCHASING_LOTTO = "%d개를 구매했습니다."
+        const val REQUEST_INPUT_WIN_NUMBERS = "당첨 번호를 입력해 주세요."
+        const val REQUEST_INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요."
+        const val TOTAL_WIN_STATISTIC = "당첨 통계\n" +
+                "---\n" +
+                "3개 일치 (5,000원) - %d개\n" +
+                "4개 일치 (50,000원) - %d개\n" +
+                "5개 일치 (1,500,000원) - %d개\n" +
+                "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n" +
+                "6개 일치 (2,000,000,000원) - %d개"
+        const val RATE_OF_RETURN = "총 수익률은 %.1f%%입니다."
+
+    }
+}
