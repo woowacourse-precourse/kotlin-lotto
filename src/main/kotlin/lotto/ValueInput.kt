6b50d1dacb4e2d70be139e4edfc70a2769122c65
otter <a33397397@gmail.com>
2022-11-14 22:38:23 +0900

    feat: 당첨 번호 입력 기능 추가

--- src/main/kotlin/lotto/ValueInput.kt
+++ src/main/kotlin/lotto/ValueInput.kt
@@ -4,8 +4,21 @@ class ValueInput {
 
     fun price(): Int {
         println(REQUIRE_MESSAGE_LOTTO_PRICE)
-        val input: String = readLine()!!
+
+        val input: String = readln()
         ValueInspection().price(input)
+
         return input.toInt()
     }
+
+    fun jackpot(): List<Int> {
+        println(REQUIRE_MESSAGE_JACKPOT_NUMBER)
+
+        val input: String = readln()
+        ValueInspection().price(input)
+
+        return input.split(",").map { it.toInt() }
+    }
+
+    
 }
