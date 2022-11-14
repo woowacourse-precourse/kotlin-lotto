01da0acda033ed8759809df4693bcb3317253b9b
조성록 <33805423+Dev-Joco@users.noreply.github.com>
2022-11-14 19:15:57 +0900

    feat(ErrorMessage.kt): Lotto 클래스에서 사용되는 에러 메시지 enum class 정의

--- src/main/kotlin/lotto/error/ErrorMessages.kt
+++ src/main/kotlin/lotto/error/ErrorMessages.kt
@@ -1,9 +1,19 @@
 package lotto.error
 
+import lotto.data.Lotto
+
 object ErrorMessages {
 
     private const val PREFIX = "[ERROR]"
 
     private fun String.applyPrefix(): String = "$PREFIX $this"
 
+    enum class LottoEnum(private val message: String) {
+
+        RequiredSixLength("${Lotto.LENGTH}개의 숫자를 입력해주세요."),
+        NotDuplicated("중복된 숫자가 있으면 안됩니다."),
+        OutOfRangeNumbers("${Lotto.Range} 범위 내의 숫자여야 합니다."),;
+
+        override fun toString(): String = message.applyPrefix()
+    }
 }
