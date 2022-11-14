a584c90aeac616855f53db3c03fd78efcb181b84
otter <a33397397@gmail.com>
2022-11-14 21:58:38 +0900

    feat: 보너스 번호의 입력값을 검사하는 기능 추가

--- src/main/kotlin/lotto/ValueInspection.kt
+++ src/main/kotlin/lotto/ValueInspection.kt
@@ -17,8 +17,8 @@ class ValueInspection {
         if (!input.contains(",")) {
             IllegalArgumentException(ERROR_MESSAGE_INPUT_FORMAT)
         }
-        if (jackpotList.all { it !in 0..45 }) {
-            IllegalArgumentException(ERROR_MESSAGE_ONLY_NUMBER)
+        if (jackpotList.all { it !in 1..45 }) {
+            IllegalArgumentException(ERROR_MESSAGE_NUMBER_OUT_RANGE)
         }
         if (jackpotList.size != 6) {
             IllegalArgumentException(ERROR_MESSAGE_ONLY_SIX_NUMBER)
@@ -26,10 +26,20 @@ class ValueInspection {
         return true
     }
 
+    fun bonus(input: String): Boolean {
+        if (input.all { it !in '0'..'9' }) {
+            IllegalArgumentException(ERROR_MESSAGE_ONLY_NUMBER)
+        }
+        if (input.toInt() !in 1..45) {
+            IllegalArgumentException(ERROR_MESSAGE_NUMBER_OUT_RANGE)
+        }
+        return true
+    }
 
 }
 
 const val ERROR_MESSAGE_ONLY_NUMBER = "[ERROR] 숫자만 입력할 수 있습니다."
 const val ERROR_MESSAGE_ONLY_SIX_NUMBER = "[ERROR] 6개의 숫자만 입력할 수 있습니다."
+const val ERROR_MESSAGE_NUMBER_OUT_RANGE = "[ERROR] 1~45 범위의 숫자만 입력할 수 있습니다."
 const val ERROR_MESSAGE_INPUT_FORMAT = "[ERROR] 입력 형식이 잘못되었습니다."
 const val ERROR_MESSAGE_ONLY_UNIT_1000 = "[ERROR] 1000원 단위의 금액만 지불이 가능합니다."
