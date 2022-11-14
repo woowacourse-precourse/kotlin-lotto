d55f8ef34d905fe93c3d53a9f4b16f9dd4a4e503
sojk401 <sojk401@gmail.com>
2022-11-14 17:39:22 +0900

    feat(lotto): 로또 당첨 내역을 관리하는 클래스 구현

---
+++ src/main/kotlin/lotto/RewardInfo.kt
@@ -0,0 +1,21 @@
+package lotto
+
+enum class Reward(val rewardMoney: String, var correctNum: String) {
+    FIFTH("5,000","3"),
+    Four("50,000","4"),
+    Third("1,500,000","5"),
+    Second("30,000,000","5 보너스"),
+    First("2,000,000,000","6"),
+}
+
+class RewardInfo {
+    companion object {
+        var rewardResult = LinkedHashMap<Reward, Int>()
+    }
+
+    fun resultInit() {
+        for (reward in Reward.values()){
+            rewardResult[reward] = 0
+        }
+    }
+}
