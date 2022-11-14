944ca1ed52a7be32d6584743e13fc44fee5da5ae
Min-Jae-Bae <qoalswo123@icloud.com>
2022-11-14 10:57:19 +0900

    [Feat] (Winning.kt) 번호가 4개 일치할 때 FOURTH 반환 기능 추가

--- src/main/kotlin/lotto/Winning.kt
+++ src/main/kotlin/lotto/Winning.kt
@@ -2,7 +2,8 @@ package lotto
 
 enum class Rank(val reward: Int) {
     LAST(0),
-    FIFTH(5000)
+    FIFTH(5000),
+    FOURTH(50000)
 }
 
 class Winning {
@@ -13,6 +14,7 @@ class Winning {
         val rankResult = when (matchCount) {
             0,1,2 -> Rank.LAST
             3 -> Rank.FIFTH
+            4 -> Rank.FOURTH
             else -> matchCount
         }
 
