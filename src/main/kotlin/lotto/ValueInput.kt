affddc3ecb6cda963a5fe71553dc7a94c30f7e61
otter <a33397397@gmail.com>
2022-11-14 22:22:46 +0900

    refactor: String 상수를 파일로 묶음

--- src/main/kotlin/lotto/ValueInput.kt
+++ src/main/kotlin/lotto/ValueInput.kt
@@ -2,12 +2,10 @@ package lotto
 
 class ValueInput {
 
-
-    // 가격을 입력받아 숫자의 형태로 반환한다.
-//    fun price() : Int {
+//    fun price(): Int {
 //        println("구입금액을 입력해 주세요.")
-//        val input : String = readLine()!!
-//
+//        val input: String = readLine()!!
+//        ValueInspection().price(input)
 //        return inputPrice
 //    }
 }
--- src/main/kotlin/lotto/ValueInspection.kt
+++ src/main/kotlin/lotto/ValueInspection.kt
@@ -37,9 +37,3 @@ class ValueInspection {
     }
 
 }
-
-const val ERROR_MESSAGE_ONLY_NUMBER = "[ERROR] 숫자만 입력할 수 있습니다."
-const val ERROR_MESSAGE_ONLY_SIX_NUMBER = "[ERROR] 6개의 숫자만 입력할 수 있습니다."
-const val ERROR_MESSAGE_NUMBER_OUT_RANGE = "[ERROR] 1~45 범위의 숫자만 입력할 수 있습니다."
-const val ERROR_MESSAGE_INPUT_FORMAT = "[ERROR] 입력 형식이 잘못되었습니다."
-const val ERROR_MESSAGE_ONLY_UNIT_1000 = "[ERROR] 1000원 단위의 금액만 지불이 가능합니다."
