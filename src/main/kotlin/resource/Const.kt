79299d29db95ed448322f552342bccbf31e3f617
Sujin Kim <sujinkim919@gmail.com>
2022-11-15 04:44:08 +0900

    refactor: 상수 추가
    - Const : 상수화
    - StringResource : 문자열

---
+++ src/main/kotlin/resource/Const.kt
@@ -0,0 +1,12 @@
+package resource
+
+const val RANGE_START = 1
+const val RANGE_END = 45
+const val LOTTO_LENGTH = 6
+const val LOTTO_PRICE = 1000
+
+const val PRIZE_FIRST = 2_000_000_000
+const val PRIZE_SECOND = 30_000_000
+const val PRIZE_THIRD = 1_500_000
+const val PRIZE_FOURTH = 50_000
+const val PRIZE_FIFTH = 5_000
---
+++ src/main/kotlin/resource/StringResource.kt
@@ -0,0 +1,15 @@
+package resource
+
+const val INPUT_MONEY = "구입금액을 입력해 주세요."
+const val INPUT_NUMBERS = "당첨 번호를 입력해 주세요."
+const val INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요."
+
+const val PURCHASE_LOTTO = "개를 구매했습니다."
+
+const val WINNING_TITLE = "당첨 통계\n---"
+const val WINNING_FIFTH = "3개 일치 (${PRIZE_FIFTH}원)"
+const val WINNING_FOURTH = "4개 일치 (${PRIZE_FOURTH}원)"
+const val WINNING_THIRD = "5개 일치 (${PRIZE_THIRD}원)"
+const val WINNING_SECOND = "5개 일치, 보너스 볼 일치 (${PRIZE_SECOND}원)"
+const val WINNING_FIRST = "6개 일치 (${PRIZE_FIRST}원)"
+
