66767d5cbe7b11323fdd13583d2285a2be4087c9
Yuhee Kim <dbgml4509@naver.com>
2022-11-14 21:10:42 +0900

    feat : 로또 생성 시 사용되는 상수 크기 모음 enum class 생성

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,13 +1,17 @@
 package lotto
 
-//Lotto 클래스에 들어갈 기능
-/*
-
- */
-
 class Lotto(private val numbers: List<Int>){
+
+    enum class Sizes(val size : Int){
+        LOTTO_NUMBER_SIZE(6),
+        LOTTO_RANGE_END(45),
+        LOTTO_RANGE_START(1);
+    }
+
     init {
-        require(numbers.size == 6)
+        require(numbers.size == Sizes.valueOf("LOTTO_NUMBER_SIZE").size){
+            throw IllegalArgumentException("[ERROR] 로또 번호는 총 6개여야 합니다.")
+        }
     }
 
     // TODO: 추가 기능 구현
