d1880ec638911828f0a0da47c32d58d2d5fdb44e
s9hn <tnghqn@naver.com>
2022-11-15 02:04:00 +0900

    [FEAT(CustomerTest)] 지불한 금액 예외처리 테스트케이스 추가

---
+++ src/test/kotlin/lotto/CustomerTest.kt
@@ -0,0 +1,16 @@
+package lotto
+
+import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class CustomerTest {
+    @Test
+    fun `지불액이 1000원 단위가 아닐 경우, 예외발생`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> {
+                Customer().throwPaymentException(5123)
+            }
+        }
+    }
+}
