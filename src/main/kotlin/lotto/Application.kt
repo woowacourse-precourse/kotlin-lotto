c6d808ee9946e2d12815bdbee15b8297526355e4
dev-worthwhile <dev.lboram@gmail.com>
2022-11-14 20:54:48 +0900

    feat: show winning statistics of Lotto

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -98,3 +98,14 @@ fun validateBonusNum(bonusNum: String) {
         throw IllegalArgumentException(ErrorCode.NUMBER_RANGE_UNQUALIFIED.message)
     }
 }
+
+fun showWinningStatistics(result: List<Int>){
+    print("\n당첨 통계\n" +
+            "---\n" +
+            "3개 일치 (5,000원) - ${result[0]}개\n" +
+            "4개 일치 (50,000원) - ${result[1]}개\n" +
+            "5개 일치 (1,500,000원) - ${result[2]}개\n" +
+            "5개 일치, 보너스 볼 일치 (30,000,000원) - ${result[3]}개\n" +
+            "6개 일치 (2,000,000,000원) - ${result[4]}개\n"
+    )
+}
