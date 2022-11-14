6b91a71fa7211c4d3fc1a91763279e46b30231d1
krrong <khk9664@naver.com>
2022-11-14 13:09:31 +0900

    test(ExceptionHandler): 보너스 번호 예외 테스트 코드

--- src/test/kotlin/lotto/ExceptionHandlerTest.kt
+++ src/test/kotlin/lotto/ExceptionHandlerTest.kt
@@ -2,41 +2,52 @@ package lotto
 
 import org.junit.jupiter.api.Test
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.assertThrows
 
 class ExceptionHandlerTest {
     @Test
-    fun `1000으로 나누어 떨어지지 않는 예외 확인1`() {
-        val money = 1001
-        val result = ExceptionHandler.isDivideBy1000(money)
-        assertThat(result).isEqualTo(false)
-    }
-
-    @Test
-    fun `1000으로 나누어 떨어지지 않는 예외 확인2`() {
-        val money = 1000
-        val result = ExceptionHandler.isDivideBy1000(money)
-        assertThat(result).isEqualTo(true)
-    }
-
-    @Test
     fun `숫자로 이루어져있는지 1000원으로 나누어떨어지는지 예외 확인1`() {
-        val money = "1234"
-        val result = ExceptionHandler.checkInputMoney(money)
-        assertThat(result).isEqualTo(false)
+        assertThrows<IllegalArgumentException> {
+            val money = "1234"
+            ExceptionHandler.checkInputMoney(money)
+        }
     }
 
     @Test
     fun `숫자로 이루어져있는지 1000원으로 나누어떨어지는지 예외 확인2`() {
-        val money = "a123"
-        val result = ExceptionHandler.checkInputMoney(money)
-        assertThat(result).isEqualTo(false)
+        assertThrows<IllegalArgumentException> {
+            val money = "a123"
+            ExceptionHandler.checkInputMoney(money)
+        }
     }
 
     @Test
     fun `숫자로 이루어져있는지 1000원으로 나누어떨어지는지 예외 확인3`() {
-        val money = "10000"
+        val money = "2000"
         val result = ExceptionHandler.checkInputMoney(money)
         assertThat(result).isEqualTo(true)
     }
 
+    @Test
+    fun `보너스 번호 확인1`() {
+        assertThrows<IllegalArgumentException> {
+            val numberList = listOf<Int>(1, 2, 3, 4, 5, 6)
+            ExceptionHandler.checkBonusNumber("6", numberList)
+        }
+    }
+
+    @Test
+    fun `보너스 번호 확인2`() {
+        assertThrows<IllegalArgumentException> {
+            val numberList = listOf<Int>(1, 2, 3, 4, 5, 6)
+            ExceptionHandler.checkBonusNumber("46", numberList)
+        }
+    }
+
+    @Test
+    fun `보너스 번호 확인3`() {
+        val numberList = listOf<Int>(1, 2, 3, 4, 5, 6)
+        val result = ExceptionHandler.checkBonusNumber("15", numberList)
+        assertThat(result).isEqualTo(true)
+    }
 }
