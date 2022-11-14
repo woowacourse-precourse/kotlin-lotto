e8e1a7924f613f8d8bfb178c1c85b2d34b4791ef
SEO-J17 <qw4643987@naver.com>
2022-11-14 23:40:23 +0900

    feat: 번호 맞은 개수 당 금액을 계산하기 위한 ENUM 클래스 생성함

---
+++ src/main/kotlin/lotto/CorrectNumber.kt
@@ -0,0 +1,14 @@
+package lotto
+
+enum class CorrectNumber(
+    private val money: Int
+) {
+    THREE(5000),
+    FOUR(50_000),
+    FIVE(1_500_000),
+    FIVE_AND_BONUS(30_000_000),
+    SIX(2_000_000_000);
+
+    fun getMoney(): Int = money
+
+}
