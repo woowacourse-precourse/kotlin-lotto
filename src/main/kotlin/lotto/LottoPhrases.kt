125d06ddaf4a92b55a6134d52c69a2811bc3af98
changhwan <chang5hustle@gmail.com>
2022-11-14 17:04:45 +0900

    feat(lotto) : 로또 구입 금액 예외검사 함수 1000원단위 체크 함수 작성 및 호출

--- src/main/kotlin/lotto/LottoPhrases.kt
+++ src/main/kotlin/lotto/LottoPhrases.kt
@@ -3,4 +3,5 @@ package lotto
 object LottoPhrases {
     const val PURCHASE_AMOUNT_INPUT_TYPE_ERROR = "[ERROR] 입력하신 구입금액이 숫자가 아닙니다."
     const val PURCHASE_AMOUNT_MINIMUM_ERROR = "[ERROR] 입력하신 구입금액이 최소 구입 금액인 1000원 보다 작습니다"
+    const val PURCHASE_AMOUNT_UNIT_ERROR = "[ERROR] 입력하신 구입금액의 단위가 1000원 단위가 아닙니다."
 }
--- src/main/kotlin/lotto/lottoshop/LottoShop.kt
+++ src/main/kotlin/lotto/lottoshop/LottoShop.kt
@@ -2,6 +2,7 @@ package lotto.lottoshop
 
 import lotto.LottoPhrases.PURCHASE_AMOUNT_INPUT_TYPE_ERROR
 import lotto.LottoPhrases.PURCHASE_AMOUNT_MINIMUM_ERROR
+import lotto.LottoPhrases.PURCHASE_AMOUNT_UNIT_ERROR
 import java.util.regex.Pattern
 
 class LottoShop() {
@@ -10,10 +11,16 @@ class LottoShop() {
         if (!isInputTypeNumber(purchaseAmount)) printErrorMessage(PURCHASE_AMOUNT_INPUT_TYPE_ERROR)
         val processedPurchaseAmount = convertToInt(purchaseAmount)
         checkMinimumPurchaseAmount(processedPurchaseAmount)
+        checkCorrectUnitPurchaseAmount(processedPurchaseAmount)
+        return processedPurchaseAmount
     }
 
     private fun checkMinimumPurchaseAmount(purchaseAmount: Int) {
         if (purchaseAmount < 1000) printErrorMessage(PURCHASE_AMOUNT_MINIMUM_ERROR)
+    }
+
+    private fun checkCorrectUnitPurchaseAmount(purchaseAmount: Int) {
+        if (purchaseAmount % 1000 != 0) printErrorMessage(PURCHASE_AMOUNT_UNIT_ERROR)
     }
 
     fun getValidateMainLottoNumbers(mainLottoNumbers: List<String>): List<Int> {
