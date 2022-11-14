3ed73f9c7f34b0957475c06a1d5ab189b8aa04f0
조성록 <33805423+Dev-Joco@users.noreply.github.com>
2022-11-15 02:49:05 +0900

    feat(LottoRank.kt): toString() 함수 오버라이딩

--- src/main/kotlin/lotto/data/LottoRank.kt
+++ src/main/kotlin/lotto/data/LottoRank.kt
@@ -1,5 +1,7 @@
 package lotto.data
 
+import java.text.DecimalFormat
+
 enum class LottoRank(
     val rank: Int,
     val hitCount: Int,
@@ -12,5 +14,13 @@ enum class LottoRank(
     THIRD(3, 5, false, 1_500_000),
     FOURTH(4, 4, false, 50_000),
     FIFTH(5, 3, false, 5_000),;
+
+    override fun toString(): String = buildString {
+        append("${hitCount}개 일치")
+        if (isBonusHit) {
+            append(", 보너스 볼 일치")
+        }
+        append(" (${DecimalFormat("#,###").format(prize)}원)")
+    }
 
 }
