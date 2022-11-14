f2e3c7537b87c7eb57f4f28a1dfe8d5f5900f4bf
choiwoosuk <wooseokchoi0508@gmail.com>
2022-11-14 15:37:03 +0900

    feat(Lotto) 로또 번호가 범위를 벗어나면 예외 발생 기능 구현

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -3,18 +3,25 @@ package lotto.domain
 class Lotto(private val numbers: List<Int>) {
     init {
         checkLottoNumberCountException()
+        checkNumberRangeException()
     }
 
-    fun checkLottoNumberCountException() {
-        if (this.numbers.size != LOTTO_NUMBER_COUNT) throw IllegalArgumentException(LOTTO_NUMBER_COUNT_ERROR_MESSAGE)
+    fun checkLottoNumberCountException(){
+        if(this.numbers.size!= LOTTO_NUMBER_COUNT) throw IllegalArgumentException(LOTTO_NUMBER_COUNT_ERROR_MESSAGE)
     }
 
-    companion object {
-        const val LOTTO_NUMBER_COUNT = 6
-        const val MIN_NUMBER = 1
-        const val MAX_NUMBER = 45
-        const val LOTTO_NUMBER_COUNT_ERROR_MESSAGE = "[ERROR] 로또 번호의 개수는 6이어야 합니다."
-        const val LOTTO_NUMBER_RANGE_ERROR_MESSAGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."
-        const val LOTTO_DUPLICATE_NUMBER_ERROR_MESSAGE = "[ERROR] 로또 번호는 중복되지 않아야 합니다."
+    fun checkNumberRangeException(){
+        this.numbers.forEach{number->
+            if(number< MIN_NUMBER || number> MAX_NUMBER) throw IllegalArgumentException(LOTTO_NUMBER_RANGE_ERROR_MESSAGE)
+        }
+    }
+
+    companion object{
+        const val LOTTO_NUMBER_COUNT=6
+        const val MIN_NUMBER=1
+        const val MAX_NUMBER=45
+        const val LOTTO_NUMBER_COUNT_ERROR_MESSAGE ="[ERROR] 로또 번호의 개수는 6이어야 합니다."
+        const val LOTTO_NUMBER_RANGE_ERROR_MESSAGE ="[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."
+        const val LOTTO_DUPLICATE_NUMBER_ERROR_MESSAGE ="[ERROR] 로또 번호는 중복되지 않아야 합니다."
     }
 }
