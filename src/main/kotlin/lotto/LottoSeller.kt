5e4c76e767874e3dbd9d5486e2b6eb8ea44f7681
dohun_ <silver2679@gmail.com>
2022-11-14 23:48:50 +0900

    Feat(LottoSeller): 보너스 번호 예외처리

--- src/main/kotlin/lotto/LottoSeller.kt
+++ src/main/kotlin/lotto/LottoSeller.kt
@@ -5,6 +5,18 @@ import camp.nextstep.edu.missionutils.Randoms
 
 class LottoSeller {
 
+    fun chkNum(str: String) : Boolean {
+        var temp: Char
+        var result = false
+        for (i in 0 until str.length) {
+            temp = str.elementAt(i)
+            if (temp.toInt() < 48 || temp.toInt() > 57) {
+                result = true
+            }
+        }
+        return result
+    }
+
     fun calLottoCount(inputPrice: Int): List<Lotto> {//로또 개수
         val amountLotto = lottoCount(inputPrice)
         return lottoNumbers(amountLotto)
@@ -45,9 +57,16 @@ class LottoSeller {
     fun inputBonusNumber(): Int {//보너스 번호 입력
         println("보너스 번호를 입력해 주세요.")
         val inputBonusNumber = Console.readLine()
+        inputBonusNumberException(inputBonusNumber.toInt())
         return inputBonusNumber.toInt()
     }
 
-
+    fun inputBonusNumberException(inputBonusNumber: Int) {
+        when {
+            inputBonusNumber < 1 -> return throw IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.")
+            inputBonusNumber > 45 -> return throw IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.")
+            chkNum(inputBonusNumber.toString()) -> return throw IllegalArgumentException("[ERROR] 숫자가 아닙니다.")
+        }
+    }
 
 }
