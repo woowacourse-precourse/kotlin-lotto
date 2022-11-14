2e4ce90b2d22fa577932c836e81fb85e8d3fa1fa
ChoHJin <cjc9739@naver.com>
2022-11-15 07:05:23 +0900

    refactor: 로또 리스트 출력 메소드 & 예외 사항 메소드 분리
    발급받은 로또 리스트를 출력하는 로직과 예외사항을 체크하는 로직을 분리하여 함수로 만듬.

--- src/main/kotlin/lotto/PurchaseLotto.kt
+++ src/main/kotlin/lotto/PurchaseLotto.kt
@@ -1,6 +1,7 @@
 package lotto
 
 import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
 
 class PurchaseLotto {
     private fun isNumber(inputPrice : String?) : Boolean {
@@ -12,13 +13,7 @@ class PurchaseLotto {
         println("구매금액을 입력해 주세요.")
 
         val inputMoney = Console.readLine()!!.trim()
-
-        if(!isNumber(inputMoney)) {
-            throw IllegalArgumentException("[ERROR] 구입 금액은 숫자로 입력하셔야 됩니다.")
-        }
-        if(inputMoney.toInt() % 1000 != 0) {
-            throw IllegalArgumentException("[ERROR] 구매 금액은 1000원 단위로 입력해주세요.")
-        }
+        checkPrice(inputMoney)
 
         return inputMoney.toInt()
     }
@@ -26,4 +21,28 @@ class PurchaseLotto {
     fun issuedLotto(money : Int) : Int{
         return money / 1000
     }
+
+    fun getLottoTicket(lottoList : MutableList<Lotto>, countLottoTicket : Int) : List<Lotto> {
+        println("\n${countLottoTicket}개를 구매했습니다.")
+        for(i in 0 until countLottoTicket) {
+            val lotto = Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6))
+            lottoList.add(lotto)
+            printIssuedLottoList(lotto.getLottoNumbers())
+        }
+        return lottoList
+    }
+
+    private fun printIssuedLottoList(lottoList : List<Int>) {
+        println(lottoList)
+    }
+
+    private fun checkPrice(inputMoney: String) {
+        if(!isNumber(inputMoney)) {
+            throw IllegalArgumentException("[ERROR] 구입 금액은 숫자로 입력하셔야 됩니다.")
+        }
+        if(inputMoney.toInt() % 1000 != 0) {
+            throw IllegalArgumentException("[ERROR] 구매 금액은 1000원 단위로 입력해주세요.")
+        }
+    }
+
 }
