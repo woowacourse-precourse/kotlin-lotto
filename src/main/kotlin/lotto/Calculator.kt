d47b236d29c5b7e546c86fd3c7949a97ee78b84a
agfalcon <agfalcon12@gmail.com>
2022-11-14 15:57:35 +0900

    calculateYield 기능 구현

--- src/main/kotlin/lotto/Calculator.kt
+++ src/main/kotlin/lotto/Calculator.kt
@@ -1,5 +1,7 @@
 package lotto
 
+import java.lang.Math.round
+
 class Calculator {
     fun compareLottoNum(lottoNum:List<List<Int>>, lotto:Lotto, bonus: Int): List<Int>{
         var score = listOf<Int>(0,0,0,0,0)
@@ -24,16 +26,32 @@ class Calculator {
                 score++
             }
         }
+
         when(score){
             3->return Answer.THREE
             4->return Answer.FOUR
             5->{
                 if(lottoNum.contains(bonus))
-                    Answer.BONUS
-                Answer.FIVE
+                    return Answer.BONUS
+                return Answer.FIVE
             }
-            6->Answer.SIX
+            6->return Answer.SIX
         }
         return Answer.NONE
+    }
+
+    fun calculateYield(amount: Int, score:List<Int>): Double{
+        var price : Double = 0.0
+        for(i in 0 until score.size){
+            when(i){
+                0 -> price += 5000 * score[i]
+                1 -> price += 50000 * score[i]
+                2 -> price += 1500000 * score[i]
+                3 -> price += 30000000 * score[i]
+                4 -> price += 2000000000 * score[i]
+                else -> continue
+            }
+        }
+        return round(price / amount / 10.0 *100)/100.0
     }
 }
--- src/test/kotlin/lotto/CalculatorTest.kt
+++ src/test/kotlin/lotto/CalculatorTest.kt
@@ -4,18 +4,19 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class CalculatorTest {
+    val calc = Calculator()
+    val lotto = Lotto(listOf(1,2,3,4,5,6))
+    @Test
+    fun compareLottoNumTest() {
+        assertThat(calc.checLotto(listOf(1,2,3,4,5,6), lotto, 7, )).isEqualTo(Answer.SIX)
+        assertThat(calc.checLotto(listOf(1,2,3,4,5,7), lotto,7, )).isEqualTo(Answer.BONUS)
+        assertThat(calc.checLotto(listOf(1,2,3,4,15,16), lotto,7, )).isEqualTo(Answer.FOUR)
+        assertThat(calc.checLotto(listOf(1,2,3,14,15,16), lotto, 7, )).isEqualTo(Answer.THREE)
+    }
 
-//    @Test
-//    fun compareLottoNumTest() {
-//        assertThat(compareLottoNum(listOf(1,2,3,4,5,6), 7, listOf(1,2,3,4,5,6))).isEqualTo(6)
-//        assertThat(compareLottoNum(listOf(1,2,3,4,5,6), 7, listOf(1,2,3,4,5,7))).isEqualTo(5)
-//        assertThat(compareLottoNum(listOf(1,2,3,4,5,6), 7, listOf(1,2,3,4,15,16))).isEqualTo(4)
-//        assertThat(compareLottoNum(listOf(1,2,3,4,5,6), 7, listOf(1,2,3,14,15,16))).isEqualTo(3)
-//    }
-//
-//    @Test
-//    fun calculateYieldTest() {
-//        assertThat(calculateYield(8, listOf(1,0,0,0))).isEqualTo(62.5)
-//        assertThat(calculateYield(8, listOf(0,1,0,0))).isEqualTo(625)
-//    }
+    @Test
+    fun calculateYieldTest() {
+        assertThat(calc.calculateYield(8, listOf(1,0,0,0,0))).isEqualTo(62.5)
+        assertThat(calc.calculateYield(8, listOf(0,1,0,0,0))).isEqualTo(625.0)
+    }
 }
