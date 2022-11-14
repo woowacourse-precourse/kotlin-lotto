f86d68ea65ed37f6f25cc70570270dbf80d07add
re4rk <37167652+re4rk@users.noreply.github.com>
2022-11-14 19:03:49 +0900

    test(lotto): Lotto 클래스 테스트 케이스 추가

--- src/test/kotlin/lotto/domain/LottoTest.kt
+++ src/test/kotlin/lotto/domain/LottoTest.kt
@@ -1,5 +1,6 @@
 package lotto.domain
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
@@ -20,4 +21,27 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+    @Test
+    fun `정상적인 로또 범위는 잘 생성된다`() {
+        assertThat(
+            Lotto(listOf(1, 2, 3, 4, 5, 6))
+        )
+    }
+
+    @Test
+    fun `로또 범위 이외의 숫자가 들어오면 에러가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(1, 2, 3, 4, 5, 46))
+        }
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(1, 2, 3, 4, 5, -1))
+        }
+    }
+
+    @Test
+    fun `로또 저장 후 매개변수를 잘 받을 수 있다`() {
+        assertThat(
+            Lotto(listOf(1, 2, 3, 4, 5, 6)).toList() == listOf(1, 2, 3, 4, 5, 6)
+        )
+    }
 }
