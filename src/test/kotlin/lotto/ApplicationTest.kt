e989d8857dd7ef21c041271d220b4fdef7ff1b96
ponopono0322 <ponopono0322@gmail.com>
2022-11-14 14:06:33 +0900

    test: 당첨 개수 검증 기능 테스트 추가
    3,4,5,5+보너스,6에 대해 각각 올바른 출력인지 확인

--- src/test/kotlin/lotto/ApplicationTest.kt
+++ src/test/kotlin/lotto/ApplicationTest.kt
@@ -4,10 +4,8 @@ import camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersI
 import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
 import camp.nextstep.edu.missionutils.test.NsTest
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
-import java.io.ByteArrayInputStream
 
 class ApplicationTest : NsTest() {
 
@@ -42,6 +40,34 @@ class ApplicationTest : NsTest() {
             listOf(7, 11, 30, 40, 42, 43),
             listOf(2, 13, 22, 32, 38, 45),
             listOf(1, 3, 5, 14, 22, 45)
+        )
+    }
+
+    @Test
+    fun `기능 테스트 등급별 당첨 개수 검증`() {
+        assertRandomUniqueNumbersInRangeTest(
+            {
+                run("5000", "1,2,3,4,5,6", "7")
+                assertThat(output()).contains(
+                    "5개를 구매했습니다.",
+                    "[1, 2, 3, 4, 5, 6]",
+                    "[1, 2, 3, 4, 5, 7]",
+                    "[1, 2, 3, 4, 5, 8]",
+                    "[1, 2, 3, 4, 8, 9]",
+                    "[1, 2, 3, 8, 9, 10]",
+                    "3개 일치 (5,000원) - 1개",
+                    "4개 일치 (50,000원) - 1개",
+                    "5개 일치 (1,500,000원) - 1개",
+                    "5개 일치, 보너스 볼 일치 (30,000,000원) - 1개",
+                    "6개 일치 (2,000,000,000원) - 1개",
+                    "총 수익률은 40631100.0%입니다."
+                )
+            },
+            listOf(1, 2, 3, 4, 5, 6),
+            listOf(1, 2, 3, 4, 5, 7),
+            listOf(1, 2, 3, 4, 5, 8),
+            listOf(1, 2, 3, 4, 8, 9),
+            listOf(1, 2, 3, 8, 9, 10)
         )
     }
 
