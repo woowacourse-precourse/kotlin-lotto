a7502e39c0c19d0de0d7c710817287b799299538
jklom0326 <jklom0326@gmail.com>
2022-11-15 07:16:34 +0900

    refactor(Lotto.kt): 상수 처리
    - 로또 번호 크기와 로또 숫자 범위 상수를 Units로 분리

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -3,12 +3,15 @@ package lotto
 import lotto.Message.Companion.DUPLICATED_NUMBERS
 import lotto.Message.Companion.NOT_NUMBER
 import lotto.Message.Companion.NOT_SIX_NUMBERS
+import lotto.Units.Companion.LOTTO_FIRST_NUMBER
+import lotto.Units.Companion.LOTTO_LAST_NUMBER
+import lotto.Units.Companion.LOTTO_UNIT
 
 class Lotto(private val numbers: List<Int>) {
 
     init {
-        require(numbers.size == 6) { NOT_SIX_NUMBERS }
-        require(numbers.distinct().size == 6) { DUPLICATED_NUMBERS }
+        require(numbers.size == LOTTO_UNIT) { NOT_SIX_NUMBERS }
+        require(numbers.distinct().size == LOTTO_UNIT) { DUPLICATED_NUMBERS }
         require(correctNumbers(numbers)) { NOT_NUMBER }
     }
 
@@ -18,7 +21,7 @@ class Lotto(private val numbers: List<Int>) {
 
     private fun correctNumbers(numbers: List<Int>): Boolean {
         for (number in numbers) {
-            if (number !in 1..45) return false
+            if (number !in LOTTO_FIRST_NUMBER..LOTTO_LAST_NUMBER) return false
         }
         return true
     }
--- src/main/kotlin/lotto/Units.kt
+++ src/main/kotlin/lotto/Units.kt
@@ -5,5 +5,8 @@ class Units {
         const val INIT_NUMBER = 0
         const val REMINDER = 0
         const val THOUSAND_UNIT = 1000
+        const val LOTTO_UNIT = 6
+        const val LOTTO_FIRST_NUMBER = 1
+        const val LOTTO_LAST_NUMBER = 45
     }
 }
