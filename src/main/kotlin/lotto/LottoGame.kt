8216c5860240f5686f3929260459369d2ca78792
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2022-11-14 15:24:33 +0900

    feat(LottoGame) : 로또 게임 구현

--- src/main/kotlin/lotto/LottoGame.kt
+++ src/main/kotlin/lotto/LottoGame.kt
@@ -1,10 +1,28 @@
 package lotto
 
+import lotto.domain.LotteryChecker
+import lotto.domain.Money
+import lotto.domain.Publisher
+import lotto.domain.WinningLottery
 import lotto.view.InputView
+import lotto.view.OutputView
 
 class LottoGame {
-
     fun run() {
-        val money = InputView.inputPurchasingAmount()
+        val money = Money(InputView.inputPurchasingAmount())
+        val lottoTicket = Publisher().generateLottery(money)
+        val winningLottery = WinningLottery(InputView.inputWinningLottery(), InputView.inputBonusNumber())
+
+        lotteryChecker.checkLotteryNumber(winningLottery, lottoTicket)
+        printResult(money, lotteryChecker)
+    }
+
+    private fun printResult(money: Money, lotteryChecker: LotteryChecker) {
+        OutputView.printResult(lotteryChecker)
+        OutputView.printRateOfProfit(money, lotteryChecker.getTotalWinnerPrice())
+    }
+
+    companion object {
+        private val lotteryChecker = LotteryChecker()
     }
 }
