b1454bcc2981ea8498a594b2568db7c0324088dc
choHarmony <jws0183@naver.com>
2022-11-15 02:12:06 +0900

    feat: 일치하는 숫자의 개수를 구해 당첨 복권의 개수 구하기

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -11,5 +11,14 @@ class Lotto(private val numbers: List<Int>) {
 
     fun checkResult(lottoNums: Array<Int>) {
         val different = lottoWinNum.toSet().minus(lottoNums.toSet()) // 당첨 번호와 선정된 번호 중 겹치지 않는 숫자 구하기
+        val winCount = lottoWinNum.size - different.size
+
+        if (winCount == 3) threeWin += 1
+        if (winCount == 4) fourWin += 1
+        if (winCount == 5) {
+            if (different.contains(bonusNum)) fiveBonusWin += 1
+            else fiveWin += 1
+        }
+        if (winCount == 6) sixWin += 1
     }
 }
