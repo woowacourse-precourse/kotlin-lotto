fff6669323739bfe5c621237525eacfa2e716eaf
bluecat0823 <star4_you@naver.com>
2022-11-14 20:42:44 +0900

    [feat] 로또 번호가 올바른 범위의 숫자로 구성되어 있는지 체크하는 기능

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -3,6 +3,11 @@ package lotto
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6)
+        for (number in 0..5){
+            if (numbers[number] !in 1..45){
+                throw IllegalArgumentException("[ERROR] 로또 번호는 1에서 45 사이의 숫자로 이루어져야 합니다.ㄹㅎ")
+            }
+        }
     }
 
     // TODO: 추가 기능 구현
