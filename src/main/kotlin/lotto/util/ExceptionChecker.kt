48c5bb5e36e6356b358973957605856ac4eac1f9
ghddbwns9808 <ghddbwns9808@gmail.com>
2022-11-15 00:50:31 +0900

    refactor: fix exception throw way Fix the way of exception throw to use require function

--- src/main/kotlin/lotto/util/ExceptionChecker.kt
+++ src/main/kotlin/lotto/util/ExceptionChecker.kt
@@ -1,22 +1,21 @@
 package lotto.util
 
 class ExceptionChecker {
-    fun priceExceptionCheck(price : Long){
-        if((price % 1000).toInt() != 0)
-            throw IllegalArgumentException("[ERROR] 로또는 1000원 단위로 구매 가능합니다.")
+    fun priceExceptionCheck(price : String){
+
+        require(price.all { it.isDigit() }){ println("[ERROR] 구입 금액은 숫자로 입력되어야 합니다.") }
+        val priceToLong = price.toLong()
+        require((priceToLong % 1000).toInt() == 0){ println("[ERROR] 로또는 1000원 단위로 구매 가능합니다.") }
+
     }
 
     fun validLottoNumExceptionCheck(winningNum: List<Int>){
-        for(i in winningNum.indices){
-            if(winningNum[i] < 1 || winningNum[i] > 45)
-                throw IllegalArgumentException("[ERROR] 로또 번호는 1~45의 정수여야 합니다.")
-        }
+        for(i in winningNum.indices)
+            require(winningNum[i] in 1 .. 45){ println("[ERROR] 로또 번호는 1~45의 정수여야 합니다.") }
     }
 
     fun validBonusNumExceptionCheck(winningNum: List<Int>, bonusNum: Int){
-        if(bonusNum < 1 || bonusNum > 45)
-            throw  IllegalArgumentException("[ERROR] 보너스 번호는 1~45의 정수여야 합니다.")
-        if(winningNum.contains(bonusNum))
-            throw IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복되지 않아야 합니다.")
+        require(bonusNum in 1..45){ println("[ERROR] 보너스 번호는 1~45의 정수여야 합니다.") }
+        require(!winningNum.contains(bonusNum)){ println("[ERROR] 보너스 번호는 당첨 번호와 중복되지 않아야 합니다.") }
     }
 }
