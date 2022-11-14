684ba4d4f0ef204ea84991d034889c91876fb285
dlgocks1 <cksgodl1@naver.com>
2022-11-14 12:17:38 +0900

    feat(Application) : 수익률 계산 기능 구현

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,9 +1,19 @@
 package lotto
 
+import java.lang.Math.pow
+import kotlin.math.floor
+import kotlin.math.pow
+import kotlin.math.round
+
 fun main() {
     TODO("프로그램 구현")
 }
 
 fun calEarningRate(purchasePrize: Int, winningPrize: Int): Double {
+    return roundDigit(winningPrize.toDouble() / purchasePrize.toDouble())
+}
 
+// 소숫점 둘 째자리에서 반올림한다.
+fun roundDigit(number: Double): Double {
+    return round(number * 1000.0) * 10.0.pow(-1.0)
 }
