3746096212577e84378879e7ce44f9a0ed4a46b3
조성록 <33805423+Dev-Joco@users.noreply.github.com>
2022-11-15 03:08:55 +0900

    feat(Console.kt): 출력 메시지 상수 enum class 정의

--- src/main/kotlin/lotto/ui/Console.kt
+++ src/main/kotlin/lotto/ui/Console.kt
@@ -2,4 +2,22 @@ package lotto.ui
 
 object Console {
 
+    enum class Message(vararg args: String) {
+
+        InputPurchaseAmount("구입금액을 입력해 주세요."),
+        InputWinningNumbers("당첨 번호를 입력해 주세요."),
+        InputBonusNumber("보너스 번호를 입력해 주세요."),
+
+        ShowLotto("개를 구매했습니다."),
+        ShowWinningStatistics(
+            "당첨 통계",
+            "---",
+            "총 수익률은 ",
+            "%입니다.",
+        );
+
+        val messages: List<String> = args.toList()
+
+        override fun toString(): String = messages.joinToString("\n")
+    }
 }
