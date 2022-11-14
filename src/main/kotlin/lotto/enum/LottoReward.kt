87976eee00f10b91511ad516185fe111697dfc4b
phs <gustjd0808@pusan.ac.kr>
2022-11-14 11:38:00 +0900

    feat(enum): make enum classes for lotto game
    - LottoGameSetting: configuration for lotto game
    - LottoGameString: string for lotto game
    - LottoReward: lotto reward amount

---
+++ src/main/kotlin/lotto/enum/LottoReward.kt
@@ -0,0 +1,13 @@
+package lotto.enum
+
+import java.text.DecimalFormat
+
+enum class LottoReward(private val reward: Int) {
+    FIFTH_REWARD(5_000),
+    FOURTH_REWARD(50_000),
+    THIRD_REWARD(1_500_000),
+    SECOND_REWARD(30_000_000),
+    FIRST_REWARD(2_000_000_000);
+
+    fun rewardString(): String = DecimalFormat("#,###").format(this.reward)
+}
