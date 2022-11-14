510d3e4b2e84b2781a5bbe66f909fd4792b50499
Gloria <ippnsj@gmail.com>
2022-11-15 08:21:38 +0900

    refactor: DivisibilityVerifier 에러 메세지 구체화
    에러 메세지가 너무 일반화되어 있어서 입력값이 어떤 수로 나누어 떨어지지
    않아서 예외가 발생했는지 알 수 없어 에러 메세지에 나누는 수가 무엇인지 추가

--- src/main/kotlin/lotto/utils/validation/DivisibilityVerifier.kt
+++ src/main/kotlin/lotto/utils/validation/DivisibilityVerifier.kt
@@ -2,10 +2,10 @@ package lotto.utils.validation
 
 class DivisibilityVerifier(private val divisor: Int) : Verifier<Int>() {
     override fun verify(input: Int) {
-        require(input % divisor == 0) { ERROR_MESSAGE }
+        require(input % divisor == 0) { "$ERROR_MESSAGE $divisor." }
     }
 
     companion object {
-        const val ERROR_MESSAGE = "Input is not divisible by the divisor."
+        const val ERROR_MESSAGE = "Input is not divisible by"
     }
 }
