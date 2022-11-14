c785ff028565db4a3c8913a718ed88f992b9e940
bluecat0823 <star4_you@naver.com>
2022-11-14 21:16:07 +0900

    [refactor] 금액 입력 기능 수정

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,21 +1,27 @@
 package lotto
 import camp.nextstep.edu.missionutils.Randoms
-import camp.nextstep.edu.missionutils.Console
 
-fun firstPay() : Int{
+fun firstPay() : Int?{
     var pay = 0
     println("구입금액을 입력해 주세요.")
     try {
         pay = readLine()!!.toInt()
+        pay/1000 != 0
+        return pay
     } catch (e : NumberFormatException){
         println("[ERROR] 입력된 금액이 올바른 금액이 아닙니다.")
     }
-    require(pay % 1000 == 0){
-        "[ERROR] 입력 금액이 1000원 단위가 아닙니다."
-    }
-    return pay
+    return null
 }
 
+fun lottoGenerator(amount : Int) : List<Lotto>{
+    val lottoBundle = mutableListOf<Lotto>()
+    for (lotto in 1..amount){
+        val lottoPaper : List<Int> = Randoms.pickUniqueNumbersInRange(1, 45, 6).sorted()
+        lottoBundle.add(Lotto(lottoPaper))
+    }
+    return lottoBundle
+}
 
 
 fun main() {
