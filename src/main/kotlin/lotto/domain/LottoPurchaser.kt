18e760440ea7bb7f9f6bb3a234a71fa4331178e7
82104 <tir2986@naver.com>
2022-11-14 19:37:06 +0900

    style(lottopurchaser): 로또 상수 부분 패키지명 명시하도록 변경

--- src/main/kotlin/lotto/domain/LottoPurchaser.kt
+++ src/main/kotlin/lotto/domain/LottoPurchaser.kt
@@ -1,31 +1,27 @@
 package lotto.domain
 
 import camp.nextstep.edu.missionutils.Randoms
-import lotto.domain.Lotto.Companion.COST
-import lotto.domain.Lotto.Companion.LENGTH
-import lotto.domain.Lotto.Companion.MAX_NUM
-import lotto.domain.Lotto.Companion.MIN_NUM
 
 class LottoPurchaser(val cost: Int) {
     init {
-        require(cost >= COST) { ERROR_COST_NOT_ENOUGH }
-        require(cost % COST == 0) { ERROR_COST_DIVIDE }
+        require(cost >= Lotto.COST) { ERROR_COST_NOT_ENOUGH }
+        require(cost % Lotto.COST == 0) { ERROR_COST_DIVIDE }
     }
 
     fun purchaseLottos(): List<Lotto> {
         val lottos = mutableListOf<Lotto>()
-        repeat(cost / COST) {
+        repeat(cost / Lotto.COST) {
             lottos.add(generateLotto())
         }
         return lottos
     }
 
     private fun generateLotto() = Lotto(
-        Randoms.pickUniqueNumbersInRange(MIN_NUM, MAX_NUM, LENGTH)
+        Randoms.pickUniqueNumbersInRange(Lotto.MIN_NUM, Lotto.MAX_NUM, Lotto.LENGTH)
     )
 
     companion object {
-        private const val ERROR_COST_NOT_ENOUGH = "구입금액은 ${COST}원 이상이어야 합니다."
-        private const val ERROR_COST_DIVIDE = "구입금액은 ${COST}로 나누어 떨어져야 합니다."
+        private const val ERROR_COST_NOT_ENOUGH = "구입금액은 ${Lotto.COST}원 이상이어야 합니다."
+        private const val ERROR_COST_DIVIDE = "구입금액은 ${Lotto.COST}로 나누어 떨어져야 합니다."
     }
 }
--- src/main/kotlin/lotto/domain/LottoRanker.kt
+++ src/main/kotlin/lotto/domain/LottoRanker.kt
@@ -1,13 +1,13 @@
 package lotto.domain
 
 class LottoRanker(val lottos: List<Lotto>, val winningNumber: WinningNumber) {
+    fun rankAll(): Map<Lotto, LottoRank> {
+        return lottos.associateWith { rank(it) }
+    }
+
     private fun rank(lotto: Lotto): LottoRank {
         val matchedCnt = lotto.compare(winningNumber.winLotto)
         val bonusMatched = lotto.contains(winningNumber.bonus)
         return LottoRank.valueOf(matchedCnt, bonusMatched)
-    }
-
-    fun rankAll(): Map<Lotto, LottoRank> {
-        return lottos.associateWith { rank(it) }
     }
 }
