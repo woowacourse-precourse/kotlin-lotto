bfa69c6514baf28ee00c8cbc01f10405427026ce
DormunDoorlock <skrudgh2001@naver.com>
2022-11-14 17:57:40 +0900

    test: 리팩토링 부분 test

--- src/test/kotlin/lotto/LottoGeneratorTest.kt
+++ src/test/kotlin/lotto/LottoGeneratorTest.kt
@@ -2,43 +2,10 @@ package lotto
 
 import lotto.domain.LottoGenerator
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
 import org.assertj.core.api.Assertions.assertThat
-
 class LottoGeneratorTest {
-
     @Test
-    fun `로또 구입 금액이 숫자가 아니면 예외가 발생한다`() {
-        assertThrows<IllegalArgumentException> {
-            val lottoGenerator = LottoGenerator()
-            lottoGenerator.createLottoNumber("1000j")
-        }
-    }
-    @Test
-    fun `로또 구입 금액이 0이하면 예외가 발생한다`() {
-        assertThrows<IllegalArgumentException> {
-            val lottoGenerator = LottoGenerator()
-            lottoGenerator.createLottoNumber("0")
-        }
-    }
-
-    @Test
-    fun `로또 구입 금액이 1,000원 단위가 아니면 예외가 발생한다`() {
-        assertThrows<IllegalArgumentException> {
-            val lottoGenerator = LottoGenerator()
-            lottoGenerator.createLottoNumber("1700")
-        }
-    }
-
-    @Test
-    fun `로또 구입 금액으로 로또 갯수 구하는 기능 테스트`() {
-        val lottoGenerator = LottoGenerator()
-        val result = lottoGenerator.getLottoCountValue(1000)
-        assertThat(result).isEqualTo(1)
-    }
-
-    @Test
-    fun `로또 구입 금액으로 로또 갯수 구하는 기능 테스트2`() {
+    fun `로또 구입 금액으로 로또 갯수 구하는 기능 테스트()`() {
         val lottoGenerator = LottoGenerator()
         val result = lottoGenerator.getLottoCountValue(10000)
         assertThat(result).isEqualTo(10)
