8887a418a97d95829ce57134c9062ec0051ad831
ponopono0322 <ponopono0322@gmail.com>
2022-11-14 14:49:34 +0900

    test: Lottery 클래스 0 입력 제한 케이스 추가
    구입 금액 0 입력시 IllegalArgumentException 반환

--- src/test/kotlin/lottery/LotteryTest.kt
+++ src/test/kotlin/lottery/LotteryTest.kt
@@ -13,6 +13,13 @@ internal class LotteryTest {
     }
 
     @Test
+    fun `0 입력은 허용하지 않음`() {
+        assertThrows<IllegalArgumentException> {
+            Lottery(0, Lotto(listOf(1, 2, 3, 4, 5, 6)), 7)
+        }
+    }
+
+    @Test
     fun `보너스 번호가 랜덤 로또 배열에 있다면 오류 발생`() {
         assertThrows<IllegalArgumentException> {
             Lottery(1000, Lotto(listOf(1, 2, 3, 4, 5, 6)), 6)
