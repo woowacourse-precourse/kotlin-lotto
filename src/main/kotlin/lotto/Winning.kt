c47a744f820f293bcb187e568eb9e19af87a2323
Min-Jae-Bae <qoalswo123@icloud.com>
2022-11-14 11:10:47 +0900

    [Feat] (Winning.kt) 번호가 5개 일치할 때 THIRD 반환 기능 추가

--- src/main/kotlin/lotto/Winning.kt
+++ src/main/kotlin/lotto/Winning.kt
@@ -3,7 +3,8 @@ package lotto
 enum class Rank(val reward: Int) {
     LAST(0),
     FIFTH(5000),
-    FOURTH(50000)
+    FOURTH(50000),
+    THIRD(1500000)
 }
 
 class Winning {
@@ -15,6 +16,7 @@ class Winning {
             0,1,2 -> Rank.LAST
             3 -> Rank.FIFTH
             4 -> Rank.FOURTH
+            5 -> Rank.THIRD
             else -> matchCount
         }
 
