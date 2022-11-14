db7dd7be3dca5196eca4ecaed81b1d7277db61e9
qwe910205 <38097088+qwe910205@users.noreply.github.com>
2022-11-14 17:42:52 +0900

    fix(domain): 로또를 생성하면 입력받은 번호를 정렬 후 저장하도록 변경

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -1,10 +1,13 @@
 package lotto.domain
 
+import java.util.Collections.sort
+
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6)
         require(numbers.all { it in 1..45 })
         require(numbers.toSet().size == numbers.size)
+        sort(numbers)
     }
 
     fun numbers() = numbers
--- src/test/kotlin/lotto/domain/LottoTest.kt
+++ src/test/kotlin/lotto/domain/LottoTest.kt
@@ -1,11 +1,10 @@
 package lotto.domain
 
-import lotto.domain.Lotto
+import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
-import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
@@ -22,26 +21,43 @@ class `Lotto 클래스의` {
                 Arguments.of(listOf(1, 2, 3, 4, 5)),
                 Arguments.of(listOf(1, 2, 3, 4, 5, 6, 7))
             )
+
             @ParameterizedTest
             @MethodSource("numbers")
             fun `예외를 던진다`(numbers: List<Int>) {
                 assertThatThrownBy { Lotto(numbers) }.isInstanceOf(IllegalArgumentException::class.java)
             }
         }
+
         @Nested
         inner class `주어지는 로또 번호가 1에서 45 사이의 숫자가 아니면` {
             private val numbers = listOf(1, 2, 3, 4, 5, 46)
+
             @Test
             fun `예외를 던진다`() {
                 assertThatThrownBy { Lotto(numbers) }.isInstanceOf(IllegalArgumentException::class.java)
             }
         }
+
         @Nested
         inner class `주어지는 로또 번호 중 중복된 숫자가 있다면` {
             private val numbers = listOf(1, 2, 3, 4, 5, 5)
+
             @Test
             fun `예외를 던진다`() {
                 assertThatThrownBy { Lotto(numbers) }.isInstanceOf(IllegalArgumentException::class.java)
+            }
+        }
+
+        @Nested
+        inner class `로또 번호를 받으면` {
+            private val numbers = listOf(6, 5, 4, 3, 2, 1)
+
+            @Test
+            fun `오름차순으로 정렬한다`() {
+                val lotto = Lotto(numbers)
+
+                assertThat(lotto.numbers()).containsExactly(1, 2, 3, 4, 5, 6)
             }
         }
     }
