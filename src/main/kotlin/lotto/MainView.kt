370890692e8caf10ee7701e8c83ecc0fb7968a37
Yuhee Kim <dbgml4509@naver.com>
2022-11-14 22:59:44 +0900

    feat : 당첨 통계를 출력하는 함수 생성

--- src/main/kotlin/lotto/MainView.kt
+++ src/main/kotlin/lotto/MainView.kt
@@ -36,4 +36,15 @@ class MainView {
         return readLine()?.toInt()
     }
 
+    fun winTotal(winCount : List<Int>){
+        println("당첨 통계")
+        println("---")
+        println("3개 일치 (5,000원) - " + winCount[0] + "개")
+        println("4개 일치 (50,000원) - " + winCount[1] + "개")
+        println("5개 일치 (1,500,000원) - " + winCount[2] + "개")
+        println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + winCount[3] + "개")
+        println("6개 일치 (2,000,000,000원) - " + winCount[4] + "개")
+        println("총 수익률은 62.5%입니다.")
+    }
+
 }
