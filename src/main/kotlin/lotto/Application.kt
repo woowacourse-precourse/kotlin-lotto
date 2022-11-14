117491d4c064a3f02452abec4eff4164d3565ce0
choHarmony <jws0183@naver.com>
2022-11-15 01:46:06 +0900

    feat: 입력받은 구입 금액에 숫자가 아닌 값이 있는 경우

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Randoms
 import camp.nextstep.edu.missionutils.Console
 
 lateinit var purchaseSum: String
+var numOfLotto = 0
 
 fun main() {
 
@@ -12,4 +13,13 @@ fun main() {
 fun costInput() {
     println("구입 금액을 입력해주세요.")
     purchaseSum = Console.readLine()
+}
+
+fun checkCostInput() {
+    try {
+        numOfLotto = purchaseSum.toInt() / 1000
+        println()
+    } catch (e: IllegalArgumentException) {
+        println("[ERROR] 금액을 천원 단위의 숫자로 입력해주세요.")
+    }
 }
