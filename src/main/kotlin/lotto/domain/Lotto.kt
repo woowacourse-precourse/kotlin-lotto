47fcc707dd0ebd1e2fb6d7e332ca5e3b77451767
leeeha <jxlhe46@seoultech.ac.kr>
2022-11-14 16:28:43 +0900

    fix: 5개 일치보다 6개 일치를 먼저 검사하도록 변경

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -12,17 +12,15 @@ class Lotto(private val winningNumber: List<Int>) {
     // 당첨 번호 vs 로또 번호
     // 5개 일치하는 경우, 보너스 번호 vs 로또 번호
     fun getWinnerList(lottoNumbers: List<List<Int>>, bonusNumber: Int): List<Int> {
-        val judgement = Judgement()
         val winners = mutableListOf(0, 0, 0, 0, 0)
+        val judgement = Judgement()
         for(lottoNumber in lottoNumbers){
-            val matchCount = judgement.countMatchNumber(lottoNumber, winningNumber)
-            if(matchCount == 5){
-                if(judgement.checkBonusBall(lottoNumber, bonusNumber)) winners[SECOND_WINNER_INDEX]++
-                else winners[THIRD_WINNER_INDEX]++
-                continue
-            }
-            when(matchCount){
+            when(judgement.countMatchNumber(lottoNumber, winningNumber)){
                 6 -> winners[FIRST_WINNER_INDEX]++
+                5 ->  {
+                    if(judgement.checkBonusBall(lottoNumber, bonusNumber)) winners[SECOND_WINNER_INDEX]++
+                    else winners[THIRD_WINNER_INDEX]++
+                }
                 4 -> winners[FORTH_WINNER_INDEX]++
                 3 -> winners[FIFTH_WINNER_INDEX]++
             }
