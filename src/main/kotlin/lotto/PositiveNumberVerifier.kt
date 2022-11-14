e0264af554d37f99f22c788a393785945ffb4c75
Gloria <ippnsj@gmail.com>
2022-11-14 21:02:12 +0900

    feat: 사용자 입력 값이 양수인지 확인하는 기능 구현

---
+++ src/main/kotlin/lotto/PositiveNumberVerifier.kt
@@ -0,0 +1,14 @@
+package lotto
+
+class PositiveNumberVerifier : Verifier<Int>() {
+    override fun verify(input: Int) {
+        require(input > 0) {
+            println("$ERROR_HEADER $ERROR_MESSAGE")
+            ERROR_MESSAGE
+        }
+    }
+
+    companion object {
+        const val ERROR_MESSAGE = "Input is not a positive number."
+    }
+}
