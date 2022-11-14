82f74f22763092b592605e063523e8c98c70e98b
idra79haza <idra79haza@github.com>
2022-11-14 17:44:50 +0900

    refactor(clean coding): clean coding
    clean coding with same indepth

--- src/main/kotlin/lotto/Rank.kt
+++ src/main/kotlin/lotto/Rank.kt
@@ -1,4 +1,5 @@
 package lotto
+
 import utils.Messages.FIFTH_RANK_MESSAGE
 import utils.Messages.FIRST_RANK_MESSAGE
 import utils.Messages.FOURTH_RANK_MESSAGE
@@ -10,29 +11,30 @@ class Ranking {
     enum class Rank {
         FIFTH {
             override fun print(winList: MutableList<Int>) {
-                println(FIFTH_RANK_MESSAGE+winList[0]+ STATICS_COUNT_MESSAGE)
+                println(FIFTH_RANK_MESSAGE + winList[0] + STATICS_COUNT_MESSAGE)
             }
         },
         FOURTH {
             override fun print(winList: MutableList<Int>) {
-                println(FOURTH_RANK_MESSAGE+winList[1]+ STATICS_COUNT_MESSAGE)
+                println(FOURTH_RANK_MESSAGE + winList[1] + STATICS_COUNT_MESSAGE)
             }
         },
         THIRD {
             override fun print(winList: MutableList<Int>) {
-                println(THIRD_RANK_MESSAGE+winList[2]+ STATICS_COUNT_MESSAGE)
+                println(THIRD_RANK_MESSAGE + winList[2] + STATICS_COUNT_MESSAGE)
             }
         },
         SECOND {
             override fun print(winList: MutableList<Int>) {
-                println(SECOND_RANK_MESSAGE+winList[3]+ STATICS_COUNT_MESSAGE)
+                println(SECOND_RANK_MESSAGE + winList[3] + STATICS_COUNT_MESSAGE)
             }
         },
         FIRST {
             override fun print(winList: MutableList<Int>) {
-                println(FIRST_RANK_MESSAGE+winList[4]+ STATICS_COUNT_MESSAGE)
+                println(FIRST_RANK_MESSAGE + winList[4] + STATICS_COUNT_MESSAGE)
             }
         };
+
         open fun print(winList: MutableList<Int>) {
             println("")
         }
