30251d26f826cd05717b606436fc64c6a21c35ef
otter <a33397397@gmail.com>
2022-11-15 03:55:31 +0900

    refactor: 파일 분리, 문구 수정

--- src/main/kotlin/data/values.kt
+++ src/main/kotlin/data/values.kt
@@ -2,10 +2,11 @@ package lotto
 
 // Error Message
 const val ERROR_MESSAGE_ONLY_NUMBER = "[ERROR] 숫자만 입력할 수 있습니다."
-const val ERROR_MESSAGE_ONLY_SIX_NUMBER = "[ERROR] 6개의 숫자만 입력할 수 있습니다."
-const val ERROR_MESSAGE_NUMBER_OUT_RANGE = "[ERROR] 1~45 범위의 숫자만 입력할 수 있습니다."
+const val ERROR_MESSAGE_ONLY_SIX_NUMBER = "[ERROR] 로또 번호는 6개의 숫자입니다."
+const val ERROR_MESSAGE_NUMBER_OUT_RANGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."
 const val ERROR_MESSAGE_INPUT_FORMAT = "[ERROR] 입력 형식이 잘못되었습니다."
-const val ERROR_MESSAGE_ONLY_UNIT_1000 = "[ERROR] 1000원 단위의 금액만 지불이 가능합니다."
+const val ERROR_MESSAGE_ONLY_UNIT_1000 = "[ERROR] 로또 구입시 지불하는 금액은 1000원 단위여야 합니다."
+const val ERROR_MESSAGE_NOT_DUPLICATION = "[ERROR] 로또의 번호는 중복될 수 없습니다."
 
 // Require Message
 const val REQUIRE_MESSAGE_LOTTO_PRICE = "구입금액을 입력해 주세요."
--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -2,8 +2,10 @@ package lotto
 
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6)
+        require(numbers.size == 6) { println(ERROR_MESSAGE_NUMBER_OUT_RANGE) }
+        require(numbers.all { number ->
+            numbers.count { it == number } < 1
+        }) { println(ERROR_MESSAGE_NOT_DUPLICATION) }
     }
 
-    // TODO: 추가 기능 구현
 }
