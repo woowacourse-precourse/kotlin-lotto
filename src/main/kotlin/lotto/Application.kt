69b9889dc89dfad5ea4a33b25ab785f99ada1b03
changhwan <chang5hustle@gmail.com>
2022-11-15 06:59:51 +0900

    feat(lotto) : main 함수 작성

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,17 @@
 package lotto
 
+import lotto.bank.BankImpl
+import lotto.lottoshop.LottoShop
+import lotto.lottoshop.lottomachine.LottoMachineImpl
+
 fun main() {
-    TODO("프로그램 구현")
+    val gameHost = GameHost(Customer(), LottoShop(), LottoMachineImpl(), BankImpl())
+
+    gameHost.apply {
+        progressPurchaseAmount()
+        progressDrawLotto()
+        progressMainLottoNumbers()
+        progressBonusLottoNumber()
+        progressBankService()
+    }
 }
