2e217c6c145c8c5288870cd327aa8c18051a4a4f
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2022-11-14 14:16:34 +0900

    refactor[lotto]: 당첨 번호 입력하는 기능 리팩토링(InputView클래스)

--- src/main/kotlin/lotto/InputView.kt
+++ src/main/kotlin/lotto/InputView.kt
@@ -1,27 +1,47 @@
 package lotto
 
 import camp.nextstep.edu.missionutils.Console
+import java.lang.NullPointerException
+import java.lang.NumberFormatException
 
 class InputView {
 
     val validator = Validator()
 
 
-    fun getInputMoney() : Int {
+    fun getInputMoney(): Int {
         return inputMoney().toInt()
     }
 
-    fun inputMoney() : String {
-        println("구입금액을 입력해 주세요.")
+    fun getInputWinningNumber(): Lotto {
+        val winninglottos = inputWinningLotto().split(",")
+        return Lotto(winninglottos.map { it.toInt() })
+    }
+
+    fun inputWinningLotto(): String {
+        println(INPUT_WINNING_LOTTO_MSG)
+        return checkInputWinningLotto(Console.readLine())
+    }
+
+    private fun checkInputWinningLotto(input: String): String {
+        validator.checkWinnigNumberInput(input)
+        return input
+    }
+
+    fun inputMoney(): String {
+        println(INPUT_MONEY_MSG)
         return checkInputMoney(Console.readLine())
     }
-    fun checkInputMoney(input : String) : String{
-        try {
-            validator.checkMoneyInput(input)
-            return input
-        } catch (e : IllegalArgumentException) {
-            e.printStackTrace()
-            throw NoSuchElementException()
-        }
+
+    fun checkInputMoney(input: String): String {
+
+        validator.checkMoneyInput(input)
+        return input
+
+    }
+
+    companion object {
+        const val INPUT_MONEY_MSG = "구입금액을 입력해 주세요."
+        const val INPUT_WINNING_LOTTO_MSG = "당첨 번호를 입력해 주세요."
     }
 }
