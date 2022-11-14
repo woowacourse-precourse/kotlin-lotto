d39ae7bfc3857d24ed94235b23575f073d7fd52c
pingu244 <69796976+pingu244@users.noreply.github.com>
2022-11-14 23:47:05 +0900

    test: 로또 결과 테스트, 입력 예외 테스트

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -1,6 +1,9 @@
 package lotto
 
 import lotto.Model.Lotto
+import lotto.Model.Money
+import lotto.Model.Rank
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
@@ -21,5 +24,44 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+    @Test
+    fun `로또 번호가 특정 범위를 벗어나면 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(1, 11, 2, 22, 23, 46))
+        }
+    }
+
+    @Test
+    fun `로또 구입 금액 예외`() {
+        assertThrows<IllegalArgumentException> {
+            Money(2200)
+        }
+        assertThrows<IllegalArgumentException> {
+            Money(0)
+        }
+    }
+
+    class LottoResultTest() {
+        private val winningNumber = Lotto(listOf(1, 2, 3, 4, 5, 6))
+        private val bonusNumber = 7
+
+        @Test
+        fun `로또 2등`() {
+            val lotto = Lotto(listOf(1, 2, 3, 4, 5, 7))
+            assertThat(lotto.calculateWinningResult(winningNumber, bonusNumber)).isEqualTo(Rank.Second)
+        }
+
+        @Test
+        fun `로또 3등`() {
+            val lotto = Lotto(listOf(1, 2, 3, 4, 5, 8))
+            assertThat(lotto.calculateWinningResult(winningNumber, bonusNumber)).isEqualTo(Rank.Third)
+        }
+
+        @Test
+        fun `로또 5등`() {
+            val lotto = Lotto(listOf(1, 2, 3, 11, 22, 33))
+            assertThat(lotto.calculateWinningResult(winningNumber, bonusNumber)).isEqualTo(Rank.Fifth)
+        }
+    }
 
 }
