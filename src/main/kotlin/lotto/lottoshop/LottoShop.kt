74b02b111c524c98efe263927071f97ddd7d2e97
changhwan <chang5hustle@gmail.com>
2022-11-14 16:32:22 +0900

    feat(lotto): LottoShop 공통함수 숫자 확인 함수 작성

--- src/main/kotlin/lotto/lottoshop/LottoShop.kt
+++ src/main/kotlin/lotto/lottoshop/LottoShop.kt
@@ -1,17 +1,17 @@
 package lotto.lottoshop
 
-import java.util.StringJoiner
+import java.util.regex.Pattern
 
 class LottoShop() {
 
     fun getValidatePurchaseAmount(purchaseAmount: String): Int {
     }
 
-    fun getValidateMainLottoNumbers(mainLottoNumbers: List<String>) : List<Int>{
-
+    fun getValidateMainLottoNumbers(mainLottoNumbers: List<String>): List<Int> {
     }
 
-    fun getValidateBonusLottoNumber(bonusLottoNumber: String): Int{
-        
+    fun getValidateBonusLottoNumber(bonusLottoNumber: String): Int {
     }
+
+    fun isInputTypeNumber(input: String): Boolean = Pattern.matches("^[0-9]+$", input)
 }
