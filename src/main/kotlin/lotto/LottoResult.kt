ef9dd167a364fd3e0daaeab2a5731303a7039c0e
SEO-J17 <qw4643987@naver.com>
2022-11-14 23:39:06 +0900

    feat: 당첨된 로또의 개수 계산 하는 기능 추가함

---
+++ src/main/kotlin/lotto/LottoResult.kt
@@ -0,0 +1,52 @@
+package lotto
+
+import lotto.constants.Print
+
+class LottoResult(
+    private val createNumbers: MutableList<List<Int>>,
+    private val inputPrice: Int
+) {
+    fun result(inputNumbers: List<String>, bonusNumber: String) {
+        showTitleMessage()
+        val winLotto = calculateLotto(inputNumbers, bonusNumber)
+        val finalMoney = CalculateMoney(winLotto).result()
+        //수익률 계산 클래스
+        EarningsRate(finalMoney, inputPrice).result()
+    }
+
+    private fun showTitleMessage() {
+        println(Print.RESULT_TITLE)
+        println("---")
+    }
+
+    private fun calculateLotto(inputNumbers: List<String>, bonusNumber: String): MutableMap<Int, Int> {
+        val lottoCount =
+            with(Print) {
+                mutableMapOf(THREE to ZERO, FOUR to ZERO, FIVE to ZERO, FIVE_BONUS to ZERO, SIX to ZERO)
+            }
+        createNumbers.forEach { lottoNumbers ->
+            var isBonus = false
+            if (lottoNumbers.contains(bonusNumber.toInt())) {
+                isBonus = true
+            }
+            val count = lottoNumbers.count { inputNumbers.contains(it.toString()) }
+            if (lottoCount.containsKey(count)) {
+                countLotto(count, isBonus, lottoCount)
+            }
+        }
+        return lottoCount
+    }
+
+    private fun countLotto(count: Int, isBonus: Boolean, lottoCount: MutableMap<Int, Int>) {
+        val countOne = 1
+        if (count == 5 && isBonus) {
+            lottoCount[577] = lottoCount.getValue(577) + countOne
+        } else {
+            lottoCount[count] = lottoCount.getValue(count) + countOne
+        }
+    }
+
+    companion object {
+
+    }
+}
