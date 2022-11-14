5ff3ab4090de6af989cbdd0f8145653d665d3757
ChoHJin <cjc9739@naver.com>
2022-11-15 07:40:57 +0900

    refactor: 게임 컨트롤 메소드 분리
    게임 진행 기능 분리 및 예외사항 메시지를 출력하도록 로직을 바꿔줬음.

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,33 +1,10 @@
 package lotto
 
-import camp.nextstep.edu.missionutils.Console
-import camp.nextstep.edu.missionutils.Randoms
-import lotto.Lotto
-import lotto.GenerateWinningNumbers
-
-fun getLottoTicket(lottoList : MutableList<Lotto>, countLottoTicket : Int) {
-    println("${countLottoTicket}개를 구매했습니다.")
-    for(i in 0 until countLottoTicket) {
-        val lotto = Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6))
-        lottoList.add(lotto)
-        println(lotto.getLottoNumbers())
-    }
-}
-
-
-
 fun main() {
 //    TODO("프로그램 구현")
-    val lottoTickets = mutableListOf<Lotto>()
-//    print(purchaseLotto())
-    val lottos = PurchaseLotto().issuedLotto(PurchaseLotto().purchase())
-
-    getLottoTicket(lottoTickets, lottos)
-    val testNum = GenerateWinningNumbers().inputWinningNumbers()
-
-
-    val testPrint = testNum.getLottoNumbers().plus(GenerateWinningNumbers().inputBonusNumber())
-    println(testPrint)
-
-
+    try {
+        LottoGame().gameControl()
+    }catch (e : IllegalArgumentException) {
+        println(e.message)
+    }
 }
