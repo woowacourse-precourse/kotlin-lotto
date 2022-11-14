7b50237379814a4bafcce713b4aa139fc13d2fb0
lee989898 <wnsrud1352@gmail.com>
2022-11-14 21:51:43 +0900

    [feat](Lotteries.kt) 랜덤 로또 초기화 수정 및 로또 승 가져오고 몇명 승리인지 계산하는 기능 구현

--- src/main/kotlin/domain/Lotteries.kt
+++ src/main/kotlin/domain/Lotteries.kt
@@ -1,12 +1,35 @@
 package domain
 
-class Lotteries {
+class Lotteries(lotteriesNumber: List<List<Int>>) {
 
     private val randomLotteries = mutableListOf<Lotto>()
+
+    init {
+        lotteriesNumber.forEach {
+            randomLotteries.add(Lotto(it))
+        }
+    }
 
     fun setRandomLotteries(lotteriesNumber: List<List<Int>>) {
         lotteriesNumber.forEach { randomLotteries.add(Lotto(it)) }
     }
 
     fun getRandomLotteries(): List<Lotto> = randomLotteries
+
+    fun getNumberOfWinners(winningLottery: WinningLottery): Map<Rank, Int> {
+        val winners = HashMap<Rank, Int>()
+        Rank.values().forEach { winners[it] = 0 }
+        winners.toMutableMap()
+        randomLotteries.forEach {
+            val rank = winningLottery.getRank(it)
+            putCountToWinner(winners, rank)
+        }
+        return winners
+    }
+
+    private fun putCountToWinner(winners: MutableMap<Rank, Int>, rank: Rank) {
+        if (rank != Rank.NOTHING) {
+            winners[rank] = winners[rank]!! + 1
+        }
+    }
 }
