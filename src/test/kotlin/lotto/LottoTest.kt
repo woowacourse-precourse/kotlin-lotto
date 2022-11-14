4b985533fd1c3fed3d74dcaf5a8e523dd1d87a60
kyounggudev <homin222@naver.com>
2022-11-15 01:01:54 +0900

    test : 테스트 케이스 추가, 예외 처리 테스트 케이스에 [ERROR]문구 확인 추가

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -16,7 +16,7 @@ class LottoTest : NsTest(){
         }
     }
 
-    // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
+    // TODO: 이 테스트가 통과할 수 있게 구현 코드
     @Test
     fun `로또 번호에 중복된 숫자가 있으면 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
@@ -26,15 +26,24 @@ class LottoTest : NsTest(){
 
     // 아래에 추가 테스트 작성 가능
     @Test
+    fun `로또 번호의 개수가 6개가 넘지못하면 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(1, 2, 3, 4, 5))
+        }
+        assertThat(output()).contains(ERROR_MESSAGE)
+    }
+
+    @Test
     fun `로또 번호에는 1부터 45 밖의 숫자가 있으면 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
             Lotto(listOf(1, 2, 3, 4, 5, 46))
         }
+        assertThat(output()).contains(ERROR_MESSAGE)
     }
 
     @Test
     fun `로또 번호 정상 작동`() {
-        Lotto.lottoes.clear()
+        Lotto.lottos.clear()
         assertDoesNotThrow {
             Lotto(listOf(1, 2, 3, 4, 5, 6))
         }
@@ -42,15 +51,15 @@ class LottoTest : NsTest(){
 
     @Test
     fun `로또 번호 오름차순`() {
-        Lotto.lottoes.clear()
+        Lotto.lottos.clear()
         Lotto(listOf(1, 2, 3, 6, 5, 4))
         val result = listOf(1,2,3,4,5,6)
-        assertThat(Lotto.lottoes[0]).isEqualTo(result)
+        assertThat(Lotto.lottos[0]).isEqualTo(result)
     }
 
     @Test
     fun `로또 목록 출력`() {
-        Lotto.lottoes.clear()
+        Lotto.lottos.clear()
         Lotto(listOf(1, 2, 3, 4, 5, 6))
         Lotto(listOf(1, 2, 3, 4, 5, 7))
         Lotto.printCount()
@@ -63,7 +72,7 @@ class LottoTest : NsTest(){
 
     @Test
     fun `랜덤 발행`() {
-        Lotto.lottoes.clear()
+        Lotto.lottos.clear()
         Assertions.assertRandomUniqueNumbersInRangeTest(
             {
                 Lotto.publish(3)
