bd4d5b5a775af88ee97f8561c72d74f0f6e9f755
jklom0326 <jklom0326@gmail.com>
2022-11-15 07:38:58 +0900

    style(Lotto.kt): 출력 함수명 변경
    - 메시지에 MESSAGE, 출력함수엔 print로 시작하는 함수명으로 변경

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,18 +1,18 @@
 package lotto
 
-import lotto.Message.Companion.DUPLICATED_NUMBERS
-import lotto.Message.Companion.NOT_NUMBER
-import lotto.Message.Companion.NOT_SIX_NUMBERS
+import lotto.Message.Companion.DUPLICATED_NUMBERS_MESSAGE
+import lotto.Message.Companion.NOT_NUMBER_MESSAGE
+import lotto.Message.Companion.NOT_SIX_NUMBERS_MESSAGE
 import lotto.Units.Companion.LOTTO_FIRST_NUMBER
 import lotto.Units.Companion.LOTTO_LAST_NUMBER
-import lotto.Units.Companion.LOTTO_UNIT
+import lotto.Units.Companion.LOTTO_SIZE
 
 class Lotto(private val numbers: List<Int>) {
 
     init {
-        require(numbers.size == LOTTO_UNIT) { NOT_SIX_NUMBERS }
-        require(numbers.distinct().size == LOTTO_UNIT) { DUPLICATED_NUMBERS }
-        require(correctNumbers(numbers)) { NOT_NUMBER }
+        require(numbers.size == LOTTO_SIZE) { NOT_SIX_NUMBERS_MESSAGE }
+        require(numbers.distinct().size == LOTTO_SIZE) { DUPLICATED_NUMBERS_MESSAGE }
+        require(correctNumbers(numbers)) { NOT_NUMBER_MESSAGE }
     }
 
     fun getNumbers(): List<Int> {
