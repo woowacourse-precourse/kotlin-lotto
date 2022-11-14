6e459d3fea478937505401ca09476032ba381142
bluecat0823 <star4_you@naver.com>
2022-11-14 20:46:39 +0900

    [style] 로또 번호 범위 예외처리 문장 수정

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -5,7 +5,7 @@ class Lotto(private val numbers: List<Int>) {
         require(numbers.size == 6)
         for (number in 0..5){
             if (numbers[number] !in 1..45){
-                throw IllegalArgumentException("[ERROR] 로또 번호는 1에서 45 사이의 숫자로 이루어져야 합니다.ㄹㅎ")
+                throw IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.")
             }
         }
     }
