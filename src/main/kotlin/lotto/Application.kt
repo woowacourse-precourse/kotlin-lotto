21436482e821375505d32974284a4a160576117a
[alsswl] <[alswl4223@naver.com]>
2022-11-14 16:06:16 +0900

    fix : numbers와 userNum을 비교할 떄 i만 사용함

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -14,6 +14,7 @@ fun checkUserMoney(userMoney:String):Int{
         throw IllegalArgumentException()}
     return userMoney
 }
+
 fun bonusSame(numbers: List<Int>, bonus: Int):Int{
     for(i in 0 until 6){
         if(numbers[i].toInt() == bonus) return 1
@@ -24,8 +25,14 @@ fun bonusSame(numbers: List<Int>, bonus: Int):Int{
 fun checkGrade(numbers:List<Int>,userNum:List<String>,bonus: Int):Int{
     var count = 0
     for(i in 0 until 6) {
-        if(numbers[i] == userNum[i].toInt()) count++
+        for(j in 0 until 6) {
+            if (numbers[i] == userNum[j].toInt()) {
+                count++
+                break;
+            }
+        }
     }
+    println(count)
     if(count==6) return 1
     else if(count == 5){
         if(bonusSame(numbers,bonus) == 1) return 2
@@ -36,12 +43,15 @@ fun checkGrade(numbers:List<Int>,userNum:List<String>,bonus: Int):Int{
     return 0
 }
 
-fun pickNum(Num:Int,userNum:List<String>,bonus:Int){
-    var ok = arrayOf(0,0,0,0,0)
+fun pickNum(Num:Int,userNum:List<String>,bonus:Int):Array<Int>{
+    var gradeNum = arrayOf(0,0,0,0,0,0)
     for(i in 1 until Num+1){
     val numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
+        println(numbers)
     var grade = checkGrade(numbers,userNum,bonus)
+        gradeNum[grade]++
     }
+    return gradeNum
 }
 
 
@@ -52,9 +62,16 @@ fun main() {
     println("당첨번호를 입력하세요")
     val userString = Console.readLine()!!.toString()
     val userNum = userString.split(",")
+
     println("보너스 점수를 입력하세요")
     var bonus = Console.readLine()!!.toInt()
+    var gradeNum = pickNum(LottoNum,userNum,bonus)
 
-    pickNum(LottoNum,userNum,bonus)
+    println("3개 일치 (5,000원) - ${gradeNum[5]}개")
+    println("4개 일치 (50,000원) - ${gradeNum[4]}개")
+    println("5개 일치 (1,500,000원) - ${gradeNum[3]}개")
+    println("5개 일치, 보너스 볼 일치 (30,000,000원) - ${gradeNum[2]}개")
+    println("6개 일치 (2,000,000,000원) - ${gradeNum[1]}개")
+
 
 }
