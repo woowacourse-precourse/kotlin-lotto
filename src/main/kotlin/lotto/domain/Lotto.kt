08fe228598283c2deba91146976095efa7cd2e2d
jiingom <vvankatam0@gmail.com>
2022-11-14 16:12:56 +0900

    refactor: try catch 문을 삭제하고 require문 으로 예외 처리하도록 변경

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -3,12 +3,7 @@ package lotto.domain
 class Lotto(private val numbers: List<Int>) {
 
     init {
-        try {
-            require(numbers.size == 6)
-        } catch (e: IllegalArgumentException) {
-            println("[ERROR]: 로또 번호의 개수는 6개여야 합니다.")
-        }
-
+        require(numbers.size == 6) {"[ERROR]: 로또 번호의 개수는 6개여야 합니다."}
         isLottoNumberDuplicated(numbers)
     }
 
