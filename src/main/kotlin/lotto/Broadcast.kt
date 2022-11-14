c54e6372d1e7ff0499eb6f74a272aca16b7f23bf
jklom0326 <jklom0326@gmail.com>
2022-11-15 06:36:36 +0900

    feat(Broadcast.kt): 보너스 번호를 받는 기능
    - 보너스 번호를 받는 함수 구현

---
+++ src/main/kotlin/lotto/Broadcast.kt
@@ -0,0 +1,37 @@
+package lotto
+
+import camp.nextstep.edu.missionutils.Console
+
+class Broadcast() {
+    var winningNumbers = listOf<Int>()
+    var bonusNumber = 0
+    val winLottery = mutableListOf(0, 0, 0, 0, 0, 0, 0)
+    var bonusCount = 0
+
+    fun broadcast(lottery: List<List<Int>>) {
+        println()
+        println("당첨 번호를 입력해 주세요.")
+        winningNumbers = winningLotto()
+        println()
+        println("보너스 번호를 입력해주세요.")
+        bonusNumber = winningBonusLotto()
+
+    }
+
+    private fun winningLotto(): List<Int> {
+        val numbers = Console.readLine()
+            .split(",")
+            .map { it.toInt() }
+            .toList()
+        if (numbers.size != 6) throw IllegalArgumentException("[ERROR] 숫자가 6개가 아닙니다.")
+        return numbers
+    }
+
+    private fun winningBonusLotto(): Int {
+        val bonus = Console.readLine()
+            .toInt()
+        if (winningNumbers.contains(bonus)) throw IllegalArgumentException("[ERROR] 당첨된 번호와 보너스번호가 중복입니다.")
+        return bonus
+    }
+
+}
