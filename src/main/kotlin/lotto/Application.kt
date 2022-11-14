8f49a5ee0daa224feeb923b96f3ae585ba4de82d
dlwgnud <wngud980411@naver.com>
2022-11-14 17:32:51 +0900

    feat(inputPrice): 로또 구입금액 입력 함수

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -2,8 +2,13 @@ package lotto
 
 fun main() {
     printStart()
+    val price = inputPrice()
 }
 
 fun printStart() {
     println("구입금액을 입력해 주세요.")
+}
+
+fun inputPrice(): Int {
+    return readLine()!!.toInt()
 }
