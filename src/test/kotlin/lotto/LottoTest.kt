1689e9df0a34cf7d018696f880efa854f7f84954
Hyemin Song <hyemdooly@gmail.com>
2022-11-14 14:23:05 +0900

    style (Lotto): newline delete

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -12,7 +12,6 @@ class LottoTest {
             Lotto(listOf(1, 2, 3, 4, 5, 6, 7))
         }
     }
-
     // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
     @Test
     fun `로또 번호에 중복된 숫자가 있으면 예외가 발생한다`() {
@@ -20,7 +19,6 @@ class LottoTest {
             Lotto(listOf(1, 2, 3, 4, 5, 5))
         }
     }
-
     // 아래에 추가 테스트 작성 가능
     // TODO: printLotto Test
     @Test
