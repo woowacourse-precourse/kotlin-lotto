2b46defbf9ab10eeb6d1c2c614218c2b4ecb2bb7
권용민 <briandr@naver.com>
2022-11-14 17:21:54 +0900

    test(LottoMachine): 당첨번호 및 보너스번호 발행 검사  - 숫자인지 검사  - 1~45인지 검사  - 숫자가 6개인지 검사

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -24,6 +24,58 @@ class LottoTest {
     // 아래에 추가 테스트 작성 가능
 
     @Nested
+    inner class LottoMachineTest {
+        private val lottoMachine = LottoMachine()
+
+        @Test
+        fun `당첨번호 생성 확인`() {
+            lottoMachine.publishWinningNumber("1,2,3,4,5,6")
+            assertThat(lottoMachine.winningNumbers).isEqualTo(listOf(1, 2, 3, 4, 5, 6))
+        }
+
+        @Test
+        fun `당첨번호가 숫자가 아닐 경우 예외가 발생한다`() {
+            assertThrows<IllegalArgumentException> {
+                lottoMachine.publishWinningNumber("1,2,3,가,나")
+            }
+        }
+
+        @Test
+        fun `당첨번호가 1~45가 아닐 경우 예외가 발생한다`() {
+            assertThrows<IllegalArgumentException> {
+                lottoMachine.publishWinningNumber("1,2,3,30,50")
+            }
+        }
+
+        @Test
+        fun `당첨번호가 6자리가 아닐 경우 예외가 발생한다`() {
+            assertThrows<IllegalArgumentException> {
+                lottoMachine.publishWinningNumber("1,2,3,4,5,6,7")
+            }
+        }
+
+        @Test
+        fun `보너스 번호 생성 확인`() {
+            lottoMachine.publishBonusNumber("30")
+            assertThat(lottoMachine.bonusNumber).isEqualTo(30)
+        }
+
+        @Test
+        fun `보너스 번호가 숫자가 아닐 경우 예외가 발생한다`() {
+            assertThrows<IllegalArgumentException> {
+                lottoMachine.publishWinningNumber("가나")
+            }
+        }
+
+        @Test
+        fun `보너스 번호가 1~45가 아닐 경우 예외가 발생한다`() {
+            assertThrows<IllegalArgumentException> {
+                lottoMachine.publishBonusNumber("70")
+            }
+        }
+    }
+
+    @Nested
     inner class TicketingMachineTest {
         private val ticketingMachine = TicketingMachine()
 
