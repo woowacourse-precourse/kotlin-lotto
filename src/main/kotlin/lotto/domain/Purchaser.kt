8cc4609f6e9746a237281706ae43fb7dcaa9e027
re4rk <37167652+re4rk@users.noreply.github.com>
2022-11-14 13:20:28 +0900

    refactor(lotto): 고정된 값이 반환 되므로 Mutable 제거

--- src/main/kotlin/lotto/domain/Purchaser.kt
+++ src/main/kotlin/lotto/domain/Purchaser.kt
@@ -5,7 +5,7 @@ import lotto.resources.LOTTO_NUMBER_COUNT
 object Purchaser {
     fun purchase(amount: Int) = Store.buyTickets(amount)
 
-    fun calculateResult(winningLotto: WinningLotto, tickets: List<Lotto>): Pair<MutableList<Int>, Int> {
+    fun calculateResult(winningLotto: WinningLotto, tickets: List<Lotto>): Pair<List<Int>, Int> {
         val resultRank = MutableList(LOTTO_NUMBER_COUNT) { 0 }
         var totalPrize = 0
         tickets.forEach {
