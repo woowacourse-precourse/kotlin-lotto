7b31844e30f53bc1ec999f012659b4b4dea00f5a
username <ieejo716@naver.com>
2022-11-15 03:37:12 +0900

    [FEAT] : 당첨 통계 print

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -5,5 +5,28 @@ class Lotto(private val numbers: List<Int>) {
         require(numbers.size == 6)
     }
 
-    // TODO: 추가 기능 구현
+    private var _correctCount = 0
+    val correctCount get() = _correctCount
+
+    val lottoStats = mutableListOf<Int>(0, 0, 0, 0, 0)
+
+
+    fun printStats() = println("\n당첨 통계\n---")
+
+    fun printLottoResult() {
+        val printResults = listOf(FIVTH, FOURTH, THIRD, SECOND, FIRST)
+        printResults.forEachIndexed { index, it ->
+            println("$it ${lottoStats[index]}")
+        }
+    }
+
+    fun printProfit(profit: Float) = println("총 수익률은 ${"%.2f".format(profit)}입니다.")
+
+    companion object {
+        const val FIVTH = "3개 일치 (5,000원) - "
+        const val FOURTH = "4개 일치 (50,000원) - "
+        const val THIRD = "5개 일치 (1,500,000원) - "
+        const val SECOND = "5개 일치, 보너스 볼 일치 (30,000,000원) - "
+        const val FIRST = "6개 일치 (2,000,000,000원) - "
+    }
 }
