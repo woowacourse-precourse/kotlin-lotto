69cfeb6dcab31c947bbea62ffcbbbb1752b9b184
jwson <sonjungwoo9@gmail.com>
2022-11-14 18:11:21 +0900

    [feat] Start message Output function 생성
    : ENUM CLASS TYPE으로 필요한 메세지 저장

---
+++ src/main/kotlin/lotto/MessageOutput.kt
@@ -0,0 +1,23 @@
+package lotto
+
+class MessageOutput {
+    enum class Notice(val messages: String) {
+        START("구입금액을 입력해 주세요."),
+        BUY("개를 구매했습니다."),
+        ANSWER("당첨 번호를 입력해 주세요."),
+        BONUS("보너스 번호를 입력해 주세요."),
+        AVERAGE("당첨 통계"),
+    }
+
+    enum class Match(val messages: String) {
+        THREE("3개 일치 (5,000원) - 1개"),
+        FOUR("4개 일치 (50,000원) - 0개"),
+        FIVE("5개 일치 (1,500,000원) - 0개"),
+        BONUS("5개 일치, 보너스 볼 일치 (30,000,000원) - 0개"),
+        SIX("6개 일치 (2,000,000,000원) - 0개")
+    }
+
+    fun printStartMessage() {
+        println(Notice.START.messages)
+    }
+}
