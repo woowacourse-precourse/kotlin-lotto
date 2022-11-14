5267b599f681e7490a02df00a7386fe6756a4f6a
Yuhee Kim <dbgml4509@naver.com>
2022-11-14 21:33:09 +0900

    feat : 로또 번호 중복을 확인하는 함수 생성

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -12,20 +12,29 @@ class Lotto(private val numbers: List<Int>){
         require(numbers.size == Sizes.valueOf("LOTTO_NUMBER_SIZE").size){
             throw IllegalArgumentException("[ERROR] 로또 번호는 총 6개여야 합니다.")
         }
-        numbers.forEach {
-            require(checkLottoRange(numbers) == true){
-                throw IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.")
-            }
+        require(checkLottoRange(numbers) == true){
+            throw IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.")
+        }
+        require(checkLottoDuplicated(numbers) == true){
+            throw IllegalArgumentException("[ERROR] 로또 번호는 서로 중복되는 숫자가 아니어야 합니다.")
         }
     }
 
     // TODO: 추가 기능 구현
-    private fun checkLottoRange(numbers: List<Int>) : Boolean {
+    fun checkLottoRange(numbers: List<Int>) : Boolean {
         numbers.forEach {
             if(Sizes.valueOf("LOTTO_RANGE_START").size <= it && it <= Sizes.valueOf("LOTTO_RANGE_END").size)
                 return true
         }
         return false
+    }
+
+    fun checkLottoDuplicated(numbers: List<Int>) : Boolean {
+        val duplicatedEle = numbers.groupingBy { it }.eachCount().filter { it.value > 1 }
+        if(duplicatedEle.isNotEmpty()) {
+            return false
+        }
+        return true
     }
 
 
