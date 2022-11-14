10f77e0677280951d4d4eb3ce127015d1a414e4f
no1msh <no1msh1217@gmail.com>
2022-11-15 02:45:15 +0900

    feat: 사용자가 구매하려는 금액에 맞춰 로또의 개수를 알려주는  메소드 구현

--- src/main/kotlin/lotto/domain/LottoMarket.kt
+++ src/main/kotlin/lotto/domain/LottoMarket.kt
@@ -13,10 +13,15 @@ class LottoMarket {
     }
 
     private fun createLotto(): List<Int> {
-        return Randoms.pickUniqueNumbersInRange(LOTTO_START_NUMBER, LOTTO_END_NUMBER, THE_NUMBER_OF_LOTTO_NUMBERS).sorted()
+        return Randoms.pickUniqueNumbersInRange(LOTTO_START_NUMBER, LOTTO_END_NUMBER, THE_NUMBER_OF_LOTTO_NUMBERS)
+            .sorted()
     }
 
-    companion object{
+    fun getNumberOfLotto(purchasingAmount: Long): Long {
+        return purchasingAmount / 1000
+    }
+
+    companion object {
         const val LOTTO_START_NUMBER = 1
         const val LOTTO_END_NUMBER = 45
         const val THE_NUMBER_OF_LOTTO_NUMBERS = 6
