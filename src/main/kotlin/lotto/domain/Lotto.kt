1a14304394f03e3ecb61a4396e8963bcc8e50b18
choiwoosuk <wooseokchoi0508@gmail.com>
2022-11-14 15:35:56 +0900

    feat(Lotto) 로또 번호 개수가 6개 아니면 예외 발생 기능 구현

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -2,8 +2,19 @@ package lotto.domain
 
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6)
+        checkLottoNumberCountException()
     }
 
-    // TODO: 추가 기능 구현
+    fun checkLottoNumberCountException() {
+        if (this.numbers.size != LOTTO_NUMBER_COUNT) throw IllegalArgumentException(LOTTO_NUMBER_COUNT_ERROR_MESSAGE)
+    }
+
+    companion object {
+        const val LOTTO_NUMBER_COUNT = 6
+        const val MIN_NUMBER = 1
+        const val MAX_NUMBER = 45
+        const val LOTTO_NUMBER_COUNT_ERROR_MESSAGE = "[ERROR] 로또 번호의 개수는 6이어야 합니다."
+        const val LOTTO_NUMBER_RANGE_ERROR_MESSAGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."
+        const val LOTTO_DUPLICATE_NUMBER_ERROR_MESSAGE = "[ERROR] 로또 번호는 중복되지 않아야 합니다."
+    }
 }
