e408e120a80cfc29b3120ad89b684056dc5c468d
lanpia <95679243+lanpia@users.noreply.github.com>
2022-11-15 07:43:52 +0900

    test(LottoTest) : insert the testcase - 로또번호에 중복된 숫자가 있으면 예외 - 로또번호에 문자열이 있으면 예외 - 로또번호에 '0'이 포함된 경우 예외 - 로또번호에 공백이 포함된 경우 예외 - 로또번호에 구분자 ","를 사용하지 않은 경우

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,9 +1,17 @@
 package lotto
+import kotlin.collections.mutableListOf
+
 
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6)
     }
-
-    // TODO: 추가 기능 구현
+}
+fun getComputerInput(): MutableList <Int> {
+    var com_number = mutableListOf<Int>()
+    while(com_number.size < 6){
+        val num = (1..45).random()
+        if(!com_number.contains(num)) com_number.add(num)
+    }
+    return com_number.sorted().toMutableList()
 }
