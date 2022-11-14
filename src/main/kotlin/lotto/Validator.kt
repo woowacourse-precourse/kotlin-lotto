09121266909c421aca78f7e71e34618b7695e934
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2022-11-14 14:23:23 +0900

    refactor[lotto]: 입력된 보너스 번호 예외 처리하는 기능 리팩토링(Validator클래스)

--- src/main/kotlin/lotto/Validator.kt
+++ src/main/kotlin/lotto/Validator.kt
@@ -13,6 +13,16 @@ class Validator {
         checkWinnigNumberToIntValidation(input)
     }
 
+    fun checkBonusInput(input : String) {
+        checkInputNullValidation(input)
+        checkStringToIntValidation(input)
+        checkBonusNumberRagngeValidation(input)
+    }
+
+    fun checkBonusNumberRagngeValidation(input : String) {
+        require(input.toInt() >= LOTTO_MIN_NUM && input.toInt() <= 45) { BONUS_RANGE_ERROR_MSG }
+    }
+
     fun checkWinnigNumberToIntValidation(input : String) {
         try {
             val nums = input.split(",")
@@ -38,9 +48,12 @@ class Validator {
 
     companion object {
         const val LOTTO_PRICE = 1000
+        const val LOTTO_MIN_NUM = 1
+        const val LOTTO_MAX_NUM = 445
 
         const val INPUT_NULL_ERROR_MSG = "[ERROR] 값이 입력되지 않았습니다."
         const val INPUT_VALUE_ERROR_MSG = "[ERROR] 값이 숫자로만 이루어져야합니다."
         const val MONEY_VALUE_ERROR_MSG = "[ERROR] 돈은 1000으로 나누어떨어지는 값이어야 합니다."
+        const val BONUS_RANGE_ERROR_MSG = "[ERROR] 보너스는 1에서 45사이의 숫자여야 합니다."
     }
 }
