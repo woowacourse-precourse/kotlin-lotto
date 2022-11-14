b42a0848e549fc36a2da350fee07c711f0125925
changhwan <chang5hustle@gmail.com>
2022-11-15 07:41:11 +0900

    refactor(lotto) :  test 통과용 lotto 중복검사 로직 작성

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,8 +1,11 @@
 package lotto
 
+import lotto.LottoPhrases.DUPLICATE_LOTTO_NUMBER_ERROR
+
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6)
+        if (numbers.size != numbers.toSet().size) throw IllegalArgumentException(DUPLICATE_LOTTO_NUMBER_ERROR)
     }
 
     fun getLottoNumbers(): List<Int> = numbers
