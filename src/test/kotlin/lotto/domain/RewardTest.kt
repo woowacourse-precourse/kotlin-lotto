6ed4a2cb07f77fad87076c1513ec13ad43fe8d87
Lee-Sunho <sunho9889@soongsil.ac.kr>
2022-11-15 04:10:20 +0900

    test(RewardTest): 수익률 출력 테스트 추가

--- src/test/kotlin/lotto/domain/RewardTest.kt
+++ src/test/kotlin/lotto/domain/RewardTest.kt
@@ -1,15 +1,21 @@
 package lotto.domain
 
-import lotto.domain.Lotto
+import lotto.views.OutputView
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
 
 class RewardTest {
     @Test
-    fun `등수 개수 구하기 테스트`(){
+    fun `등수 개수 구하기 테스트`() {
         var rewards = mutableListOf<Reward>(Reward.FIFTH, Reward.FIFTH, Reward.THIRD, Reward.FIRST)
         var count = countReward(rewards)
         assertThat(count).containsSequence(1, 0, 1, 0, 2)
+    }
+
+    @Test
+    fun `수익률 출력 테스트`() {
+        var rewards = mutableListOf<Reward>(Reward.FIFTH)
+        var result = calculateYield(rewards, 8)
+        OutputView.printYield(result)
     }
 }
