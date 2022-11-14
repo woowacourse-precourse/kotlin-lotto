362deb4ce844c49767855b274ca204a1df32a514
phs <gustjd0808@pusan.ac.kr>
2022-11-15 02:11:07 +0900

    feat(domain): add requirement and exception message

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -1,9 +1,19 @@
 package lotto.domain
 
+import lotto.enum.Configuration
+import lotto.enum.ErrorString
+
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6)
+        require(numbers.size == Configuration.LOTTO_SIZE.number) {
+            ErrorString.LOTTO_SIZE.string
+        }
+        require(numbers.distinct().size == Configuration.LOTTO_SIZE.number) {
+            ErrorString.DUPLICATED_DRAWN_NUMBERS
+        }
+        require(numbers.all{ number ->
+            number in Configuration.START_LOTTO_NUMBER.number..Configuration.END_LOTTO_NUMBER.number}){
+            ErrorString.OUT_OF_BOUND
+        }
     }
-
-    // TODO: 추가 기능 구현
 }
