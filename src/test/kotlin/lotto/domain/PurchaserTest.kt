ef041c38bf1172b22d482bfa7767c0f8dbe4ba8a
re4rk <37167652+re4rk@users.noreply.github.com>
2022-11-14 19:28:56 +0900

    test(lotto): PurchaserTest 케이스 추가

--- src/test/kotlin/lotto/domain/PurchaserTest.kt
+++ src/test/kotlin/lotto/domain/PurchaserTest.kt
@@ -1,9 +1,27 @@
 package lotto.domain
 
+import lotto.resources.LOTTO_TICKET_PRICE
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import java.lang.IllegalArgumentException
 
 class PurchaserTest {
+    @Test fun `티켓을 잘 구매한다`() {
+        assertThat(
+            Purchaser.purchase(LOTTO_TICKET_PRICE * 17).size == 17
+        )
+    }
+
+    @Test fun `잘 못된 값을 입력하면 오류 발생`() {
+        assertThrows<IllegalArgumentException> {
+            Purchaser.purchase(LOTTO_TICKET_PRICE * -1)
+        }
+        assertThrows<IllegalArgumentException> {
+            Purchaser.purchase(LOTTO_TICKET_PRICE - 1)
+        }
+    }
+
     @Test
     fun `수익률 테스트`() {
         assertThat(
