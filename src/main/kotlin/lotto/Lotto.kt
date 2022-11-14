a7790faee753b9a1cdf6050fe06cf7974259ae17
dlgocks1 <cksgodl1@naver.com>
2022-11-14 09:52:26 +0900

    feat(Lotto) : 로또 번호 생성 및 예외처리구현

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,9 +1,33 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6)
+        checkValidation()
     }
 
-    // TODO: 추가 기능 구현
+    private fun checkValidation() {
+        require(numbers.size == 6) {
+            "$ERROR_MESSAGE 로또의 길이는 6이여야 합니다."
+        }
+        require(numbers.distinct().size == 6) {
+            "$ERROR_MESSAGE 중복된 숫자가 없어야 합니다."
+        }
+        numbers.forEach { number ->
+            require(number in 1..45) {
+                "$ERROR_MESSAGE 중복된 숫자가 없어야 합니다."
+            }
+        }
+    }
+
+
+    companion object {
+        private const val ERROR_MESSAGE = "[ERROR]"
+        fun newInstance(): Lotto {
+            return Lotto(makeRandomNum())
+        }
+
+        private fun makeRandomNum(): List<Int> = Randoms.pickUniqueNumbersInRange(1, 45, 6)
+    }
 }
