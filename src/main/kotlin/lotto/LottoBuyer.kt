8bf807d3a7c5ea5491b911ec74edd0337c533ff9
dohun_ <silver2679@gmail.com>
2022-11-14 23:06:16 +0900

    Feat(LottoBuyer): 로또 금액 예외처리

--- src/main/kotlin/lotto/LottoBuyer.kt
+++ src/main/kotlin/lotto/LottoBuyer.kt
@@ -3,9 +3,19 @@ package lotto
 import camp.nextstep.edu.missionutils.Console
 
 class LottoBuyer {
+
     fun inputLottoPurchasePrice(): Int {// 로또 구입 금액
         println("구입금액을 입력해 주세요.")
-        val inputPrice = Console.readLine()//String
+        val inputPrice = Console.readLine()
+        inputLottoPurchasePriceError(inputPrice)
         return inputPrice.toInt()
     }
+
+    fun inputLottoPurchasePriceError(inputPrice: String) {// 로또 금액 예외처리
+        when {
+            inputPrice.toInt() < 1000 -> return throw IllegalArgumentException("[ERROR] 천원보다 적은 숫자 입니다.")
+            inputPrice.toInt() % 1000 != 0 -> return throw IllegalArgumentException("[ERROR] 천원으로 나누어 떨어지지 않는 숫자입니다.")
+        }
+    }
+
 }
