69af1b694b02a09efdc96f4ee47735fc08b9fdee
dlwgnud <wngud980411@naver.com>
2022-11-14 23:12:19 +0900

    feat(getLottos): 사용자가 구입한 금액에 따른 로또 생성

--- src/main/kotlin/lotto/MakeLottos.kt
+++ src/main/kotlin/lotto/MakeLottos.kt
@@ -6,4 +6,15 @@ class MakeLottos {
     private fun getRandomNumber(): List<Int> {
         return Randoms.pickUniqueNumbersInRange(1, 45, 6).sorted()
     }
+
+    fun getLottos(count: Int): List<Lotto> {
+        val lottos = mutableListOf<Lotto>()
+
+        repeat(count){
+            val lotto = getRandomNumber()
+            lottos.add(Lotto(lotto))
+        }
+
+        return lottos
+    }
 }
