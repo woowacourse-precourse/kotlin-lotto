0053d9d5858b29ed299c1a8e3a4b003b4fb5159e
choiwoosuk <wooseokchoi0508@gmail.com>
2022-11-14 13:29:47 +0900

    feat(prizeResult): 전체 당첨 내역정보를 담고 있는 dataClass 구현

---
+++ src/main/kotlin/lotto/domain/PrizeResult.kt
@@ -0,0 +1,9 @@
+package lotto.domain
+
+data class PrizeResult(
+    var firstPlace:Int=0,
+    var secondPlace:Int=0,
+    var thirdPlace:Int=0,
+    var fourthPlace:Int=0,
+    var fifthPlace:Int=0,
+)
