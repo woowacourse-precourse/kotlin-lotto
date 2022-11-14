4dc0cf358d21682a5c8291802dc607e767f583c9
whk06061 <63198157+whk06061@users.noreply.github.com>
2022-11-14 21:56:42 +0900

    test(lotto): LottoTest의 패키지를 lotto로 변경

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -1,5 +1,6 @@
-package lotto.domain
+package lotto
 
+import lotto.domain.Lotto
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.assertj.core.api.Assertions.assertThat
