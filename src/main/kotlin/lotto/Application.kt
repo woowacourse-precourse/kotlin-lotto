34fad5f5c06327b1531e8991db275dba70cb8715
bluecat0823 <star4_you@naver.com>
2022-11-14 18:20:24 +0900

    [feat] 로또 구입 금액을 입력받는다.

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,4 +1,20 @@
 package lotto
+import camp.nextstep.edu.missionutils.Randoms
+import camp.nextstep.edu.missionutils.Console
+
+fun payForLotto() : Int{
+    var pay = 0
+    println("구입금액을 입력해 주세요.")
+    try {
+        pay = readLine()!!.toInt()
+    } catch (e : NumberFormatException){
+        println("[ERROR] 입력된 금액이 올바른 금액이 아닙니다.")
+    }
+    require(pay % 1000 == 0){
+        "[ERROR] 입력 금액이 1000원 단위가 아닙니다."
+    }
+    return pay
+}
 
 fun main() {
     TODO("프로그램 구현")
