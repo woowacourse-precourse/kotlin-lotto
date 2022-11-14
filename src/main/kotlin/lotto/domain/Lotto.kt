60d13571c780f023a5f441b60b26d3197cd73476
whk06061 <63198157+whk06061@users.noreply.github.com>
2022-11-14 19:41:10 +0900

    refactor(lotto): 패키지 이동
    domain 패키지로 이동

---
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -0,0 +1,12 @@
+package lotto.domain
+
+class Lotto(private val numbers: List<Int>) {
+
+    init {
+        require(numbers.size == 6)
+    }
+
+    fun getNumbers(): List<Int> {
+        return numbers.sorted()
+    }
+}
