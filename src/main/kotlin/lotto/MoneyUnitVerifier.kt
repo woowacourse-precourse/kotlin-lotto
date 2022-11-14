39277f96820076672443c8b9f30e1a2b8dec058b
Gloria <ippnsj@gmail.com>
2022-11-14 22:12:39 +0900

    feat: 특정 숫자로 나누어 떨어지는 수인지 확인하는 기능 구현

--- src/main/kotlin/lotto/MoneyUnitVerifier.kt
+++ src/main/kotlin/lotto/MoneyUnitVerifier.kt
@@ -1,5 +1,14 @@
 package lotto
 
-class MoneyUnitVerifier : Verifier<Int>() {
-    override fun verify(input: Int) {}
+class MoneyUnitVerifier(private val divisor: Int) : Verifier<Int>() {
+    override fun verify(input: Int) {
+        require(input % divisor == 0) {
+            println("$ERROR_HEADER $ERROR_MESSAGE")
+            ERROR_MESSAGE
+        }
+    }
+
+    companion object {
+        const val ERROR_MESSAGE = "Input is not divisible by the divisor."
+    }
 }
