982a1a5a0a068181cc31db27d5dfe61addeb7fa5
Gloria <ippnsj@gmail.com>
2022-11-15 10:24:48 +0900

    feat: 사용자가 입력한 당첨 번호를 받는 기능 구현

--- src/main/kotlin/lotto/view/Input.kt
+++ src/main/kotlin/lotto/view/Input.kt
@@ -8,11 +8,17 @@ class Input {
         return get()
     }
 
+    fun getWinningNumbers(): String {
+        println(INPUT_WINNING_NUMBERS_GUIDE)
+        return get()
+    }
+
     private fun get(): String {
         return Console.readLine()
     }
 
     companion object {
         const val INPUT_MONEY_GUIDE = "구입금액을 입력해 주세요."
+        const val INPUT_WINNING_NUMBERS_GUIDE = "당첨 번호를 입력해 주세요."
     }
 }
