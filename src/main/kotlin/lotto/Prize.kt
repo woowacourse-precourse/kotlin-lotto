4d3569c1ebac7d54ec6c1f3b4394fbf19a8cc4d3
healthykim <bsbs8645@snu.ac.kr>
2022-11-15 13:30:34 +0900

    feat(prize): Add function for updating prize according to equal number

--- src/main/kotlin/lotto/Prize.kt
+++ src/main/kotlin/lotto/Prize.kt
@@ -7,4 +7,17 @@ enum class Prize(val earnings: Int) {
     FOURTH(50_000),
     FIFTH(5_000),
     NOTHING(0);
+
+    /**
+     * return Prize according to [ball] and [bonus]
+     */
+    fun updatePrize(ball: Int, bonus: Boolean): Prize {
+        when(ball) {
+            6 -> return FIRST
+            5 -> return if(bonus) SECOND else THIRD
+            4 -> return FOURTH
+            3 -> return FIFTH
+        }
+        return NOTHING
+    }
 }
