f04c00fc9cc48ec557f3db71a3ffe8f7fc608271
jjw0071 <jjw0071@naver.com>
2022-11-15 19:19:35 +0900

    feat : 입력한 개수만큼 로또 생성하는 함수

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,7 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
     TODO("프로그램 구현")
 }
@@ -16,5 +18,14 @@ fun moneyValidCheck(a : String) : Int {
     if(money%1000 != 0)
         throw IllegalArgumentException("[ERROR] 숫자가 1000으로 나누어 떨어지지 않습니다.")
     return money
+}
+
+fun lottoMaker(lottoCount : Int) : MutableList<Lotto>{
+    var lottoPack : MutableList<Lotto> = mutableListOf()
+    for(i : Int in 1..lottoCount){
+        lottoPack.add(Lotto(Randoms.pickUniqueNumbersInRange(1,45,6).sorted()))
+        lottoPack[i-1].printLotto()
+    }
+    return lottoPack
 }
 
