76426db6626fe65efb7c573162e6679cb6241fc8
s9hn <tnghqn@naver.com>
2022-11-15 22:54:39 +0900

    [REFACTOR(Customer)] 문자열 포함한 입력값 예외 처리

--- src/main/kotlin/lotto/domain/Customer.kt
+++ src/main/kotlin/lotto/domain/Customer.kt
@@ -7,9 +7,10 @@ import java.text.DecimalFormat
 class Customer() {
 
     fun payMoney(): Int {
-        val payment = Console.readLine().toInt()
+        val payment = Console.readLine()
         throwPaymentException(payment)
-        return payment
+        return payment.toInt()
+
     }
 
     fun calculateIncome(money: Int, payment: Int): Double {
@@ -17,7 +18,15 @@ class Customer() {
         return formattingNum.format(money.toDouble() / payment.toDouble() * 100).toDouble()
     }
 
-    fun throwPaymentException(payment: Int) {
-        if ((payment % 1000) != 0) throw IllegalArgumentException(Message.PAYMENT_RULE.message)
+    fun throwPaymentException(payment: String) {
+
+        payment.forEach { char ->
+            val charConvertedToCode = char.code
+            if ((charConvertedToCode > 57) or (charConvertedToCode < 48)) {
+                throw IllegalArgumentException("[ERROR] 정수만 입력 가능합니다")
+            }
+        }
+
+        if ((payment.toInt() % 1000) != 0) throw IllegalArgumentException(Message.PAYMENT_RULE.message)
     }
 }
