eefd3fd0dc451f9220c2872d8c67915a4cc36101
phs <gustjd0808@pusan.ac.kr>
2022-11-15 16:45:21 +0900

    feat(domain): add methods to play lotto game
    methods
    - caculate earning rate
    - get winning list
    - play game and return game result

--- src/main/kotlin/lotto/domain/LottoGame.kt
+++ src/main/kotlin/lotto/domain/LottoGame.kt
@@ -8,6 +8,28 @@ class LottoGame(
     private val bonusNumber: BonusNumber,
     private val userLotto: LottoList
 ) {
+    fun play(): Pair<List<Int>, Double> {
+        val winningResult = userLotto.compare(winningLotto, bonusNumber)
+        val totalReward = calculateTotalReward(winningResult)
+        val earningRate = calculateEarningRate(totalReward)
+        val winningList = getWinningList(winningResult)
+        return winningList to earningRate
+    }
+
+    private fun getWinningList(winningResult: Map<WinningRank, Int>): List<Int>{
+        val result = mutableListOf<Int>()
+        result.add(winningResult[WinningRank.FIFTH]?:0)
+        result.add(winningResult[WinningRank.FOURTH]?:0)
+        result.add(winningResult[WinningRank.THIRD]?:0)
+        result.add(winningResult[WinningRank.SECOND]?:0)
+        result.add(winningResult[WinningRank.FIRST]?:0)
+        return result
+    }
+    private fun calculateEarningRate(totalReward: Int): Double {
+        val purchaseAmount = userLotto.purchaseAmount().toDouble()
+        return totalReward / purchaseAmount * 100
+    }
+
     private fun calculateTotalReward(winningResult: Map<WinningRank, Int>): Int {
         return winningResult.map {
             Reward.valueOf(it.key.toString()).reward * it.value
