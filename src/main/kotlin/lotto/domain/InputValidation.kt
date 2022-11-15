969ef1556cc9aabacbf3f689364ea5336b52a8a6
ChoiWooSuk <wooseokchoi0508@gmail.com>
2022-11-15 17:50:20 +0900

    feat(inputValidation): 보너스 번호가 1~45 에 포함되지 않을 경우 예외 발생 기능 구현

--- src/main/kotlin/lotto/domain/InputValidation.kt
+++ src/main/kotlin/lotto/domain/InputValidation.kt
@@ -6,26 +6,31 @@ class InputValidation {
         return try {
             input.toInt()
         } catch (e: NumberFormatException) {
-            throw IllegalArgumentException(NOT_INTEGER_ERROR_MESSAGE)
+            throw IllegalArgumentException(INTEGER_ERROR_MESSAGE)
         }
     }
 
     fun checkAmountMultipleOfThousand(amount:Int):Int{
-        if(amount % LOTTO_PRICE!=0) throw IllegalArgumentException(NOT_MULTIPLE_OF_THOUSAND_ERROR_MESSAGE)
+        if(amount % LOTTO_PRICE!=0) throw IllegalArgumentException(MULTIPLE_OF_THOUSAND_ERROR_MESSAGE)
         return amount
     }
 
     fun checkInputSplitWithComma(input:String):List<String>{
         val winningNumbers=input.split(",")
-        if(winningNumbers.size!=WINNING_NUMBER_COUNT) throw java.lang.IllegalArgumentException(NOT_SPLIT_WITH_COMMA_ERROR_MESSAGE)
+        if(winningNumbers.size!=WINNING_NUMBER_COUNT) throw java.lang.IllegalArgumentException(SPLIT_WITH_COMMA_ERROR_MESSAGE)
         return winningNumbers
+    }
+
+    fun checkBonusNumberRange(bonusNumber: Int){
+        if(bonusNumber< Lotto.MIN_NUMBER || bonusNumber> Lotto.MAX_NUMBER) throw IllegalArgumentException(BONUS_NUMBER_RANGE_ERROR_MESSAGE)
     }
 
     companion object {
         const val LOTTO_PRICE = 1000
         const val WINNING_NUMBER_COUNT = 6
-        const val NOT_INTEGER_ERROR_MESSAGE = "[ERROR] 정수를 입력해 주셔야 합니다."
-        const val NOT_MULTIPLE_OF_THOUSAND_ERROR_MESSAGE = "[ERROR] 1000의 배수 단위로 구매 금액을 입력해 주셔야 합니다."
-        const val NOT_SPLIT_WITH_COMMA_ERROR_MESSAGE = "[ERROR] 숫자 6개를 ,로 구분해 주세요"
+        const val INTEGER_ERROR_MESSAGE = "[ERROR] 정수를 입력해 주셔야 합니다."
+        const val MULTIPLE_OF_THOUSAND_ERROR_MESSAGE = "[ERROR] 1000의 배수 단위로 구매 금액을 입력해 주셔야 합니다."
+        const val SPLIT_WITH_COMMA_ERROR_MESSAGE = "[ERROR] 숫자 6개를 ,로 구분해 주세요"
+        const val BONUS_NUMBER_RANGE_ERROR_MESSAGE ="[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다."
     }
 }
