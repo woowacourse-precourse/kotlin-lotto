7ee1a48da38940cbbf20bc2bc35d4fd286ac3528
Yuhee Kim <dbgml4509@naver.com>
2022-11-15 20:52:03 +0900

    feat : 로또 구입 금액 입력 시 예외 처리 추가

--- src/main/kotlin/lotto/CreateLottos.kt
+++ src/main/kotlin/lotto/CreateLottos.kt
@@ -5,14 +5,12 @@ import camp.nextstep.edu.missionutils.Randoms
 class CreateLottos {
 
     companion object{
-        // 1-1. 로또 수량 계산
         fun getLottoCount(lottoPrice : Int) : Int{
+            LottoExecptions().getInputLottoPriceUnitException(lottoPrice)
             val lottoCount = lottoPrice / 1000
-            //만약 나누어지지 않을 시에 발생하는 예외 사항 처리 필요
             return lottoCount
         }
 
-        //4. 1~45까지의 숫자 중 중복되지 않는 6개의 숫자를 뽑는다
         fun getLottoNums() : List<Int> {
             val numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6).sorted()
             return numbers
