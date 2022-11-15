9273c75b4d326585d5f0a0e6269b257e7eb799ba
YeonwooLee <fox_93@naver.com>
2022-11-15 16:42:44 +0900

    buy lotto

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,44 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
-    TODO("프로그램 구현")
+    //금액설정
+    val moneyString:String = "3000"
+
+    //몇장인지 설정
+    val buyCount = moneyToCount(moneyString)
+    //로또용지생성
+    val paper = Array<Lotto>(buyCount){Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6))}
+    //로또용지출력
+    printLotto(paper)
+
 }
+
+fun printLotto(paper:Array<Lotto>){
+    println("${paper.size}개를 구매했습니다.")
+    paper.forEach{println(it)}
+
+}
+fun moneyToCount(money:String):Int{
+    money.forEach{
+        isNumber(it)
+    }
+    isThousand(money.toInt())
+    return money.toInt()/1000
+}
+fun isNumber(c:Char){
+    val code = c.code
+    if(code<'0'.code || code>'9'.code){
+        throw IllegalArgumentException("[ERROR] 금액이 숫자 형태가 아닙니다")
+    }
+}
+
+fun isThousand(money:Int){
+    if(money%1000!=0){
+        throw IllegalArgumentException("[ERROR] 금액은 1000 단위로 입력해주세요")
+    }
+}
+
+
+
--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -2,8 +2,27 @@ package lotto
 
 class Lotto(private val numbers: List<Int>) {
     init {
+        if(numbers.size!=6){
+            throw IllegalArgumentException("[ERROR] 로또 번호가 6개가 아닙니다")
+        }
         require(numbers.size == 6)
     }
 
     // TODO: 추가 기능 구현
+    fun game(answer:Array<Int>,bonusNumber:Int):Int{
+        //정식번호 맞은 개수
+        var sameCnt = 7
+        answer.forEach{
+            if(numbers.contains(it)){
+                sameCnt--;
+            }
+        }
+        return sameCnt
+
+    }
+
+    override fun toString(): String {
+        return numbers.toString()
+    }
 }
+
