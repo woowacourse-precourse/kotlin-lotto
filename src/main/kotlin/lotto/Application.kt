c6ab42ef6ecb5ae75c19c4b8bd64ae9019f2087b
Jaehun.Kim <rlawogns0411@naver.com>
2022-11-15 22:05:27 +0900

    remove : print

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -3,7 +3,6 @@ package lotto
 fun main() {
     val baseData = BaseData()
     baseData.baseData()
-    print(baseData.LottoNum)
     val lotto = Lotto(baseData.LottoNum)
     val winLotto = lotto.checkAllWinLotto(baseData.purchasedLotto,baseData.bonusNum)
     lotto.printResult(winLotto,baseData.inputMoney)
