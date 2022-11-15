44b7fa88b5b784d8ffbea1c2c746bb59c68753a3
Yerin <myheaven_b@naver.com>
2022-11-15 18:03:31 +0900

    test: add testcase for nummbers prints in ascending order

--- src/test/kotlin/lotto/LottoGameTest.kt
+++ src/test/kotlin/lotto/LottoGameTest.kt
@@ -1,5 +1,6 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.test.Assertions
 import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
 import camp.nextstep.edu.missionutils.test.NsTest
 import org.assertj.core.api.Assertions.assertThat
@@ -20,6 +21,24 @@ class LottoGameTest : NsTest() {
             runException("one")
             assertThat(output()).contains(ERROR_MESSAGE)
         }
+    }
+
+    @Test
+    fun `로또 생성기 오름차순 확인 테스트`() {
+        Assertions.assertRandomUniqueNumbersInRangeTest(
+            {
+                run("3000", "1,2,3,4,5,6", "7")
+                assertThat(output()).contains(
+                    "3개를 구매했습니다.",
+                    "[8, 21, 23, 41, 42, 43]",
+                    "[3, 5, 11, 16, 32, 38]",
+                    "[7, 11, 16, 35, 36, 44]"
+                )
+            },
+            listOf(41, 42, 43, 8, 21, 23),
+            listOf(3, 5, 32, 38, 11, 16),
+            listOf(7, 16, 11, 44, 35, 36)
+        )
     }
 
     override fun runMain() {
