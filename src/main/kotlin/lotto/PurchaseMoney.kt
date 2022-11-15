e540a23a4f266eb3a1c8f49bd4997345a8470339
joh9911 <joh9911@naver.com>
2022-11-15 16:01:46 +0900

    feat: 구매 금액 저장하는 클래스 PurchaseMoney 생성

---
+++ src/main/kotlin/lotto/PurchaseMoney.kt
@@ -0,0 +1,12 @@
+package lotto
+import lotto.ExceptionHandler.throwExceptionForPurchaseMoney
+
+class PurchaseMoney(private val purchaseMoney: Int) {
+    init{
+        throwExceptionForPurchaseMoney(purchaseMoney)
+    }
+
+    fun getPurchaseMoney(): Int {
+        return this.purchaseMoney
+    }
+}
