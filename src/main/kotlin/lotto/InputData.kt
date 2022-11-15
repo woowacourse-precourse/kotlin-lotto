bdac080feadb07b606e0e52951aea7d5a1ff78d4
Jaehun.Kim <rlawogns0411@naver.com>
2022-11-15 19:51:03 +0900

    feat : 구입금액 입력 기능

--- src/main/kotlin/lotto/InputData.kt
+++ src/main/kotlin/lotto/InputData.kt
@@ -1,7 +1,14 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Console
+
 class InputData {
     var inputMoney = 0
     var LottoNum = mutableListOf<Int>()
     var bonusNum = 0
+
+    fun inputMoney(): Int {
+        println("구입금앱을 입력해 주세요.")
+        return Console.readLine().toInt()
+    }
 }
