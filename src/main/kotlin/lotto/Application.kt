fc6d61ccfaa556c9e0cd0b2497e35a90a4553915
KimDongHyun0907 <ehdgus8397@naver.com>
2022-11-15 22:38:20 +0900

    BuyLottoCount main 함수 추가

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -3,5 +3,9 @@ package lotto
 fun main() {
     val input_buy = Money().InputMoney()
     val buy = Money().InputError(input_buy)
+
+    println()
+
+    val buy_count = Number().BuyLottoCount(buy)
 }
 
