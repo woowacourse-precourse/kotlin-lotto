6c4b90111d694f4e8f49c30454346cb272013854
dlwgnud <wngud980411@naver.com>
2022-11-15 20:30:37 +0900

    feat(result): 로또 결과 계산하는 함수

--- src/main/kotlin/lotto/ResultLotto.kt
+++ src/main/kotlin/lotto/ResultLotto.kt
@@ -6,5 +6,42 @@ class ResultLotto {
         println("---")
     }
 
+    fun result(
+        lottoList: List<List<Int>>,
+        winningNumber: List<Int>,
+        bonusNumber: Int,
+    ): Long {
+        var money = 0L
+        val prizeResult = mutableListOf(0, 0, 0, 0, 0)
 
+        for (lotto in lottoList) {
+            val rank = compareWinningNumber(lotto, winningNumber)
+            val bonus = compareBonusNumber(lotto, bonusNumber)
+
+            when {
+                rank == 3 -> {
+                    prizeResult[0]++
+                    money += 5_000
+                }
+                rank == 4 -> {
+                    prizeResult[1]++
+                    money += 50_000
+                }
+                rank == 5 -> {
+                    prizeResult[2]++
+                    money += 1_500_000
+                }
+                rank == 5 && bonus -> {
+                    prizeResult[3]++
+                    money += 30_000_000
+                }
+                rank == 6 -> {
+                    prizeResult[4]++
+                    money += 2_000_000_000
+                }
+            }
+        }
+        printResult(prizeResult)
+        return money
+    }
 }
