0e429f456d5c947436060b74b524e35dd51158e4
healthykim <bsbs8645@snu.ac.kr>
2022-11-15 14:00:43 +0900

    feat(ui): Add function for printing statistics

--- src/main/kotlin/lotto/UI.kt
+++ src/main/kotlin/lotto/UI.kt
@@ -33,4 +33,19 @@ class UI {
             println(lottery.lottoToString())
         }
     }
+    /**
+     * @brief Print statistics
+     * @param winnings : map of (prize, the number of lotto won that prize)
+     * @param earningsRate : rate of return
+     */
+    fun printStatistics(winnings: Map<Prize,Int>, earningsRate: Double) {
+        println("당첨 통계\n" +
+                "---\n" +
+                "3개 일치 (5,000원) - ${if(winnings[Prize.FIFTH]==null) 0 else winnings[Prize.FIFTH]}개\n" +
+                "4개 일치 (50,000원) - ${if(winnings[Prize.FOURTH]==null) 0 else winnings[Prize.FOURTH]}개\n" +
+                "5개 일치 (1,500,000원) - ${if(winnings[Prize.THIRD]==null) 0 else winnings[Prize.THIRD]}개\n" +
+                "5개 일치, 보너스 볼 일치 (30,000,000원) - ${if(winnings[Prize.SECOND]==null) 0 else winnings[Prize.SECOND]}개\n" +
+                "6개 일치 (2,000,000,000원) - ${if(winnings[Prize.FIRST]==null) 0 else winnings[Prize.FIRST]}개\n" +
+                "총 수익률은 ${decFormat.format(earningsRate)}%입니다.")
+    }
 }
