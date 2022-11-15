ff454a9b8fd5f86998b233ba23953f99c20e1c8d
phs <gustjd0808@pusan.ac.kr>
2022-11-15 19:27:56 +0900

    fix(enum): change logic that check whether win

--- src/main/kotlin/lotto/enum/WinningRank.kt
+++ src/main/kotlin/lotto/enum/WinningRank.kt
@@ -1,13 +1,30 @@
 package lotto.enum
 
-enum class WinningRank(val matches: Int, val bonusMatch: Boolean) {
-    FIFTH(3, false),
-    FOURTH(4, false),
-    THIRD(5, false),
-    SECOND(5, true),
-    FIRST(6, false),
-    NOTHING(0, false);
+enum class WinningRank(val matches: Int, val bonusMatch: Boolean = false) {
+    FIFTH(3) {
+        override fun isWinning(matches: Int, bonusMatch: Boolean): Boolean =
+            this.matches == matches
+    },
+    FOURTH(4) {
+        override fun isWinning(matches: Int, bonusMatch: Boolean): Boolean =
+            this.matches == matches
+    },
+    THIRD(5, false) {
+        override fun isWinning(matches: Int, bonusMatch: Boolean): Boolean =
+            this.matches == matches && this.bonusMatch == bonusMatch
+    },
+    SECOND(5, true) {
+        override fun isWinning(matches: Int, bonusMatch: Boolean): Boolean =
+            this.matches == matches && this.bonusMatch == bonusMatch
+    },
+    FIRST(6) {
+        override fun isWinning(matches: Int, bonusMatch: Boolean): Boolean =
+            this.matches == matches
+    },
+    NOTHING(0) {
+        override fun isWinning(matches: Int, bonusMatch: Boolean): Boolean =
+            this.matches == matches
+    };
 
-    fun isWinning(matches: Int, bonusMatch: Boolean): Boolean =
-        this.matches == matches && this.bonusMatch == bonusMatch
+    abstract fun isWinning(matches: Int, bonusMatch: Boolean): Boolean
 }
