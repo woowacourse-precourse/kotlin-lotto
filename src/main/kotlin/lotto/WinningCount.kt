a7a09d4f7b91df49e09d1f51bdcdb891f9baf6f4
KimDongHyun0907 <ehdgus8397@naver.com>
2022-11-15 22:57:50 +0900

    당첨 번호의 개수에 따른 value update

--- src/main/kotlin/lotto/WinningCount.kt
+++ src/main/kotlin/lotto/WinningCount.kt
@@ -6,8 +6,47 @@ class WinningCount{
 
         for(elem in total_numbers){
             val winning_numbers=lotto_numbers_list.toSet().intersect(elem)
-
+            ThreeNumbers(winning_numbers,winning_numbers_count)
+            FourNumbers(winning_numbers,winning_numbers_count)
+            FiveNumbersAndBonus(winning_numbers,winning_numbers_count,bonus,elem)
+            FiveNumbers(winning_numbers,winning_numbers_count,bonus,elem)
+            SixNumbers(winning_numbers,winning_numbers_count)
         }
         return winning_numbers_count
+    }
+
+    fun ThreeNumbers(winning_numbers:Set<Int>,winning_numbers_count:MutableMap<Int,Int>){
+        if(winning_numbers.size==3){
+            winning_numbers_count[5000] = winning_numbers_count[5000]!! + 1
+            return
+        }
+    }
+
+    fun FourNumbers(winning_numbers:Set<Int>,winning_numbers_count:MutableMap<Int,Int>){
+        if(winning_numbers.size==4){
+            winning_numbers_count[50000] = winning_numbers_count[50000]!! + 1
+            return
+        }
+    }
+
+    fun FiveNumbersAndBonus(winning_numbers:Set<Int>,winning_numbers_count:MutableMap<Int,Int>,bonus: Int,elem:List<Int>){
+        if(winning_numbers.size==5 && bonus in elem){
+            winning_numbers_count[30000000] = winning_numbers_count[30000000]!! + 1
+            return
+        }
+    }
+
+    fun FiveNumbers(winning_numbers:Set<Int>,winning_numbers_count:MutableMap<Int,Int>,bonus: Int,elem:List<Int>){
+        if(winning_numbers.size==5 && bonus !in elem){
+            winning_numbers_count[1500000] = winning_numbers_count[1500000]!! + 1
+            return
+        }
+    }
+
+    fun SixNumbers(winning_numbers:Set<Int>,winning_numbers_count:MutableMap<Int,Int>){
+        if(winning_numbers.size==6){
+            winning_numbers_count[2000000000] = winning_numbers_count[2000000000]!! + 1
+            return
+        }
     }
 }
