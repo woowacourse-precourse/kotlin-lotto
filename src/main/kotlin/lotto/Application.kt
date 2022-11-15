99b087ec49e11d4e478341fe02c73fcfac4667a9
jjw0071 <jjw0071@naver.com>
2022-11-15 19:21:38 +0900

    feat : 로또번호의 적합성 확인하는 함수

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -29,3 +29,101 @@ fun lottoMaker(lottoCount : Int) : MutableList<Lotto>{
     return lottoPack
 }
 
+fun lottoNumberValidCheck(a : CharArray) : CharArray{
+    var check : Int
+    for(i : Int in 0..a.size-1){
+        check = Character.getNumericValue(a[i])
+        if(check < -1 || check >9)
+            throw IllegalArgumentException("[ERROR] 숫자를 입력해 주세요")
+        if(check == -1 && a[i] != ',')
+            throw IllegalArgumentException("[ERROR] 적절하지 않은 입력 값이 있습니다")
+    }
+    return a
+}
+
+fun lottoZeroCheck(a : CharArray) : CharArray{
+    var beforecheck : Int
+    var check : Int
+    var nextcheck : Int
+    for(i : Int in 0..a.size-3){
+        beforecheck = Character.getNumericValue(a[i])
+        check = Character.getNumericValue(a[i+1])
+        nextcheck = Character.getNumericValue(a[i+2])
+        if(beforecheck == -1 && check == 0 && nextcheck == -1)
+            throw IllegalArgumentException("[ERROR] 0은 당첨번호가 될 수 없습니다.")
+    }
+    return a
+}
+
+fun lottoCommaCheck(a : CharArray) : CharArray{
+    var check : Int
+    var nextcheck : Int
+    for(i : Int in 0..a.size-2){
+        check = Character.getNumericValue(a[i])
+        nextcheck = Character.getNumericValue(a[i+1])
+        if(check == nextcheck && check == -1)
+            throw IllegalArgumentException("[ERROR] ,가 연속적으로 입력되었습니다. 제대로 입력해 주세요")
+    }
+    return a
+}
+
+fun lottoLastNumberCheck(a : CharArray) : CharArray{
+    if(Character.getNumericValue(a[a.size-1]) == -1){
+        throw IllegalArgumentException("[ERROR] 마지막 값은 숫자여야 합니다.")
+    }
+    return a
+}
+
+fun toIntegerList(a : CharArray) : MutableList<Int>{
+    var lottoNumbers : MutableList<Int> = mutableListOf()
+    var i = 0
+    if(a.size<11)
+        throw IllegalArgumentException("[ERROR] 입력값이 너무 적습니다.2312")
+    do{
+        i = makeInteger(i,a, lottoNumbers)
+    }while(lottoNumbers.size <6)
+
+    return lottoNumbers
+}
+
+fun lottoSizeCheck(a : MutableList<Int>):MutableList<Int>{
+    if(a.size !=6 ){
+        throw IllegalArgumentException("[ERROR] 6개의 숫자가 입력되지 않았습니다.")
+    }
+    return a
+}
+
+fun makeInteger(i : Int, a : CharArray, lottoNumbers : MutableList<Int>) : Int {
+    var tempInt: MutableList<Int> = mutableListOf()
+    var check = i
+    do {
+        tempInt.add(Character.getNumericValue(a[check]))
+        check++
+    } while (check < a.size && Character.getNumericValue(a[check]) != -1)
+    if (tempInt.size == 1)
+        lottoNumbers.add(tempInt.get(0))
+    if (tempInt.size == 2)
+        lottoNumbers.add(tempInt.get(0) * 10 + tempInt.get(1))
+    if (tempInt.size > 2)
+        throw IllegalArgumentException("[ERROR] 입력한 숫자가 너무 큽니다.")
+    return check + 1
+}
+
+fun sameCheck(lottoNumbers : MutableList<Int>) : MutableList<Int>{
+    var tempNums = lottoNumbers
+    tempNums = tempNums.sorted().toMutableList()
+    for(i : Int in 0..tempNums.size-2){
+        if(tempNums[i] == tempNums[i+1])
+            throw IllegalArgumentException("[ERROR] 중복된 숫자가 있습니다.")
+    }
+    return tempNums
+}
+
+fun numberSizeCheck(lottoNumbers : MutableList<Int>) : MutableList<Int>{
+    for(i : Int in 0..lottoNumbers.size-1){
+        if(lottoNumbers[i]>45)
+            throw IllegalArgumentException("[ERROR] 45보다 큰 숫자가 있습니다.")
+    }
+    return lottoNumbers
+}
+
