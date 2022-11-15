34a8de798341096862d2ea84156cb648742e2173
KimDongHyun0907 <ehdgus8397@naver.com>
2022-11-15 23:13:08 +0900

    Rate 클래스 추가 및 수익률 계산 함수 구현

--- src/main/kotlin/lotto/Rate.kt
+++ src/main/kotlin/lotto/Rate.kt
@@ -1,4 +1,14 @@
 package lotto
 
-class Rate {
+class Rate{
+    fun PrintRate(winning_numbers_count: MutableMap<Int, Int>, buy: Int){
+        var total_money=0
+        for(money in winning_numbers_count.keys){
+            total_money+= winning_numbers_count[money]!! *money
+        }
+
+        print("총 수익률은 ")
+        print(String.format("%.1f",total_money.toFloat()/buy.toFloat()*100))
+        print("%입니다.")
+    }
 }
