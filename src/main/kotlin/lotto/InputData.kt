f41b07bf1f42d695c6d00a258de90d94e0412823
Jaehun.Kim <rlawogns0411@naver.com>
2022-11-15 19:53:33 +0900

    feat : 당첨번호 입력 기능

--- src/main/kotlin/lotto/InputData.kt
+++ src/main/kotlin/lotto/InputData.kt
@@ -17,4 +17,12 @@ class InputData {
             throw IllegalArgumentException("[ERROR] 구입금액은 1000원 단위입니다.")
         }
     }
+
+    fun inputLottoNum(): MutableList<Int> {
+        println("당첨 번호를 입력해 주세요.")
+        val Lotto = Console.readLine()
+        return makeLottoNum(Lotto)
+    }
+
+
 }
