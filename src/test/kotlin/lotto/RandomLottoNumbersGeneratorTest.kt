67252d1185cd091616d086104027cb1fa5e2cd7c
joh9911 <joh9911@naver.com>
2022-11-15 18:22:18 +0900

    test(RandomLottoNumbersGeneratorTest): 랜덤 로또 번호 생성 테스트 코드

---
+++ src/test/kotlin/lotto/RandomLottoNumbersGeneratorTest.kt
@@ -0,0 +1,12 @@
+package lotto
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+class RandomLottoNumbersGeneratorTest {
+    @Test
+    fun `구입 금액 만큼 로또 번호를 생성`() {
+        val purchaseMoney = 6000
+        Assertions.assertThat(RandomLottoNumbersGenerator(purchaseMoney).getRandomLottoes().size == 6).isTrue
+    }
+}
