259e82db66cdd942783ee2af8edf363abea0cf2e
joh9911 <joh9911@naver.com>
2022-11-15 14:48:33 +0900

    refactor(ExceptionHandler): 보너스 번호, 구입 금액 예외처리 코드 추가

--- src/main/kotlin/lotto/ExceptionHandler.kt
+++ src/main/kotlin/lotto/ExceptionHandler.kt
@@ -3,18 +3,18 @@ package lotto
 object ExceptionHandler {
 
     /**
-     *  각 조건에 따라 예외를 던져주는 함수
+     *  입력한 로또 번호에 대해 조건에 따라 예외를 던져주는 함수
      */
-    fun throwException(numbers: List<Int>){
+    fun throwExceptionForLottoNumbers(lottoNumbers: List<Int>){
         when{
-            !hasDistinctNumber(numbers) -> throw IllegalArgumentException("[ERROR] 로또 번호는 서로 중복되지 않은 숫자여야 합니다.")
-            !getIsSizeSix(numbers) -> throw IllegalArgumentException("[ERROR] 로또 번호는 6개의 숫자여야 합니다.")
-            !getIsBetweenOneAndFortyFive(numbers) -> throw IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.")
+            !getHasDistinctNumber(lottoNumbers) -> throw IllegalArgumentException("[ERROR] 로또 번호는 서로 중복되지 않은 숫자여야 합니다.")
+            !getIsSizeSix(lottoNumbers) -> throw IllegalArgumentException("[ERROR] 로또 번호는 6개의 숫자여야 합니다.")
+            !getIsBetweenOneAndFortyFive(lottoNumbers) -> throw IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.")
         }
     }
 
     /**
-     *  숫자가 아니라면 false 를 반환해주는 함수
+     *  입력한 값이 숫자가 아니라면 false 를 반환해주는 함수
      */
     fun getIsInputFormRight(inputSymbol: String): Boolean{
         try {
@@ -27,27 +27,70 @@ object ExceptionHandler {
     }
 
     /**
-     *  중복된 숫자들이 있으면 false 를 반환해주는 함수
+     *  입력한 로또 번호 중 중복된 숫자들이 있으면 false 를 반환해주는 함수
      */
-    fun hasDistinctNumber(numbers: List<Int>): Boolean{
-        return numbers.size == numbers.distinct().size
+    fun getHasDistinctNumber(lottoNumbers: List<Int>): Boolean{
+        return lottoNumbers.size == lottoNumbers.distinct().size
     }
 
     /**
-     *  숫자가 6개가 넘어가면 false 를 반환해주는 함수
+     *  입력한 로또 번호 중 숫자가 6개가 넘어가면 false 를 반환해주는 함수
      */
-    fun getIsSizeSix(numbers: List<Int>): Boolean{
-        return numbers.size == 6
+    fun getIsSizeSix(lottoNumbers: List<Int>): Boolean{
+        return lottoNumbers.size == 6
     }
 
     /**
-     * 1 ~ 45 이외의 숫자가 있으면 false 를 반환해주는 함수
+     * 입력한 로또 번호 중 1 ~ 45 이외의 숫자가 있으면 false 를 반환해주는 함수
      */
-    fun getIsBetweenOneAndFortyFive(numbers: List<Int>):  Boolean{
-        for (index in numbers.indices){
-            if (numbers[index] !in 1..45)
+    fun getIsBetweenOneAndFortyFive(lottoNumbers: List<Int>):  Boolean{
+        for (index in lottoNumbers.indices){
+            if (lottoNumbers[index] !in 1..45)
                 return false
         }
         return true
+    }
+
+    /**
+     * 입력한 보너스 번호에 대해 조건에 따라 예외를 던져주는 함수
+     */
+    fun throwExceptionForBonusNumbers(bonusNumber: Int, lottoNumbers: List<Int>){
+        when {
+            !getIsBetweenOneAndFortyFive(bonusNumber) ->
+                throw IllegalArgumentException("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.")
+            !getHasDistinctNumberInLottoNumbers(bonusNumber, lottoNumbers) ->
+                throw IllegalArgumentException("[ERROR] 보너스 번호는 입력했던 6개의 숫자와 중복되지 않아야 합니다.")
+        }
+    }
+
+    /**
+     * 보너스 번호가 1과 45 사이가 아니라면 false 를 반환하는 함수
+     */
+    fun getIsBetweenOneAndFortyFive(bonusNumber: Int): Boolean{
+        return bonusNumber in 1..45
+    }
+
+    /**
+     * 보너스 번호가 입력한 6개의 로또 번호와 중복된다면 false 를 반환하는 함수
+     */
+    fun getHasDistinctNumberInLottoNumbers(bonusNumber: Int, lottoNumbers: List<Int>): Boolean{
+        return !lottoNumbers.contains(bonusNumber)
+    }
+
+    /**
+     * 입력한 구입 금액에 대해 조건에 따라 예외를 던져주는 함수
+     */
+    fun throwExceptionForPurchaseMoney(purchaseMoney: Int){
+        when {
+            !getIsThousandUnit(purchaseMoney) ->
+                throw IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.")
+        }
+    }
+
+    /**
+     * 구입 금액이 1000원 단위가 아니라면 false 를 반환하는 함수
+     */
+    fun getIsThousandUnit(purchaseMoney: Int): Boolean {
+        return purchaseMoney.toDouble() % 1000.0 == 0.0
     }
 }
