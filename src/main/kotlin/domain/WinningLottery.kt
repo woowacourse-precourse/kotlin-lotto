34a02d21bd1aa685d430b1f073850e746d37ecbb
lee989898 <wnsrud1352@gmail.com>
2022-11-15 14:20:50 +0900

    [refactor](WinningLottery) 매직넘버, 매직리터럴 상수로 리펙토링

--- src/main/kotlin/domain/WinningLottery.kt
+++ src/main/kotlin/domain/WinningLottery.kt
@@ -1,14 +1,21 @@
 package domain
 
+import lotto.LottoMessage
+
 class WinningLottery(private val winningNumbers: List<Int>, private val bonusBall: Int) {
 
     init {
-        require(bonusBall in 1..45) { "보너스볼의 번호는 1~45 사이여야 합니다" }
-        require(!winningNumbers.stream().anyMatch { it == bonusBall }) { "당첨번호와 보너스볼에 중복된 번호가 있으면 안됩니다." }
+        require(bonusBall in LOTTO_MIN_BOUND..LOTTO_MAX_BOUND) { LottoMessage.LOTTERY_RANGE_ERROR }
+        require(!winningNumbers.stream().anyMatch { it == bonusBall }) { LottoMessage.NO_DUPLICATED_WITH_BONUS }
     }
 
     fun getRank(lottery: Lotto): Rank {
         val winningCount: Int = Lotto(winningNumbers).countSameNumber(lottery)
         return Rank.getRank(winningCount, lottery.contains(bonusBall))
+    }
+
+    companion object {
+        private const val LOTTO_MIN_BOUND = 1
+        private const val LOTTO_MAX_BOUND = 45
     }
 }
