5b4f021ed057d5f658fce9e05bd4e9ae263e4775
healthykim <bsbs8645@snu.ac.kr>
2022-11-15 13:53:47 +0900

    feat(application): Add function for calculating rate of return

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,6 +1,7 @@
 package lotto
 
 import camp.nextstep.edu.missionutils.Randoms
+import kotlin.math.round
 
 const val LOTTERY_PRICE = 1000
 const val LOTTERY_SIZE = 6
@@ -18,6 +19,19 @@ fun buyLottery(money: Int): List<Lotto> {
         lotteryList.add(Lotto(Randoms.pickUniqueNumbersInRange(NUMBER_RANGE.first, NUMBER_RANGE.last, 6)))
     }
     return lotteryList
+}
+
+/**
+ * @param winnings : map of (prize, the number of lotto won that prize)
+ * @param money : money spent on purchasing lotto
+ * @return rate of return
+ */
+fun calculateEarnings(winnings: Map<Prize,Int>, money: Int): Double {
+    var totalPrize = 0
+    for(prize in winnings) {
+        totalPrize += prize.key.earnings * prize.value
+    }
+    return round(totalPrize.toDouble()/money.toDouble()*1000) /10.0
 }
 
 fun main() {
