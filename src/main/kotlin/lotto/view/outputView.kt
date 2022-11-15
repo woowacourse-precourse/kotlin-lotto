31dac586986506cb80472a2faddf1e38882e4cf1
ChoiWooSuk <wooseokchoi0508@gmail.com>
2022-11-15 20:11:57 +0900

    feat(outputView): 총 수익률 출력하는 기능 구현

--- src/main/kotlin/lotto/view/outputView.kt
+++ src/main/kotlin/lotto/view/outputView.kt
@@ -25,6 +25,10 @@ class outputView {
         println("${FIRST_PLACE_OUTPUT_MESSAGE} ${prizeResult.firstPlace}개")
     }
 
+    fun printProfitPercentage(profitPercentage:Double){
+        println("${PROFIT_PERCENTAGE_OUTPUT_MESSAGE} ${profitPercentage}%입니다.")
+    }
+
     companion object{
         const val LOTTO_COUNT_OUTPUT_MESSAGE = "개를 구매했습니다."
         const val LOTTO_RESULT_INTRO_OUTPUT_MESSAGE = "당첨 통계\n---"
@@ -33,5 +37,6 @@ class outputView {
         const val THIRD_PLACE_OUTPUT_MESSAGE = "5개 일치 (1,500,000원) -"
         const val SECOND_PLACE_OUTPUT_MESSAGE = "5개 일치, 보너스 볼 일치 (30,000,000원) -"
         const val FIRST_PLACE_OUTPUT_MESSAGE = "6개 일치 (2,000,000,000원) -"
+        const val PROFIT_PERCENTAGE_OUTPUT_MESSAGE = "총 수익률은"
     }
 }
