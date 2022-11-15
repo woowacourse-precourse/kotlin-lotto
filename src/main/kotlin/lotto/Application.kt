2064e632ae10192e642b0f7bf7922a2f5552b7a1
lee989898 <wnsrud1352@gmail.com>
2022-11-15 14:33:19 +0900

    [feat](Application) 로또 main에서 LottoGameController 생성해 로또 로직 시작 코드 추가

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,10 @@
+
 package lotto
 
+import controller.LottoGameController
+
 fun main() {
-    TODO("프로그램 구현")
+    val lottoGameController = LottoGameController()
+    lottoGameController.startPurchaseLotteries()
+    lottoGameController.makeResult()
 }
