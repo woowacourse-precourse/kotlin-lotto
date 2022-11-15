209e8e24837285ec0361a39d59fe0e6a3bc6489e
권용민 <briandr@naver.com>
2022-11-15 19:12:21 +0900

    refactor(Cacher): Cacher는 제대로 입력된 돈만 받는 것으로 역할 변경
    돈이 제대로 입력됐는지 확인하는 행동은 하지 않는다.

---
+++ src/main/kotlin/lotto/domain/Cacher.kt
@@ -0,0 +1,22 @@
+package lotto.domain
+
+private const val PURCHASE_STANDARD_UNIT = 1000
+
+class Cacher {
+    private val ticketingMachine = TicketingMachine()
+
+    private var _numberOfLotteries: Int = 0
+    val numberOfLotteries: Int get() = _numberOfLotteries
+
+    fun returnCreatedLotteries(): List<Lotto> {
+        return ticketingMachine.createLottoNumbers(_numberOfLotteries)
+    }
+
+    fun receiveMoney(money: Int) {
+        _numberOfLotteries = calculateNumberOfLotteries(money)
+    }
+
+    private fun calculateNumberOfLotteries(money: Int): Int {
+        return money / PURCHASE_STANDARD_UNIT
+    }
+}
--- src/test/kotlin/lotto/CacherTest.kt
+++ src/test/kotlin/lotto/CacherTest.kt
@@ -1,40 +1,22 @@
 package lotto
 
+import lotto.domain.Cacher
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
 
 class CacherTest {
     private val cacher = Cacher()
 
     @Test
-    fun `금액입력 형식 확인`() {
-        assertThat(cacher.enterPurchaseMoney("123000")).isEqualTo(123000)
+    fun `돈을 받고 로또의 개수를 계산한다`() {
+        cacher.receiveMoney(23000)
+        assertThat(cacher.numberOfLotteries).isEqualTo(23)
     }
 
     @Test
-    fun `금액입력이 숫자가 아닐 경우 예외가 발생한다`() {
-        assertThrows<IllegalArgumentException> {
-            cacher.enterPurchaseMoney("123가나")
-        }
-    }
-
-    @Test
-    fun `금액입력이 자연수가 아닐 경우 예외가 발생한다`() {
-        assertThrows<IllegalArgumentException> {
-            cacher.enterPurchaseMoney("-12345")
-        }
-    }
-
-    @Test
-    fun `금액입력이 천원 단위로 나누어 떨어지지 않을 경우 예외가 발생한다`() {
-        assertThrows<IllegalArgumentException> {
-            cacher.enterPurchaseMoney("12300")
-        }
-    }
-
-    @Test
-    fun `로또의 개수를 계산한다`() {
-        assertThat(cacher.calculateNumberOfLottos(23000)).isEqualTo(23)
+    fun `개수만큼 로또 발행`() {
+        cacher.receiveMoney(23000)
+        val result = cacher.returnCreatedLotteries()
+        assertThat(result).hasSize(23)
     }
 }
