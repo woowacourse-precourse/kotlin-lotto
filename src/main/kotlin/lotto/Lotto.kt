0ba642d22d4398ed6d109205d012c43a72d5c311
bmlsj <gaga2353@gmail.com>
2022-11-15 19:39:43 +0900

    feat: 1 ~ 45까지의 숫자가 아닌 경우 예외 발생

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -5,6 +5,7 @@ class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "[ERROR] 숫자가 6개가 아닙니다." }
         require(numbers.distinct().size == 6) { "[ERROR] 중복된 숫자가 존재합니다." }
+        require(numbers.all{it.toInt() in 1..45}){"[ERROR] 1 ~ 45까지의 숫자를 입력해야 합니다."}
     }
 
 }
