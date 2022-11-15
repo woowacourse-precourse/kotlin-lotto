8f107f1a82620a49401e7bb46f7fe7a9798df299
bmlsj <gaga2353@gmail.com>
2022-11-15 22:33:55 +0900

    fix: 로또 번호가 아닌 보너스 숫자에서 숫자 범위를 체크해야한다

--- src/main/kotlin/lotto/domain/BonusNumberError.kt
+++ src/main/kotlin/lotto/domain/BonusNumberError.kt
@@ -4,7 +4,7 @@ class BonusNumberError(lottoNumber: List<Int>, bonusNumber: Int) {
 
     init {
         require(!lottoNumber.contains(bonusNumber)) { "[ERROR] 로또 당첨 번호와 같은 수가 포함되어 있습니다." }
-        require(lottoNumber.all { it.toInt() in 1..45 }) { "[ERROR] 1 ~ 45까지의 숫자를 입력하세요." }
+        require(bonusNumber.toInt() in 1..45) { "[ERROR] 1 ~ 45까지의 숫자를 입력하세요." }
     }
 
 }
