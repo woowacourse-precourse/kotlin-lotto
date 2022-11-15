a6c6081db2066f88b76cb8c641a5a77c1c996a3e
Yujin <jini1514@soongsil.ac.kr>
2022-11-15 18:16:49 +0900

    refactoring: inputWinNumber()함수로 로또 당첨 번호 입력 분리, 구입 금액이 1000원 단위가 아닐 시 예외처리

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -3,7 +3,9 @@ package lotto
 import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
 
-private var lucky = mutableListOf<Int>()
+private var lucky = mutableListOf<Int>() //로또 당첨번호
+private var bonus =0 //보너스 번호
+
 private var one =0
 private var two =0
 private var three =0
@@ -11,8 +13,6 @@ private var four =0
 private var five =0
 private var stat=0
 
-//private var luckylist = mutableListOf<Int>() //로또 당첨 번호
-private var bonus =0 //보너스 번호
 
 fun makeRandomNumbers(lottoCount: Int, lotto:MutableList<List<Int>>){
     for(i in 0 until lottoCount){
@@ -20,6 +20,7 @@ fun makeRandomNumbers(lottoCount: Int, lotto:MutableList<List<Int>>){
         lotto.add(numbers)
         println(numbers)
     }
+    println()
 }
 
 
@@ -45,18 +46,31 @@ fun lottoWinCheck(lucky:MutableList<Int>, bonus:Int){
         five++
     if (map.count() == 4)//4등
         four++
-    if ((map.count() == 5) && !(lucky.contains(bonus)))//3등
+    if ((map.count() == 5) && !(luckyList.contains(bonus)))//3등
         three++
-    if ((map.count() == 5) && (lucky.contains(bonus)))//2등
+    if ((map.count() == 5) && (luckyList.contains(bonus)))//2등
         two++
     if (map.count() == 6)//1등
         one++
-
-    showResult()
 }
+
+fun inputWinNumber(){
+    println("당첨 번호를 입력해 주세요.")
+    lucky = Console.readLine()!!.split(",").map { it.toInt() }.toMutableList()
+    println()
+
+    println("보너스 번호를 입력해 주세요.")
+    bonus=Console.readLine()!!.toInt()
+
+}
+
 fun main() {
     println("구입 금액을 입력하세요")
     val money = Console.readLine()!!.toInt() //정수형 변환
+    if(money%0!=0){
+        println("[ERROR] 로또 1장의 가격은 1000원입니다. 1000원 단위로 입력해주세요")
+        throw IllegalArgumentException()
+    }
     val lottoCount = money/1000 //로또 횟수
 
     print("\n")
@@ -64,15 +78,6 @@ fun main() {
 
     val lotto= mutableListOf<List<Int>>()//사용자가 구매한 로또(랜덤번호로)
     makeRandomNumbers(lottoCount,lotto)
-
-    println("당첨 번호를 입력해 주세요.")
-    lucky = readLine()!!.split(",").map { it.toInt() }.toMutableList()
-
-    println("보너스 번호를 입력해 주세요.")
-    bonus=Console.readLine()!!.toInt()
-
+    inputWinNumber()
     showResult()
-
-
-
 }
