68013de5d4bbfa5b529afc6ec8233d7d79ddeb97
KimDongHyun0907 <ehdgus8397@naver.com>
2022-11-15 22:33:58 +0900

    금액 입력 오류 구현 및 정수형 변환

--- src/main/kotlin/lotto/Money.kt
+++ src/main/kotlin/lotto/Money.kt
@@ -6,4 +6,19 @@ class Money{
         val input_buy = readLine()
         return input_buy
     }
+    fun InputError(input_buy: String?):Int{
+        val buy:Int
+
+        try{
+            buy= input_buy!!.toInt()
+        }catch (e:NumberFormatException){
+            println("[ERROR] 숫자형태로 입력해주세요.")
+            throw NoSuchElementException()
+        }
+        if(buy%1000!=0){
+            throw IllegalArgumentException("[ERROR] 1,000원 단위로 입력 하시오.")
+        }
+
+        return buy
+    }
 }
