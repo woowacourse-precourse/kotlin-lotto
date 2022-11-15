005705398103c98e4639bb65f58570326eb8e02c
ChoiWooSuk <wooseokchoi0508@gmail.com>
2022-11-15 17:34:42 +0900

    feat(inputValidation): 입력값을 쉼표로 구분했을 때, 6개가 나오지 않는 경우 예외 발생 기능 구현

--- src/main/kotlin/lotto/domain/InputValidation.kt
+++ src/main/kotlin/lotto/domain/InputValidation.kt
@@ -15,9 +15,17 @@ class InputValidation {
         return amount
     }
 
+    fun checkInputSplitWithComma(input:String):List<String>{
+        val winningNumbers=input.split(",")
+        if(winningNumbers.size!=WINNING_NUMBER_COUNT) throw java.lang.IllegalArgumentException(NOT_SPLIT_WITH_COMMA_ERROR_MESSAGE)
+        return winningNumbers
+    }
+
     companion object {
         const val LOTTO_PRICE = 1000
+        const val WINNING_NUMBER_COUNT = 6
         const val NOT_INTEGER_ERROR_MESSAGE = "[ERROR] 정수를 입력해 주셔야 합니다."
         const val NOT_MULTIPLE_OF_THOUSAND_ERROR_MESSAGE = "[ERROR] 1000의 배수 단위로 구매 금액을 입력해 주셔야 합니다."
+        const val NOT_SPLIT_WITH_COMMA_ERROR_MESSAGE = "[ERROR] 숫자 6개를 ,로 구분해 주세요"
     }
 }
