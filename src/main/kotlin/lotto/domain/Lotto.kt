8a3ed5c466b99614842da1f6e4b6962c2881e920
Gloria <ippnsj@gmail.com>
2022-11-15 09:34:38 +0900

    feat: 로또 번호의 유효성을 검사하는 기능 구현
    로또 번호가 6개인지 확인
    로또 번호가 1 ~ 45 사이의 숫자인지 확인
    로또 번호가 중복되지 않는지 확인

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -2,7 +2,7 @@ package lotto.domain
 
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6)
+        checkValidation()
     }
 
     fun getRank(winningNumbers: List<Int>, bonusNumber: Int): Rank {
@@ -11,5 +11,20 @@ class Lotto(private val numbers: List<Int>) {
 
     fun getTicket(): String {
         return ""
+    }
+
+    private fun checkValidation() {
+        require(numbers.size == LOTTO_SIZE) { SIZE_ERROR_MESSAGE }
+        numbers.map { number -> require(number in START_NUMBER..END_NUMBER) { RANGE_ERROR_MESSAGE } }
+        require(numbers.distinct().size == numbers.size) { DISTINCT_ERROR_MESSAGE }
+    }
+
+    companion object {
+        const val LOTTO_SIZE = 6
+        const val START_NUMBER = 1
+        const val END_NUMBER = 45
+        const val SIZE_ERROR_MESSAGE = "로또 번호가 6개가 아닙니다."
+        const val RANGE_ERROR_MESSAGE = "로또 번호가 1 ~ 45 사이의 숫자가 아닙니다."
+        const val DISTINCT_ERROR_MESSAGE = "로또 번호가 중복됩니다."
     }
 }
