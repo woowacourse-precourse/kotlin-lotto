2379965c550d011548223b0118086f3ca862b74f
yeonsu10 <94501462+yeonsu10@users.noreply.github.com>
2022-11-15 21:02:09 +0900

    fix: 입력값이 ,로 나눠지지 않고 숫자로 이루어지지 않을 때 예외사항 추가

--- src/main/kotlin/lotto/WinningNumber.kt
+++ src/main/kotlin/lotto/WinningNumber.kt
@@ -3,8 +3,11 @@ package lotto
 class WinningNumber {
     var winningNumber:List<Int> = listOf()
     fun changeNumberList(winning: String) {
-        winningNumber = winning.split(',').map{it.toInt()}
-        print(winningNumber)
+        try{
+            winningNumber = winning.split(',').map{it.trim().toInt()}
+        }catch(e: IllegalArgumentException){
+            println("[ERROR] 로또 번호는 ,로 나뉘어야 하며 숫자로 구성되어야 합니다")
+        }
         Lotto(winningNumber)
     }
 }
