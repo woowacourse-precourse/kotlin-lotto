c0022e745b8383e1697ba972fbef218ea1d46941
healthykim <bsbs8645@snu.ac.kr>
2022-11-15 17:41:38 +0900

    feat(Validator): Add function for validation of lottery numbers

--- src/main/kotlin/lotto/Validator.kt
+++ src/main/kotlin/lotto/Validator.kt
@@ -12,6 +12,26 @@ class Validator {
         return moneyString.toInt()
     }
 
+    /**
+     * @brief Validate lottery numbers
+     * @param lotteryNumberList : List of lottery number(String)
+     * @return List of lottery number(Int) if there is no exception case
+     */
+    fun validateLotteryNumber(lotteryNumberList: List<String>): List<Int> {
+        if(lotteryNumberList.size != LOTTERY_SIZE) {
+            throw IllegalArgumentException("$TAG ${LOTTERY_SIZE}개의 숫자를 입력하세요.")
+        }
+        val checkedList = mutableListOf<Int>()
+        for(str in lotteryNumberList) {
+            if(!isNumber(str)) throw IllegalArgumentException("$TAG 문자가 포함되지 않은 숫자를 입력하세요.")
+            if(str.toIntOrNull() == null) throw IllegalArgumentException("$TAG 로또 번호는 ${NUMBER_RANGE.first}부터 ${NUMBER_RANGE.last} 사이의 숫자여야 합니다.")
+            if(!NUMBER_RANGE.contains(str.toInt())) throw IllegalArgumentException("$TAG 로또 번호는 ${NUMBER_RANGE.first}부터 ${NUMBER_RANGE.last} 사이의 숫자여야 합니다.")
+            if(checkedList.contains(str.toInt())) throw IllegalArgumentException("$TAG 로또 번호는 서로 다른 숫자로 구성되어야 합니다.")
+            checkedList.add(str.toInt())
+        }
+        return checkedList
+    }
+
     private fun isNumber(str: String): Boolean {
         for (c in str) {
             if (!c.isDigit()) {
