084352819736b5b3aa90ceaa571241ba890845d0
leehandsub <gustjq7027@naver.com>
2022-11-15 15:48:55 +0900

    feat(user): 로또 구입 1000으로 나누어 떨어지는지 체크

--- src/main/kotlin/lotto/domain/User.kt
+++ src/main/kotlin/lotto/domain/User.kt
@@ -3,6 +3,7 @@ package lotto.domain
 import camp.nextstep.edu.missionutils.Console
 import lotto.AMOUNT_PURCHASE_MESSAGE
 import lotto.ERROR_AMOUNT_BIG_NUMBER_MESSAGE
+import lotto.ERROR_AMOUNT_DIVIDE_MESSAGE
 import lotto.ERROR_AMOUNT_NUMBER_MESSAGE
 
 class User {
@@ -21,13 +22,19 @@ class User {
             throw IllegalArgumentException(ERROR_AMOUNT_NUMBER_MESSAGE)
         if (checkBigNumAmount(userAmount) == null)
             throw IllegalArgumentException(ERROR_AMOUNT_BIG_NUMBER_MESSAGE)
-
+        if (!checkDivideAmount(userAmount))
+            throw IllegalArgumentException(ERROR_AMOUNT_DIVIDE_MESSAGE)
     }
 
     private fun checkNumAmount(userAmount: String): Boolean =
         userAmount.count() == userAmount.replace("\\D".toRegex(), "").count()
 
     private fun checkBigNumAmount(userAmount: String) = userAmount.toIntOrNull()
+    private fun checkDivideAmount(userAmount: String): Boolean {
+        if (userAmount.toInt() % 1000 != 0)
+            return false
+        return true
+    }
 
 
 }
