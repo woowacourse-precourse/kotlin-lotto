552b7b1e133bdbf99aeee4bd1fd3cfdf9d54ef2d
yeonsu10 <94501462+yeonsu10@users.noreply.github.com>
2022-11-15 15:32:15 +0900

    fix: 5개일 때 보너스 숫자 확인시 안되는 현상 수정

--- src/main/kotlin/lotto/Reward.kt
+++ src/main/kotlin/lotto/Reward.kt
@@ -12,8 +12,7 @@ class Reward {
         for(i:Int in lottoNumber.indices){
             val difference = winningNumber.toSet().minus(lottoNumber[i].toSet())
             val duplicationCount = winningNumber.size - difference.size
-            print(duplicationCount)
-            val countWinning = calculateReward(duplicationCount,bonusNumber,winningNumber)
+            val countWinning = calculateReward(duplicationCount,bonusNumber,lottoNumber[i])
             for(index:Int in 0 until countReward.size){
                 countReward[index] = countReward[index] + countWinning[index]
             }
@@ -32,22 +31,21 @@ class Reward {
                 4 -> money += countReward[4] * 2000000000
             }
         }
-        println(money)
     }
 
-    private fun calculateReward(duplicationCount: Int, bonusNumber:Int, winningNumber: List<Int>): MutableList<Int> {
+    private fun calculateReward(duplicationCount: Int, bonusNumber:Int, lottoNumber: List<Int>): MutableList<Int> {
         val countReward:MutableList<Int> = mutableListOf(0,0,0,0,0)
         when(duplicationCount){
             3 -> countReward[0]=+1
             4 -> countReward[1]=+1
-            5 -> whenCountFive(countReward, bonusNumber, winningNumber)
+            5 -> whenCountFive(countReward, bonusNumber, lottoNumber)
             6 -> countReward[4]+=1
         }
         return countReward
     }
 
-    private fun whenCountFive(countReward: MutableList<Int>, bonusNumber: Int, winningNumber: List<Int>): MutableList<Int> {
-        if(winningNumber.contains(bonusNumber)){
+    private fun whenCountFive(countReward: MutableList<Int>, bonusNumber: Int, lottoNumber: List<Int>): MutableList<Int> {
+        if(lottoNumber.contains(bonusNumber)){
             countReward[3] =+ 1
             return countReward
         }
@@ -58,16 +56,6 @@ class Reward {
     fun calculateRevenue(money:Int, price:Int) {
         revenue =(money.toFloat()/price.toFloat())*100
         revenue = round(revenue*10) /10
-        printReward(countReward,revenue)
+        Output.printReward(countReward,revenue)
     }
-
-    fun printReward(countReward: MutableList<Int>, revenue:Float){
-        println("3개 일치 (5,000원) - "+ countReward[0] +"개\n" +
-                "4개 일치 (50,000원) - "+countReward[1] +"개\n" +
-                "5개 일치 (1,500,000원) - "+ countReward[2] +"개\n" +
-                "5개 일치, 보너스 볼 일치 (30,000,000원) - "+ countReward[3] +"개\n" +
-                "6개 일치 (2,000,000,000원) - "+ countReward[4] +"개\n"+
-                "총 수익률은 "+revenue+"%입니다.")
-    }
-
 }
