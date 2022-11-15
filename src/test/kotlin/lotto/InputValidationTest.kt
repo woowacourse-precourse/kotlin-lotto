a2b96db0e4a321fecebfd184deedaf065c874cfe
ChoiWooSuk <wooseokchoi0508@gmail.com>
2022-11-15 16:56:33 +0900

    test(InputValidationTest): 정수가 1000으로 나누어 떨어지지 않을 경우 예외 발생 테스트 구현

--- src/test/kotlin/lotto/InputValidationTest.kt
+++ src/test/kotlin/lotto/InputValidationTest.kt
@@ -7,10 +7,18 @@ import org.junit.jupiter.api.assertThrows
 class InputValidationTest {
 
     val inputValidation= InputValidation()
+
     @Test
     fun `입력값이 정수가 아니라면 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
             inputValidation.checkInputInteger("십삼")
+        }
+    }
+
+    @Test
+    fun `구매금액이 1000으로 나누어 떨지지지 않을 경우 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            inputValidation.checkAmountMultipleOfThousand(2500)
         }
     }
 }
