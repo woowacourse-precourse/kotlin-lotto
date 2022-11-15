53c210928617436c736bc1bb2de4d341f2784ca2
bmlsj <gaga2353@gmail.com>
2022-11-15 19:39:02 +0900

    feat: 중복된 숫자가 있는 경우 예외 발생

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,9 +1,10 @@
 package lotto
 
+
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6)
+        require(numbers.size == 6) { "[ERROR] 숫자가 6개가 아닙니다." }
+        require(numbers.distinct().size == 6) { "[ERROR] 중복된 숫자가 존재합니다." }
     }
 
-    // TODO: 추가 기능 구현
 }
