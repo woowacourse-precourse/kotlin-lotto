1dbb364d0087d623fef6a49db1c03188c16aa9f4
[alsswl] <[alswl4223@naver.com]>
2022-11-15 20:48:26 +0900

    fix(Lotto) : 10보다 커야한다는 조건 삭제

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -7,7 +7,7 @@ class Lotto(private val numbers: List<Int>) {
         require(numbers.size == 6)
         var already = arrayOf(0,0,0,0,0,0,0,0,0,0);
         for(i in 0 until 6){
-            if(numbers[i] <=0 || numbers[i]>=10) throw IllegalArgumentException()
+            if(numbers[i] <=0 ) throw IllegalArgumentException()
             already[numbers[i]]++
             if(already[numbers[i]]>=2) throw IllegalArgumentException()
         }
