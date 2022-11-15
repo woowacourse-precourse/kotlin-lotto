39a3c5780bfdf2b72f92b329ca14c5cfcbb1254c
ChanwooAn <55792553+ChanwooAn@users.noreply.github.com>
2022-11-15 13:31:53 +0900

    feat: Lotto 번호 생성시 1~45사이의 숫자인지 check하도록 변경.
    Lotto번호 생성시에 1~45사이의 숫자가 아닐경우 예외발생하도록 변경.

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,12 +1,14 @@
 package lotto
 
 import lotto.constants.MAKE_LOTTO_NUM_DUPLICATION_ERROR_MSG
+import lotto.constants.MAKE_LOTTO_NUM_SCOPE_ERROR_MSG
 import lotto.constants.MAKE_LOTTO_NUM_SIZE_ERROR_MSG
 
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { println(MAKE_LOTTO_NUM_SIZE_ERROR_MSG) }
         require(numbers.distinct().size == 6) { println(MAKE_LOTTO_NUM_DUPLICATION_ERROR_MSG) }
+        require(numbers.count{ num-> num in 1..45 }==6){ MAKE_LOTTO_NUM_SCOPE_ERROR_MSG}
     }
 
     fun printLottoWinNum() {
