4f8ce04b98637466164872187cbf5e242a13ea61
lsakee <lsls4868@gmail.com>
2022-11-15 16:11:19 +0900

    feat - interface 구현 로또 생성기 인터페이스 구현

--- src/main/kotlin/lotto/lottoStore/LottoCreate.kt
+++ src/main/kotlin/lotto/lottoStore/LottoCreate.kt
@@ -3,14 +3,17 @@ package lotto.lottoStore
 import camp.nextstep.edu.missionutils.Randoms
 
 
-class LottoCreate(){
-fun createLotto(inputUserNumber: String): MutableList<List<Int>> {
-    val lotto = mutableListOf<List<Int>>()
-    for (i in 1..inputUserNumber.toInt()) {
-        val numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
-        println(numbers.sorted())
-        lotto.add(Lotto.Lotto(numbers.sorted()).getNumbers())
-    }
-    return lotto
+interface LottoCreate{
+    fun LottoCreate(inputUserNumber: String): MutableList<List<Int>>
 }
+class LottoCreateImpl : LottoCreate {
+    override fun LottoCreate(inputUserNumber: String):MutableList<List<Int>>  {
+        val lotto = mutableListOf<List<Int>>()
+        for (i in 1..inputUserNumber.toInt()) {
+            val numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
+            println(numbers.sorted())
+            lotto.add(Lotto.Lotto(numbers.sorted()).getNumbers())
+        }
+        return lotto
+    }
 }
