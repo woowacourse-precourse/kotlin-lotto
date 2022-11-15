e48faea58ed6b3b294156f66115d044b51bc1ed5
phs <gustjd0808@pusan.ac.kr>
2022-11-15 16:57:59 +0900

    feat(ui): add methods to print purchase number, user lotto

--- src/main/kotlin/lotto/ui/OutputManager.kt
+++ src/main/kotlin/lotto/ui/OutputManager.kt
@@ -1,9 +1,19 @@
 package lotto.ui
 
+import lotto.domain.LottoList
+import lotto.enum.OutputString
 import lotto.enum.StatisticsString
 
 class OutputManager {
-    fun printWinningStatistics(winningList: List<Int>, earningsRate:Int){
+    fun printWinningStatistics(winningList: List<Int>, earningsRate: Double) {
         StatisticsString.printStatics(winningList, earningsRate)
+    }
+
+    fun printPurchaseNumber(number: Int) {
+        OutputString.PURCHASE_NUMBER.print(number)
+    }
+
+    fun printUserLotto(userLotto: LottoList) {
+        println("$userLotto")
     }
 }
