ea1f73b7135d5a5328b51a1cf8e5a1bda2646efc
username <ieejo716@naver.com>
2022-11-15 13:11:14 +0900

    [FEAT] : 당첨 번호 입력 예외 사항(범위)

--- src/main/kotlin/util/InputException.kt
+++ src/main/kotlin/util/InputException.kt
@@ -1,5 +1,7 @@
 package util
 
+import lotto.User
+
 class InputException {
     fun checkTypeException(input: String?) {
         try {
@@ -9,11 +11,37 @@ class InputException {
         }
     }
 
-    fun invalidException(message: String) {
+    fun checkTypeException(input: List<String>) {
+        try {
+            input.map { it.toInt() }
+        } catch (e: Exception) {
+            invalidException(TYPE_EXCEPTION)
+        }
+    }
+
+    fun checkNullException(input: String?) {
+        if (input.isNullOrEmpty()) {
+            invalidException(NULL_EXCEPTION)
+        }
+    }
+
+    fun inputMoneyException(money: String) {
+        if (money.toInt() % 1000 != 0) invalidException(VALUE_EXCEPTION)
+    }
+
+    fun checkRangeException(numbers: List<Int>) {
+        numbers.forEach { num ->
+            if (num !in 1..45) invalidException(VALUE_EXCEPTION)
+        }
+    }
+
+    private fun invalidException(message: String) {
         throw IllegalArgumentException("[ERROR] : $message")
     }
 
     companion object {
         const val TYPE_EXCEPTION = "올바른 형식이 아닙니다."
+        const val NULL_EXCEPTION = "아무 것도 입력하지 않았습니다."
+        const val VALUE_EXCEPTION = "잘못된 값입니다."
     }
 }
