71298991103fc39bf0d528ee4300a92cd6d45276
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2022-11-15 21:29:51 +0900

    feat[lotto]: 입력값이 빈 문자열인 경우 예외 사항 처리 기능 구현

--- src/main/kotlin/lotto/util/Validator.kt
+++ src/main/kotlin/lotto/util/Validator.kt
@@ -4,17 +4,20 @@ class Validator {
 
     fun checkMoneyInput(input : String?) {
         checkInputNullValidation(input)
+        checkInputEmptyValidation(input!!)
         checkStringToIntValidation(input!!)
         checkMoneyValueValidation(input)
     }
 
     fun checkWinnigNumberInput(input : String?) {
         checkInputNullValidation(input)
+        checkInputEmptyValidation(input!!)
         checkWinnigNumberToIntValidation(input!!)
     }
 
     fun checkBonusInput(input : String?) {
         checkInputNullValidation(input)
+        checkInputEmptyValidation(input!!)
         checkStringToIntValidation(input!!)
         checkBonusNumberRangeValidation(input!!)
     }
@@ -30,6 +33,10 @@ class Validator {
         } catch(e : NumberFormatException) {
             throw IllegalArgumentException(INPUT_VALUE_ERROR_MSG)
         }
+    }
+
+    fun checkInputEmptyValidation(input : String) {
+        require(input != "") { INPUT_EMPTY_ERROR_MSG }
     }
 
     fun checkInputNullValidation(input: String?) {
@@ -52,6 +59,7 @@ class Validator {
         const val LOTTO_MAX_NUM = 45
 
         const val INPUT_NULL_ERROR_MSG = "[ERROR] 값이 입력되지 않았습니다."
+        const val INPUT_EMPTY_ERROR_MSG = "[ERROR] 값이 입력되지 않았습니다."
         const val INPUT_VALUE_ERROR_MSG = "[ERROR] 값이 숫자로만 이루어져야합니다."
         const val MONEY_VALUE_ERROR_MSG = "[ERROR] 돈은 1000으로 나누어떨어지는 값이어야 합니다."
         const val BONUS_RANGE_ERROR_MSG = "[ERROR] 보너스는 1에서 45사이의 숫자여야 합니다."
