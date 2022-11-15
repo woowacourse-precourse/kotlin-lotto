3aa892708421849aaa96c990b9a914f6ce421627
Hyuna Kim <akxmcse@gmail.com>
2022-11-15 16:51:56 +0900

    feat(Lotto): 로또 당첨 결과를 계산하는 함수 구현

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -3,7 +3,24 @@ package lotto
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6)
+        require(numbers.distinct() == numbers)
+        numbers.forEach { n ->
+            require(n in 1..45)
+        }
     }
-
-    // TODO: 추가 기능 구현
+    fun calculateWinningResult(lottoNumbersList: MutableList<List<Int>>, winningNumbers: List<Int>, bonusNumber: String): MutableList<Int> {
+        val winningResult: MutableList<Int> = mutableListOf(0,0,0,0,0)
+        for (i in lottoNumbersList.indices) {
+            when (Winning().getCountOfMatchWinning(lottoNumbersList[i], winningNumbers)) {
+                3 -> { winningResult[0] += 1 }
+                4 -> { winningResult[1] += 1 }
+                5 -> { winningResult[2] += 1 }
+                6 -> {winningResult[4] += 1 }
+            }
+            if(Winning().getCountOfMatchWinning(lottoNumbersList[i], winningNumbers) == 5 && Winning().getCountOfMatchBonus(lottoNumbersList[i],bonusNumber)){
+                winningResult[3] += 1
+            }
+        }
+        return winningResult
+    }
 }
