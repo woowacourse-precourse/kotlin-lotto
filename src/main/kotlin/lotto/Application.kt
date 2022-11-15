36ba468807d3e544720def1eeba08ed71046d37d
LHS0731 <gustjq2536@gmail.com>
2022-11-15 20:48:26 +0900

    보너스 클래스 생성

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -6,9 +6,11 @@ import camp.nextstep.edu.missionutils.Randoms
 fun main() {
     println("구입금액을 입력해 주세요.")
     val moneyInput = Console.readLine()
-    val moneyCount = Money(moneyInput)
-    println("${moneyCount.run()}개를 구매했습니다.")
-    val randomlotto = makeRandomLotto(moneyCount.run())
+    val moneCheck = Money(moneyInput)
+    val moneyCount = moneCheck.run()
+    println("${moneyCount}개를 구매했습니다.")
+
+    val randomlotto = makeRandomLotto(moneyCount)
 
     println("당첨 번호를 입력해 주세요.")
     val myNumber = Console.readLine()
@@ -17,11 +19,8 @@ fun main() {
 
     println("보너스 번호를 입력해 주세요.")
     val myBonus = Console.readLine()
-    val bonusNumber = myBonus.toInt()
-    if (bonusNumber !in (1..45))
-        throw IllegalArgumentException("[ERROR] : 올바른 범위의 숫자를 입력해 주세요.(1~45)")
-    if (myList.contains(bonusNumber))
-        throw IllegalArgumentException("[ERROR] : 입력한 당첨 번호와 중복됩니다.")
+    val bonusCheck = Bonus(myBonus, myList)
+    val bonusNumber = bonusCheck.run()
 
     val lottotStatistic = Lotto(myList)
     lottotStatistic.run(randomlotto, myList, bonusNumber, moneyInput.toInt())
---
+++ src/main/kotlin/lotto/Bonus.kt
@@ -0,0 +1,31 @@
+package lotto
+
+class Bonus(private val number: String, private val list: List<Int>) {
+    init {
+        require(stringCheck(number)) {
+            print("[ERROR] : 숫자 형태로 입력해 주세요.")
+            throw NoSuchElementException()
+        }
+        require(number.toInt() <= 45 || number.toInt() > 1) {
+            "[ERROR] : 올바른 범위의 숫자를 입력해 주세요.(1~45)."
+        }
+        require(!(list.contains(number.toInt()))) {
+            "[ERROR] : 입력한 당첨 번호와 중복됩니다."
+        }
+    }
+    fun run(): Int{
+        return number.toInt()
+    }
+    fun stringCheck(str: String): Boolean {
+        var element: Char
+        var flag = true
+
+        for (i in str.indices) {
+            element = str.elementAt(i)
+            if (element.toInt() < 48 || element.toInt() > 57) {
+                flag = false
+            }
+        }
+        return flag
+    }
+}
