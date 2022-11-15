4df4415643c15539347cd2825bf6078c296b44a7
KimDongHyun0907 <ehdgus8397@naver.com>
2022-11-15 22:36:51 +0900

    로또 구매한 개수 함수 구현

--- src/main/kotlin/lotto/Number.kt
+++ src/main/kotlin/lotto/Number.kt
@@ -1,4 +1,9 @@
 package lotto
 
-class Number {
+class Number{
+    fun BuyLottoCount(buy:Int): Int {
+        val buy_count=buy/1000
+        println("${buy_count}개를 구매했습니다.")
+        return buy_count
+    }
 }
