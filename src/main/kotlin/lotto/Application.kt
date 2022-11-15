94f8e504d1fef29719f4acce0dde0284a5e5cb8d
bmlsj <gaga2353@gmail.com>
2022-11-15 19:58:44 +0900

    feat: 보너스 번호를 입력받는 함수

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -13,8 +13,19 @@ class LottoNumber {
         val winNumber = Console.readLine().split(",").map { it.toInt() }
         Lotto(winNumber)
 
-        // println("당첨 번호 : $winNumber")
+        println("당첨 번호 : $winNumber")
         return winNumber
+    }
+
+    private fun bonusNumber(lottoNumber: List<Int>): Int {
+
+        println("\n보너스 번호를 입력해 주세요.")
+
+        val bonusNumber = Console.readLine().toInt()
+        require(!lottoNumber.contains(bonusNumber)) { "[ERROR] 같은 수가 포함되어 있다." }
+
+        println(bonusNumber)
+        return bonusNumber
     }
 
 }
