a707caf5ded1120a84e1e7c22fd17344a7c1f9d1
ChoiWooSuk <wooseokchoi0508@gmail.com>
2022-11-15 23:39:07 +0900

    test(domainTest): 총 수익률 확인하는 테스트 작성

--- src/test/kotlin/lotto/DomainTest.kt
+++ src/test/kotlin/lotto/DomainTest.kt
@@ -1,10 +1,12 @@
 package lotto
 
+import lotto.domain.Lotto
 import lotto.domain.LottoStaff
 import lotto.domain.Prize
 import lotto.domain.PrizeResult
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 
@@ -33,6 +35,26 @@ class DomainTest {
         val isBonusMatch = lottoStaff.checkBonusNumber(lottoNumbers,bonusNumber)
         val actual = lottoStaff.checkPrize(matchingCount,isBonusMatch).prizeMoney
         assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `구입 금액부터 수익률 확인까지 테스트`(){
+        val purchaseMoney = 5000
+        val lotteries = listOf<Lotto>(Lotto(listOf(11,12,13,14,15,16))
+            ,Lotto(listOf(12,11,13,4,5,6))
+            ,Lotto(listOf(11,12,13,18,23,26))
+            ,Lotto(listOf(25,27,20,19,11,16))
+            ,Lotto(listOf(1,2,3,11,12,16)))
+
+        lotteries.forEach{ lotto->
+            val matchNumberCount = lottoStaff.countMatchingNumber(lotto.getNumbers(),winningNumbers)
+            val isBonusMatch = lottoStaff.checkBonusNumber(lotto.getNumbers(),bonusNumber)
+            val prize = lottoStaff.checkPrize(matchNumberCount,isBonusMatch)
+            prizeResult.updatePrizeResult(prize)
+        }
+
+        val actual = lottoStaff.getProfit(prizeResult.getTotalPrizeMoney(),purchaseMoney)
+        assertThat(actual).isEqualTo(200.0)
     }
 
 
