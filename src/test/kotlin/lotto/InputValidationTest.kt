a3b09d9054c12bf63704c66f0b9c0c0c892585f4
ChoiWooSuk <wooseokchoi0508@gmail.com>
2022-11-15 17:50:04 +0900

    test(inputValidationTest): 보너스 번호가 1~45 에 포함되지 않을 경우 예외 발생 테스트 구현

--- src/test/kotlin/lotto/InputValidationTest.kt
+++ src/test/kotlin/lotto/InputValidationTest.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.assertThrows
 
 class InputValidationTest {
 
-    val inputValidation= InputValidation()
+    private val inputValidation= InputValidation()
 
     @Test
     fun `입력값이 정수가 아니라면 예외가 발생한다`() {
@@ -26,6 +26,13 @@ class InputValidationTest {
     fun `입력값을 쉼표로 구분했을 때, 6개가 나오지 않을 경우 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
             inputValidation.checkInputSplitWithComma("1,2,3,4.5")
+        }
+    }
+
+    @Test
+    fun `보너스 번호가 1~45에 포함되지 않을 경우 예외 발생`() {
+        assertThrows<IllegalArgumentException> {
+            inputValidation.checkBonusNumberRange(46)
         }
     }
 }
