82f750d796667dc78ccccb2a7e84d289055334cb
Hyeongseon Jeon <hsun5163@gmail.com>
2022-11-15 18:16:42 +0900

    feat : lottoBonus 함수 추가 및 파라미터 수정
    필드를 추가하지 않고 매개 변수를 추가하여 6개의 정답 숫자와 보너스 숫자 처리

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,20 +1,30 @@
 package lotto
 
-class Lotto(private val numbers: List<Int>) {
+class Lotto(_numbers: List<Int>, _bonus: Int) {
+    private var numbers = mutableListOf<Int>()
     init {
         try {
-            require(numbers.size == 6)
+            require(_numbers.size == 6)
         } catch (e:Exception) {
             throw IllegalArgumentException("[ERROR] 6개만 입력해야 합니다.")
         }
+        lottoAnswer(_numbers)
+        lottoBonus(_numbers, _bonus)
     }
 
-    fun lottoAnswer(answers : List<Int>){
+    private fun lottoAnswer(answers : List<Int>) {
         if(answers.size != (answers.filter{ it in 1..45 }).size){
             throw IllegalArgumentException("[ERROR] 1부터 45 숫자를 입력하셔야 합니다.")
         } else if (answers.size != answers.distinct().size){
             throw IllegalArgumentException("[ERROR] 중복이 없어야 합니다.")
         }
     }
-    // TODO: 추가 기능 구현
+
+    private fun lottoBonus(numbers: List<Int>, bonus : Int){
+        if(bonus !in 1..45){
+            throw IllegalArgumentException("[ERROR] 1부터 45 숫자를 입력하셔야 합니다.")
+        } else if (numbers.contains(bonus)){
+            throw IllegalArgumentException("[ERROR] 정답과 숫자와 중복이 없어야 합니다.")
+        }
+    }
 }
