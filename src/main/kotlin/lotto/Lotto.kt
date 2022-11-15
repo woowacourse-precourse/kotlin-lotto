b68dc5867a696d6cb4a9ef8e9744dc46893789e9
WoeunJung <leena09123@naver.com>
2022-11-15 12:51:28 +0900

    fix: mke bonus confirmed only in case that count is 5

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -9,9 +9,7 @@ class Lotto(private val numbers: List<Int>) {
     private var totalPrice = 0
     fun compareLotto(correct: List<Int>, bonus: Int) {
         var count = numbers.intersect(correct.toSet()).size
-        print(count)
-        print(numbers.intersect(correct.toSet()))
-        if (bonus in numbers) count += 10
+        if (count == 5 && bonus in numbers) count += 10
         val lottoMap = mapOf(
             3 to LottoEnum.THREE_CORRECT,
             4 to LottoEnum.FOUR_CORRECT,
@@ -19,7 +17,7 @@ class Lotto(private val numbers: List<Int>) {
             6 to LottoEnum.SIX_CORRECT,
             15 to LottoEnum.FIVE_CORRECT_BONUS
         )
-        if (!(count in lottoMap.keys)) return
+        if (count !in lottoMap.keys) return
         lottoMap[count]!!.count += 1
         totalPrice += lottoMap[count]!!.price
     }
