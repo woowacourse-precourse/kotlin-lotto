9eaa26052588f541f6d5a506052de569599d9cf5
username <ieejo716@naver.com>
2022-11-15 22:08:29 +0900

    [CHORE] : 중복 되는 수 예외 처리

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -3,6 +3,7 @@ package lotto
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6)
+        require(numbers.size == numbers.toSet().size)
     }
 
     private fun winningsCount(lotto: List<Int>): Int = numbers.filter { it ->
