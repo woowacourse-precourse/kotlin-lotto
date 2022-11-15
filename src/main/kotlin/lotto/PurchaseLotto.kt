c98fd78dc4a57fd0ada0db33765de52fca1c265f
jinsilWoo <wryt9224@gmail.com>
2022-11-15 21:38:34 +0900

    [FEAT] 구매 금액 입력 예외 처리

--- src/main/kotlin/lotto/PurchaseLotto.kt
+++ src/main/kotlin/lotto/PurchaseLotto.kt
@@ -8,6 +8,7 @@ class PurchaseLotto {
      private fun inputUserMoney() : Int{
         println("구매 금액을 입력해주세요.")
         val inputMoney = Console.readLine()
+        inputMoneyException(inputMoney)
         return inputMoney.toInt()
     }
 
@@ -17,4 +18,10 @@ class PurchaseLotto {
         return "${lottoCount}개를 구매했습니다."
     }
 
+     fun inputMoneyException(money : String){
+        var payment = money.toInt()
+        if(payment % 1000 !=0){
+            throw IllegalArgumentException("[ERROR] 금액이 1000원으로 나누어 떨어지지 않습니다." )
+        }
+    }
 }
