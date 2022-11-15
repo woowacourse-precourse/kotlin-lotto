1c14af3cbd3664b60e98bbee7313f068cfb4b19b
ChoiWooSuk <wooseokchoi0508@gmail.com>
2022-11-15 16:56:48 +0900

    feat(InputValidation): 정수가 1000으로 나누어 떨어지지 않을 경우 예외 발생 기능 구현

--- src/main/kotlin/lotto/domain/InputValidation.kt
+++ src/main/kotlin/lotto/domain/InputValidation.kt
@@ -10,7 +10,14 @@ class InputValidation {
         }
     }
 
+    fun checkAmountMultipleOfThousand(amount:Int):Int{
+        if(amount % LOTTO_PRICE!=0) throw IllegalArgumentException(NOT_MULTIPLE_OF_THOUSAND_ERROR_MESSAGE)
+        return amount
+    }
+
     companion object {
+        const val LOTTO_PRICE = 1000
         const val NOT_INTEGER_ERROR_MESSAGE = "[ERROR] 정수를 입력해 주셔야 합니다."
+        const val NOT_MULTIPLE_OF_THOUSAND_ERROR_MESSAGE = "[ERROR] 1000의 배수 단위로 구매 금액을 입력해 주셔야 합니다."
     }
 }
