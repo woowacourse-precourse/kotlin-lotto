7b4cd4f0a8dcce9f81f976609702d2e0961f249a
Hyeongseon Jeon <hsun5163@gmail.com>
2022-11-16 00:19:10 +0900

    refactor : init 안에 있던 기능들 함수화

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -12,19 +12,20 @@ class Lotto(_numbers: List<Int>, _bonus: Int) {
         lottoBonus(_numbers, _bonus)
         numbers.addAll(_numbers)
         numbers.add(_bonus)
-        for(i in 0 until userLottoList.size){
-            LottoStats.lottoMatchLists(userLottoList[i], numbers)
+    }
+    fun calcuateRanking(userLottoList:List<List<Int>>) {
+        for(element in userLottoList){
+            LottoStats.lottoMatchLists(element, numbers)
         }
+    }
+    fun getSumPrice(userLottoList:List<List<Int>>): Int {
+        calcuateRanking(userLottoList)
         var sumPrice = 0
-        println("\n당첨 통계\n---")
         LottoStats.values().forEach{
-            println("${it.message} - ${it.Cnt}개")
             sumPrice += (it.price/1000 * it.Cnt)
         }
-        val rateOfReturn = ((sumPrice.toDouble() / userLottoList.size.toDouble())*100.0)
-        println("총 수익률은 ${round(rateOfReturn*10)/10}%입니다.")
+        return sumPrice
     }
-
     private fun lottoAnswer(answers : List<Int>) {
         if(answers.size != (answers.filter{ it in 1..45 }).size){
             throw IllegalArgumentException("[ERROR] 1부터 45 숫자를 입력하셔야 합니다.")
