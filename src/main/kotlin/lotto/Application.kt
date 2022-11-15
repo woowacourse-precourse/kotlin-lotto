82a7614b43f1ccdaad685ea41f33a9c171dae48e
healthykim <bsbs8645@snu.ac.kr>
2022-11-15 13:41:23 +0900

    feat(application): Add function for purchasing lotto

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,9 +1,24 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Randoms
+
 const val LOTTERY_PRICE = 1000
 const val LOTTERY_SIZE = 6
 const val TAG = "ERROR"
 val NUMBER_RANGE = 1..45
+
+/**
+ * @param money : user money to purchase lotto
+ * @return List of Lotto that user bought
+ */
+fun buyLottery(money: Int): List<Lotto> {
+    val lotteryList = mutableListOf<Lotto>()
+    val numOfLotteries = money / LOTTERY_PRICE
+    for(i in 1..numOfLotteries) {
+        lotteryList.add(Lotto(Randoms.pickUniqueNumbersInRange(NUMBER_RANGE.first, NUMBER_RANGE.last, 6)))
+    }
+    return lotteryList
+}
 
 fun main() {
     TODO("프로그램 구현")
