0898d6a798454b5840883df5c10c4b046f7a47e5
joh9911 <joh9911@naver.com>
2022-11-15 16:09:00 +0900

    feat: 로또가 당첨된 횟수를 저장하는 클래스 MatchStorage 생성

---
+++ src/main/kotlin/lotto/MatchStorage.kt
@@ -0,0 +1,15 @@
+package lotto
+
+enum class MatchStorage(val matchMessage: String,
+                        val matchPrice: Int,
+                        var matchCount: Int) {
+        MATCH_THREE(Constants.MATCH_THREE_NUMBER_MESSAGE, 5000, 0),
+        MATCH_FOUR(Constants.MATCH_FOUR_NUMBER_MESSAGE, 50000, 0),
+        MATCH_FIVE(Constants.MATCH_FIVE_NUMBER_MESSAGE, 1500000, 0),
+        MATCH_FIVE_WITH_BONUS_NUMBER(Constants.MATCH_FIVE_WITH_BONUS_NUMBER_MESSAGE, 30000000, 0),
+        MATCH_SIX(Constants.MATCH_SIX_NUMBER_MESSAGE, 2000000000, 0)
+}
+
+
+
+
