d229415b6fcaa810aa461635ab97e6e7cc9a8f5f
KimDongHyun0907 <ehdgus8397@naver.com>
2022-11-15 22:43:27 +0900

    WinningNumber 클래스 추가 및 당첨 번호 입력

--- src/main/kotlin/lotto/WinningNumber.kt
+++ src/main/kotlin/lotto/WinningNumber.kt
@@ -1,4 +1,15 @@
 package lotto
 
-class WinningNumber {
+class WinningNumber{
+    fun InputLottoNumber(): String? {
+        println("당첨 번호를 입력해 주세요.")
+
+        val lotto_numbers=readLine()
+        for(i in lotto_numbers!!.toList()){
+            if(i.code>=48 && i.code<=57){ continue }
+            if(i.code==44){ continue }
+            throw IllegalArgumentException("[ERROR] 입력 형식을 다시 확인하세요.")
+        }
+        return lotto_numbers
+    }
 }
