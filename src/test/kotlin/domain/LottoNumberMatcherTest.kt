509362656ec3761d29dd6b0e95d76fd8fdf87383
joh9911 <joh9911@naver.com>
2022-11-15 21:55:09 +0900

    test(LottoNumberMatcherTest): 각 테스트 코드 끝에 enum class 초기화 해주는 함수 추가

--- src/test/kotlin/domain/LottoNumberMatcherTest.kt
+++ src/test/kotlin/domain/LottoNumberMatcherTest.kt
@@ -2,8 +2,9 @@ package domain
 
 import domain.LottoNumbersMatcher
 import domain.MatchStorage
-import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import program.LottoProgram
 
 class LottoNumberMatcherTest {
 
@@ -13,16 +14,18 @@ class LottoNumberMatcherTest {
         val bonusNumber = 7
         val randomLottoes = listOf(listOf(1,2,3,8,9,10))
         LottoNumbersMatcher(lottoNumbers, bonusNumber, randomLottoes).calculateTotalMatchCount()
-        Assertions.assertThat(MatchStorage.MATCH_THREE.matchCount).isEqualTo(1)
+        assertThat(MatchStorage.MATCH_THREE.matchCount).isEqualTo(1)
+        initializeMatchStorage()
     }
 
     @Test
     fun `4개의 숫자가 일치했을 때의 경우`() {
-        val lottoNumbers = listOf(1,2,3,4,5,6)
+        val lottoNumbers = listOf(5,7,9,10,13,17)
         val bonusNumber = 7
-        val randomLottoes = listOf(listOf(1,2,3,4,9,10))
+        val randomLottoes = listOf(listOf(5,7,9,10,30,34))
         LottoNumbersMatcher(lottoNumbers, bonusNumber, randomLottoes).calculateTotalMatchCount()
-        Assertions.assertThat(MatchStorage.MATCH_FOUR.matchCount).isEqualTo(1)
+        assertThat(MatchStorage.MATCH_FOUR.matchCount).isEqualTo(1)
+        initializeMatchStorage()
     }
 
     @Test
@@ -31,7 +34,8 @@ class LottoNumberMatcherTest {
         val bonusNumber = 7
         val randomLottoes = listOf(listOf(1,2,3,4,5,10), listOf(1,2,3,4,5,10))
         LottoNumbersMatcher(lottoNumbers, bonusNumber, randomLottoes).calculateTotalMatchCount()
-        Assertions.assertThat(MatchStorage.MATCH_FIVE.matchCount).isEqualTo(2)
+        assertThat(MatchStorage.MATCH_FIVE.matchCount).isEqualTo(2)
+        initializeMatchStorage()
     }
 
     @Test
@@ -40,7 +44,8 @@ class LottoNumberMatcherTest {
         val bonusNumber = 7
         val randomLottoes = listOf(listOf(1,2,3,4,5,7))
         LottoNumbersMatcher(lottoNumbers, bonusNumber, randomLottoes).calculateTotalMatchCount()
-        Assertions.assertThat(MatchStorage.MATCH_FIVE_WITH_BONUS_NUMBER.matchCount).isEqualTo(1)
+        assertThat(MatchStorage.MATCH_FIVE_WITH_BONUS_NUMBER.matchCount).isEqualTo(1)
+        initializeMatchStorage()
     }
 
     @Test
@@ -49,6 +54,9 @@ class LottoNumberMatcherTest {
         val bonusNumber = 7
         val randomLottoes = listOf(listOf(1,2,3,4,5,6))
         LottoNumbersMatcher(lottoNumbers, bonusNumber, randomLottoes).calculateTotalMatchCount()
-        Assertions.assertThat(MatchStorage.MATCH_SIX.matchCount).isEqualTo(1)
+        assertThat(MatchStorage.MATCH_SIX.matchCount).isEqualTo(1)
+        initializeMatchStorage()
     }
+
+
 }
