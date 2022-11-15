1d845ae6fd9ca3164d5fc77f137dc99f9a8dea79
권용민 <briandr@naver.com>
2022-11-15 20:52:19 +0900

    refactor(main): World 클래스로 메인 로직 실행

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,28 +1,14 @@
 package lotto
 
 fun main() {
-    val printer = Printer()
-    val cacher = Cacher()
-    val ticketingMachine = TicketingMachine()
-    val lottoMachine = LottoMachine()
-    val purchaser = Purchaser()
+    val world = World()
+    try {
+        world.purchaseLotto()
+        world.publishWinningLotto()
+    } catch (e: Exception) {
+        println(e.message)
+        return
+    }
 
-    printer.printEnterMoneyRequest()
-    val money: Int = cacher.enterPurchaseMoney()
-    val numberOfLotto = cacher.calculateNumberOfLottos(money)
-
-    val purchasedLottos = ticketingMachine.createLottoNumbers(numberOfLotto)
-    printer.printNumberOfPurchaseLottos(numberOfLotto)
-    printer.printPurchasedLottos(purchasedLottos)
-
-    printer.printEnterWinningNumbers()
-    lottoMachine.publishWinningNumber()
-
-    printer.printEnterBonusNumber()
-    lottoMachine.publishBonusNumber()
-
-    purchaser.determineRank(purchasedLottos, lottoMachine.winningNumbers, lottoMachine.bonusNumber)
-    printer.printWinStatistics()
-    printer.printLottoResult(purchaser.rankResult)
-    printer.printEarningRate(purchaser.calculateEarningRate(money))
+    world.showResult()
 }
