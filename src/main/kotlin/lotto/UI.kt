0f0fb6a04d05ebd78b39ae8061cd581aaf288967
healthykim <bsbs8645@snu.ac.kr>
2022-11-15 12:49:26 +0900

    chore: Fix comments in UI class

--- src/main/kotlin/lotto/UI.kt
+++ src/main/kotlin/lotto/UI.kt
@@ -4,26 +4,20 @@ import camp.nextstep.edu.missionutils.Console
 
 class UI {
     /**
-     * Returns string of money from user input
+     * Functions named get[]()
+     *
+     * @return [] in string from user input
      */
     fun getMoney(): String { //-1 : error
         println("구입금액을 입력해 주세요.")
         return Console.readLine()
     }
-
-    /**
-     * Returns list of lottery number(string) from user input
-     */
     fun getLotteryNumber(): List<String> {
         println("당첨 번호를 입력해 주세요.")
 
         val lotteryNumber = Console.readLine()
         return lotteryNumber.split(",")
     }
-
-    /**
-     * Returns string of bonus number from user input
-     */
     fun getBonusNumber(): String {
         println("보너스 번호를 입력해 주세요.")
         return Console.readLine()
