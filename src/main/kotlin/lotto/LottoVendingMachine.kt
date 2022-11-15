bc8ba223f181a62649e4decd050f5eac3313d6ee
username <ieejo716@naver.com>
2022-11-15 12:40:58 +0900

    [FEAT] : 구매한 로또 개수 출력

--- src/main/kotlin/lotto/LottoVendingMachine.kt
+++ src/main/kotlin/lotto/LottoVendingMachine.kt
@@ -4,15 +4,22 @@ import util.Printer
 
 class LottoVendingMachine(
     private val user: User,
+    private val machine: Machine,
     private val printer: Printer
 ) {
     fun execution() {
         purchaseLotto()
+        issueLotto()
     }
 
     private fun purchaseLotto() {
         printer.printPurchaseMessage()
-        val money = user.inputMoney()
+        user.inputMoney()
+    }
+
+    private fun issueLotto() {
         val lottoCount = user.getLottos()
+        printer.printCountLotto(lottoCount)
+
     }
 }
