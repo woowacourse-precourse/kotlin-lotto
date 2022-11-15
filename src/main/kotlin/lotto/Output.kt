dcb9a48967658f1e31059219e007f690bbc0ce94
yeonsu10 <94501462+yeonsu10@users.noreply.github.com>
2022-11-15 17:57:56 +0900

    refactor: 당첨 통계 출력 클래스에 추가

--- src/main/kotlin/lotto/Output.kt
+++ src/main/kotlin/lotto/Output.kt
@@ -9,6 +9,17 @@ class Output {
                 println(lottoNumber[count])
             }
         }
+
+        fun printReward(countReward: MutableList<Int>, revenue:Float){
+            println("당첨 통계\n"+
+                    "---\n"+
+                    "3개 일치 (5,000원) - "+ countReward[0] +"개\n" +
+                    "4개 일치 (50,000원) - "+countReward[1] +"개\n" +
+                    "5개 일치 (1,500,000원) - "+ countReward[2] +"개\n" +
+                    "5개 일치, 보너스 볼 일치 (30,000,000원) - "+ countReward[3] +"개\n" +
+                    "6개 일치 (2,000,000,000원) - "+ countReward[4] +"개\n"+
+                    "총 수익률은 "+revenue+"%입니다.")
+        }
     }
 
 }
