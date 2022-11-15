3c5b686ae8053a7297f7f0cf862c735bf9085395
ChoiWooSuk <wooseokchoi0508@gmail.com>
2022-11-15 20:07:26 +0900

    feat(outputView): 당첨 내역 출력하는 기능 구현

--- src/main/kotlin/lotto/view/outputView.kt
+++ src/main/kotlin/lotto/view/outputView.kt
@@ -16,8 +16,22 @@ class outputView {
         }
         println()
     }
+    fun printLottoPrizeResult(prizeResult: PrizeResult){
+        println(LOTTO_RESULT_INTRO_OUTPUT_MESSAGE)
+        println("${FIFTH_PLACE_OUTPUT_MESSAGE} ${prizeResult.fifthPlace}개")
+        println("${FOURTH_PLACE_OUTPUT_MESSAGE} ${prizeResult.fourthPlace}개")
+        println("${THIRD_PLACE_OUTPUT_MESSAGE} ${prizeResult.thirdPlace}개")
+        println("${SECOND_PLACE_OUTPUT_MESSAGE} ${prizeResult.secondPlace}개")
+        println("${FIRST_PLACE_OUTPUT_MESSAGE} ${prizeResult.firstPlace}개")
+    }
 
     companion object{
         const val LOTTO_COUNT_OUTPUT_MESSAGE = "개를 구매했습니다."
+        const val LOTTO_RESULT_INTRO_OUTPUT_MESSAGE = "당첨 통계\n---"
+        const val FIFTH_PLACE_OUTPUT_MESSAGE = "3개 일치 (5,000원) -"
+        const val FOURTH_PLACE_OUTPUT_MESSAGE = "4개 일치 (50,000원) -"
+        const val THIRD_PLACE_OUTPUT_MESSAGE = "5개 일치 (1,500,000원) -"
+        const val SECOND_PLACE_OUTPUT_MESSAGE = "5개 일치, 보너스 볼 일치 (30,000,000원) -"
+        const val FIRST_PLACE_OUTPUT_MESSAGE = "6개 일치 (2,000,000,000원) -"
     }
 }
