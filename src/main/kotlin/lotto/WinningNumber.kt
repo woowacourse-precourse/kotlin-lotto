e8c3d61dc1455eafa4d2c5b14ae480060220e357
yeonsu10 <94501462+yeonsu10@users.noreply.github.com>
2022-11-15 21:02:57 +0900

    feat: 보너스 점수가 조건에 맞는지 확인 하는 함수

--- src/main/kotlin/lotto/WinningNumber.kt
+++ src/main/kotlin/lotto/WinningNumber.kt
@@ -10,4 +10,11 @@ class WinningNumber {
         }
         Lotto(winningNumber)
     }
+
+    fun checkBonus(bonus: Int){
+        if(winningNumber.contains(bonus)){
+            throw IllegalArgumentException("[ERROR] 보너스 번호는 기존 당첨번호와 중복되지 않아야 합니다.")
+        }
+        require(bonus in 1..45){"[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다."}
+    }
 }
