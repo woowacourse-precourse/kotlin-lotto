e6f9f482441f5858fb4edbfc1911d003305e270b
Gloria <ippnsj@gmail.com>
2022-11-15 12:20:01 +0900

    feat: 보너스 번호가 당첨 번호와 중복되지 않는지 확인하는 기능 구현

--- src/main/kotlin/lotto/utils/validation/UniquenessVerifier.kt
+++ src/main/kotlin/lotto/utils/validation/UniquenessVerifier.kt
@@ -1,5 +1,11 @@
 package lotto.utils.validation
 
 class UniquenessVerifier(private val numbers: List<Int>) : Verifier<Int>() {
-    override fun verify(input: Int) {}
+    override fun verify(input: Int) {
+        require(!numbers.contains(input)) { ERROR_MESSAGE }
+    }
+
+    companion object {
+        const val ERROR_MESSAGE = "Input is not an unique number."
+    }
 }
