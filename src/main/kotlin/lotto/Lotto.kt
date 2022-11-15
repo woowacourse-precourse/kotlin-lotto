096e0db2040c27ebd3b37b801aa241ac9d4aa2f9
YeonwooLee <fox_93@naver.com>
2022-11-15 17:49:57 +0900

    로또 등수 확인 테스트 구현 완료

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -27,5 +27,23 @@ class Lotto(private val numbers: List<Int>) {
     override fun toString(): String {
         return numbers.toString()
     }
+    
+    //랭킹
+    fun rank(solution:List<Int>,bonus:Int):Int{
+        val sameCnt = sameCount(solution)
+        if(sameCnt==6) return 1
+        if(sameCnt==5&&numbers.contains(bonus)) return 2
+        if(sameCnt==5) return 3
+        if(sameCnt==4) return 4
+        if(sameCnt==3) return 5
+        return -1
+    }
+    //일반번호 동일 갯수
+    fun sameCount(solution:List<Int>):Int{
+        var count=0
+        for(i in solution){if(numbers.contains(i)){count++}}
+        return count
+
+    }
 }
 
