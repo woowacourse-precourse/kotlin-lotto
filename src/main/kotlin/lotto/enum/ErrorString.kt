731ad3646af6a6163cc6067535a592e58855bca5
phs <gustjd0808@pusan.ac.kr>
2022-11-15 23:16:33 +0900

    fix(enum): fix error string

--- src/main/kotlin/lotto/enum/ErrorString.kt
+++ src/main/kotlin/lotto/enum/ErrorString.kt
@@ -7,7 +7,7 @@ enum class ErrorString(val string: String) {
         "[ERROR] 로또 번호는 ${Configuration.START_LOTTO_NUMBER.number}부터 " +
                 "${Configuration.END_LOTTO_NUMBER.number} 사이의 숫자여야 합니다."
     ),
-    LOTTO_SIZE("[ERROR] 로또 번호는 ${Configuration.LOTTO_SIZE}개를 입력해야 합니다."),
+    LOTTO_SIZE("[ERROR] 로또 번호는 ${Configuration.LOTTO_SIZE.number}개를 입력해야 합니다."),
     DUPLICATED_DRAWN_NUMBERS("[ERROR] 당첨 번호와 보너스 번호에 중복된 번호는 없어야 합니다."),
     MIN_PURCHASE_AMOUNT("[ERROR] 구입 금액은 ${Configuration.LOTTO_PRICE.number}원 이상이어야 합니다.")
 }
