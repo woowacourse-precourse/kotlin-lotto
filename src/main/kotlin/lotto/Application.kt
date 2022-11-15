7d561cda9d61484dec625d3675d25dfdc93feed9
healthykim <bsbs8645@snu.ac.kr>
2022-11-15 13:02:46 +0900

    feat(lotto): Add constructor to Lotto class

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,10 @@
 package lotto
 
+const val LOTTERY_PRICE = 1000
+const val LOTTERY_SIZE = 6
+const val TAG = "ERROR"
+val NUMBER_RANGE = 1..45
+
 fun main() {
     TODO("프로그램 구현")
 }
--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -2,7 +2,15 @@ package lotto
 
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6)
+        require(numbers.size == LOTTERY_SIZE) {
+            "$TAG 로또 번호는 ${LOTTERY_SIZE}개여야 합니다."
+        }
+        val checkedList = mutableListOf<Int>()
+        for(i in numbers) {
+            if(!NUMBER_RANGE.contains(i)) throw IllegalArgumentException("$TAG 로또 번호는 ${NUMBER_RANGE.first}부터 ${NUMBER_RANGE.last} 사이의 숫자여야 합니다.")
+            if(checkedList.contains(i)) throw IllegalArgumentException("$TAG 로또 번호는 서로 다른 숫자로 구성되어야 합니다.")
+            checkedList.add(i)
+        }
     }
 
     // TODO: 추가 기능 구현
