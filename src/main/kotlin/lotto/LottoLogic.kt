f1e0c1fc3c8d60bf3974dea0bffe73c3dcce5398
Hyeongseon Jeon <hsun5163@gmail.com>
2022-11-15 10:52:30 +0900

    feat : lottoPurchaseAmount 함수 추가
    금액을 입력받아 로또를 몇장 샀는지 반환

--- src/main/kotlin/lotto/LottoLogic.kt
+++ src/main/kotlin/lotto/LottoLogic.kt
@@ -1,15 +1,30 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
 
+const val PRICE = 1000
 class LottoLogic {
 
     private var userLotto = mutableListOf<Int>()
+    private val userLottoList = mutableListOf<List<Int>>()
+    private var userPurchaseAmount = ""
+    private var lottoCount = 0
 
     fun lottoGenerator(): List<Int>{
         userLotto.clear()
         userLotto = Randoms.pickUniqueNumbersInRange(1, 45, 6)
         userLotto.sort()
         return userLotto
+    }
+
+    fun lottoPurchaseAmount(lottoCount: Int): Int {
+        println(lottoCount % PRICE)
+        if (lottoCount < PRICE) {
+            throw IllegalArgumentException("[ERROR] 1,000원 이상을 입력하셔야 합니다.")
+        } else if (lottoCount % PRICE != 0) {
+            throw IllegalArgumentException("[ERROR] 1,000원 단위를 입력하셔야 합니다.")
+        }
+        return lottoCount / PRICE
     }
 }
