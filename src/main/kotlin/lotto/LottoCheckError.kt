9dd4285903fdb82ec485cacc4dc0739b12ac81ec
Jaehun-Jo <chojh922@gmail.com>
2022-11-15 10:33:56 +0900

    [Refactor] LottoCheckError 클래스 불필요 코드 정리

--- src/main/kotlin/lotto/LottoCheckError.kt
+++ src/main/kotlin/lotto/LottoCheckError.kt
@@ -1,22 +1,20 @@
 package lotto
 
 class LottoCheckError(private val numbers: List<Int>) {
-
     fun checkLottoThrowException() {
-        throwLottoSizeException(numbers)
-        throwLottoNumberRangeException(numbers)
-        throwLottoNumberDuplicationException(numbers)
+        throwLottoSizeException()
+        throwLottoNumberRangeException()
+        throwLottoNumberDuplicationException()
     }
 
-
-    private fun throwLottoSizeException(numbers: List<Int>) {
+    private fun throwLottoSizeException() {
         if (numbers.size != 6) {
             println("[ERROR] 로또 번호는 6개의 숫자여야 합니다.")
             throw IllegalArgumentException()
         }
     }
 
-    private fun throwLottoNumberRangeException(numbers: List<Int>) {
+    private fun throwLottoNumberRangeException() {
         for (number in numbers) {
             if (number !in 1..45) {
                 println("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.")
@@ -25,7 +23,7 @@ class LottoCheckError(private val numbers: List<Int>) {
         }
     }
 
-    private fun throwLottoNumberDuplicationException(numbers: List<Int>) {
+    private fun throwLottoNumberDuplicationException() {
         val distinctNumbers = numbers.distinct()
         if(numbers.size != distinctNumbers.size) {
             println("[ERROR] 로또 번호는 중복이 불가능 합니다.")
