b666a8b13a6e391d24f587d7cabf37b3e7890801
bmlsj <gaga2353@gmail.com>
2022-11-15 20:12:27 +0900

    refactor: enum 클래스에서 사용되지 않는 match 변수 제거

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -3,13 +3,13 @@ package lotto
 import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
 
-enum class Rank(var match: Int, var reward: Int) {
+enum class Rank(var reward: Int) {
 
-    FIRST(6, 2000000000),
-    SECOND(7, 30000000),
-    THIRD(5, 1500000),
-    FOURTH(4, 50000),
-    FIFTH(3, 5000)
+    FIRST(2000000000),
+    SECOND(30000000),
+    THIRD(1500000),
+    FOURTH(50000),
+    FIFTH(5000)
 
 }
 
