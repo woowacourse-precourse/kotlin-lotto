e41c0331fa02a07bf22c52a275420999cec9fb85
phs <gustjd0808@pusan.ac.kr>
2022-11-15 19:22:40 +0900

    test(domain): test that get winning rank by arguments
    arguments
    - number of matching digits
    - bonus number match

--- src/test/kotlin/lotto/domain/LottoListTest.kt
+++ src/test/kotlin/lotto/domain/LottoListTest.kt
@@ -1,6 +1,8 @@
 package lotto.domain
 
+import lotto.enum.WinningRank
 import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.*
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -18,6 +20,19 @@ class LottoListTest {
 
     @Test
     fun `purchaseAmount 메서드를 사용해 로또 구매 금액을 반환`() {
-        Assertions.assertThat().isEqualTo(4000)
+        val result = lottoList.purchaseAmount()
+        assertThat(result).isEqualTo(4000)
+    }
+
+    @Test
+    fun `getWinningRank 메서드에 일치하는 숫자 갯수와 보너스 번호 일치 여부를 인자로 주어 해당하는 WinningRank를 반환`() {
+        val method =
+            lottoList.javaClass.getDeclaredMethod("getWinningRank", Int::class.java, Boolean::class.java)
+        method.isAccessible = true
+
+        val inputMatches = 5
+        val inputBonusMatch = true
+        val result = method.invoke(lottoList, inputMatches, inputBonusMatch)
+        assertThat(result).isEqualTo(WinningRank.SECOND)
     }
 }
