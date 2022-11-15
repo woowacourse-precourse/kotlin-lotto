144160fda121b813dda827830a267a2cb9141f1b
joh9911 <joh9911@naver.com>
2022-11-15 18:21:48 +0900

    test(PurchaseMoneyTest): 구입 금액 테스트 코드

---
+++ src/test/kotlin/lotto/PurchaseMoneyTest.kt
@@ -0,0 +1,21 @@
+package lotto
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class PurchaseMoneyTest {
+
+    @Test
+    fun `구입 금액이 1000원 단위가 아니면 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            PurchaseMoney(1200)
+        }
+    }
+
+    @Test
+    fun `구입 금액이 0보다 작으면 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            PurchaseMoney(-1000)
+        }
+    }
+}
