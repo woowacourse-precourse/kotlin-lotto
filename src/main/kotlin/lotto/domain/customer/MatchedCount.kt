557ff2b9ac98d6debfd19e59873d94e5517e690c
권용민 <briandr@naver.com>
2022-11-15 23:02:17 +0900

    refactor(MatchedCount): 상수화

--- src/main/kotlin/lotto/domain/customer/MatchedCount.kt
+++ src/main/kotlin/lotto/domain/customer/MatchedCount.kt
@@ -6,6 +6,12 @@ private const val FIVE_PRIZE = 1500000
 private const val FIVE_BONUS_PRIZE = 30000000
 private const val SIX_PRIZE = 2000000000
 
+private const val THREE_RESULT = "3개 일치 (5,000원) - "
+private const val FOUR_RESULT = "4개 일치 (50,000원) - "
+private const val FIVE_RESULT = "5개 일치 (1,500,000원) - "
+private const val FIVE_BONUS_RESULT = "5개 일치, 보너스 볼 일치 (30,000,000원) - "
+private const val SIX_RESULT = "6개 일치 (2,000,000,000원) - "
+
 private const val INVALID_ORDINAL = "[ERROR] ENUM 클래스에 없는 값입니다."
 
 enum class MatchedCount(val matchCount: Int, val prizeMoney: Int) {
@@ -13,16 +19,16 @@ enum class MatchedCount(val matchCount: Int, val prizeMoney: Int) {
         override val gradeDescription: String = THREE_RESULT
     },
     FOUR(4, FOUR_PRIZE) {
-        override val gradeDescription: String = "4개 일치 (50,000원) - "
+        override val gradeDescription: String = FOUR_RESULT
     },
     FIVE(5, FIVE_PRIZE) {
-        override val gradeDescription: String = "5개 일치 (1,500,000원) - "
+        override val gradeDescription: String = FIVE_RESULT
     },
     FIVE_BONUS(5, FIVE_BONUS_PRIZE) {
-        override val gradeDescription: String = "5개 일치, 보너스 볼 일치 (30,000,000원) - "
+        override val gradeDescription: String = FIVE_BONUS_RESULT
     },
     SIX(6, SIX_PRIZE) {
-        override val gradeDescription: String = "6개 일치 (2,000,000,000원) - "
+        override val gradeDescription: String = SIX_RESULT
     };
 
     abstract val gradeDescription: String
@@ -33,10 +39,11 @@ enum class MatchedCount(val matchCount: Int, val prizeMoney: Int) {
     }
 
     companion object {
+        private const val FIVE = 5
 
         fun matchRank(ordinal: Int, bonusMatch: Boolean): MatchedCount {
             values().forEach {
-                if ((ordinal == 5) and bonusMatch) return FIVE_BONUS
+                if ((ordinal == FIVE) and bonusMatch) return FIVE_BONUS
                 if (it.matchCount == ordinal) return it
             }
             throw IllegalArgumentException(INVALID_ORDINAL)
