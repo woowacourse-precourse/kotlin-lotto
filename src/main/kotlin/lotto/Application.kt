7b09d184f73f158ed8fa8ef5fcf3e666683bbe4d
no1msh <no1msh1217@gmail.com>
2022-11-15 18:32:13 +0900

    feat: 전체적인 로또게임 진행 순서에 맞춰 메소드 호출

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,41 @@
 package lotto
 
+import lotto.domain.Bank
+import lotto.domain.LottoMarket
+
 fun main() {
-    TODO("프로그램 구현")
+    val lottoMarket = LottoMarket()
+    val ui = UI()
+    val bank: Bank
+
+    try {
+        ui.printRequestPurchasingAmount()
+        val purchasingAmount = ui.inputPurchasingAmount()
+        ui.printBlankLine()
+
+        val theNumberOfLotto = lottoMarket.getTheNumberOfLotto(purchasingAmount)
+        ui.printTheNumberOfLotto(theNumberOfLotto)
+        val lottos = lottoMarket.buyLottos(theNumberOfLotto)
+        ui.printLottos(lottos)
+        ui.printBlankLine()
+
+        ui.printRequestWinNumbers()
+        val winNumbers = ui.inputWinNumbers()
+        ui.printBlankLine()
+
+        ui.printRequestBonusNumber()
+        val bonusNumber = ui.inputBonusNumber()
+        ui.printBlankLine()
+
+        bank = Bank(lottos)
+        bank.writeWinStatistic(winNumbers, bonusNumber)
+        ui.printTotalWinStatistic(bank.winStatistic)
+
+        val totalIncome = bank.getTotalIncome()
+        val rateOfReturn = bank.getRateOfReturn(totalIncome, purchasingAmount)
+        ui.printRateOfReturn(rateOfReturn)
+    } catch (error : Exception){
+        println(error.toString())
+    }
+
 }
