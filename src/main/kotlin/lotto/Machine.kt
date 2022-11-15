ec7c628b6ba015e598b577e981af655ee209fc89
username <ieejo716@naver.com>
2022-11-15 18:08:29 +0900

    [FEAT] : 수익률 구현 로직 구현

--- src/main/kotlin/lotto/Machine.kt
+++ src/main/kotlin/lotto/Machine.kt
@@ -6,8 +6,12 @@ class Machine {
     val lottos = mutableListOf<List<Int>>()
 
     fun issueLottos(): List<Int> {
-        val lotto = Randoms.pickUniqueNumbersInRange(1, 45, 6)
+        val lotto = Randoms.pickUniqueNumbersInRange(1, 45, 6).sorted()
         lottos.add(lotto)
         return lotto
     }
+
+    fun winningsRate(money: Int, profit: Int) = profit.toFloat() / money.toFloat()
+
+
 }
