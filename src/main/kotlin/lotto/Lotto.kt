8d6e7e5ab79660f6cd5ae9c5b28b984dc6760627
jjw0071 <jjw0071@naver.com>
2022-11-15 19:27:23 +0900

    feat : lotto 출력함수와 당첨확인하는 함수

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -5,5 +5,33 @@ class Lotto(private val numbers: List<Int>) {
         require(numbers.size == 6)
     }
 
-    // TODO: 추가 기능 구현
+    fun printLotto(){
+        println(numbers)
+    }
+
+    fun winLottoCheck(lottoNum : MutableList<Int>,bonusNum : Int) : Int{
+        var matchCount : Int = 0
+        for(i : Int in 0..5){
+            matchCount += lottoCheck(i, lottoNum)
+        }
+        if(matchCount == 5 && bonusCheck(bonusNum))
+            return 7
+        return matchCount
+
+    }
+
+    fun lottoCheck(num : Int, lottoNum : MutableList<Int>) : Int {
+        for(i : Int in 0..5){
+            if(numbers[num] == lottoNum[i])
+                return 1
+        }
+        return 0
+    }
+
+    fun bonusCheck(bonusNum : Int ) : Boolean{
+        for(i : Int in 0..5)
+            if(numbers[i]==bonusNum)
+                return true
+        return false
+    }
 }
