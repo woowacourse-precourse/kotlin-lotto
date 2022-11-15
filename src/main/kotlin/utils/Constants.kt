2d0e1a1b6d45784cf9aabd7cce98ddd5c7dbe5d4
joh9911 <joh9911@naver.com>
2022-11-15 18:47:53 +0900

    refactor(Constants): utils 디렉토리로 이동

--- src/main/kotlin/utils/Constants.kt
+++ src/main/kotlin/utils/Constants.kt
@@ -1,4 +1,4 @@
-package lotto
+package utils
 
 object Constants {
     /**
@@ -16,14 +16,14 @@ object Constants {
     val MATCH_SIX_NUMBER_MESSAGE = "6개 일치 (2,000,000,000원) - "
 
     val ERROR_MESSAGE = "[ERROR]"
-    val ERROR_WRONG_INPUT_FORM = "${ERROR_MESSAGE} 입력값이 올바른 형식이 아닙니다."
-    val ERROR_LOTTO_NUMBER_DUPLICATED_NUMBER = "${ERROR_MESSAGE} 로또 번호는 서로 중복되지 않은 숫자여야 합니다."
-    val ERROR_LOTTO_NUMBER_OVER_SIX_NUMBER = "${ERROR_MESSAGE} 로또 번호는 6개의 숫자여야 합니다."
-    val ERROR_LOTTO_NUMBER_NOT_BETWEEN_ONE_AND_FORTYFIVE = "${ERROR_MESSAGE} 로또 번호는 1부터 45 사이의 숫자여야 합니다."
+    val ERROR_WRONG_INPUT_FORM = "$ERROR_MESSAGE 입력값이 올바른 형식이 아닙니다."
+    val ERROR_LOTTO_NUMBER_DUPLICATED_NUMBER = "$ERROR_MESSAGE 로또 번호는 서로 중복되지 않은 숫자여야 합니다."
+    val ERROR_LOTTO_NUMBER_OVER_SIX_NUMBER = "$ERROR_MESSAGE 로또 번호는 6개의 숫자여야 합니다."
+    val ERROR_LOTTO_NUMBER_NOT_BETWEEN_ONE_AND_FORTYFIVE = "$ERROR_MESSAGE 로또 번호는 1부터 45 사이의 숫자여야 합니다."
 
-    val ERROR_BONUS_NUMBER_DUPLICATED_IN_LOTTO_NUMBERS = "${ERROR_MESSAGE} 보너스 번호는 입력했던 6개의 숫자와 중복되지 않아야 합니다."
-    val ERROR_BONUS_NUMBER_NOT_BETWEEN_ONE_AND_FORTYFIVE = "${ERROR_MESSAGE} 보너스 번호는 1부터 45 사이의 숫자여야 합니다."
+    val ERROR_BONUS_NUMBER_DUPLICATED_IN_LOTTO_NUMBERS = "$ERROR_MESSAGE 보너스 번호는 입력했던 6개의 숫자와 중복되지 않아야 합니다."
+    val ERROR_BONUS_NUMBER_NOT_BETWEEN_ONE_AND_FORTYFIVE = "$ERROR_MESSAGE 보너스 번호는 1부터 45 사이의 숫자여야 합니다."
 
-    val ERROR_PURCHASE_MONEY_NOT_THOUSAND_UNIT = "${ERROR_MESSAGE} 구입 금액은 1000원 단위여야 합니다."
-    val ERROR_PURCHASE_MONEY_NOT_POSITIVE_NUMBER = "${ERROR_MESSAGE} 구입 금액은 0보다 커야 합니다."
+    val ERROR_PURCHASE_MONEY_NOT_THOUSAND_UNIT = "$ERROR_MESSAGE 구입 금액은 1000원 단위여야 합니다."
+    val ERROR_PURCHASE_MONEY_NOT_POSITIVE_NUMBER = "$ERROR_MESSAGE 구입 금액은 0보다 커야 합니다."
 }
