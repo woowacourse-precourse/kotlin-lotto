312086489691f406c0d7b54304a06cfcd35d1c86
dlwgnud <wngud980411@naver.com>
2022-11-15 20:28:42 +0900

    feat(Output): enum 상수 추가

--- src/main/kotlin/lotto/Output.kt
+++ src/main/kotlin/lotto/Output.kt
@@ -5,5 +5,13 @@ enum class Output(val message: String) {
     WinningNumber("당첨 번호를 입력해 주세요."),
     BonusNumber("보너스 번호를 입력해 주세요."),
     NumberOfPurchases("개를 구매했습니다."),
-    Result("당첨 통계")
+    Result("당첨 통계"),
+
+    ThreeMatch("3개 일치 (5,000원) - "),
+    FourMatch("4개 일치 (50,000원) - "),
+    FiveMatch("5개 일치 (1,500,000원) - "),
+    FiveAndBonusMatch("5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+    SixMatch("6개 일치 (2,000,000,000원) - "),
+
+    EarningRate("총 수익률은 ")
 }
