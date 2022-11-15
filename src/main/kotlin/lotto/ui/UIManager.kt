9b58078ab9768685a0180634f243e90de0c30949
phs <gustjd0808@pusan.ac.kr>
2022-11-15 17:07:59 +0900

    feat(ui): add methods to use OutputManager methods

--- src/main/kotlin/lotto/ui/UIManager.kt
+++ src/main/kotlin/lotto/ui/UIManager.kt
@@ -2,17 +2,27 @@ package lotto.ui
 
 import lotto.domain.BonusNumber
 import lotto.domain.Lotto
+import lotto.domain.LottoList
 
 class UIManager(private val inputManager: InputManager, private val outputManager: OutputManager) {
     fun askPurchaseAmount(): Int {
         return inputManager.askPurchaseAmount()
     }
 
+    fun printPurchaseNumber(number: Int) {
+        println()
+        outputManager.printPurchaseNumber(number)
+    }
+
     fun askDrawnNumbers(): Pair<Lotto, BonusNumber> {
         return inputManager.askDrawnNumbers()
     }
 
-    fun printWinningStatistics(winningList: List<Int>, earningsRate: Int) {
+    fun printWinningStatistics(winningList: List<Int>, earningsRate: Double) {
         outputManager.printWinningStatistics(winningList, earningsRate)
+    }
+
+    fun printUserLotto(userLotto: LottoList) {
+        outputManager.printUserLotto(userLotto)
     }
 }
