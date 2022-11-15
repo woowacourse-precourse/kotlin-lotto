a96ae953ecc1bdb0516b1644a4adf8ea60556217
s9hn <tnghqn@naver.com>
2022-11-15 22:54:16 +0900

    [REFACTOR(Application)] try-catch 삽입

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,34 +1,38 @@
 package lotto
 
-import lotto.data.Message
 import lotto.domain.*
 import lotto.ui.GuideLotto
 
 fun main() {
-    val guideLotto = GuideLotto()
-    guideLotto.printEnterPayment() // 지불 금액 안내
+    try {
+        val guideLotto = GuideLotto()
+        guideLotto.printEnterPayment() // 지불 금액 안내
 
-    val customer = Customer()
-    val payment = customer.payMoney() // 지불 금액
+        val customer = Customer()
+        val payment = customer.payMoney() // 지불 금액
 
-    val seller = Seller(payment) // 판매자에게 금액 지불
-    val lottoBundle = seller.setLotto() // 판매자 로또 세팅
+        val seller = Seller(payment) // 판매자에게 금액 지불
+        val lottoBundle = seller.setLotto() // 판매자 로또 세팅
 
-    val lottoHost = LottoHost()
-    val winningNumber = lottoHost.createWinningNumber() // 주최자 당첨번호 선정
+        val lottoHost = LottoHost()
+        val winningNumber = lottoHost.createWinningNumber() // 주최자 당첨번호 선정
 
-    val checkLotto = CheckLotto(lottoBundle, winningNumber)
-    val lottoResult = checkLotto.compareLotto() // 로또 확인 및 비교대조 작업
+        val checkLotto = CheckLotto(lottoBundle, winningNumber)
+        val lottoResult = checkLotto.compareLotto() // 로또 확인 및 비교대조 작업
 
-    println(lottoResult)
+        println(lottoResult)
 
-    val comparator = Comparator()
+        val comparator = Comparator()
 
 
 
-    guideLotto.printLottoResult(comparator.showLottoResult(lottoResult))
+        guideLotto.printLottoResult(comparator.showLottoResult(lottoResult))
 
-    guideLotto.printIncomeRate(customer.calculateIncome(comparator.getMoney(), payment))
+        guideLotto.printIncomeRate(customer.calculateIncome(comparator.getMoney(), payment))
+    } catch (e: Exception) {
+        println(e.message)
+        return
+    }
 
 }
 
