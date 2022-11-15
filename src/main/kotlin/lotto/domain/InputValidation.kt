c6354899169e229a5d1161c2d31994c3cae5e69c
ChoiWooSuk <wooseokchoi0508@gmail.com>
2022-11-15 17:55:26 +0900

    feat(inputValidation): 보너스 번호가 당첨 번호와 중복 될 경우 예외 발생 기능 구현

--- src/main/kotlin/lotto/domain/InputValidation.kt
+++ src/main/kotlin/lotto/domain/InputValidation.kt
@@ -25,6 +25,10 @@ class InputValidation {
         if(bonusNumber< Lotto.MIN_NUMBER || bonusNumber> Lotto.MAX_NUMBER) throw IllegalArgumentException(BONUS_NUMBER_RANGE_ERROR_MESSAGE)
     }
 
+    fun checkWinningNumberContainBonusNumber(winningNumbers:List<Int>, bonusNumber: Int){
+        if(winningNumbers.contains(bonusNumber)) throw IllegalArgumentException(BONUS_NUMBER_DUPLICATE_ERROR_MESSAGE)
+    }
+
     companion object {
         const val LOTTO_PRICE = 1000
         const val WINNING_NUMBER_COUNT = 6
@@ -32,5 +36,6 @@ class InputValidation {
         const val MULTIPLE_OF_THOUSAND_ERROR_MESSAGE = "[ERROR] 1000의 배수 단위로 구매 금액을 입력해 주셔야 합니다."
         const val SPLIT_WITH_COMMA_ERROR_MESSAGE = "[ERROR] 숫자 6개를 ,로 구분해 주세요"
         const val BONUS_NUMBER_RANGE_ERROR_MESSAGE ="[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다."
+        const val BONUS_NUMBER_DUPLICATE_ERROR_MESSAGE ="[ERROR] 보너스 번호는 당첨 번호와 중복되지 않아야 합니다."
     }
 }
