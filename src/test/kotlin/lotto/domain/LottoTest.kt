0c6640d20272e79cf4bc030f12b4ff3c8dc4dd3c
phs <gustjd0808@pusan.ac.kr>
2022-11-15 21:02:22 +0900

    chore: LottoTest migrate to domain packager

--- src/test/kotlin/lotto/domain/LottoTest.kt
+++ src/test/kotlin/lotto/domain/LottoTest.kt
@@ -1,8 +1,5 @@
-package lotto
+package lotto.domain
 
-import lotto.domain.BonusNumber
-import lotto.domain.Lotto
-import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
