18054ad8a6cc38b6cae92dbf935e3f91d031556e
ghwlsdl <hojinlee93@naver.com>
2022-11-15 21:58:23 +0900

    3week mission complete

---
+++ src/main/kotlin/lotto/Money.kt
@@ -0,0 +1,50 @@
+package lotto
+
+const val LOTTO_PRICE: Int = 1000
+class Money {
+
+    var money: String = ""
+
+    enum class WinMoney(private val prize: Int) {
+        FifthPlace(5000),
+        FourthPlace(50000),
+        ThirdPlace(1500000),
+        SecondPlace(30000000),
+        FirstPlace(2000000000);
+
+        fun getPrize(): Int {
+            return prize
+        }
+    }
+
+    fun isError(): Boolean {
+        if (money == "ERROR") {
+            return true
+        }
+        return false
+    }
+
+    fun getNumberOfPurchases(): Int {
+        return money.toInt() / LOTTO_PRICE
+    }
+
+    fun getProfitRate(winningResult: List<Int>): Double {
+        val totalPrize = getTotalPrize(winningResult)
+        return roundOff(calculateProfitRate(totalPrize))
+    }
+
+    private fun getTotalPrize(winningResult: List<Int>): Int {
+        var totalPrize = 0
+        winningResult.forEachIndexed { index, Num -> totalPrize += WinMoney.values()[index].getPrize() * Num }
+
+        return totalPrize
+    }
+
+    private fun calculateProfitRate(totalPrize: Int): Double {
+        return (totalPrize / money.toDouble() * 100)
+    }
+
+    private fun roundOff(d: Double): Double {
+        return String.format("%.2f", d).toDouble()
+    }
+}
