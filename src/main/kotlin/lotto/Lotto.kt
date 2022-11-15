81e11d0a18cfb549aed5fd747dcd9bbb0289ff4a
Jaehun.Kim <rlawogns0411@naver.com>
2022-11-15 20:18:18 +0900

    feat : 로또 당첨 체크 기능

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -6,5 +6,14 @@ class Lotto(private val numbers: List<Int>) {
     }
 
     // TODO: 추가 기능 구현
+    fun checkAllWinLotto(purchasedLotto : List<List<Int>>,bonusNum : Int): MutableList<Int> {
+        val winLotto = mutableListOf<Int>(0,0,0,0,0)
+        for(i in purchasedLotto){
+            val sameNum = countSameNum(i,bonusNum)
+            checkWinLotto(sameNum,winLotto)
+        }
+        return winLotto
+    }
+
 
 }
