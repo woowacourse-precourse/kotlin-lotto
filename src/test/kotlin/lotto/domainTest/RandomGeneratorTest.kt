e43b5899a0dd1e8c65c801b389994cb2f7c03848
vrexpert <vrexpert2302@gmail.com>
2022-11-15 17:22:37 +0900

    test(lotto): 금액에 맞는 로또 개수 생성 테스트
    RandomGeneratorTest 의 생성된 로또 개수 확인
    5000원 입력시 5개 구매

--- src/test/kotlin/lotto/domainTest/RandomGeneratorTest.kt
+++ src/test/kotlin/lotto/domainTest/RandomGeneratorTest.kt
@@ -1,4 +1,15 @@
 package lotto.domainTest
 
+import lotto.domain.RandomGenerator
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
 class RandomGeneratorTest {
+    @Test
+    fun `생성된 로또 개수 확인`() {
+        val randomGenerator = RandomGenerator()
+        val lottos = randomGenerator.createRandomlottos(5000)
+        val result = lottos.size
+        assertThat(result).isEqualTo(5)
+    }
 }
