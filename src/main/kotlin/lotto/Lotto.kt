afbe5904713ab2ab039d25dd0acd759a206a3df8
Hyeongseon Jeon <hsun5163@gmail.com>
2022-11-15 15:35:31 +0900

    feat : lottoAnswer 함수 추가
    숫자의 개수가 맞는지 체크
    숫자의 범위가 맞는지 체크
    숫자가 중복이 없는지 체크

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -2,8 +2,19 @@ package lotto
 
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6)
+        try {
+            require(numbers.size == 6)
+        } catch (e:Exception) {
+            throw IllegalArgumentException("[ERROR] 6개만 입력해야 합니다.")
+        }
     }
 
+    fun lottoAnswer(answers : List<Int>){
+        if(answers.size != (answers.filter{ it in 1..45 }).size){
+            throw IllegalArgumentException("[ERROR] 1부터 45 숫자를 입력하셔야 합니다.")
+        } else if (answers.size != answers.distinct().size){
+            throw IllegalArgumentException("[ERROR] 중복이 없어야 합니다.")
+        }
+    }
     // TODO: 추가 기능 구현
 }
