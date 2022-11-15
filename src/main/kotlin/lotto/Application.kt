90cd2b611e587c60bb4217740675703cc3a55f0b
bmlsj <gaga2353@gmail.com>
2022-11-15 19:42:44 +0900

    feat: 로또 당첨 번호를 입력하는 기능

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,20 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
-    TODO("프로그램 구현")
+
+}
+
+class LottoNumber {
+    private fun winnerNumber(): List<Int> {
+        println("\n당첨 번호를 입력해 주세요.")
+        val winNumber = Console.readLine().split(",").map { it.toInt() }
+        Lotto(winNumber)
+
+        // println("당첨 번호 : $winNumber")
+        return winNumber
+    }
+
 }
