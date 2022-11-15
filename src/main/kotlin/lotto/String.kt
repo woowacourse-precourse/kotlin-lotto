db9fd5bb930951de7862e46435e54e4d09561d7c
leehandsub <gustjq7027@naver.com>
2022-11-15 15:43:52 +0900

    test(user): 로또 구입 int 범위보다 큰지 체크 테스트

--- src/main/kotlin/lotto/String.kt
+++ src/main/kotlin/lotto/String.kt
@@ -2,3 +2,4 @@ package lotto
 
 const val AMOUNT_PURCHASE_MESSAGE = "구입금액을 입력해 주세요."
 const val ERROR_AMOUNT_NUMBER_MESSAGE = "[ERROR] 구입금액은 숫자여야 합니다."
+const val ERROR_AMOUNT_BIG_NUMBER_MESSAGE = "[ERROR] 숫자가 너무 큽니다."
--- src/test/kotlin/lotto/UserTest.kt
+++ src/test/kotlin/lotto/UserTest.kt
@@ -1,16 +1,23 @@
 package lotto
 
 import lotto.domain.User
-import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
 class UserTest {
     private val userTest = User()
+
     @Test
     fun `로또 구입 금액이 숫자인지 확인한다`() {
         assertThrows<IllegalArgumentException> {
             userTest.checkAmount("1231a")
+        }
+    }
+
+    @Test
+    fun `로또 구입 금액이 int보다 큰지 확인한다`() {
+        assertThrows<IllegalArgumentException> {
+            userTest.checkAmount("12311234123124213124")
         }
     }
 }
