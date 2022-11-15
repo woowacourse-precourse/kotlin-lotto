dc0a251e997499a21a4b4647b01f2276075996b2
vrexpert <vrexpert2302@gmail.com>
2022-11-15 17:32:49 +0900

    test(lotto): 등수 확인 함수 테스트
    RankingTest : 당첨 번호 맞는 갯수와 보너스 번호로 등수확인
    1. 당첨번호 6개 1등
    2. 당첨번호 5개에 보너스는 2등
    3. 당첨번호 4개는 보너스가 맞아도 4등

--- src/test/kotlin/lotto/domainTest/RankingTest.kt
+++ src/test/kotlin/lotto/domainTest/RankingTest.kt
@@ -1,4 +1,29 @@
 package lotto.domainTest
 
+import lotto.domain.RandomGenerator
+import lotto.domain.Ranking
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
 class RankingTest {
+    @Test
+    fun `6개는 1등`() {
+        val ranking = Ranking()
+        val result = ranking.checkRanking(6,false)
+        Assertions.assertThat(result).isEqualTo(1)
+    }
+
+    @Test
+    fun `5개에 보너스는 2등`() {
+        val ranking = Ranking()
+        val result = ranking.checkRanking(5,true)
+        Assertions.assertThat(result).isEqualTo(2)
+    }
+
+    @Test
+    fun `4개에 보너스는 4등`() {
+        val ranking = Ranking()
+        val result = ranking.checkRanking(4,true)
+        Assertions.assertThat(result).isEqualTo(4)
+    }
 }
