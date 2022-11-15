153b03edd1e3c64486da362526e079b333ffb24c
yeonsu10 <94501462+yeonsu10@users.noreply.github.com>
2022-11-15 21:00:03 +0900

    fix: 일정 숫자가 넘어갈 시 출력 오류 수정

--- src/main/kotlin/lotto/Output.kt
+++ src/main/kotlin/lotto/Output.kt
@@ -17,8 +17,9 @@ class Output {
                     "4개 일치 (50,000원) - "+countReward[1] +"개\n" +
                     "5개 일치 (1,500,000원) - "+ countReward[2] +"개\n" +
                     "5개 일치, 보너스 볼 일치 (30,000,000원) - "+ countReward[3] +"개\n" +
-                    "6개 일치 (2,000,000,000원) - "+ countReward[4] +"개\n"+
-                    "총 수익률은 "+revenue+"%입니다.")
+                    "6개 일치 (2,000,000,000원) - "+ countReward[4] +"개\n")
+            print("총 수익률은 %.1f".format(revenue))
+            print("%입니다.")
         }
     }
 
