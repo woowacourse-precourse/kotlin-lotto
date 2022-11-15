570502f41364b7d15425ba557d238a786d845e46
phs <gustjd0808@pusan.ac.kr>
2022-11-15 16:50:54 +0900

    chore(enum): add enum for not winning

--- src/main/kotlin/lotto/enum/Reward.kt
+++ src/main/kotlin/lotto/enum/Reward.kt
@@ -3,11 +3,12 @@ package lotto.enum
 import java.text.DecimalFormat
 
 enum class Reward(val reward: Int) {
-    FIFTH_REWARD(5_000),
-    FOURTH_REWARD(50_000),
-    THIRD_REWARD(1_500_000),
-    SECOND_REWARD(30_000_000),
-    FIRST_REWARD(2_000_000_000);
+    FIFTH(5_000),
+    FOURTH(50_000),
+    THIRD(1_500_000),
+    SECOND(30_000_000),
+    FIRST(2_000_000_000),
+    NOTHING(0);
 
     fun rewardString(): String = DecimalFormat("#,###").format(this.reward)
 }
