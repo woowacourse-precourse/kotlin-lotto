13d660f9353497ac412898fa710b7ddb581b1c63
lsakee <lsls4868@gmail.com>
2022-11-15 15:10:57 +0900

    fix : 보너스 번호 오류 수정 -보너스 번호가 당첨번호와 중첩이 될경우에서 일의자리가 같으면 오류로 판정 되는 오류 수정

---
+++ src/main/kotlin/lotto/input/InputBonus.kt
@@ -0,0 +1,22 @@
+package lotto.input
+
+class InputBonus {
+    fun inputBonusNumber(lottoNumber: String): Int {
+        print("보너스 번호를 입력해 주세요.")
+        val userInputNumber = inputUser()
+        checkBonusStandard(userInputNumber, lottoNumber)
+        println(userInputNumber.toInt())
+        return userInputNumber.toInt()
+    }
+    fun checkBonusStandard(userInputNumber: String?, lottoNumber: String) {
+        userInputNumber ?: throw IllegalArgumentException()
+        val lottoOverlapCheck = mutableListOf<String>()
+        if (userInputNumber.toInt() >= 46 || userInputNumber.toInt() < 1) throw IllegalArgumentException()
+        lottoOverlapCheck.add(userInputNumber)
+        checkInputMessagePatten(lottoOverlapCheck)
+        if (lottoOverlapCheck.size != 1 ||lottoNumber.contains(lottoOverlapCheck[0])){
+            println("[ERROR]")
+            throw IllegalArgumentException()
+        }
+    }
+}
