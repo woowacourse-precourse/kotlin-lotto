96cc7653c0e15ef2ff22b36cb67646aaa345f46b
noion0511 <noion0511@gmail.com>
2022-11-15 19:56:48 +0900

    Feat : 서로 다른 수들을 1부터 45이내로 7개를 뽑는 기능

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,9 +1,28 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6)
+        lotteries.add(numbers)
     }
 
-    // TODO: 추가 기능 구현
+    companion object {
+        val lotteries = mutableListOf<List<Int>>()
+
+        fun printLotto() {
+            println("${lotteries.size}개를 구매했습니다.")
+            for (lottery in lotteries) {
+                println(lottery)
+            }
+        }
+
+        fun createLotto(count: Int) {
+            for (c in 1..count) {
+                val lottoNumber = Randoms.pickUniqueNumbersInRange(1, 45, 6).sorted()
+                Lotto(lottoNumber)
+            }
+        }
+    }
 }
