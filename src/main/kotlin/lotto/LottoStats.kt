e98ad16cb605cb321fd50e25721e446cc78274cd
Hyeongseon Jeon <hsun5163@gmail.com>
2022-11-15 19:53:06 +0900

    feat : findRank 함수 추가
    LottoStats 클래스에서 등수 정보의 개수를 다 가지고 있게 수정

--- src/main/kotlin/lotto/LottoStats.kt
+++ src/main/kotlin/lotto/LottoStats.kt
@@ -1,9 +1,40 @@
 package lotto
 
-enum class LottoStats(val message: String, val price: Int, val answerCnt: Int, val bonusCnt: Int) {
-    FIRST_RANK("6개 일치", 2_000_000_000, 6, 0),
-    SECOND_RANK("5개 일치, 보너스볼 일치", 30_000_000, 5, 1),
-    THIRD_RANK("5개 일치", 1500000, 5, 0),
-    FORTH_RANK("4개 일치", 50000, 4, 0),
-    FIFTH_RANK("3개 일치", 5000, 3, 0);
+import lotto.LottoStats as LottoStats
+
+enum class LottoStats(val message: String, val price: Int, private var Cnt: Int) {
+    FIRST_RANK("6개 일치", 2_000_000_000, 0),
+    SECOND_RANK("5개 일치, 보너스볼 일치", 30_000_000, 0),
+    THIRD_RANK("5개 일치", 1_500_000, 0),
+    FORTH_RANK("4개 일치", 50_000, 0),
+    FIFTH_RANK("3개 일치", 5_000, 0);
+    fun lottoMatchLists(userLottoList : List<Int>, comLottoList : List<Int>) {
+        var answerCnt = 0
+        var bonusCnt = 0
+        var cnt = 0
+        for (target in userLottoList){
+            if(comLottoList.subList(0,6).contains(target)){
+                answerCnt += 1
+            } else if(comLottoList.subList(6,7).contains(target)) {
+                bonusCnt += 1
+            }
+        }
+        if(answerCnt == 6){
+            answerCnt += 1
+        } else if (answerCnt == 5 && bonusCnt == 1){
+            cnt = 6
+        }
+        cnt = answerCnt
+        return findRank(cnt)
+    }
+
+    private fun findRank(cnt : Int){
+        when(cnt){
+            7 -> FIRST_RANK.Cnt += 1
+            6 -> SECOND_RANK.Cnt += 1
+            5 -> THIRD_RANK.Cnt += 1
+            4 -> FORTH_RANK.Cnt += 1
+            3 -> FIFTH_RANK.Cnt += 1
+            }
+    }
 }
