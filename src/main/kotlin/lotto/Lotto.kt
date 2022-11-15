311c560db38185e6f86d68415685cf91ec9200cf
lsakee <lsls4868@gmail.com>
2022-11-15 15:30:52 +0900

    feat : Lotto 클래스 구현 로또 클래스 구현

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -4,13 +4,26 @@ import camp.nextstep.edu.missionutils.Randoms
 
 class Lotto(private val numbers: List<Int>) {
 
-    init {
-        require(numbers.size == 6)
-        lottoCreate()
-    }
-    fun lottoCreate(){
-        val numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
-        println(numbers.sorted())
+    class Lotto(private val numbers: List<Int>) {
+
+        init {
+            require(numbers.size == 6)
+        }
+
+        fun contains(number: Int): Boolean {
+            if (numbers.contains(number)) {
+                return true
+            }
+            return false
+        }
+
+        fun getValue(index: Int): Int {
+            return numbers[index]
+        }
+
+        fun getNumbers(): List<Int> {
+            return numbers
+        }
     }
     // TODO: 추가 기능 구현
 }
