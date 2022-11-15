6380cee782c4a958f6eb0a6a49e6919936f0c204
KimDongHyun0907 <ehdgus8397@naver.com>
2022-11-15 22:49:58 +0900

    Bonus 클래스 추가 및 보너스 번호 입력 구현

--- src/main/kotlin/lotto/Bonus.kt
+++ src/main/kotlin/lotto/Bonus.kt
@@ -1,4 +1,12 @@
 package lotto
 
-class Bonus {
+class Bonus{
+    fun InputBonus():Int{
+        println("보너스 번호를 입력해 주세요.")
+        val bonus = readLine()!!.toInt()
+        if(bonus<1 || bonus>45){
+            throw IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.")
+        }
+        return bonus
+    }
 }
