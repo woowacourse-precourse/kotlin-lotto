5db57a590fdceb79f8b76c924452ba01656d91b9
username <ieejo716@naver.com>
2022-11-15 16:10:37 +0900

    [FEAT] : 당첨자 결과 메서드

--- src/main/kotlin/lotto/LottoVendingMachine.kt
+++ src/main/kotlin/lotto/LottoVendingMachine.kt
@@ -14,11 +14,21 @@ class LottoVendingMachine(
         issueLotto()
         val numbers = inputLottoNumbers()
         val bonus = inputBonusNumber(numbers)
-        resultLottoWinnings(numbers,bonus)
+        resultLottoWinnings(numbers, bonus)
     }
 
-    private fun resultLottoWinnings(numbers: List<Int>, bonus: Unit) {
-
+    private fun resultLottoWinnings(numbers: List<Int>, bonus: Int): List<Int> {
+        val lotto = Lotto(numbers)
+        val lottoResult = mutableListOf<Int>(0, 0, 0, 0, 0, 0, 0, 0)
+        machine.lottos.forEach { it ->
+            val count = lotto.winningsCount(it)
+            if (lotto.isBonusTrue(bonus)) {
+                lottoResult[count + 1]++
+                return@forEach
+            }
+            lottoResult[count]++
+        }
+        return lottoResult
     }
 
     private fun purchaseLotto() {
@@ -40,9 +50,10 @@ class LottoVendingMachine(
         return user.inputNumbers()
     }
 
-    private fun inputBonusNumber(numbers: List<Int>) {
+    private fun inputBonusNumber(numbers: List<Int>): Int {
         printer.printInputBonusMessage()
         val bonus = user.inputBonus()
         if (numbers.contains(bonus)) inputException.checkOverlapException()
+        return bonus
     }
 }
