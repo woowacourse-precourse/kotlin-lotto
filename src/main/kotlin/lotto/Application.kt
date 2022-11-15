606869407b679cea984a455246fd8f0df8226548
bmlsj <gaga2353@gmail.com>
2022-11-15 23:01:26 +0900

    refactor: 당첨 금액을 숫자로 두지 않고 상수로 표현

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -8,11 +8,11 @@ import lotto.domain.Lotto
 
 enum class Rank(var reward: Int) {
 
-    FIRST(2000000000),
-    SECOND(30000000),
-    THIRD(1500000),
-    FOURTH(50000),
-    FIFTH(5000)
+    FIRST(FIRSTREWARD),
+    SECOND(SECONEREWARD),
+    THIRD(THIRDREWARD),
+    FOURTH(FOURTHREWARD),
+    FIFTH(FIFTHREWARD)
 
 }
 
