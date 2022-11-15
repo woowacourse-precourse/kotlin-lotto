8ddd5585ede56686c7756b9217371de683b7072e
Gloria <ippnsj@gmail.com>
2022-11-15 09:44:59 +0900

    feat: 로또 구입 금액만큼 로또를 발행하는 기능 구현

--- src/main/kotlin/lotto/domain/LottoMachine.kt
+++ src/main/kotlin/lotto/domain/LottoMachine.kt
@@ -4,6 +4,13 @@ class LottoMachine(private val generator: Generator) {
     private val tickets = mutableListOf<Lotto>()
 
     fun publish(money: Int): List<Lotto> {
+        var numberOfTickets = money / 1000
+        while(numberOfTickets != 0) {
+            val numbers = generator.generateNumbers()
+            val lotto = Lotto(numbers)
+            tickets.add(lotto)
+            numberOfTickets--
+        }
         return tickets
     }
 }
