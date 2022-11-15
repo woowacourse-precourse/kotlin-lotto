307ae1bf4c1ff26e5b4aead9778e350433643ca8
KimDongHyun0907 <ehdgus8397@naver.com>
2022-11-15 22:40:45 +0900

    구매한 개수의 6개의 로또 번호 출력

--- src/main/kotlin/lotto/Number.kt
+++ src/main/kotlin/lotto/Number.kt
@@ -1,9 +1,21 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class Number{
     fun BuyLottoCount(buy:Int): Int {
         val buy_count=buy/1000
         println("${buy_count}개를 구매했습니다.")
         return buy_count
+    }
+
+    fun PrintNumberList(buy_count:Int):MutableList<List<Int>>{
+        val total_numbers= mutableListOf<List<Int>>()
+        for(i in 1..buy_count){
+            val numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
+            println(numbers.sorted())
+            total_numbers.add(numbers.sorted())
+        }
+        return total_numbers
     }
 }
