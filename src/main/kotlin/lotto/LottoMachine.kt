fb3bb39dbec95234a01eb85a25392115d552b1e3
2taezeat <xorudfl772@naver.com>
2022-11-15 14:11:08 +0900

    feat(LottoMachine): 로또 번호 6개를 생성하고 리스트에 담아 리턴하는 함수 작성

--- src/main/kotlin/lotto/LottoMachine.kt
+++ src/main/kotlin/lotto/LottoMachine.kt
@@ -1,8 +1,14 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class LottoMachine(private val customerBuyLottoCount: Int) {
     fun printCustomerBuyLottoCount() {
         println("${customerBuyLottoCount / 1000}" + LottoProcessStatement.COUNT_LOTTO_BUY + "\n")
+    }
+
+    fun createCustomerLottoNumber(): List<Int>{
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6)
     }
 
 }
