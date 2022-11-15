ea63ea0968b56649e8ac1c2eb3d3a63af2d84fe4
ChoHJin <cjc9739@naver.com>
2022-11-15 19:37:50 +0900

    refactor : 기능 추가 및 변수명 변경
    당첨된 로또의 등수를 카운트하고 등수마다 개수를 출력하는 기능을 추가함.
    
    각 변수명을 기능에 맞게 변경함.

--- src/main/kotlin/lotto/LottoGame.kt
+++ src/main/kotlin/lotto/LottoGame.kt
@@ -9,13 +9,34 @@ class LottoGame {
         val lottoList = mutableListOf<Lotto>()
 
         val lottoQuantity = PurchaseLotto().issuedLotto(PurchaseLotto().purchase())
+
         PurchaseLotto().getLottoTicket(lottoList, lottoQuantity)
 
-        val testNum = GenerateWinningNumbers().inputWinningNumbers()
-        val testBonus = GenerateBonusNumber(testNum).inputBonusNumber()
-        val testWinningNumbers = testNum.getLottoNumbers().plus(testBonus)
+        val setWinningNumbers = GenerateWinningNumbers().inputWinningNumbers()
+        val setBonusNumber = GenerateBonusNumber(setWinningNumbers).inputBonusNumber()
 
-        println(testWinningNumbers)
+        for(issuedLotto in lottoList) {
+            val checkLotto = MatchLotto().countLottoNumber(issuedLotto.getLottoNumbers(), setWinningNumbers, setBonusNumber)
+
+            if(checkLotto == LottoRank.MATCH_THREE) LottoRank.MATCH_THREE.ticketCount++
+            if(checkLotto == LottoRank.MATCH_FOUR) LottoRank.MATCH_FOUR.ticketCount++
+            if(checkLotto == LottoRank.MATCH_FIVE) LottoRank.MATCH_FIVE.ticketCount++
+            if(checkLotto == LottoRank.MATCH_FIVE_BONUS) LottoRank.MATCH_FIVE_BONUS.ticketCount++
+            if(checkLotto == LottoRank.MATCH_SIX) LottoRank.MATCH_SIX.ticketCount++
+        }
+
+        printResult()
+
+    }
+
+    private fun printResult() {
+        println("당첨 통계")
+        println("---")
+        println("3개 일치 (5,000원) - ${LottoRank.MATCH_THREE.ticketCount}개")
+        println("4개 일치 (50,000원) - ${LottoRank.MATCH_FOUR.ticketCount}개")
+        println("5개 일치 (1,500,000원) - ${LottoRank.MATCH_FIVE.ticketCount}개")
+        println("5개 일치, 보너스 볼 일치 (30,000,000원) - ${LottoRank.MATCH_FIVE_BONUS.ticketCount}개")
+        println("6개 일치 (2,000,000,000원) - ${LottoRank.MATCH_SIX.ticketCount}개")
     }
 
 }
