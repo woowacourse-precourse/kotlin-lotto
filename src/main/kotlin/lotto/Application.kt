f7a34e6acfa1a6913af11e93b670259583797466
s9hn <tnghqn@naver.com>
2022-11-15 22:08:19 +0900

    [REFACTOR(Application)] 클래스 호출 및 과제플로우 코드 구현

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,35 @@
 package lotto
 
+import lotto.data.Message
+import lotto.domain.*
+import lotto.ui.GuideLotto
+
 fun main() {
-    TODO("프로그램 구현")
+    val guideLotto = GuideLotto()
+    guideLotto.printEnterPayment() // 지불 금액 안내
+
+    val customer = Customer()
+    val payment = customer.payMoney() // 지불 금액
+
+    val seller = Seller(payment) // 판매자에게 금액 지불
+    val lottoBundle = seller.setLotto() // 판매자 로또 세팅
+
+    val lottoHost = LottoHost()
+    val winningNumber = lottoHost.createWinningNumber() // 주최자 당첨번호 선정
+
+    val checkLotto = CheckLotto(lottoBundle, winningNumber)
+    val lottoResult = checkLotto.compareLotto() // 로또 확인 및 비교대조 작업
+
+    println(lottoResult)
+
+    val comparator = Comparator()
+
+
+
+    guideLotto.printLottoResult(comparator.showLottoResult(lottoResult))
+
+    guideLotto.printIncomeRate(customer.calculateIncome(comparator.getMoney(), payment))
+
 }
+
+
