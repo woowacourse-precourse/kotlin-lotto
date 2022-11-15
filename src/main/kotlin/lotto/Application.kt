d7e42937fb5b06de82f2673af71416109c4f4ec2
hahayeon <54923842+hahayeon@users.noreply.github.com>
2022-11-15 23:05:39 +0900

    Feat: 로또 게임 실행 기능 구현

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,10 @@
 package lotto
 
 fun main() {
-    TODO("프로그램 구현")
+    try {
+        val lottoGame = LottoGameStarter()
+        lottoGame.gameStarter()
+    } catch (e: Exception) {
+        println(e.message)
+    }
 }
