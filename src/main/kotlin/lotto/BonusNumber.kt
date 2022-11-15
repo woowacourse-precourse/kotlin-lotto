6a83cfb7d870a64df530a29f76abcab9b0573ff0
joh9911 <joh9911@naver.com>
2022-11-15 16:07:23 +0900

    feat: 보너스 숫자를 저장하는 클래스 BonusNumber 생성

---
+++ src/main/kotlin/lotto/BonusNumber.kt
@@ -0,0 +1,12 @@
+package lotto
+import lotto.ExceptionHandler.throwExceptionForBonusNumbers
+class BonusNumber(private val bonusNumber: Int, private val lottoNumbers: List<Int>) {
+    init {
+        throwExceptionForBonusNumbers(bonusNumber, lottoNumbers)
+    }
+
+    fun getBonusNumber(): Int {
+        return this.bonusNumber
+    }
+
+}
