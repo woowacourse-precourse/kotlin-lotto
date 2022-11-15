329356e0ef9a4aa82198d8d171c5076a4a5de3e3
Hyeongseon Jeon <hsun5163@gmail.com>
2022-11-16 00:55:51 +0900

    feat : getRate 함수, getUserLottoList 함수 추가

--- src/main/kotlin/lotto/LottoLogic.kt
+++ src/main/kotlin/lotto/LottoLogic.kt
@@ -1,7 +1,8 @@
 package lotto
 
 import camp.nextstep.edu.missionutils.Randoms
-var userLottoList = mutableListOf<List<Int>>()
+private var userLottoList = mutableListOf<List<Int>>()
+private var purchaseAmount = 0
 const val PRICE = 1000
 class LottoLogic {
     fun lottoGenerator(): List<Int>{
@@ -27,6 +28,14 @@ class LottoLogic {
         for(i in 0 until count){
             userLottoList.add(lottoGenerator())
         }
+        return userLottoList
+    }
+    fun getRate(sumPrice : Int) : Double{
+        val rateOfReturn = ((sumPrice.toDouble() / userLottoList.size.toDouble())*100.0)
+        return rateOfReturn
+    }
+
+    fun getUserLottoList() : List<List<Int>>{
         return userLottoList
     }
 }
