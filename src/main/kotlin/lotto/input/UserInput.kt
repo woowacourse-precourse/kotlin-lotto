211bacef3e29a2e6b2acc89bb6a363d6438f8365
lsakee <lsls4868@gmail.com>
2022-11-15 15:15:17 +0900

    refactor - 유저가 지불한 금액 기능 패키지 이동 유저가 지불한 금액 패키지 이동

--- src/main/kotlin/lotto/input/UserInput.kt
+++ src/main/kotlin/lotto/input/UserInput.kt
@@ -9,24 +9,7 @@ fun inputUser(): String {
     return userInputMoney.toString()
 }
 
-fun inputUserMoney(): String {
-    println("구입금액을 입력해 주세요.")
-    val userInputNumber = inputUser()
-    checkMoneyStandard(userInputNumber)
-    println(userInputNumber.replace("000", "")+"개를 구매했습니다.")
-    return userInputNumber.replace("000", "")
-}
 
-fun checkMoneyStandard(userInputNumber: String?) {
-    userInputNumber ?: throw IllegalArgumentException("[ERROR]")
-    val checkMoney = userInputNumber.split(",").toList() as MutableList<String>
-    checkInputMessagePatten(checkMoney)
-    println(userInputNumber.toInt())
-    if (userInputNumber.toInt() % 1000 != 0 || userInputNumber.toInt() == 0) {
-        println("[ERROR]")
-        throw IllegalArgumentException()
-    }
-}
 
 fun inputWinningNumber(): String {
     println("당첨 번호를 입력해 주세요.")
