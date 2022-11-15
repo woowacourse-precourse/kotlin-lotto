040684bfa28bc68dd0afc4bf9a0fe835f63ab5a1
sujin-kk <sjoo0002@gmail.com>
2022-11-15 23:47:25 +0900

    fix(application): main에서 runcathing으로 익셉션 잡아준다.

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -12,35 +12,39 @@ import lotto.ui.view.ticket.BuyTicketView
 import lotto.ui.view.winning.MakeWinningNumberView
 
 fun main() {
-    val buyTicketView = BuyTicketView()
-    val lottoTicket = LottoTicket.emptyLottoTicket() // 구매한 로또 티켓 데이터
+    runCatching {
+        val buyTicketView = BuyTicketView()
+        val lottoTicket = LottoTicket.emptyLottoTicket() // 구매한 로또 티켓 데이터
 
-    val buyTicketController =
-        BuyTicketController(
-            buyTicketView = buyTicketView,
-            lottoTicket = lottoTicket
+        val buyTicketController =
+            BuyTicketController(
+                buyTicketView = buyTicketView,
+                lottoTicket = lottoTicket
+            )
+
+        val makeWinningNumberView = MakeWinningNumberView()
+        val winningLotto = WinningLotto.emptyWinningLotto() // 당첨 로또 번호 데이터
+
+        val makeWinningNumberController =
+            MakeWinningNumberController(
+                makeWinningNumberView = makeWinningNumberView,
+                winningLotto = winningLotto
+            )
+
+        buyTicketController.run()
+        makeWinningNumberController.run()
+
+        val showStatisticsView = ShowStatisticsView()
+        val winningTotalStatistics =
+            LottoCalculator.getWinningTotalStatistics(lottoTicket = lottoTicket, winningLotto = winningLotto)
+
+        val showStatisticsController = ShowStatisticsController(
+            showStatisticsView = showStatisticsView,
+            winningTotalStatistics = winningTotalStatistics
         )
 
-    val makeWinningNumberView = MakeWinningNumberView()
-    val winningLotto = WinningLotto.emptyWinningLotto() // 당첨 로또 번호 데이터
-
-    val makeWinningNumberController =
-        MakeWinningNumberController(
-            makeWinningNumberView = makeWinningNumberView,
-            winningLotto = winningLotto
-        )
-
-    buyTicketController.run()
-    makeWinningNumberController.run()
-
-    val showStatisticsView = ShowStatisticsView()
-    val winningTotalStatistics =
-        LottoCalculator.getWinningTotalStatistics(lottoTicket = lottoTicket, winningLotto = winningLotto)
-
-    val showStatisticsController = ShowStatisticsController(
-        showStatisticsView = showStatisticsView,
-        winningTotalStatistics = winningTotalStatistics
-    )
-
-    showStatisticsController.run()
+        showStatisticsController.run()
+    }.onFailure {
+        println(it)
+    }
 }
