4a80e4efa7f69380f5d017563d1e96b53214f338
joh9911 <joh9911@naver.com>
2022-11-15 18:54:04 +0900

    test(ApplicationTest): lotto 디렉토리로 이동

--- src/test/kotlin/lotto/ApplicationTest.kt
+++ src/test/kotlin/lotto/ApplicationTest.kt
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
 import camp.nextstep.edu.missionutils.test.NsTest
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import program.main
 
 class ApplicationTest : NsTest() {
 
