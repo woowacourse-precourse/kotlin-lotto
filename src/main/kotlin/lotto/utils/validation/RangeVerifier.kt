5ca64ae8bd3d3317b0434e287d01463c91b0ec7a
Gloria <ippnsj@gmail.com>
2022-11-15 12:10:48 +0900

    feat: 사용자가 입력한 숫자가 1 ~ 45 사이의 숫자인지 확인하는 기능 구현

--- src/main/kotlin/lotto/utils/validation/RangeVerifier.kt
+++ src/main/kotlin/lotto/utils/validation/RangeVerifier.kt
@@ -1,5 +1,11 @@
 package lotto.utils.validation
 
 class RangeVerifier(private val min: Int, private val max: Int) : Verifier<Int>() {
-    override fun verify(input: Int) {}
+    override fun verify(input: Int) {
+        require(input in min..max) { "$ERROR_MESSAGE $min and $max." }
+    }
+
+    companion object {
+        const val ERROR_MESSAGE = "Input is not between"
+    }
 }
--- src/test/kotlin/lotto/utils/validation/RangesVerifierTest.kt
+++ src/test/kotlin/lotto/utils/validation/RangesVerifierTest.kt
@@ -5,23 +5,23 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
 
-class RangeVerifierTest {
-    private lateinit var verifier: RangeVerifier
+class RangesVerifierTest {
+    private lateinit var verifier: RangesVerifier
 
     @BeforeEach
     fun setUp() {
-        verifier = RangeVerifier(MIN, MAX)
+        verifier = RangesVerifier(MIN, MAX)
     }
 
     @Test
-    fun `1과 45사이의 숫자면 예외가 발생하지 않는다`() {
-        val input = 45
+    fun `1과 45사이의 숫자로만 이루어져 있으면 예외가 발생하지 않는다`() {
+        val input = listOf(1, 3, 5, 7, 22, 45)
         assertDoesNotThrow { verifier.verify(input) }
     }
 
     @Test
-    fun `1과 45사이의 숫자가 아니면 예외가 발생한다`() {
-        val input = 46
+    fun `1과 45사이의 숫자가 아닌 수가 포함되어 있으면 예외가 발생한다`() {
+        val input = listOf(1, 3, 5, 7, 22, 46)
         assertThrows<IllegalArgumentException> { verifier.verify(input) }
     }
 
