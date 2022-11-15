007bf864a99c497c7fc1673c829e24daf4d215f6
최재훈 <be_clever@naver.com>
2022-11-15 18:49:01 +0900

    feat(input processing): 로또 구입 금액의 예외를 처리하는 기능
    - 로또 구입 금액이 1000의 배수인지 검증하는 기능 추가
    - 예외 발생 시 ErrorType enum class 에서 적절한 예외를 선택하도록 기능 추가

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -2,15 +2,37 @@ package lotto
 
 private var purchaseAmount = 0
 
+enum class ErrorType {
+    AMOUNT {
+        override fun print() {
+            super.print()
+            println("입력은 1,000의 배수여야 합니다.")
+        }
+    };
+
+    open fun print() {
+        print("[ERROR] ")
+    }
+}
+
 fun main() {
 
 }
 
 fun getPurchaseAmount() {
     val inputAmount = getPurchaseAmountInput()
+    checkInputAmountValid(inputAmount)
+
 }
 
 fun getPurchaseAmountInput(): Int {
     print("구입금액을 입력해 주세요.\n")
     return readLine()?.toInt() ?: 0
+}
+
+fun checkInputAmountValid(amount: Int) {
+    if(amount == 0 || amount % 1000 != 0) {
+        ErrorType.AMOUNT.print()
+        throw IllegalArgumentException()
+    }
 }
