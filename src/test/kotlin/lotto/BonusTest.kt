a693918edea7cd81c5e4c98feec4140fa15ff747
Yuhee Kim <dbgml4509@naver.com>
2022-11-15 21:20:45 +0900

    refactor : 보너스 번호 개수 테스트 함수 분리

--- src/test/kotlin/lotto/BonusTest.kt
+++ src/test/kotlin/lotto/BonusTest.kt
@@ -1,4 +1,15 @@
 package lotto
 
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
 class BonusTest {
+
+    @Test
+    fun `로또 보너스 번호가 1개 이상 이라면 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            Bonus(12)
+        }
+    }
+
 }
