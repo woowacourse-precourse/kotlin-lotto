3f9d4a77b0649414340ce9bcde03f609d7e8819c
username <ieejo716@naver.com>
2022-11-15 15:41:28 +0900

    [FEAT] : 총 수익률 출력

--- src/main/kotlin/util/Printer.kt
+++ src/main/kotlin/util/Printer.kt
@@ -3,9 +3,13 @@ package util
 class Printer {
     fun printPurchaseMessage() = println("구입금액을 입력해 주세요.")
 
-    fun printCountLotto(count : Int) = println("${count}개를 구매했습니다.")
+    fun printCountLotto(count: Int) = println("${count}개를 구매했습니다.")
 
     fun printInputLottoMessage() = println("당첨 번호를 입력해 주세요.")
 
     fun printInputBonusMessage() = println("보너스 번호를 입력해 주세요.")
+
+    fun printLottoResult() = println("당첨 통계")
+
+    fun printReturnRate(rate: Float) = println("총 수익률은" + "%.2f".format(rate) + "입니다.")
 }
