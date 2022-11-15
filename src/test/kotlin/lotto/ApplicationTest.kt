91acec5359ca36cf6836a9a858e256fd3198840c
ChanwooAn <55792553+ChanwooAn@users.noreply.github.com>
2022-11-15 16:28:55 +0900

    test : 기능 test와 예외 unit test추가.
    - Lotto번호 생성에 대한 예외 case test를 추가함.
    - brute force test case를 추가함.

--- src/test/kotlin/lotto/ApplicationTest.kt
+++ src/test/kotlin/lotto/ApplicationTest.kt
@@ -3,10 +3,144 @@ package lotto
 import camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest
 import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
 import camp.nextstep.edu.missionutils.test.NsTest
+import lotto.constants.earningRateMSG
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class ApplicationTest : NsTest() {
+
+    @Test
+    fun `로또 구입 금액 1000원 단위가 아닐 경우`(){
+        assertSimpleTest {
+            runException("1111")
+            assertThat(output()).contains(ERROR_MESSAGE)
+        }
+    }
+    @Test
+    fun `로또 번호 오름차순 출력 test`(){
+        assertRandomUniqueNumbersInRangeTest(
+            {
+                run("1000", "1,2,3,4,5,6", "7")
+                assertThat(output()).contains(
+                    "[1, 2, 3, 4, 5, 6]"
+                )
+            },
+            listOf(1, 6, 3, 4, 5, 2)
+        )
+    }
+    @Test
+    fun `로또 개수가 구입한 가격에 맞게 계산되는지 test`(){
+        assertThat(User("10000").numOfLotto).isEqualTo(10)
+    }
+
+    @Test
+    fun `로또 당첨 test1 - 1등`(){
+        assertRandomUniqueNumbersInRangeTest(
+            {
+                run("1000", "1,2,3,4,5,6", "7")
+                assertThat(output()).contains(
+                    "3개 일치 (5,000원) - 0개",
+                    "4개 일치 (50,000원) - 0개",
+                    "5개 일치 (1,500,000원) - 0개",
+                    "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
+                    "6개 일치 (2,000,000,000원) - 1개",
+                )
+            },
+            listOf(1, 2, 3, 4, 5, 6)
+        )
+    }
+    @Test
+    fun `로또 당첨 test2 - 2등`(){
+        assertRandomUniqueNumbersInRangeTest(
+            {
+                run("1000", "1,2,3,4,5,6", "7")
+                assertThat(output()).contains(
+                    "3개 일치 (5,000원) - 0개",
+                    "4개 일치 (50,000원) - 0개",
+                    "5개 일치 (1,500,000원) - 0개",
+                    "5개 일치, 보너스 볼 일치 (30,000,000원) - 1개",
+                    "6개 일치 (2,000,000,000원) - 0개",
+                )
+            },
+            listOf(1, 2, 3, 4, 5, 7)
+        )
+    }
+    @Test
+    fun `로또 당첨 test3 - 3등`(){
+        assertRandomUniqueNumbersInRangeTest(
+            {
+                run("1000", "1,2,3,4,5,6", "7")
+                assertThat(output()).contains(
+                    "3개 일치 (5,000원) - 0개",
+                    "4개 일치 (50,000원) - 0개",
+                    "5개 일치 (1,500,000원) - 1개",
+                    "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
+                    "6개 일치 (2,000,000,000원) - 0개",
+                )
+            },
+            listOf(1, 2, 3, 4, 6, 8)
+        )
+    }
+    @Test
+    fun `로또 당첨 test4 - 4등`(){
+        assertRandomUniqueNumbersInRangeTest(
+            {
+                run("1000", "1,2,3,4,5,6", "7")
+                assertThat(output()).contains(
+                    "3개 일치 (5,000원) - 0개",
+                    "4개 일치 (50,000원) - 1개",
+                    "5개 일치 (1,500,000원) - 0개",
+                    "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
+                    "6개 일치 (2,000,000,000원) - 0개",
+                )
+            },
+            listOf(1, 2, 3, 4, 7, 8)
+        )
+    }
+    @Test
+    fun `로또 당첨 test5 - 5등`(){
+        assertRandomUniqueNumbersInRangeTest(
+            {
+                run("1000", "1,2,3,4,5,6", "7")
+                assertThat(output()).contains(
+                    "3개 일치 (5,000원) - 1개",
+                    "4개 일치 (50,000원) - 0개",
+                    "5개 일치 (1,500,000원) - 0개",
+                    "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
+                    "6개 일치 (2,000,000,000원) - 0개",
+                )
+            },
+            listOf(1, 2, 3, 10, 7, 8)
+        )
+    }
+    @Test
+    fun `로또 당첨 test6 - 1등부터 5등까지 하나씩 걸렸을 때`(){
+        assertRandomUniqueNumbersInRangeTest(
+            {
+                run("5000", "1,2,3,4,5,6", "7")
+                assertThat(output()).contains(
+                    "3개 일치 (5,000원) - 1개",
+                    "4개 일치 (50,000원) - 1개",
+                    "5개 일치 (1,500,000원) - 1개",
+                    "5개 일치, 보너스 볼 일치 (30,000,000원) - 1개",
+                    "6개 일치 (2,000,000,000원) - 1개",
+                )
+            },
+            listOf(1, 2, 3, 4, 5, 6),
+            listOf(1, 2, 3, 4, 5, 7),
+            listOf(1, 2, 3, 4, 5, 10),
+            listOf(1, 2, 3, 4, 7, 8),
+            listOf(1, 2, 3, 10, 7, 8)
+        )
+    }
+    @Test
+    fun `로또 당첨금이 매우 클 경우의 수익률`(){
+        assertThat(earningRateMSG(200000000000.0)).isEqualTo("총 수익률은 200000000000%입니다.")
+    }
+    @Test
+    fun `수익률이 소수점 둘째 자리에서 정상적으로 반올림 되는지 test`(){
+        assertThat(calcEarningRate(5000,7000)).isEqualTo(71.4)
+    }
 
     @Test
     fun `기능 테스트`() {
--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -12,13 +12,38 @@ class LottoTest {
         }
     }
 
-    // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
     @Test
     fun `로또 번호에 중복된 숫자가 있으면 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
             Lotto(listOf(1, 2, 3, 4, 5, 5))
         }
     }
+    @Test
+    fun `로또 번호에 1~45에 해당하는 숫자가 아닌 다른 값이 배정되면 예외가 발생한다`(){
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(1, 2, 3, 4, 5, 47))
+        }
+    }
+    @Test
+    fun `로또 당첨 번호에 중복된 숫자가 있으면 예외가 발생한다`(){
+        assertThrows<IllegalArgumentException> {
+            LottoWinNums(listOf(1, 2, 3, 4, 5, 5),7)
+        }
+    }
+    @Test
+    fun `로또 당첨 번호와 보너스 숫자가 중복이면 예외가 발생한다`(){
+        assertThrows<IllegalArgumentException> {
+            LottoWinNums(listOf(1, 2, 3, 4, 5, 6),6)
+        }
+    }
+    @Test
+    fun `로또 당첨 번호와 보너스 숫자가 1~45에 해당하는 숫자가 아닌 다른 값이 배정되면 예외가 발생한다`(){
+        assertThrows<IllegalArgumentException> {
+            LottoWinNums(listOf(1, 2, 3, 4, 5, 47),1)
+        }
+        assertThrows<IllegalArgumentException> {
+            LottoWinNums(listOf(1, 2, 3, 4, 5, 45),47)
+        }
+    }
 
-    // 아래에 추가 테스트 작성 가능
 }
