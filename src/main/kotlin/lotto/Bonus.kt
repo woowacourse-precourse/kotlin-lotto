dcc36b251d9515c9cce796fd6ec6bb152d2a737d
[alsswl] <[alswl4223@naver.com]>
2022-11-15 20:48:01 +0900

    fix : 10보다 커야한다는 조건 삭제

--- src/main/kotlin/lotto/Bonus.kt
+++ src/main/kotlin/lotto/Bonus.kt
@@ -2,6 +2,6 @@ package lotto
 
 class Bonus (private val bonus: Int){
     init{
-        if(bonus <= 0||bonus>=10) throw IllegalArgumentException()
+        if(bonus <= 0) throw IllegalArgumentException()
     }
 }
