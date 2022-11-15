7b2553e2eb295310998b2cbb0d0dcb1644eca854
Sujin Kim <sujinkim919@gmail.com>
2022-11-15 18:53:26 +0900

    feat: 로또 번호의 일치 개수 및 당첨 여부 판단하는 기능 구현

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -11,4 +11,26 @@ class Lotto(private val numbers: List<Int>) {
     fun print() {
         println(numbers.toString())
     }
+
+    fun calculateWinnings(winningNumbers: Lotto, bonusNumber: Int): Int {
+        var countMatching = 0
+
+        for (number in numbers) {
+            if (winningNumbers.numbers.contains(number)) {
+                countMatching++
+            }
+        }
+
+        return when(countMatching) {
+            Winning.FIFTH.countMatching -> Winning.FIFTH.ordinal
+            Winning.FOURTH.countMatching -> Winning.FOURTH.ordinal
+            Winning.THIRD.countMatching -> {
+                if (numbers.contains(bonusNumber)) { Winning.SECOND.ordinal }
+                else { Winning.THIRD.ordinal }
+            }
+            Winning.FIRST.countMatching -> Winning.FIRST.ordinal
+            else -> -1
+        }
+
+    }
 }
