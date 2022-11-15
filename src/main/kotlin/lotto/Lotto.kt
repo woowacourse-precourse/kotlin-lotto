9d72dcaf1a76264caf5e843d73bdb3b020eabf6d
jinsilWoo <wryt9224@gmail.com>
2022-11-15 22:05:37 +0900

    [FEAT] 로또 번호에 따른 결과 출력하기

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -10,5 +10,18 @@ class Lotto(private val numbers: List<Int>) {
         println(numbers)
     }
 
+    fun printResult(lottoNumber: List<Int>, bonusNUmber: Int): LottoWinningPlace {
+        val correctNum = numbers.count { lottoNumber.contains(it) }
 
+        when (correctNum) {
+            6 -> return LottoWinningPlace.FIRST
+            4 -> return LottoWinningPlace.FOURTH
+            3 -> return LottoWinningPlace.FIFTH
+        }
+
+        if (correctNum == 5)
+            if (numbers.contains(bonusNUmber)) return LottoWinningPlace.SECOND
+        return LottoWinningPlace.THIRD
+
+    }
 }
