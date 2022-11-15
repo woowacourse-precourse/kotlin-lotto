6d5867efc0b3c9f9ad157ede74226f22daf479a5
2taezeat <xorudfl772@naver.com>
2022-11-15 16:18:41 +0900

    feat(LottoOrganizer): 유저 보너스 점수, 유효성 검사, 로직 개선

--- src/main/kotlin/lotto/LottoOrganizer.kt
+++ src/main/kotlin/lotto/LottoOrganizer.kt
@@ -7,12 +7,32 @@ class LottoOrganizer(private val producedLottoNumbers: MutableList<List<Int>>) {
 
     fun inputWinLottoNumber() {
         val userInput = Console.readLine()
+        if (!winLottoNumberInputIsValid(userInput)) {
+            throw IllegalArgumentException(WIN_NUMBER_PRICE_ERROR_MESSAGE)
+        }
+
+    }
+
+    private fun winLottoNumberInputIsValid(userInput: String?): Boolean {
+        if (userInput.isNullOrBlank()) {
+            return false
+        }
+
+        val userInputSplit = userInput.split(',')
+
+//        if () {
+//            return false
+//        }
+
+        println(userInputSplit)
+
+        return true
     }
 
 
     fun inputBonusLottoNumber(): Int {
         val userInput = Console.readLine()
-        if (bonusLottoNumberInputIsValid(userInput)) {
+        if (!bonusLottoNumberInputIsValid(userInput)) {
             throw IllegalArgumentException(BONUS_NUMBER_PRICE_ERROR_MESSAGE)
         }
 
@@ -24,7 +44,7 @@ class LottoOrganizer(private val producedLottoNumbers: MutableList<List<Int>>) {
             return false
         }
 
-        if (userInput.toInt() in LottoProcessConstValue.LOTTO_NUMBER_RANGE_START..LottoProcessConstValue.LOTTO_NUMBER_RANGE_END) {
+        if (userInput.toInt() <= LottoProcessConstValue.LOTTO_NUMBER_RANGE_START || userInput.toInt() >= LottoProcessConstValue.LOTTO_NUMBER_RANGE_END) {
             return false
         }
 
@@ -42,7 +62,8 @@ class LottoOrganizer(private val producedLottoNumbers: MutableList<List<Int>>) {
 
 
     companion object {
-        const val BONUS_NUMBER_PRICE_ERROR_MESSAGE = "[ERROR] 보너스 점수는 1이상 45이하의 정수 값이어야 합니다."
+        const val BONUS_NUMBER_PRICE_ERROR_MESSAGE = "[ERROR] 보너스 번호는 1이상 45이하의 정수 값이어야 합니다."
+        const val WIN_NUMBER_PRICE_ERROR_MESSAGE = "[ERROR] 당첨 번호는 쉼표를 기준으로 나누어, 6개의 1이상 45이하의 정수 값들이여야 합니다."
     }
 
 }
