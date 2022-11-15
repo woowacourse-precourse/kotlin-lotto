54b144b1c0a2797dfb8a1b664df0a2948237e417
username <ieejo716@naver.com>
2022-11-15 12:09:11 +0900

    [FEAT] : 자료형 예외 처리 구현

---
+++ src/main/kotlin/util/InputException.kt
@@ -0,0 +1,19 @@
+package util
+
+class InputException {
+    fun checkTypeException(input: String?) {
+        try {
+            input?.toInt()
+        } catch (e: Exception) {
+            invalidException(TYPE_EXCEPTION)
+        }
+    }
+
+    private fun invalidException(message: String) {
+        throw IllegalArgumentException("[ERROR] : $message")
+    }
+
+    companion object {
+        const val TYPE_EXCEPTION = "올바른 형식이 아닙니다."
+    }
+}
