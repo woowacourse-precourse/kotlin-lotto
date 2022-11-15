2e0b88dd507e83689fc89f845f796436297a0ac3
2taezeat <xorudfl772@naver.com>
2022-11-15 13:59:33 +0900

    feat(Application.kt, LottoCustomer): 유저의 로또 구입 금액을 입력받는 기능 구현 및 어플리케이션 반영

--- src/main/kotlin/lotto/LottoCustomer.kt
+++ src/main/kotlin/lotto/LottoCustomer.kt
@@ -1,4 +1,11 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Console
+
 class LottoCustomer {
+    fun customerLottoBuyPriceInput(): Int {
+        println(LottoProcessStatement.INPUT_LOTTO_BUY_PRICE)
+        val userInput = Console.readLine()
+        return userInput.toInt()
+    }
 }
