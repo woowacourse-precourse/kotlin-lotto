f785bcd5447eb802f7b68ba4dbb6d4aca0ce5c35
phs <gustjd0808@pusan.ac.kr>
2022-11-15 18:33:19 +0900

    test(domain): test BonusNumber class execption and method
    exception
    - out of bound(1~45)
    methods
    - confirm winning numbers contains bonus number

---
+++ src/test/kotlin/lotto/domain/BonusNumberTest.kt
@@ -0,0 +1,21 @@
+package lotto.domain
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class BonusNumberTest{
+    @Test
+    fun `로또 번호 범위를 벗어나는 보너스 번호면 예외가 발생한다`() {
+        org.junit.jupiter.api.assertThrows<IllegalArgumentException> {
+            BonusNumber(46)
+        }
+    }
+
+    @Test
+    fun `isMatched 메서드에 당첨 번호를 인자로 주어 보너스 번호와 일치하는 게 있는지 판단`() {
+        val input = listOf(1, 2, 3, 4, 5, 6)
+        val bonusNumber = BonusNumber(7)
+        Assertions.assertThat(bonusNumber.isMatched(input)).isEqualTo(false)
+    }
+}
