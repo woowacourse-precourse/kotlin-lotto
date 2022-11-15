bf04d2b9c5125d3e63ba3383d038b2a580316c23
Jaeheon Jeong <drdd1120@naver.com>
2022-11-15 12:09:04 +0900

    <fix> 최종코드

--- src/main/kotlin/lotto/User.kt
+++ src/main/kotlin/lotto/User.kt
@@ -5,13 +5,13 @@ import camp.nextstep.edu.missionutils.Console
 class User {
 
     // 당첨번호 입력
-    fun userInput() : Lotto{
+    fun userInput(): Lotto {
         println(Lotto.INPUTWINMONEY)
         var user = Console.readLine()
         var token = user.split(",")
         val userInputNum = mutableListOf<Int>()
 
-        for(i in token){
+        for (i in token) {
             LottoError.checkIsNum(i)
             userInputNum.add(i.toInt())
         }
@@ -19,7 +19,7 @@ class User {
     }
 
     // 보너스 번호 입력
-    fun userBonusInput(userNum: List<Int>): Int{
+    fun userBonusInput(userNum: List<Int>): Int {
         println(Lotto.INPUTBONUSNUM)
 
         var bonusNum = Console.readLine()
@@ -30,10 +30,28 @@ class User {
     }
 
     // 통계
-    fun makeStatistics(money : Int, lottery : MutableList<MutableList<Int>>, userNum : List<Int>, bonusNum : Int){
-        Lotto.SHOWSTATISTICS
+    fun makeStatistics(money: Int, lottery: MutableList<MutableList<Int>>, userNum: List<Int>, bonusNum: Int) {
+        var winLottery: HashMap<Int, Int> = lottoDictionary(lottery, userNum, bonusNum)
+        var sum: Long = 0
+        for (i in winLottery.keys) {
+            if (i == LotteryResult.FIVEANDBONUS.correct) {
+                println("5개 일치" + LotteryResult.printCost(i) + " - " + "${winLottery[i]}개")
+                sum = calculateSum(winLottery, i, sum)
+            } else if (i > 2) {
+                println("$i" + "개 일치" + LotteryResult.printCost(i) + " - " + "${winLottery[i]}개")
+                sum = calculateSum(winLottery, i, sum)
+            }
+        }
 
-        var winLottery : HashMap<Int, Int> = mutableMapOf(
+        var earningsRate = (sum / money.toDouble()) * 100
+        val result = String.format("%.1f", earningsRate)
+        println("총 수익률은 $result%입니다.")
+    }
+
+    // 당첨번호와 보너스 번호를 바탕으로 해시맵에 추가
+    private fun lottoDictionary(lottery: MutableList<MutableList<Int>>, userNum: List<Int>, bonusNum: Int): HashMap<Int, Int> {
+        Lotto.SHOWSTATISTICS
+        var winLottery: HashMap<Int, Int> = mutableMapOf(
                 LotteryResult.THREE.correct to 0,
                 LotteryResult.FOUR.correct to 0,
                 LotteryResult.FIVE.correct to 0,
@@ -43,28 +61,12 @@ class User {
 
         for (i in 0 until lottery.size) {
             var cnt = counting(lottery, i, userNum)
-
             if (cnt == 5 && lottery[i].contains(bonusNum)) {
                 cnt = LotteryResult.FIVEANDBONUS.correct
             }
             winLottery[cnt] = winLottery.getOrDefault(cnt, 0) + 1
         }
-
-        var sum : Long = 0
-        for (i in winLottery.keys) {
-            if (i == LotteryResult.FIVEANDBONUS.correct) {
-                println("5개 일치" + LotteryResult.printCost(i) + " - " + "${winLottery[i]}개")
-                sum = calculateSum(winLottery, i, sum)
-            } else if(i > 2){
-                println("$i" + "개 일치" + LotteryResult.printCost(i) + " - " + "${winLottery[i]}개")
-                sum = calculateSum(winLottery, i, sum)
-            }
-        }
-
-        var  earningsRate = (sum/money.toDouble())*100
-
-        val result = String.format("%.1f", earningsRate)
-        println("총 수익률은 $result%입니다.")
+        return winLottery
     }
 
     // 맞춘 개수 별 금액 계산
