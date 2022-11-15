24b330dd43bce698135a78e0b31769969866c3d8
Jaehun.Kim <rlawogns0411@naver.com>
2022-11-15 19:51:38 +0900

    feat : 구입금액 입력값 단위 체크

--- src/main/kotlin/lotto/InputData.kt
+++ src/main/kotlin/lotto/InputData.kt
@@ -11,4 +11,10 @@ class InputData {
         println("구입금앱을 입력해 주세요.")
         return Console.readLine().toInt()
     }
+
+    fun validInputMoney(inputMoney : Int){
+        if(inputMoney%1000 != 0) {
+            throw IllegalArgumentException("[ERROR] 구입금액은 1000원 단위입니다.")
+        }
+    }
 }
