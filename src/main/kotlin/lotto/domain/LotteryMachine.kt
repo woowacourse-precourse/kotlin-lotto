167b6acb35f8b7fe5d43d1173c4639c5bb9f6453
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2022-11-15 15:37:45 +0900

    refactor(LotteryMachine) : 로또 생성할 때, 중복값을 거르기 위한 로직 제거

--- src/main/kotlin/lotto/domain/LotteryMachine.kt
+++ src/main/kotlin/lotto/domain/LotteryMachine.kt
@@ -11,21 +11,13 @@ class LotteryMachine {
         val lottoTicket = mutableListOf<Lotto>()
 
         while (lottoTicket.size < lottoCount) {
-            lottoTicket.add(Lotto(createRandomLotto()))
+            val lotto = Randoms.pickUniqueNumbersInRange(
+                LOTTO_MIN_RANGE, LOTTO_MAX_RANGE, LOTTO_NUMBER_COUNT
+            ).sorted()
+
+            lottoTicket.add(Lotto(lotto))
         }
 
         return lottoTicket
-    }
-
-    private fun createRandomLotto(): List<Int> {
-        var lotto = mutableSetOf<Int>()
-
-        while (lotto.size != LOTTO_NUMBER_COUNT) {
-            lotto = Randoms.pickUniqueNumbersInRange(
-                LOTTO_MIN_RANGE, LOTTO_MAX_RANGE, LOTTO_NUMBER_COUNT
-            ).toSet() as MutableSet<Int>
-        }
-
-        return lotto.toList().sorted()
     }
 }
