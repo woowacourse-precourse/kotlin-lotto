b15312a61d5c8b25cab80b016560942f25995260
권용민 <briandr@naver.com>
2022-11-15 21:47:44 +0900

    test(Cacher): TicketingMachineTest 삭제, Cacher 변경
     - TicketingMachine을 Cacher이 갖고있게 되면서 Test코드도 변경

--- src/test/kotlin/lotto/CacherTest.kt
+++ src/test/kotlin/lotto/CacherTest.kt
@@ -1,6 +1,6 @@
 package lotto
 
-import lotto.domain.Cacher
+import lotto.domain.store.Cacher
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
@@ -14,9 +14,16 @@ class CacherTest {
     }
 
     @Test
-    fun `개수만큼 로또 발행`() {
-        cacher.receiveMoney(23000)
-        val result = cacher.returnCreatedLotteries()
-        assertThat(result).hasSize(23)
+    fun `로또를 개수에 맞게 생산했는지 확인`() {
+        cacher.receiveMoney(10000)
+        val lotteries = cacher.returnCreatedLotteries()
+        assertThat(lotteries).hasSize(10)
+    }
+
+    @Test
+    fun `생성된 로또에 중복값이 없는지 확인`(){
+        cacher.receiveMoney(5000)
+        val lottereis = cacher.returnCreatedLotteries()
+        assertThat(lottereis.toSet().size).isEqualTo(cacher.numberOfLotteries)
     }
 }
