0c5b71770dd85e70eb45ffc94353912310b16635
s9hn <tnghqn@naver.com>
2022-11-15 23:06:12 +0900

    [REFACTOR(Lotto)] 출력문 [ERROR] 추가

--- src/main/kotlin/lotto/domain/LottoHost.kt
+++ src/main/kotlin/lotto/domain/LottoHost.kt
@@ -37,11 +37,11 @@ class LottoHost {
     }
 
     fun throwWinningNumber(inputWinningNumber: String) {
-        if (!inputWinningNumber.contains(",")) throw IllegalArgumentException("양식을 지켜주세요. ex)1,2,3,4,5,6")
+        if (!inputWinningNumber.contains(",")) throw IllegalArgumentException("[ERROR] 양식을 지켜주세요. ex)1,2,3,4,5,6")
     }
 
     fun throwBonusNumber(inputBonusNumber: Int) {
-        if (inputBonusNumber !in 1..45) throw IllegalArgumentException("보너스 번호는 1개의 숫자만 입력 가능합니다")
+        if (inputBonusNumber !in 1..45) throw IllegalArgumentException("[ERROR] 보너스 번호는 1개의 숫자만 입력 가능합니다")
     }
 
     fun throwLottoException(
@@ -49,14 +49,14 @@ class LottoHost {
         bonusNumber: Int
     ) {
 
-        if (winningNumber.size != 6) throw IllegalArgumentException("당첨 번호는 6개의 숫자만 입력 가능합니다")
-        if (winningNumber.distinct().size != 6) throw IllegalArgumentException("중복된 숫자가 존재합니다")
+        if (winningNumber.size != 6) throw IllegalArgumentException("[ERROR] 당첨 번호는 6개의 숫자만 입력 가능합니다")
+        if (winningNumber.distinct().size != 6) throw IllegalArgumentException("[ERROR] 중복된 숫자가 존재합니다")
 
         for (number in winningNumber.indices) {
             if (winningNumber[number] !in 1 until 46)
                 throw IllegalArgumentException(Message.LOTTO_NUMBER_RULE.message)
         }
 
-        if (winningNumber.contains(bonusNumber)) throw IllegalArgumentException("당첨번호와 보너스번호는 중복될 수 없습니다.")
+        if (winningNumber.contains(bonusNumber)) throw IllegalArgumentException("[ERROR] 당첨번호와 보너스번호는 중복될 수 없습니다.")
     }
 }
