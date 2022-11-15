0ae3a46c025c1200de0a955aec955b6758b15850
2taezeat <xorudfl772@naver.com>
2022-11-15 14:00:49 +0900

    feat(LottoCustomer): 입력 받은 구입 금액이 1,000원으로 나누어 떨어지는 유효한 정수값인지 검사하고 예외 처리

--- src/main/kotlin/lotto/LottoCustomer.kt
+++ src/main/kotlin/lotto/LottoCustomer.kt
@@ -6,6 +6,25 @@ class LottoCustomer {
     fun customerLottoBuyPriceInput(): Int {
         println(LottoProcessStatement.INPUT_LOTTO_BUY_PRICE)
         val userInput = Console.readLine()
+        if (customerPriceInputIsValid(userInput)) {
+            throw IllegalArgumentException(CUSTOMER_BUY_PRICE_ERROR_MESSAGE)
+        }
+
         return userInput.toInt()
+    }
+    private fun customerPriceInputIsValid(userInput: String?): Boolean {
+        if (userInput.isNullOrBlank()) {
+            return false
+        }
+
+        if ((userInput.toInt() % 1000) == 0) {
+            return false
+        }
+
+        return true
+    }
+
+    companion object {
+        const val CUSTOMER_BUY_PRICE_ERROR_MESSAGE = "[ERROR] 로또 구입 금액은 1,000원 단위의 정수여야 합니다."
     }
 }
