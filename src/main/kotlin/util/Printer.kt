6da775c2078765820610019db1f98d0cf1fbb9e4
username <ieejo716@naver.com>
2022-11-15 12:54:16 +0900

    [FEAT] : 당첨,보너스 번호 입력 메세지 출력

--- src/main/kotlin/util/Printer.kt
+++ src/main/kotlin/util/Printer.kt
@@ -4,4 +4,8 @@ class Printer {
     fun printPurchaseMessage() = println("구입금액을 입력해 주세요.")
 
     fun printCountLotto(count : Int) = println("${count}개를 구매했습니다.")
+
+    fun printInputLottoMessage() = println("당첨 번호를 입력해 주세요.")
+
+    fun printInputBonusMessage() = println("보너스 번호를 입력해 주세요.")
 }
