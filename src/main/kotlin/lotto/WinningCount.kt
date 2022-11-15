971b3729f4a79bb019247803189e760df0a949fa
KimDongHyun0907 <ehdgus8397@naver.com>
2022-11-15 22:53:51 +0900

    구매한 번호와 당첨 번호의 숫자 개수 확인

--- src/main/kotlin/lotto/WinningCount.kt
+++ src/main/kotlin/lotto/WinningCount.kt
@@ -1,4 +1,13 @@
 package lotto
 
-class WinningCount {
+class WinningCount{
+    fun WinningNumbers(total_numbers:MutableList<List<Int>>,lotto_numbers_list:MutableList<Int>,bonus:Int): MutableMap<Int, Int> {
+        val winning_numbers_count= mutableMapOf<Int,Int>(5000 to 0, 50000 to 0, 1500000 to 0, 30000000 to 0, 2000000000 to 0)
+
+        for(elem in total_numbers){
+            val winning_numbers=lotto_numbers_list.toSet().intersect(elem)
+
+        }
+        return winning_numbers_count
+    }
 }
