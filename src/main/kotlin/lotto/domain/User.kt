1410b4b206c8727483ed4f184d564dcc09578aed
leehandsub <gustjq7027@naver.com>
2022-11-15 15:32:17 +0900

    feat(user): 로또 구입 숫자 체크

--- src/main/kotlin/lotto/domain/User.kt
+++ src/main/kotlin/lotto/domain/User.kt
@@ -2,12 +2,24 @@ package lotto.domain
 
 import camp.nextstep.edu.missionutils.Console
 import lotto.AMOUNT_PURCHASE_MESSAGE
+import lotto.ERROR_AMOUNT_NUMBER_MESSAGE
 
 class User {
     fun inputUserAmount(): Int {
         println(AMOUNT_PURCHASE_MESSAGE)
         val userAmount = Console.readLine()
 
+        checkAmount(userAmount)
+
         return userAmount.toInt()
     }
+
+    fun checkAmount(userAmount: String) {
+        if (!checkNumAmount(userAmount))
+            throw IllegalArgumentException(ERROR_AMOUNT_NUMBER_MESSAGE)
+    }
+
+    private fun checkNumAmount(userAmount: String): Boolean =
+        userAmount.count() == userAmount.replace("\\D".toRegex(), "").count()
+
 }
