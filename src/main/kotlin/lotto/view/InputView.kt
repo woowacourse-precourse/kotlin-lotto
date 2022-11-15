b3383e0bd2f6e182133ae11f07547a59b971e8f0
ChoiWooSuk <wooseokchoi0508@gmail.com>
2022-11-15 18:53:10 +0900

    feat(inputView): 당첨 번호를 입력받는 기능 구현

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -2,6 +2,7 @@ package lotto.view
 
 import camp.nextstep.edu.missionutils.Console
 import lotto.domain.InputValidation
+import lotto.domain.Lotto
 
 class InputView {
 
@@ -9,13 +10,24 @@ class InputView {
 
     fun getPurchasePrice(): Int {
         println(PURCHASE_PRICE_INPUT_MESSAGE)
-        val input= Console.readLine()
+        val input = Console.readLine()
         val purchasePrice = inputValidation.checkInputInteger(input)
         inputValidation.checkAmountMultipleOfThousand(purchasePrice)
         return purchasePrice
     }
 
+    fun getWinningLottoNumbers(): Lotto {
+        println(WINNING_NUMBER_INPUT_MESSAGE)
+        val input = Console.readLine()
+        var splitedInput = inputValidation.checkInputSplitWithComma(input)
+        val winningNumbers = splitedInput.map { lottoNumber ->
+            inputValidation.checkInputInteger(lottoNumber)
+        }
+        return Lotto(winningNumbers)
+    }
+
     companion object {
         const val PURCHASE_PRICE_INPUT_MESSAGE = "구입금액을 입력해 주세요."
+        const val WINNING_NUMBER_INPUT_MESSAGE = "당첨 번호를 입력해 주세요."
     }
 }
