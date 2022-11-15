a8f956e6061173954df07a4f59611b4f0246f40b
ChanwooAn <55792553+ChanwooAn@users.noreply.github.com>
2022-11-15 13:50:37 +0900

    feat: bonus번호 중복 예외처리 추가
    bonus 번호와 lotto번호의 중복검사 기능이 빠져있어 기능을 추가함.

--- src/main/kotlin/lotto/LottoWinNums.kt
+++ src/main/kotlin/lotto/LottoWinNums.kt
@@ -1,14 +1,13 @@
 package lotto
 
-import lotto.constants.INPUT_BONUS_NUM_ERROR_MSG
-import lotto.constants.INPUT_WIN_NUM_SCOPE_ERROR_MSG
-import lotto.constants.INPUT_WIN_NUM_SIZE_ERROR_MSG
+import lotto.constants.*
 
 class LottoWinNums(val winNums: List<Int>, val bonusNum: Int) {
     init {
         require(winNums.size == 6) { println(INPUT_WIN_NUM_SIZE_ERROR_MSG) }
-        require(bonusNum in 1..45) { println(INPUT_BONUS_NUM_ERROR_MSG) }
+        require(bonusNum in 1..45) { println(INPUT_BONUS_NUM_SCOPE_ERROR_MSG) }
         require(winNums.count{it in 1..45}==6){ println(INPUT_WIN_NUM_SCOPE_ERROR_MSG) }
+        require(!winNums.contains(bonusNum)){ println(INPUT_BONUS_NUM_DUPLICATION_ERROR_MSG) }
     }
 
 }
