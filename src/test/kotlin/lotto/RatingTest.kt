238b7e0c471890dba32adde480c02c1e08144494
HwangSooHyeon <48857651+HwangSooHyeon@users.noreply.github.com>
2022-11-15 17:17:16 +0900

    test(RatingTest): getHit 테스트 케이스 리팩토링 및 다른 함수 테스트 케이스 추가
    getHit 테스트 케이스 리팩토링 및 다른 함수 테스트 케이스 추가

--- src/test/kotlin/lotto/RatingTest.kt
+++ src/test/kotlin/lotto/RatingTest.kt
@@ -1,12 +1,25 @@
 package lotto
 
 import lotto.domain.Rating
+import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 
 class RatingTest {
     @Test
-    fun `Rating 추상 메서드 테스트`() {
+    fun `Rating 메서드 - getHit 리턴 테스트`() {
         val test = Rating.values()[0].getHit()
-        println(test)
+        Assertions.assertThat(test.also { println(it) }).isEqualTo(6)
+    }
+
+    @Test
+    fun `Rating 메서드 - getPrize 리턴 테스트`() {
+        val test = Rating.values()[0].getPrize()
+        Assertions.assertThat(test.also { println(it) }).isEqualTo(2_000_000_000)
+    }
+
+    @Test
+    fun `Rating 메서드 - getBonus 리턴 테스트`() {
+        val test = Rating.values()[0].getBonus()
+        Assertions.assertThat(test.also { println(it) }).isEqualTo(false)
     }
 }
