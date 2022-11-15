e29a06e61587315026d4a9a9effaee3c6f42684e
s9hn <tnghqn@naver.com>
2022-11-15 22:07:11 +0900

    [FEAT(CustomerTest)] 수익률 양식 테스트케이스 추가 및 진행

--- src/test/kotlin/lotto/CustomerTest.kt
+++ src/test/kotlin/lotto/CustomerTest.kt
@@ -2,16 +2,27 @@ package lotto
 
 import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
 import lotto.domain.Customer
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
 class CustomerTest {
     @Test
     fun `지불액이 1000원 단위가 아닐 경우, 예외발생`() {
+        val customer = Customer()
+
         assertSimpleTest {
             assertThrows<IllegalArgumentException> {
-                Customer().throwPaymentException(5123)
+                customer.throwPaymentException(5123)
             }
         }
+    }
+
+    @Test
+    fun `수익률 계산, 소수점 체크`() {
+        val a = 5000
+        val b = 8000
+        val result = 62.5
+        assertThat(Customer().calculateIncome(a, b)).isEqualTo(result)
     }
 }
