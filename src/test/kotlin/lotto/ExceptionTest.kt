f180f6a1df08a89d08eb7e9f2298a00675ba2a56
joh9911 <joh9911@naver.com>
2022-11-15 14:49:33 +0900

    test(ExceptionTest): 예외 처리 테스트 코드 작성

--- src/test/kotlin/lotto/ExceptionTest.kt
+++ src/test/kotlin/lotto/ExceptionTest.kt
@@ -4,37 +4,45 @@ import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import lotto.ExceptionHandler
 
 class ExceptionTest {
-    private val exception = Exception()
 
     @Test
-    fun `로또 번호 입력 형식이 틀렸을 경우 예외를 던짐`() {
-        val input = "1,2,3,34,,5,7"
-        assertThrows<IllegalArgumentException>{
-            exception.throwException(input)
-        }
+    fun `로또 번호 입력 형식이 틀렸을 경우 false 를 반환`() {
+        val inputSymbol = "d"
+        assertThat(ExceptionHandler.getIsInputFormRight(inputSymbol)).isFalse
     }
 
     @Test
-    fun `입력한 것들 중 중복된 숫자가 있는 경우 예외를 던짐`() {
-        val input = "1,2,3,5,5,7"
-        assertThrows<IllegalArgumentException>{
-            exception.throwException(input)
-        }
+    fun `입력한 것들 중 중복된 숫자가 있는 경우 false 를 반환`() {
+        val lottoNumbers = listOf(1, 2, 2, 3, 4, 5)
+        assertThat(ExceptionHandler.getHasDistinctNumber(lottoNumbers)).isFalse
     }
 
     @Test
-    fun `숫자를 7개 이상 입력했다면 예외를 던짐`() {
-        val input = "1,2,3,5,6,7,4"
-        assertThrows<IllegalArgumentException>{
-            exception.throwException(input)
-        }    }
+    fun `입력한 로또 번호가 6개가 아닐 경우 false 를 반환`() {
+        val lottoNumbers = listOf(1, 2, 3, 4, 5)
+        assertThat(ExceptionHandler.getIsSizeSix(lottoNumbers)).isFalse
+    }
 
     @Test
-    fun `1 ~ 45 이외의 숫자가 있으면 예외를 던짐`() {
-        val input = "1,45,23,37,5,90"
-        assertThrows<IllegalArgumentException>{
-            exception.throwException(input)
-        }    }
+    fun `보너스 번호가 1과 45 사이가 아니라면 false 를 반환`() {
+        val bonusNumber = 46
+        assertThat(ExceptionHandler.getIsBetweenOneAndFortyFive(bonusNumber)).isFalse
+    }
+
+    @Test
+    fun `보너스 번호가 입력한 6개의 로또 번호와 중복된다면 false 를 반환`() {
+        val lottoNumbers = listOf(1, 2, 3, 4, 5,45)
+        val bonusNumber = 3
+        assertThat(ExceptionHandler.getHasDistinctNumberInLottoNumbers(bonusNumber, lottoNumbers)).isFalse
+    }
+
+    @Test
+    fun `구입 금액이 1000원 단위가 아니라면 false 를 반환`() {
+        val purchaseMoney = 4500
+        assertThat(ExceptionHandler.getIsThousandUnit(purchaseMoney)).isFalse
+    }
+
 }
