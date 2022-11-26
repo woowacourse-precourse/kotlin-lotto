06f1723b53c12188ce5e5c47168656d66e577b54
dlgocks1 <cksgodl1@naver.com>
2022-11-26 22:32:09 +0900

    feat(LottoNumber) : 로또 넘버 추가
    * 로또 번호에 대한 밸리데이션 체크 및 캐싱처리

---
+++ src/main/kotlin/lotto/domain/model/LottoNumber.kt
@@ -0,0 +1,29 @@
+package lotto.domain.model
+
+import lotto.utils.Constants.LOTTO_NUM_IN_RANGE_TEXT
+import lotto.utils.Constants.MAX_LOTTO_NUM
+import lotto.utils.Constants.MIN_LOTTO_NUM
+
+class LottoNumber(private val number: Int) {
+
+    init {
+        validationNumber(number)
+    }
+
+
+    companion object {
+        private val CACHED_LOTTO_NUMBER = List(MAX_LOTTO_NUM) { idx ->
+            LottoNumber(idx + 1)
+        }
+
+        fun valueOf(number: Int): LottoNumber {
+            return CACHED_LOTTO_NUMBER[number - 1]
+        }
+
+        private fun validationNumber(number: Int) {
+            require(number in MIN_LOTTO_NUM..MAX_LOTTO_NUM) {
+                LOTTO_NUM_IN_RANGE_TEXT
+            }
+        }
+    }
+}
