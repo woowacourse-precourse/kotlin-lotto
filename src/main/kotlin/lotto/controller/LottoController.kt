181240cbaa7079354517c0dfdaac1594c5c67d3e
dlgocks1 <cksgodl1@naver.com>
2022-11-28 10:09:24 +0900

    refactor(LottoController) : LottoRepository의 함수 이동 및 뷰를 객체로 선언하여 사용

--- src/main/kotlin/lotto/controller/LottoController.kt
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -1,35 +1,48 @@
 package lotto.controller
 
-import lotto.domain.model.Money
-import lotto.domain.model.Rank
-import lotto.domain.model.WinningLottery
-import lotto.domain.repository.LottoRepository
+import lotto.domain.model.*
 import lotto.view.InputView
 import lotto.view.OutputView
 
 class LottoController {
 
-    private val lottoRepository = LottoRepository()
+    private val inputView = InputView()
+    private val outputView = OutputView()
+    private val lottoStore = LottoStore()
 
     fun play() {
-        val purchaseMoney = Money(InputView.inputMoney())
-        val purchaseLottos = lottoRepository.generateLottos(purchaseMoney.getMoney())
-        OutputView.purchasingLottos(purchaseLottos)
+        val inputMoney = Money(inputView.inputMoney())
+        val purchaseLottos = purchaceLotto(inputMoney)
 
-        val winningLottery = WinningLottery(InputView.inputWinningNum(), InputView.inputBonusNum())
-        val statistics = lottoRepository.calStatistics(winningLottery, purchaseLottos)
-        val earningRate = lottoRepository.calEarningRate(purchaseMoney, getTotalPrice(statistics))
+        val winningLottery = WinningLottery(inputView.inputWinningNum(), inputView.inputBonusNum())
+        val statistics = calStatistics(winningLottery, purchaseLottos)
+        val earningRate = calEarningRate(inputMoney, getTotalPrice(statistics))
         printResult(statistics, earningRate)
+    }
+
+    private fun purchaceLotto(inputMoney: Money): List<Lotto> {
+        val purchaseLottos = lottoStore.purchaseLotto(inputMoney.getMoney())
+        outputView.purchasingLottos(purchaseLottos)
+        return purchaseLottos
     }
 
     private fun getTotalPrice(statistics: List<Rank>) = statistics.sumOf { it.prize.toLong() }
 
     private fun printResult(statistics: List<Rank>, earningRate: Double) {
-        OutputView.run {
+        outputView.run {
             printSummary(statistics)
             earningRate(earningRate)
         }
     }
 
+    private fun calEarningRate(purchasePrize: Money, totalPrice: Long): Double {
+        return (totalPrice / purchasePrize.getMoney().toDouble() * 100.0)
+    }
+
+    private fun calStatistics(winningLottery: WinningLottery, purchaseLottos: List<Lotto>): List<Rank> {
+        return purchaseLottos.map { lotto ->
+            winningLottery.compareLotto(lotto)
+        }
+    }
 
 }
