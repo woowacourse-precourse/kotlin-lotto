16498480839455dc5dd56ecb3e2f12919c9b699f
sojk401 <sojk401@gmail.com>
2022-12-14 23:14:10 +0900

    test(lotto): 금액 입력에 대한 예외 발생 테스트

--- src/test/kotlin/lotto/InputTest.kt
+++ src/test/kotlin/lotto/InputTest.kt
@@ -1,21 +1,29 @@
 package lotto
 
 import lotto.util.Validator
+import lotto.util.Validator.checkMoney
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+import java.lang.IllegalArgumentException
 
 class InputTest {
-/*    @Test
-    fun `로또 구매 금액이 1000원으로 나누어 떨어지지 않으면 예외가 발생한다`() {
+
+    @ValueSource(strings = ["1000j", " ", "abc"])
+    @ParameterizedTest
+    fun `로또 구매 금액이 숫자 형태가 아니면 예외 발생`(input: String) {
         assertThrows<IllegalArgumentException> {
-            Validator.checkPurchaseAmount("5001")
+            checkMoney(input)
         }
     }
 
-    @Test
-    fun `로또 구매 금액이 숫자 형태가 아니면 예외가 발생한다`() {
+    @ValueSource(strings = ["100", "1302"])
+    @ParameterizedTest
+    fun `로또 구매 금액이 1000원으로 나누어 떨어지지 않으면 예외 발생`(input: String) {
         assertThrows<IllegalArgumentException> {
-            Validator.checkPurchaseAmount("   5000")
+            checkMoney(input)
         }
-    }*/
+    }
+
 }
